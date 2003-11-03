2ed77dafbcdc0dda688e9a66026dabf717f2f24d
janwas <janwas@3db68df2-c116-0410-a063-a993310a9797>
2003-11-03 16:43:26 +0000

    no message
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@10 3db68df2-c116-0410-a063-a993310a9797

--- source/vfs.h
+++
@@ -1,8 +0,0 @@
-
-extern int vfs_add_search_path(const char* path);
-extern int vfs_remove_first_path();
-
-extern Handle vfs_open(const char* fn);
-extern int vfs_close(Handle h);
-extern int vfs_read(Handle h, void*& p, size_t& size, size_t ofs = 0);
-
