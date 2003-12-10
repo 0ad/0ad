a1e1905efc100c8184db87fec73bad5e56051698
olsner <olsner@3db68df2-c116-0410-a063-a993310a9797>
2003-12-10 19:31:25 +0000

    WSAAsyncSelect declaration and define values
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@135 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/win.h
+++ source/lib/win.h
@@ -86,6 +86,23 @@ extern int aio_open_winhandle(HANDLE);
 #pragma comment(lib, "ws2_32.lib")
 #endif
 
+extern __declspec(dllimport) int __stdcall WSAAsyncSelect(int s, HANDLE hWnd, unsigned int wMsg, long lEvent);
+
+#define FD_READ_BIT      0
+#define FD_READ          (1 << FD_READ_BIT)
+
+#define FD_WRITE_BIT     1
+#define FD_WRITE         (1 << FD_WRITE_BIT)
+
+#define FD_ACCEPT_BIT    3
+#define FD_ACCEPT        (1 << FD_ACCEPT_BIT)
+
+#define FD_CONNECT_BIT   4
+#define FD_CONNECT       (1 << FD_CONNECT_BIT)
+
+#define FD_CLOSE_BIT     5
+#define FD_CLOSE         (1 << FD_CLOSE_BIT)
+
 extern __declspec(dllimport) int __stdcall WSAStartup(WORD, void*);
 extern __declspec(dllimport) int __stdcall WSAGetLastError();
 #endif
