772003243b7cc5f6066c20a1cf5826766a1342e3
olsner <olsner@3db68df2-c116-0410-a063-a993310a9797>
2004-03-08 02:08:12 +0000

    - Naming Convention\n- Cleanup
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@185 3db68df2-c116-0410-a063-a993310a9797

--- source/ps/Network/ServerSocket.cpp
+++ source/ps/Network/ServerSocket.cpp
@@ -2,23 +2,11 @@
 
 CServerSocket::~CServerSocket()
 {
-	// We must ensure that the CSocket destructor doesn't try to
-	// disconnect the server socket
-	//FIXME stuff
 }
 	
-/*void CServerSocket::GetRemoteAddress(CSocketInternal *pInt, u8 (&address)[4], int &port)
-{
-	port=ntohs(pInt->m_RemoteAddr.sin_port);
-	address[0]=(u8)(pInt->m_RemoteAddr.sin_addr.s_addr & 0xff);
-	address[1]=(u8)((pInt->m_RemoteAddr.sin_addr.s_addr >> 8) & 0xff);
-	address[2]=(u8)((pInt->m_RemoteAddr.sin_addr.s_addr >> 16) & 0xff);
-	address[3]=(u8)(pInt->m_RemoteAddr.sin_addr.s_addr >> 24);
-}*/
-
 void CServerSocket::OnRead()
 {
-	SocketAddress remoteAddr;
+	CSocketAddress remoteAddr;
 	
 	PS_RESULT res=PreAccept(remoteAddr);
 	if (res==PS_OK)
