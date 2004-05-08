c61a4d91497d11bc53f915a9b2453ee687ba2f8d
janwas <janwas@3db68df2-c116-0410-a063-a993310a9797>
2004-05-08 01:18:15 +0000

    precompiled headers. need to change workspace settings for them to take effect
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@222 3db68df2-c116-0410-a063-a993310a9797

---
+++ source/lib/precompiled.h
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <math.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <time.h>
+
+#include <wchar.h>
+
+
+#include <list>
+#include <map>
+#include <vector>
+#include <stack>
+#include <string>
+
+#include <functional>
+#include <algorithm>
+
+#include <numeric>
