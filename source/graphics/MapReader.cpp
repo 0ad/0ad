5e265571aff49e3ed9a84723a40a7b3706d3dea0
notpete <notpete@3db68df2-c116-0410-a063-a993310a9797>
2004-05-29 23:35:31 +0000

    Fixed 180 degree rotation issue caused by missed merge with Mark's code on last check in.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@316 3db68df2-c116-0410-a063-a993310a9797

--- source/graphics/MapReader.cpp
+++ source/graphics/MapReader.cpp
@@ -160,7 +160,7 @@ void CMapReader::ApplyData(CFileUnpacker& unpacker)
 			CBaseEntity* templateObject = g_EntityTemplateCollection.getTemplateByActor( objentry );
 			if( templateObject )
 			{
-				CVector3D orient = ((CMatrix3D*)m_Objects[i].m_Transform)->GetIn();
+				CVector3D orient = -((CMatrix3D*)m_Objects[i].m_Transform)->GetIn();
 				CVector3D position = ((CMatrix3D*)m_Objects[i].m_Transform)->GetTranslation();
 
 				g_EntityManager.create( templateObject, position, atan2( orient.X, orient.Z ) );
