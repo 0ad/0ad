85bac7c15acc1fe5a204a3821e8633d9d450db31
olsner <olsner@3db68df2-c116-0410-a063-a993310a9797>
2004-06-02 14:31:27 +0000

    - Added a OS_UNIX define for unix variants (Linux and Mac OS X) - OS_UNIX => HAVE_GETTIMOFDAY - OS_LINUX => HAVE_X
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@346 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/config.h
+++ source/lib/config.h
@@ -2,10 +2,12 @@
 # define OS_WIN
 #elif defined(linux)
 # define OS_LINUX
+# define OS_UNIX
 #elif defined(macintosh)
 # define OS_MACOS
 #elif defined(__APPLE__)  && defined(__MACH__)
 # define OS_MACOSX
+# define OS_UNIX
 #else
 # error "unknown OS - add define here"
 #endif
@@ -23,5 +25,12 @@
 #undef HAVE_GETTIMEOFDAY
 #undef HAVE_X
 
+#ifdef OS_UNIX
+# define HAVE_GETTIMEOFDAY
+#endif
+
+#ifdef OS_LINUX
+# define HAVE_X
+#endif
 
 #undef CONFIG_DISABLE_EXCEPTIONS
