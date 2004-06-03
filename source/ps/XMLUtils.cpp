3a9d0d68ee458cc3aaf548e156f6bd8c46d318e5
olsner <olsner@3db68df2-c116-0410-a063-a993310a9797>
2004-06-03 17:34:49 +0000

    \ handling for vfs entity resolver.. ugly as hell, but works :P
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@388 3db68df2-c116-0410-a063-a993310a9797

--- source/ps/XMLUtils.cpp
+++ source/ps/XMLUtils.cpp
@@ -1,5 +1,6 @@
 #include "XML.h"
 #include "CStr.h"
+#include "CLogger.h"
 
 #include "res/vfs.h"
 
@@ -24,10 +25,16 @@ int CVFSInputSource::OpenFile(const char *path)
 	debug_out("CVFSInputSource::OpenFile(): opening file %s.\n", path);
 
 	m_hFile=vfs_open(path);
-	if (m_hFile <= 0) return -1;
-	
-	if (vfs_map(m_hFile, 0, m_pBuffer, m_BufferSize) != 0)
+	if (m_hFile <= 0)
 	{
+		LOG(ERROR, "CVFSInputSource: file %s couldn't be opened (vfs_open)\n", path);
+		return -1;
+	}
+	
+	int err;
+	if ((err=vfs_map(m_hFile, 0, m_pBuffer, m_BufferSize)) != 0)
+	{
+		LOG(ERROR, "CVFSInputSource: file %s couldn't be opened (vfs_map: %d)\n", path, err);
 		vfs_close(m_hFile);
 		m_hFile=0;
 		return -1;
@@ -67,29 +74,36 @@ InputSource *CVFSEntityResolver::resolveEntity(const XMLCh *const publicId,
 	char *path=XMLString::transcode(systemId);
 	char *orgpath=path;
 	
-	CStr base=m_DocName;
-	const char *end=m_DocName+base.Length();
-	if (strncmp(path, "../", 3) == 0)
+	CStr abspath=m_DocName;
+	const char *end=m_DocName+abspath.Length();
+	if (strncmp(path, "..", 2) == 0 && (path[2] == '/' || path[2] == '\\'))
 	{
 		do
 			--end;
-		while (end > m_DocName && *end != '/');
+		while (end > m_DocName && (*end != '/' && *end != '\\'));
 
-		while (strncmp(path, "../", 3) == 0)
+		while (strncmp(path, "..", 2) == 0 && (path[2] == '/' || path[2] == '\\'))
 		{
 			path += 3;
 
 			do
 				--end;
-			while (end > m_DocName && *end != '/');
+			while (end > m_DocName && (*end != '/' && *end != '\\'));
 		}
 		
 		--path;
+		
+		abspath=abspath.Left(end-m_DocName)+path;
+		
+		int pos=0;
+		if (abspath.Find('\\') != -1)
+		{
+			LOG(WARNING, "While resolving XML entities for %s: path %s [%s] contains non-portable path separator \\", m_DocName, orgpath, abspath.c_str());
+			abspath.Replace("\\", "/");
+		}
 	}
 	
-	if (
-		(orgpath != path &&
-			ret->OpenFile((base.Left(end-m_DocName)+path).c_str())!=0)
+	if ((orgpath != path &&	ret->OpenFile(abspath)!=0)
 		|| (orgpath == path && ret->OpenFile(path)!=0))
 	{
 		delete ret;
