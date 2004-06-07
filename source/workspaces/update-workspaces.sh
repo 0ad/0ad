c3dd2cf5dfb6b8a7943daebc2fbf03850ae306fd
olsner <olsner@3db68df2-c116-0410-a063-a993310a9797>
2004-06-07 10:45:48 +0000

    Unix/Linux update script
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@408 3db68df2-c116-0410-a063-a993310a9797

---
+++ source/workspaces/update-workspaces.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd premake
+
+premake --target gnu
+
+cd ../gcc
+mv -f ../premake/Makefile ../premake/prometheus.make .
+
+# These files need to be linked; premake makefiles assume that the
+# lua file is accessible from the makefile directory
+
+ln -f -s ../premake/premake.lua ../premake/functions.lua .
+if [ -x ../premake/premake ]; then
+	ln -f -s ../premake/premake .
+fi
+
+cd ..
