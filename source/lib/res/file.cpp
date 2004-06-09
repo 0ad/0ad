7852345527b50bfd60e9471a3049c4c4cb6dc313
janwas <janwas@3db68df2-c116-0410-a063-a993310a9797>
2004-06-09 16:10:23 +0000

    fail file_map requests for empty files (avoids bc warning)
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@460 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/res/file.cpp
+++ source/lib/res/file.cpp
@@ -1360,6 +1360,12 @@ int file_map(File* const f, void*& p, size_t& size)
 		goto have_mapping;
 	}
 
+	// don't allow mapping zero-length files (doesn't make sense,
+	// and BoundsChecker complains about wposix mmap failing)
+	if(f->size == 0)
+		return -1;
+
+	const int prot = (f->flags & FILE_WRITE)? PROT_WRITE : PROT_READ;
 	f->mapping = mmap((void*)0, f->size, prot, MAP_PRIVATE, f->fd, (off_t)0);
 	if(!f->mapping)
 		return ERR_NO_MEM;
