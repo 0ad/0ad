9f8b321db723192152f55625829a7976157a476e
janwas <janwas@3db68df2-c116-0410-a063-a993310a9797>
2004-08-14 12:28:54 +0000

    wasn't converting native path to portable before sending to VFS. fixes assert after screenshot write reported by stu
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@990 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/res/res.cpp
+++ source/lib/res/res.cpp
@@ -36,11 +36,13 @@ int res_reload_changed_files()
 	static double last_time;
 	static char last_fn[VFS_MAX_PATH];
 
-	char path[PATH_MAX];
-	while(dir_get_changed_file(path) == 0)
+	char n_path[PATH_MAX];
+	while(dir_get_changed_file(n_path) == 0)
 	{
+		char p_path[PATH_MAX];
+		CHECK_ERR(file_make_portable_path(n_path, p_path));
 		char vfs_path[VFS_MAX_PATH];
-		CHECK_ERR(vfs_make_vfs_path(path, vfs_path));
+		CHECK_ERR(vfs_make_vfs_path(p_path, vfs_path));
 
 		const char* fn = vfs_path;
 
