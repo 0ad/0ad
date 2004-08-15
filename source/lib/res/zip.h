b0ca17a81fe7091c58ca6155719e0b704dd9b690
janwas <janwas@3db68df2-c116-0410-a063-a993310a9797>
2004-08-15 21:47:47 +0000

    async routines now fill a control block, instead of returning a handle (that's now done by VFS)
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@998 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/res/zip.h
+++ source/lib/res/zip.h
@@ -106,14 +106,14 @@ extern int zip_unmap(ZFile* zf);
 
 // begin transferring <size> bytes, starting at <ofs>. get result
 // with zip_wait_io; when no longer needed, free via zip_discard_io.
-extern Handle zip_start_io(ZFile* const zf, off_t ofs, size_t size, void* buf);
+extern int zip_start_io(ZFile* const zf, off_t ofs, size_t size, void* buf, FileIO* io);
 
 // wait until the transfer <hio> completes, and return its buffer.
 // output parameters are zeroed on error.
-extern int zip_wait_io(Handle hio, void*& p, size_t& size);
+extern int zip_wait_io(FileIO* io, void*& p, size_t& size);
 
 // finished with transfer <hio> - free its buffer (returned by vfs_wait_io)
-extern int zip_discard_io(Handle& hio);
+extern int zip_discard_io(FileIO* io);
 
 
 //
