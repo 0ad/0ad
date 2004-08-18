b170b5fa256eb2cf5f6d0ba625e82c79193b3d20
janwas <janwas@3db68df2-c116-0410-a063-a993310a9797>
2004-08-18 02:13:53 +0000

    add STL_HASH_(MULTI)MAP
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@1018 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/sysdep/sysdep.h
+++ source/lib/sysdep/sysdep.h
@@ -49,8 +49,32 @@ extern float fmaxf(float a, float b);
 #define strnicmp strncasecmp
 #endif
 
+
 #ifdef __cplusplus
 }
 #endif
+
+
+// C++ linkage
+
+// STL_HASH_MAP, STL_HASH_MULTIMAP
+#ifdef __GNUC__
+// GCC
+# include <ext/hash_map>
+# define STL_HASH_MAP __gnu_cxx::hash_map
+# define STL_HASH_MULTIMAP __gnu_cxx::hash_multimap
+#else	// !__GNUC__
+# include <hash_map>
+# if defined(_MSC_VER) && (_MSC_VER >= 1300)
+// VC7 or above
+#  define STL_HASH_MAP stdext::hash_map
+#  define STL_HASH_MULTIMAP stdext::hash_multimap
+# else
+// VC6 and anything else (most likely name)
+#  define STL_HASH_MAP std::hash_map
+#  define STL_HASH_MULTIMAP std::hash_multimap
+# endif	// defined(_MSC_VER) && (_MSC_VER >= 1300)
+#endif	// !__GNUC__
+
 
 #endif	// #ifndef SYSDEP_H__
