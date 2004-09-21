a4a694934c6fcdd7d455d9e7a5094ae973b22d95
janwas <janwas@3db68df2-c116-0410-a063-a993310a9797>
2004-09-21 13:40:14 +0000

    cleanup + bulletproofing
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@1176 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/sysdep/win/waio.cpp
+++ source/lib/sysdep/win/waio.cpp
@@ -23,6 +23,7 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <malloc.h>		// _aligned_malloc
 
 
 #define lock() win_lock(WAIO_CS)
@@ -36,16 +37,22 @@ WIN_REGISTER_FUNC(waio_shutdown);
 #pragma data_seg()
 
 
+// Win32 functions require sector aligned transfers.
+// max of all drives' size is checked in waio_init().
+static size_t sector_size = 4096;	// minimum: one page
+
+
 //////////////////////////////////////////////////////////////////////////////
 //
 // associate async-capable handle with POSIX file descriptor (int)
 //
 //////////////////////////////////////////////////////////////////////////////
 
-// current implementation: open both versions of the file on open()
-//   wastes 1 handle/file, but we don't have to remember the filename/mode
+// current implementation: open file again for async access on each open();
+// wastes 1 HANDLE per file, but that's less overhead than storing the
+// filename/mode for every file and re-opening that on demand.
 //
-// note: current Windows lowio handle limit is 2k
+// note: current Windows lowio file descriptor limit is 2k
 
 static HANDLE* aio_hs;
 	// array; expanded when needed in aio_h_set
@@ -54,7 +61,7 @@ static int aio_hs_size;
 	// often compared against fd => int
 
 
-// no init needed.
+// aio_h: no init needed.
 
 static void aio_h_cleanup()
 {
@@ -85,14 +92,14 @@ static bool is_valid_file_handle(const HANDLE h)
 }
 
 
-// get async capable handle to file <fd>
+// return async-capable handle associated with file <fd>
 HANDLE aio_h_get(const int fd)
 {
 	HANDLE h = INVALID_HANDLE_VALUE;
 
 	lock();
 
-	if(0 <= fd && fd < (int)aio_hs_size)
+	if(0 <= fd && fd < aio_hs_size)
 	{
 		h = aio_hs[fd];
 		if(!is_valid_file_handle(h))
@@ -108,11 +115,12 @@ HANDLE aio_h_get(const int fd)
 }
 
 
+// associate h (an async-capable file handle) with fd;
+// returned by subsequent aio_h_get(fd) calls.
+// setting h = INVALID_HANDLE_VALUE removes the association.
 int aio_h_set(const int fd, const HANDLE h)
 {
 	lock();
-
-	const char* msg = 0;
 
 	if(fd < 0)
 		goto fail;
@@ -138,16 +146,12 @@ int aio_h_set(const int fd, const HANDLE h)
 		;
 	else
 	{
+		// already set
 		if(aio_hs[fd] != INVALID_HANDLE_VALUE)
-		{
-			msg = "aio_h_set: handle already set!";
 			goto fail;
-		}
+		// setting invalid handle
 		if(!is_valid_file_handle(h))
-		{
-			msg = "aio_h_set: setting invalid handle";
 			goto fail;
-		}
 	}
 
 	aio_hs[fd] = h;
@@ -159,209 +163,6 @@ fail:
 	unlock();
 	debug_warn("aio_h_set failed");
 	return -1;
-}
-
-
-//////////////////////////////////////////////////////////////////////////////
-//
-// Req
-//
-//////////////////////////////////////////////////////////////////////////////
-
-
-// information about active transfers (reused)
-struct Req
-{
-	// used to identify this request; != 0 <==> request valid
-	aiocb* cb;
-
-	OVERLAPPED ovl;
-	// hEvent signals when transfer complete
-
-	// read into a separate align buffer if necessary
-	// (note: unaligned writes aren't supported. see aio_rw)
-	void* buf;		// reused; resized if too small
-	size_t buf_size;
-
-	HANDLE hFile;
-
-	size_t pad;		// offset from starting sector
-	bool use_align_buffer;
-};
-
-
-// an aiocb is used to pass the request from caller to aio,
-// and serves as a "token" identifying the IO - its address is unique.
-// Req holds some state needed for the Windows AIO calls (OVERLAPPED).
-//
-// cb -> req (e.g. in aio_return) is accomplished by searching reqs
-// for the given cb (no problem since MAX_REQS is small).
-// req -> cb via Req.cb pointer.
-
-
-const int MAX_REQS = 8;
-static Req reqs[MAX_REQS];
-
-
-static void req_cleanup(void)
-{
-	Req* r = reqs;
-
-	for(int i = 0; i < MAX_REQS; i++, r++)
-	{
-		HANDLE& h = r->ovl.hEvent;
-		if(h != INVALID_HANDLE_VALUE)
-		{
-			CloseHandle(h);
-			h = INVALID_HANDLE_VALUE;
-		}
-
-		::free(r->buf);
-		r->buf = 0;
-	}
-}
-
-
-static void req_init()
-{
-	for(int i = 0; i < MAX_REQS; i++)
-		reqs[i].ovl.hEvent = CreateEvent(0,1,0,0);	// manual reset
-
-	// buffers are allocated on-demand.
-}
-
-
-// return first Req with given cb field
-// (may be 0; useful if searching for a free Req)
-static Req* req_find(const aiocb* cb)
-{
-	Req* r = reqs;
-	for(int i = 0; i < MAX_REQS; i++, r++)
-		if(r->cb == cb)
-			goto found;
-
-	r = 0;	// not found
-found:
-
-#ifdef PARANOIA
-debug_out("req_find  cb=%p r=%p\n", cb, r);
-#endif
-
-	return r;
-}
-
-
-static Req* req_alloc(aiocb* cb)
-{
-	assert(cb);
-
-	// first free Req, or 0
-	Req* r = req_find(0);
-	if(r)
-		r->cb = cb;
-
-#ifdef PARANOIA
-debug_out("req_alloc cb=%p r=%p\n", cb, r);
-#endif
-
-	return r;
-}
-
-
-static int req_free(Req* r)
-{
-#ifdef PARANOIA
-debug_out("req_free  cb=%p r=%p\n", r->cb, r);
-#endif
-
-	assert(r->cb != 0 && "req_free: not currently in use");
-	r->cb = 0;
-	return 0;
-}
-
-
-//////////////////////////////////////////////////////////////////////////////
-//
-// init / cleanup
-//
-//////////////////////////////////////////////////////////////////////////////
-
-
-// Win32 functions require sector aligned transfers.
-// max of all drives' size is checked in init().
-static size_t sector_size = 4096;	// minimum: one page
-
-
-// caller ensures this is not re-entered!
-static int waio_init()
-{
-	req_init();
-
-	const UINT old_err_mode = SetErrorMode(SEM_FAILCRITICALERRORS);
-
-	// Win32 requires transfers to be sector aligned.
-	// find maximum of all drive's sector sizes, then use that.
-	// (it's good to know this up-front, and checking every open() is slow).
-	const DWORD drives = GetLogicalDrives();
-	char drive_str[4] = "?:\\";
-	for(int drive = 2; drive <= 26; drive++)	// C: .. Z:
-	{
-		// avoid BoundsChecker warning by skipping invalid drives
-		if(!(drives & (1ul << drive)))
-			continue;
-
-		drive_str[0] = (char)('A'+drive);
-
-		DWORD spc, nfc, tnc;	// don't need these
-		DWORD sector_size2;
-		if(GetDiskFreeSpace(drive_str, &spc, &sector_size2, &nfc, &tnc))
-		{
-			if(sector_size < sector_size2)
-				sector_size = sector_size2;
-		}
-		// otherwise, it's probably an empty CD drive. ignore the
-		// BoundsChecker error; GetDiskFreeSpace seems to be the
-		// only way of getting at the sector size.
-	}
-
-	SetErrorMode(old_err_mode);
-
-	assert(is_pow2((long)sector_size));
-
-	return 0;
-}
-
-
-static int waio_shutdown()
-{
-	req_cleanup();
-	aio_h_cleanup();
-	return 0;
-}
-
-
-int aio_assign_handle(uintptr_t handle)
-{
-	// 
-	// CRT stores osfhandle. if we pass an invalid handle (say, 0),
-	// we get an exception when closing the handle if debugging.
-	// events can be created relatively quickly (~1800 clocks = 1µs),
-	// and are also freed with CloseHandle, so just pass that.
-	HANDLE h = CreateEvent(0,0,0,0);
-	if(h == INVALID_HANDLE_VALUE)
-	{
-		debug_warn("aio_assign_handle failed");
-		return -1;
-	}
-
-	int fd = _open_osfhandle((intptr_t)h, 0);
-	if(fd < 0)
-	{
-		debug_warn("aio_assign_handle failed");
-		return fd;
-	}
-
-	return aio_h_set(fd, (HANDLE)handle);
 }
 
 
@@ -389,15 +190,11 @@ int aio_reopen(int fd, const char* fn, int oflag, ...)
 
 	// open file
 	DWORD flags = FILE_FLAG_OVERLAPPED|FILE_FLAG_NO_BUFFERING|FILE_FLAG_SEQUENTIAL_SCAN;
-	WIN_SAVE_LAST_ERROR;	// CreateFile
+WIN_SAVE_LAST_ERROR;	// CreateFile
 	HANDLE h = CreateFile(fn, access, share, 0, create, flags, 0);
-	WIN_RESTORE_LAST_ERROR;
+WIN_RESTORE_LAST_ERROR;
 	if(h == INVALID_HANDLE_VALUE)
-	{
-fail:
-		debug_warn("aio_open failed");
-		return -1;
-	}
+		goto fail;
 
 	if(aio_h_set(fd, h) < 0)
 	{
@@ -405,11 +202,11 @@ fail:
 		goto fail;
 	}
 
-#ifdef PARANOIA
-debug_out("aio_reopen fd=%d fn=%s\n", fd, fn);
-#endif
-
 	return 0;
+
+fail:
+	debug_warn("aio_reopen failed");
+	return -1;
 }
 
 
@@ -423,13 +220,115 @@ int aio_close(int fd)
 	}
 
 	if(!CloseHandle(h))
-		assert(0);
+		debug_warn("aio_close: CloseHandle failed");
 	aio_h_set(fd, INVALID_HANDLE_VALUE);
 
-#ifdef PARANOIA
-debug_out("aio_close fd=%d\n", fd);
-#endif
+	return 0;
+}
 
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// Req
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+// information about active transfers (reused)
+struct Req
+{
+	// used to identify this request; != 0 <==> request valid.
+	// set by req_alloc.
+	aiocb* cb;
+
+	OVERLAPPED ovl;
+		// hEvent signals when transfer complete
+
+	// align buffer - unaligned reads are padded to sector boundaries and
+	// go here; the desired data is then copied into the user's buffer.
+	// reused, since the Req has global lifetime; resized if too small.
+	void* buf;
+	size_t buf_size;
+
+	HANDLE hFile;
+		// needed to GetOverlappedResult in aio_return
+
+	size_t pad;		// offset from starting sector
+	bool read_into_align_buffer;
+};
+
+
+// an aiocb is used to pass the request from caller to aio,
+// and serves as a "token" identifying the IO - its address is unique.
+// Req holds some state needed for the Windows AIO calls (OVERLAPPED).
+//
+// cb -> req (e.g. in aio_return) is accomplished by searching reqs
+// for the given cb (no problem since MAX_REQS is small).
+// req stores a pointer to its associated cb.
+
+
+const int MAX_REQS = 8;
+static Req reqs[MAX_REQS];
+
+
+static void req_cleanup(void)
+{
+	Req* r = reqs;
+
+	for(int i = 0; i < MAX_REQS; i++, r++)
+	{
+		HANDLE& h = r->ovl.hEvent;
+		assert(h != INVALID_HANDLE_VALUE);
+		CloseHandle(h);
+		h = INVALID_HANDLE_VALUE;
+
+		_aligned_free(r->buf);
+		r->buf = 0;
+	}
+}
+
+
+static void req_init()
+{
+	for(int i = 0; i < MAX_REQS; i++)
+		reqs[i].ovl.hEvent = CreateEvent(0,1,0,0);	// manual reset
+
+	// buffers are allocated on-demand.
+}
+
+
+// return first Req with given cb field
+// (0 if searching for a free Req)
+static Req* req_find(const aiocb* cb)
+{
+	Req* r = reqs;
+	for(int i = 0; i < MAX_REQS; i++, r++)
+		if(r->cb == cb)
+			return r;
+
+	// not found
+	return 0;
+}
+
+
+static Req* req_alloc(aiocb* cb)
+{
+	assert(cb);
+
+	// first free Req, or 0
+	Req* r = req_find(0);
+	// .. found one: mark it in-use
+	if(r)
+		r->cb = cb;
+
+	return r;
+}
+
+
+static int req_free(Req* r)
+{
+	assert(r->cb != 0 && "req_free: not currently in use");
+	r->cb = 0;
 	return 0;
 }
 
@@ -446,10 +345,6 @@ static int aio_rw(struct aiocb* cb)
 	int ret = -1;
 	Req* r = 0;
 
-#ifdef PARANOIA
-debug_out("aio_rw cb=%p\n", cb);
-#endif
-
 	WIN_SAVE_LAST_ERROR;
 
 	// no-op from lio_listio
@@ -463,14 +358,6 @@ debug_out("aio_rw cb=%p\n", cb);
 		goto fail;
 	}
 
-	// allocate IO request
-	r = req_alloc(cb);
-	if(!r)
-	{
-		debug_warn("aio_rw: cannot allocate a Req (too many concurrent IOs)");
-		goto fail;
-	}
-
 	// extract aiocb fields for convenience
 	const bool is_write = (cb->aio_lio_opcode == LIO_WRITE);
 	const int fd        = cb->aio_fildes;
@@ -479,6 +366,14 @@ debug_out("aio_rw cb=%p\n", cb);
 	void* const buf     = (void*)cb->aio_buf; // from volatile void*
 	assert(buf);
 
+	// allocate IO request
+	r = req_alloc(cb);
+	if(!r)
+	{
+		debug_warn("aio_rw: cannot allocate a Req (too many concurrent IOs)");
+		goto fail;
+	}
+
 	HANDLE h = aio_h_get(fd);
 	if(h == INVALID_HANDLE_VALUE)
 	{
@@ -486,40 +381,45 @@ debug_out("aio_rw cb=%p\n", cb);
 		ret = -EINVAL;
 		goto fail;
 	}
-	const bool is_file = (GetFileType(h) == FILE_TYPE_DISK);
+
 
 	r->hFile = h;
 	r->pad = 0;
-	r->use_align_buffer = false;
+	r->read_into_align_buffer = false;
+
 
 	//
 	// align
 	//
 
+	// actual transfer parameters
+	// (possibly rounded up/down to satisfy Win32 alignment requirements)
 	size_t actual_ofs = 0;
 		// assume socket; if file, set below
 	size_t actual_size = size;
 	void* actual_buf = buf;
 
 	// leave offset 0 if h is a socket (don't support seeking);
-	// otherwise, calculate aligned offset
+	// otherwise, calculate aligned offset/size
+	const bool is_file = (GetFileType(h) == FILE_TYPE_DISK);
 	if(is_file)
 	{
-		r->pad = ofs % sector_size;		// offset to start of sector
-		actual_ofs = ofs - r->pad;
-		actual_size = round_up(size + r->pad, sector_size);
+		const size_t ofs_misalign = ofs % sector_size;
+		const bool buf_misaligned = (uintptr_t)buf % sector_size != 0;
+		const size_t size_misalign = (size + ofs_misalign) % sector_size;
 
-		const bool misaligned = (size != actual_size);
-			// either ofs or size is unaligned
-		const bool buf_misaligned = ((uintptr_t)buf % sector_size != 0);
+		r->pad = ofs_misalign;
+		actual_ofs = ofs - ofs_misalign;
+		actual_size = size + (size_misalign? sector_size : 0);
 
-		// not aligned
-		if(misaligned || buf_misaligned)
+		// need to realign if ofs, size, or buf % sector_size != 0
+		bool misaligned = ofs_misalign || buf_misaligned || size_misalign;
+		if(misaligned)
 		{
 			// expand current align buffer if too small
 			if(r->buf_size < actual_size)
 			{
-				void* buf2 = realloc(r->buf, actual_size);
+				void* buf2 = _aligned_realloc(r->buf, actual_size, sector_size);
 				if(!buf2)
 				{
 					ret = -ENOMEM;
@@ -529,24 +429,34 @@ debug_out("aio_rw cb=%p\n", cb);
 				r->buf_size = actual_size;
 			}
 
-			if(is_write)
+			if(!is_write)
 			{
-				// file offset isn't aligned. we don't support this -
-				// we'd have to read padding, then write our data. ugh.
-				if(misaligned)
+				actual_buf = r->buf;
+				r->read_into_align_buffer = true;
+			}
+			else
+			{
+				// unaligned offset: not supported.
+				// (we'd have to read padding, then write our data. ugh.)
+				if(ofs_misalign)
 				{
 					ret = -EINVAL;
 					goto fail;
 				}
-				// only the buffer is misaligned - copy data to align buffer
-				else
-					memcpy(r->buf, buf, actual_size);
-			}
 
-			actual_buf = r->buf;
-			r->use_align_buffer = true;
-		}
-	}
+				// unaligned buffer: copy to align buffer and write from there.
+				if(buf_misaligned)
+				{
+					memcpy(r->buf, buf, size);
+					memset((char*)r->buf + size, 0, actual_size - size);
+						// clear previous contents at end of align buf
+					actual_buf = r->buf;
+				}
+
+				// unaligned size: already taken care of (we round up)
+			}
+		}	// misaligned
+	}	// is_file
 
 	// set OVERLAPPED fields
 	ResetEvent(r->ovl.hEvent);
@@ -581,51 +491,9 @@ fail:
 }
 
 
-int aio_read(struct aiocb* cb)
-{
-	cb->aio_lio_opcode = LIO_READ;
-	return aio_rw(cb);
-}
-
-
-int aio_write(struct aiocb* cb)
-{
-	cb->aio_lio_opcode = LIO_WRITE;
-	return aio_rw(cb);
-}
-
-
-int lio_listio(int mode, struct aiocb* const cbs[], int n, struct sigevent* se)
-{
-	UNUSED(se)
-
-	int err = 0;
-
-	for(int i = 0; i < n; i++)
-	{
-		int ret = aio_rw(cbs[i]);		// aio_rw checks for 0 param
-		// don't CHECK_ERR - want to try to issue each one
-		if(ret < 0)
-			err = ret;
-	}
-
-	if(err < 0)
-		return err;
-
-	if(mode == LIO_WAIT)
-		return aio_suspend(cbs, n, 0);
-
-	return 0;
-}
-
-
 // return status of transfer
 int aio_error(const struct aiocb* cb)
 {
-#ifdef PARANOIA
-debug_out("aio_error cb=%p\n", cb);
-#endif
-
 	Req* const r = req_find(cb);
 	if(!r)
 		return -1;
@@ -636,8 +504,6 @@ debug_out("aio_error cb=%p\n", cb);
 		return 0;
 	case STATUS_PENDING:
 		return EINPROGRESS;
-
-	// TODO: errors
 	default:
 		return -1;
 	}
@@ -647,10 +513,6 @@ debug_out("aio_error cb=%p\n", cb);
 // get bytes transferred. call exactly once for each op.
 ssize_t aio_return(struct aiocb* cb)
 {
-#ifdef PARANOIA
-debug_out("aio_return cb=%p\n", cb);
-#endif
-
 	Req* const r = req_find(cb);
 	if(!r)
 	{
@@ -660,12 +522,16 @@ debug_out("aio_return cb=%p\n", cb);
 
 	assert(r->ovl.Internal == 0 && "aio_return with transfer in progress");
 
-	BOOL wait = FALSE;	// should already be done!
+	const BOOL wait = FALSE;	// should already be done!
 	DWORD bytes_transferred;
-	GetOverlappedResult(r->hFile, &r->ovl, &bytes_transferred, wait);
+	if(!GetOverlappedResult(r->hFile, &r->ovl, &bytes_transferred, wait))
+	{
+		debug_warn("aio_return: GetOverlappedResult failed");
+		return -1;
+	}
 
 	// we read into align buffer - copy to user's buffer
-	if(r->use_align_buffer)
+	if(r->read_into_align_buffer)
 		memcpy((void*)cb->aio_buf, (u8*)r->buf + r->pad, cb->aio_nbytes);
 
 	// TODO: this copies data back into original buffer from align buffer
@@ -677,34 +543,9 @@ debug_out("aio_return cb=%p\n", cb);
 }
 
 
-int aio_cancel(int fd, struct aiocb* cb)
-{
-	UNUSED(cb)
-
-	const HANDLE h = aio_h_get(fd);
-	if(h == INVALID_HANDLE_VALUE)
-		return -1;
-
-	// Win32 limitation: can't cancel single transfers -
-	// all pending reads on this file are cancelled.
-	CancelIo(h);
-	return AIO_CANCELED;
-}
-
-
-int aio_fsync(int, struct aiocb*)
-{
-	return -1;
-}
-
-
 int aio_suspend(const struct aiocb* const cbs[], int n, const struct timespec* ts)
 {
 	int i;
-
-#ifdef PARANOIA
-debug_out("aio_suspend cb=%p\n", cbs[0]);
-#endif
 
 	if(n <= 0 || n > MAXIMUM_WAIT_OBJECTS)
 		return -1;
@@ -735,7 +576,8 @@ debug_out("aio_suspend cb=%p\n", cbs[0]);
 	if(ts)
 		timeout = (DWORD)(ts->tv_sec*1000 + ts->tv_nsec/1000000);
 
-	DWORD result = WaitForMultipleObjects(cnt, hs, FALSE, timeout);
+	const BOOL wait_all = FALSE;
+	DWORD result = WaitForMultipleObjects(cnt, hs, wait_all, timeout);
 
 	for(i = 0; i < cnt; i++)
 		ResetEvent(hs[i]);
@@ -747,4 +589,119 @@ debug_out("aio_suspend cb=%p\n", cbs[0]);
 	}
 	else
 		return (result == WAIT_FAILED)? -1 : 0;
+}
+
+
+int aio_cancel(int fd, struct aiocb* cb)
+{
+	UNUSED(cb)
+
+	const HANDLE h = aio_h_get(fd);
+	if(h == INVALID_HANDLE_VALUE)
+		return -1;
+
+	// Win32 limitation: can't cancel single transfers -
+	// all pending reads on this file are cancelled.
+	CancelIo(h);
+	return AIO_CANCELED;
+}
+
+
+
+
+int aio_read(struct aiocb* cb)
+{
+	cb->aio_lio_opcode = LIO_READ;
+	return aio_rw(cb);
+}
+
+
+int aio_write(struct aiocb* cb)
+{
+	cb->aio_lio_opcode = LIO_WRITE;
+	return aio_rw(cb);
+}
+
+
+int lio_listio(int mode, struct aiocb* const cbs[], int n, struct sigevent* se)
+{
+	UNUSED(se)
+
+		int err = 0;
+
+	for(int i = 0; i < n; i++)
+	{
+		int ret = aio_rw(cbs[i]);		// aio_rw checks for 0 param
+		// don't CHECK_ERR - want to try to issue each one
+		if(ret < 0)
+			err = ret;
+	}
+
+	if(err < 0)
+		return err;
+
+	if(mode == LIO_WAIT)
+		return aio_suspend(cbs, n, 0);
+
+	return 0;
+}
+
+
+int aio_fsync(int, struct aiocb*)
+{
+	return -ENOSYS;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// init / cleanup
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+static int waio_init()
+{
+	req_init();
+
+	const UINT old_err_mode = SetErrorMode(SEM_FAILCRITICALERRORS);
+
+	// Win32 requires transfers to be sector aligned.
+	// find maximum of all drive's sector sizes, then use that.
+	// (it's good to know this up-front, and checking every open() is slow).
+	const DWORD drives = GetLogicalDrives();
+	char drive_str[4] = "?:\\";
+	for(int drive = 2; drive <= 26; drive++)	// C: .. Z:
+	{
+		// avoid BoundsChecker warning by skipping invalid drives
+		if(!(drives & BIT(drive)))
+			continue;
+
+		drive_str[0] = (char)('A'+drive);
+
+		DWORD spc, nfc, tnc;	// don't need these
+		DWORD sector_size2;
+		if(GetDiskFreeSpace(drive_str, &spc, &sector_size2, &nfc, &tnc))
+		{
+			if(sector_size < sector_size2)
+				sector_size = sector_size2;
+		}
+		// otherwise, it's probably an empty CD drive. ignore the
+		// BoundsChecker error; GetDiskFreeSpace seems to be the
+		// only way of getting at the sector size.
+	}
+
+	SetErrorMode(old_err_mode);
+
+	assert(is_pow2((long)sector_size));
+
+	return 0;
+}
+
+
+static int waio_shutdown()
+{
+	req_cleanup();
+	aio_h_cleanup();
+	return 0;
 }
