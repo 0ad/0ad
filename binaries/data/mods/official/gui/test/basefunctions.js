fbc93c17bf1b6295be2289daeb3dc49ea5c9bd18
markt <markt@3db68df2-c116-0410-a063-a993310a9797>
2004-10-07 19:34:19 +0000

    Updates to the entity XMLs (they work like the DD says they should, now - more or less)
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@1221 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/official/gui/test/basefunctions.js
+++ binaries/data/mods/official/gui/test/basefunctions.js
@@ -30,13 +30,13 @@ function GUIUpdateObjectInfo() {
         ObjectNameText = getGUIObjectByName("selection_name_test");
         ObjectPositionText = getGUIObjectByName("selection_pos_test");
         ObjectSpeedText = getGUIObjectByName("selection_speed_test");
-	ObjectPortrait = getGUIObjectByName("selection_portrait_test");
-	ObjectStatAttack = getGUIObjectByName("statistic_attack");
-	ObjectStatHack = getGUIObjectByName("statistic_hack");
-	ObjectStatPierce = getGUIObjectByName("statistic_pierce");
-	ObjectStatAccuracy = getGUIObjectByName("statistic_accuracy");
-	ObjectStatLOS = getGUIObjectByName("statistic_los");
-	ObjectStatSpeed = getGUIObjectByName("statistic_speed");
+		ObjectPortrait = getGUIObjectByName("selection_portrait_test");
+		ObjectStatAttack = getGUIObjectByName("statistic_attack");
+		ObjectStatHack = getGUIObjectByName("statistic_hack");
+		ObjectStatPierce = getGUIObjectByName("statistic_pierce");
+		ObjectStatAccuracy = getGUIObjectByName("statistic_accuracy");
+		ObjectStatLOS = getGUIObjectByName("statistic_los");
+		ObjectStatSpeed = getGUIObjectByName("statistic_speed");
 
         // Check number of selected entities
         if (selection.length > 1) {
@@ -49,81 +49,60 @@ function GUIUpdateObjectInfo() {
 
         } else {
 
-                if (!selection[0]) {
+                if ( !selection.length ) {
 
-			// Reset portrait
-			ObjectPortrait.hidden = true;
+					// Reset portrait
+					ObjectPortrait.hidden = true;
 
-			// Reset statistic icons.
-			ObjectStatAttack.hidden = true;
-			ObjectStatHack.hidden = true;
-			ObjectStatPierce.hidden = true;
-			ObjectStatAccuracy.hidden = true;
-			ObjectStatLOS.hidden = true;
-			ObjectStatSpeed.hidden = true;
+					// Reset statistic icons.
+					ObjectStatAttack.hidden = true;
+					ObjectStatHack.hidden = true;
+					ObjectStatPierce.hidden = true;
+					ObjectStatAccuracy.hidden = true;
+					ObjectStatLOS.hidden = true;
+					ObjectStatSpeed.hidden = true;
 
-                        // Reset object name
-                        ObjectNameText.caption = "";
-                        ObjectNameText.hidden = true;
+					 // Reset object name
+                    ObjectNameText.caption = "";
+                    ObjectNameText.hidden = true;
 
-                        // Reset position
-                        ObjectPositionText.caption = "";
-                        ObjectPositionText.hidden = true;
+					// Reset position
+                    ObjectPositionText.caption = "";
+                    ObjectPositionText.hidden = true;
 
-                        // Reset Speed
-                        ObjectSpeedText.caption = "";
-                        ObjectSpeedText.hidden = true;
+                    // Reset Speed
+                    ObjectSpeedText.caption = "";
+					ObjectSpeedText.hidden = true;
 
                 } else {
 
-			// Update portrait (temporary switch statement until entity icons are implemented).
-			switch (selection[0].name)
-			{
-				case ("Prometheus Dude"):
-					ObjectPortrait.sprite="portrait_unit_dude_lrg";
-				break;
-				case ("Prometheus Dudette"):
-					ObjectPortrait.sprite="portrait_unit_dudette_lrg";
-				break;
-				case ("Cavalry Dude"):
-					ObjectPortrait.sprite="portrait_unit_cavalrydude_lrg";
-				break;
-				case ("Deciduous Tree 1"):
-				case ("Deciduous Tree 2"):
-					ObjectPortrait.sprite="portrait_flora_deciduotree_lrg";
-				break;
-				case ("House"):
-					ObjectPortrait.sprite="portrait_structure_heleho_lrg";
-				break;
-				default:
-					ObjectPortrait.sprite="portrait_unknown_lrg";
-				break;
+					// Update portrait
+					ObjectPortrait.sprite = selection[0].traits.id.icon;
+			
+					ObjectPortrait.hidden = false;
 
-			}
+					// Turn on statistic icons.
+					ObjectStatAttack.hidden = false;
+					ObjectStatHack.hidden = false;
+					ObjectStatPierce.hidden = false;
+					ObjectStatAccuracy.hidden = false;
+					ObjectStatLOS.hidden = false;
+					ObjectStatSpeed.hidden = false;
 
-			ObjectPortrait.hidden = false;
+					// Update object name
+					ObjectNameText.caption = selection[0].traits.id.generic;
+					ObjectNameText.hidden = false;
 
-			// Turn on statistic icons.
-			ObjectStatAttack.hidden = false;
-			ObjectStatHack.hidden = false;
-			ObjectStatPierce.hidden = false;
-			ObjectStatAccuracy.hidden = false;
-			ObjectStatLOS.hidden = false;
-			ObjectStatSpeed.hidden = false;
+					// Update position
+					var strString = "" + selection[0].position;
+					ObjectPositionText.caption = "Position: " + strString.substring(20,strString.length-3);
+					
+					//ObjectPositionText.caption = strString;
+					ObjectPositionText.hidden = false;
 
-                        // Update object name
-                        ObjectNameText.caption = selection[0].name;
-                        ObjectNameText.hidden = false;
-
-                        // Update position
-                        var strString = "" + selection[0].position;
-                        ObjectPositionText.caption = "Position: " + strString.substring(20,strString.length-3);
-                        //ObjectPositionText.caption = strString;
-                        ObjectPositionText.hidden = false;
-
-                        // Update speed
-                        ObjectSpeedText.caption = selection[0].speed;
-                        ObjectSpeedText.hidden = false;
+					// Update speed
+					ObjectSpeedText.caption = selection[0].actions.move.speed;
+					ObjectSpeedText.hidden = false;
                 }
         
         }
