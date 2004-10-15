4578045d299ab3eadca77fdbb80fa5028d9349e2
acumen <acumen@3db68df2-c116-0410-a063-a993310a9797>
2004-10-15 18:01:22 +0000

    Added load for 4_manual.xml.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@1243 3db68df2-c116-0410-a063-a993310a9797

--- source/main.cpp
+++ source/main.cpp
@@ -711,6 +711,7 @@ static void psInit()
 	g_GUI.LoadXMLFile("gui/test/1_global.xml");
 	g_GUI.LoadXMLFile("gui/test/2_mainmenu.xml");
 	g_GUI.LoadXMLFile("gui/test/3_session.xml");
+	g_GUI.LoadXMLFile("gui/test/4_manual.xml");
 
 	g_GUI.LoadXMLFile("gui/test/hello.xml");
 #endif
