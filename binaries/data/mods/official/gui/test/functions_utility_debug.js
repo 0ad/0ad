d6438f11792913594e033650b91837cd28ea3457
malte <malte@3db68df2-c116-0410-a063-a993310a9797>
2004-11-08 00:40:10 +0000

    Commiting Stuart's GUI updates (Mouse position shown right to FPS counter).
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@1291 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/official/gui/test/functions_utility_debug.js
+++ binaries/data/mods/official/gui/test/functions_utility_debug.js
@@ -1,5 +1,7 @@
-function UpdateFPSCounter()
+function UpdateFPSCounter(mouse)
 {
-	getGUIObjectByName('FPS_Counter').caption = "FPS: " + getFPS();
+	g_mouse_x = mouse.x;
+	g_mouse_y = mouse.y;
+	getGUIObjectByName('FPS_Counter').caption = "FPS: " + getFPS() + " " + g_mouse_x + " " + g_mouse_y;
 }
 
