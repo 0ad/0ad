dcf11182b5044d81bfe3e20f0e5505b5d3e00f34
malte <malte@3db68df2-c116-0410-a063-a993310a9797>
2004-11-15 10:05:12 +0000

    Fixed a missing comma on line 76, which made the batch-file not work any more.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@1325 3db68df2-c116-0410-a063-a993310a9797

--- source/workspaces/premake/premake.lua
+++ source/workspaces/premake/premake.lua
@@ -21,73 +21,73 @@ package.config["Testing"].target = "ps_test"
 
 -- Files
 package.files = {
-	-- ps/
-	{ sourcesfromdirs("../../ps") },
-	{ sourcesfromdirs("../../ps/scripting") },
-	{ sourcesfromdirs("../../ps/Network") },
+        -- ps/
+        { sourcesfromdirs("../../ps") },
+        { sourcesfromdirs("../../ps/scripting") },
+        { sourcesfromdirs("../../ps/Network") },
 
-	-- simulation/
-	{ sourcesfromdirs("../../simulation") },
-	{ sourcesfromdirs("../../simulation/scripting") },
+        -- simulation/
+        { sourcesfromdirs("../../simulation") },
+        { sourcesfromdirs("../../simulation/scripting") },
 
-	-- lib/
-	{ sourcesfromdirs(
-		"../../lib",
-		"../../lib/sysdep",
-		"../../lib/res") },
+        -- lib/
+        { sourcesfromdirs(
+                "../../lib",
+                "../../lib/sysdep",
+                "../../lib/res") },
 
-	-- graphics/
-	{ sourcesfromdirs(
-		"../../graphics") },
-	{ sourcesfromdirs( "../../graphics/scripting" ) },
+        -- graphics/
+        { sourcesfromdirs(
+                "../../graphics") },
+        { sourcesfromdirs( "../../graphics/scripting" ) },
 
-	-- maths/
-	{ sourcesfromdirs(
-		"../../maths") },
-	{ sourcesfromdirs( "../../maths/scripting" ) },
+        -- maths/
+        { sourcesfromdirs(
+                "../../maths") },
+        { sourcesfromdirs( "../../maths/scripting" ) },
 
-	-- renderer/
-	{ sourcesfromdirs(
-		"../../renderer") },
+        -- renderer/
+        { sourcesfromdirs(
+                "../../renderer") },
 
-	-- gui/
-	{ sourcesfromdirs(
-		"../../gui") },
-	{ sourcesfromdirs( "../../gui/scripting" ) },
+        -- gui/
+        { sourcesfromdirs(
+                "../../gui") },
+        { sourcesfromdirs( "../../gui/scripting" ) },
 
-	-- terrain/
-	{ sourcesfromdirs(
-		"../../terrain") },
+        -- terrain/
+        { sourcesfromdirs(
+                "../../terrain") },
 
-	-- sound/
-	{ sourcesfromdirs(
-		"../../sound") },
+        -- sound/
+        { sourcesfromdirs(
+                "../../sound") },
 
-	-- main
-	{ "../../main.cpp" },
+        -- main
+        { "../../main.cpp" },
 
-	-- scripting
-	{ sourcesfromdirs("../../scripting") },
+        -- scripting
+        { sourcesfromdirs("../../scripting") },
 
-	-- i18n
-	{ sourcesfromdirs("../../i18n") }
+        -- i18n
+        { sourcesfromdirs("../../i18n") },
 
-	-- tests
-	{ sourcesfromdirs("../../tests") }
+        -- tests
+        { sourcesfromdirs("../../tests") }
 }
 
 package.includepaths = {
-	"../../ps",
-	"../../simulation",
-	"../../lib",
-	"../../graphics",
-	"../../maths",
-	"../../renderer",
-	"../../terrain",
-	"../.." }
+        "../../ps",
+        "../../simulation",
+        "../../lib",
+        "../../graphics",
+        "../../maths",
+        "../../renderer",
+        "../../terrain",
+        "../.." }
 
 package.libpaths = {
-	}
+        }
 
 package.buildflags = { "no-rtti" }
 
@@ -102,64 +102,64 @@ package.config["Debug"].buildflags = { "with-symbols" }
 
 -- Platform Specifics
 if (OS == "windows") then
-	-- Libraries
-	package.links = { "opengl32" }
---	package.defines = { "XERCES_STATIC_LIB" }
-	tinsert(package.files, sourcesfromdirs("../../lib/sysdep/win"))
-	tinsert(package.files, {"../../lib/sysdep/win/assert_dlg.rc"})
-	package.linkoptions = { "/ENTRY:entry",
-		"/DELAYLOAD:opengl32.dll",
-		"/DELAYLOAD:advapi32.dll",
-		"/DELAYLOAD:gdi32.dll",
-		"/DELAYLOAD:user32.dll",
-		"/DELAYLOAD:ws2_32.dll",
-		"/DELAYLOAD:version.dll",
-		"/DELAYLOAD:ddraw.dll",
-		"/DELAYLOAD:glu32.dll",
-		"/DELAYLOAD:openal32.dll",
-		"/DELAY:UNLOAD"		-- allow manual unload of delay-loaded DLLs
-	}
-	package.config["Debug"].linkoptions = {
-		"/DELAYLOAD:js32d.dll",
-		"/DELAYLOAD:zlib1d.dll",
-		"/DELAYLOAD:libpng13d.dll",
-	}
-	
-	-- Testing uses Debug DLL's
-	package.config["Testing"].linkoptions = package.config["Debug"].linkoptions
-	
-	package.config["Release"].linkoptions = {
-		"/DELAYLOAD:js32.dll",
-		"/DELAYLOAD:zlib1.dll",
-		"/DELAYLOAD:libpng13.dll",
-	}
-	
-	tinsert(package.buildflags, { "no-main" })
-	package.config["Testing"].buildoptions = {
-		" /Zi"
-	}
-	
-	package.pchHeader = "precompiled.h"
-	package.pchSource = "precompiled.cpp"
+        -- Libraries
+        package.links = { "opengl32" }
+--        package.defines = { "XERCES_STATIC_LIB" }
+        tinsert(package.files, sourcesfromdirs("../../lib/sysdep/win"))
+        tinsert(package.files, {"../../lib/sysdep/win/assert_dlg.rc"})
+        package.linkoptions = { "/ENTRY:entry",
+                "/DELAYLOAD:opengl32.dll",
+                "/DELAYLOAD:advapi32.dll",
+                "/DELAYLOAD:gdi32.dll",
+                "/DELAYLOAD:user32.dll",
+                "/DELAYLOAD:ws2_32.dll",
+                "/DELAYLOAD:version.dll",
+                "/DELAYLOAD:ddraw.dll",
+                "/DELAYLOAD:glu32.dll",
+                "/DELAYLOAD:openal32.dll",
+                "/DELAY:UNLOAD"                -- allow manual unload of delay-loaded DLLs
+        }
+        package.config["Debug"].linkoptions = {
+                "/DELAYLOAD:js32d.dll",
+                "/DELAYLOAD:zlib1d.dll",
+                "/DELAYLOAD:libpng13d.dll",
+        }
+        
+        -- Testing uses Debug DLL's
+        package.config["Testing"].linkoptions = package.config["Debug"].linkoptions
+        
+        package.config["Release"].linkoptions = {
+                "/DELAYLOAD:js32.dll",
+                "/DELAYLOAD:zlib1.dll",
+                "/DELAYLOAD:libpng13.dll",
+        }
+        
+        tinsert(package.buildflags, { "no-main" })
+        package.config["Testing"].buildoptions = {
+                " /Zi"
+        }
+        
+        package.pchHeader = "precompiled.h"
+        package.pchSource = "precompiled.cpp"
 else -- Non-Windows, = Unix
 
-	tinsert(package.files, sourcesfromdirs("../../lib/sysdep/unix"))
+        tinsert(package.files, sourcesfromdirs("../../lib/sysdep/unix"))
 
-	-- Libraries
-	package.links = {
-		-- OpenGL and X-Windows
-		"GL", "GLU", "X11",
-		"SDL", "png",
-		"fam",
-		-- Audio
-		"openal", "vorbisfile", 
-		-- Utilities
-		"xerces-c", "z", "rt", "js"
-	}
-	tinsert(package.libpaths, { "/usr/X11R6/lib" } )
-	-- Defines
-	package.defines = {
-		"__STDC_VERSION__=199901L" }
-	-- Includes
-	tinsert(package.includepaths, { "/usr/X11R6/include/X11" } )
+        -- Libraries
+        package.links = {
+                -- OpenGL and X-Windows
+                "GL", "GLU", "X11",
+                "SDL", "png",
+                "fam",
+                -- Audio
+                "openal", "vorbisfile", 
+                -- Utilities
+                "xerces-c", "z", "rt", "js"
+        }
+        tinsert(package.libpaths, { "/usr/X11R6/lib" } )
+        -- Defines
+        package.defines = {
+                "__STDC_VERSION__=199901L" }
+        -- Includes
+        tinsert(package.includepaths, { "/usr/X11R6/include/X11" } )
 end
