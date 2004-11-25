e40b038d7a60690115f29ec394ba6a9056e850df
root <root@3db68df2-c116-0410-a063-a993310a9797>
2004-11-25 15:40:24 +0000

    Removing outdated CVS files from the main trunk
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@1398 3db68df2-c116-0410-a063-a993310a9797

--- backup/source/lib/res/res.h
+++
@@ -1,5 +0,0 @@
-#include "res/h_mgr.h"
-#include "res/vfs.h"
-#include "res/tex.h"
-#include "res/mem.h"
-#include "res/font.h"
--- backup/source/ps/Prometheus.cpp
+++
@@ -1,7 +0,0 @@
-#include "Prometheus.h"
-
-// Globals
-int g_xres = 800, g_yres = 600;
-
-DEFINE_ERROR(PS_OK, "OK");
-DEFINE_ERROR(PS_FAIL, "Fail");
