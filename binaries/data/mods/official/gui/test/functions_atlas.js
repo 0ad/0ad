b64ea4c3a4d70bb388de0261238bfc922d8b7869
stuart <stuart@3db68df2-c116-0410-a063-a993310a9797>
2004-12-23 21:21:02 +0000

    Packing it in for tonight.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@1562 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/official/gui/test/functions_atlas.js
+++ binaries/data/mods/official/gui/test/functions_atlas.js
@@ -5,6 +5,41 @@
 
 function initAtlas()
 {
+	// Top-left corner piece of main editor frame.
+	crd_atlas_mainborder_lt_corner_width = 42;
+	crd_atlas_mainborder_lt_corner_height = 42;
+	crd_atlas_mainborder_lt_corner_x = 0;
+	crd_atlas_mainborder_lt_corner_y = 0;
+
+	// Top-right corner piece of main editor frame ("Info Selection Box").
+	crd_atlas_mainborder_rt_corner_width = 203;
+	crd_atlas_mainborder_rt_corner_height = 54;
+	crd_atlas_mainborder_rt_corner_x = -crd_atlas_mainborder_rt_corner_width;
+	crd_atlas_mainborder_rt_corner_y = crd_atlas_mainborder_lt_corner_y;
+
+	// Top menu bar.
+	crd_atlas_mainborder_menu_bkg_width = crd_atlas_mainborder_rt_corner_width;
+	crd_atlas_mainborder_menu_bkg_height = 19;
+	crd_atlas_mainborder_menu_bkg_x = crd_atlas_mainborder_lt_corner_x+crd_atlas_mainborder_lt_corner_width;
+	crd_atlas_mainborder_menu_bkg_y = crd_atlas_mainborder_lt_corner_y;
+
+	// Top tool bar.
+	crd_atlas_mainborder_toolbar_bkg_width = crd_atlas_mainborder_rt_corner_width-crd_atlas_mainborder_lt_corner_width;
+	crd_atlas_mainborder_toolbar_bkg_height = 22;
+	crd_atlas_mainborder_toolbar_bkg_x = crd_atlas_mainborder_lt_corner_x;
+	crd_atlas_mainborder_toolbar_bkg_y = crd_atlas_mainborder_menu_bkg_y+crd_atlas_mainborder_menu_bkg_height;
+
+	// Bottom-right Mini Map Background.
+	crd_atlas_minimap_bkg_width = 180;
+	crd_atlas_minimap_bkg_height = 180;
+	crd_atlas_minimap_bkg_x = 0;
+	crd_atlas_minimap_bkg_y = 0;
+
+	// Bottom-right Mini Map.
+	crd_atlas_minimap_width = 140;
+	crd_atlas_minimap_height = 140;
+	crd_atlas_minimap_x = 20;
+	crd_atlas_minimap_y = 20;
 }
 
 // ====================================================================
