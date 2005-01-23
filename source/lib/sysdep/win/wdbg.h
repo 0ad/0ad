c0909e9461484a4248e782a0a521ebe9201fe284
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2005-01-23 22:42:37 +0000

    Let Testing/Release builds be built
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@1797 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/sysdep/win/wdbg.h
+++ source/lib/sysdep/win/wdbg.h
@@ -7,6 +7,10 @@
 extern "C" {
 #endif
 
+// TODO: remove this from here, and make all the exception debugging stuff nicer
+struct _EXCEPTION_POINTERS;
+typedef struct _EXCEPTION_POINTERS* PEXCEPTION_POINTERS;
+extern int debug_main_exception_filter(unsigned int code, PEXCEPTION_POINTERS ep);
 
 extern void* wdbg_get_nth_caller(uint n);
 extern int wdbg_resolve_symbol(void* ptr_of_interest, char* sym_name, char* file, int* line);
