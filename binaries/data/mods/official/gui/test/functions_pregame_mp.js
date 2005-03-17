3adbbbc05e7223c06188cd49d6f0373b9cb02902
malte <malte@3db68df2-c116-0410-a063-a993310a9797>
2005-03-17 19:30:37 +0000

    First drafts of new MP GUI, commiting to stay in sync with the repository
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@2003 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/official/gui/test/functions_pregame_mp.js
+++ binaries/data/mods/official/gui/test/functions_pregame_mp.js
@@ -1,8 +1,8 @@
 function initIPHost()
 {
         // IP Host Window background.
-        crd_pregame_iphost_bkg_x = -250;
-        crd_pregame_iphost_bkg_y = -200;
+        crd_pregame_iphost_bkg_x = -400;
+        crd_pregame_iphost_bkg_y = -300;
         crd_pregame_iphost_bkg_width = (crd_pregame_iphost_bkg_x * -1) * 2;
         crd_pregame_iphost_bkg_height = (crd_pregame_iphost_bkg_y * -1) * 2;
 
@@ -13,9 +13,9 @@ function initIPHost()
         crd_pregame_iphost_exit_button_y = crd_pregame_iphost_bkg_y-25;
 
         // IP Host Window titlebar.
-        crd_pregame_iphost_titlebar_width = crd_pregame_iphost_bkg_width;
+        crd_pregame_iphost_titlebar_width = crd_pregame_iphost_bkg_width/2;
         crd_pregame_iphost_titlebar_height = 16;
-        crd_pregame_iphost_titlebar_x = crd_pregame_iphost_bkg_x;
+        crd_pregame_iphost_titlebar_x = crd_pregame_iphost_bkg_x+crd_pregame_iphost_bkg_width/4;
         crd_pregame_iphost_titlebar_y = crd_pregame_iphost_bkg_y-25;
 }
 
@@ -23,6 +23,9 @@ function initIPHost()
 
 function initMPSessionHost(playerName, mapName)
 {
+        GUIObjectHide("pregame_mp_ip");
+        GUIObjectHide("pregame_subwindow_bkg");
+
         var server = createServer();
         
         // Set the map to use
@@ -45,9 +48,6 @@ function initMPSessionHost(playerName, mapName)
                 messageBox(400, 200, event.sender+" says: "+event.message, "Chat Message", 2, new Array(), new Array());
         };
 
-        GUIObjectHide("pregame_mp_ip");
-        GUIObjectHide("pregame_subwindow_bkg");
-
         // Need "waiting for more players to join and start game" code here
 
         btCaptions = new Array("OK");
@@ -59,6 +59,9 @@ function initMPSessionHost(playerName, mapName)
 
 function initMPSessionClient(playerName, serverIP)
 {
+        GUIObjectHide("pregame_mp_ip");
+        GUIObjectHide("pregame_subwindow_bkg");
+
         var client=createClient();
         
         client.playerName=playerName;
@@ -81,9 +84,6 @@ function initMPSessionClient(playerName, serverIP)
         if(!success) {
                 messageBox(400, 200, "Failed to join game. Please review the logfile for more information on the problem.", "Failure", 2, new Array(), new Array());
         }
-
-        GUIObjectHide("pregame_mp_ip");
-        GUIObjectHide("pregame_subwindow_bkg");
 
         // Need "waiting for game to start" code here - it should automatically start if the client recieves the start signal from the server,
         // but I currently don't know how that could be done.
