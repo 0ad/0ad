65d910ed5d7d7b9d569b772e4afaa9dac7c93f2c
jan <jan@3db68df2-c116-0410-a063-a993310a9797>
2005-05-03 21:37:49 +0000

    revise thunks to not free their MemFun_t if the registered function times out
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@2232 3db68df2-c116-0410-a063-a993310a9797

--- source/ps/LoaderThunks.h
+++ source/ps/LoaderThunks.h
@@ -1,21 +1,30 @@
+// rationale for allocating MemFun_t dynamically:
+// need to store class pointer, function, and argument for each registered
+// function; single static storage isn't possible. we don't want to break
+// C compat in the Loader.h interface, so we can't have it take care of this.
+// that leaves dynamic alloc or reserving some static storage freed when
+// load registration begins. the former is slower and requires checking
+// the thunked function's return value (because we mustn't free MemFun_t
+// if the function times out), but is simpler.
 
 template<class T> struct MemFun_t
 {
 	T* const this_;
-	void(T::*func)(void);
-	MemFun_t(T* this__, void(T::*func_)(void))
+	int(T::*func)(void);
+	MemFun_t(T* this__, int(T::*func_)(void))
 		: this_(this__), func(func_) {}
 };
 
 template<class T> static int MemFunThunk(void* param, double time_left)
 {
 	MemFun_t<T>* const mf = (MemFun_t<T>*)param;
-	(mf->this_->*mf->func)();
-	delete mf;
-	return 0;
+	int ret = (mf->this_->*mf->func)();
+	if(ret <= 0)	// did not time out
+		delete mf;
+	return ret;
 }
 
-template<class T> void RegMemFun(T* this_, void(T::*func)(void),
+template<class T> void RegMemFun(T* this_, int(T::*func)(void),
 								 const wchar_t* description, int estimated_duration_ms)
 {
 	void* param = new MemFun_t<T>(this_, func);
@@ -30,20 +39,21 @@ template<class T, class Arg> struct MemFun1_t
 {
 	T* const this_;
 	Arg arg;
-	void(T::*func)(Arg);
-	MemFun1_t(T* this__, void(T::*func_)(Arg), Arg arg_)
+	int(T::*func)(Arg);
+	MemFun1_t(T* this__, int(T::*func_)(Arg), Arg arg_)
 		: this_(this__), func(func_), arg(arg_) {}
 };
 
 template<class T, class Arg> static int MemFun1Thunk(void* param, double time_left)
 {
 	MemFun1_t<T, Arg>* const mf = (MemFun1_t<T, Arg>*)param;
-	(mf->this_->*mf->func)(mf->arg);
-	delete mf;
-	return 0;
+	int ret = (mf->this_->*mf->func)(mf->arg);
+	if(ret <= 0)	// did not time out
+		delete mf;
+	return ret;
 }
 
-template<class T, class Arg> void RegMemFun1(T* this_, void(T::*func)(Arg), Arg arg,
+template<class T, class Arg> void RegMemFun1(T* this_, int(T::*func)(Arg), Arg arg,
 											 const wchar_t* description, int estimated_duration_ms)
 {
 	void* param = new MemFun1_t<T, Arg>(this_, func, arg);
