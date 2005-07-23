6fd47d2f1a724fcb64058c7bcb79b831e2d737ee
gustav <gustav@3db68df2-c116-0410-a063-a993310a9797>
2005-07-23 20:17:36 +0000

    Added more functions. Changed addItem to insertItem
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@2522 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/official/gui/test/functions_utility_list.js
+++ binaries/data/mods/official/gui/test/functions_utility_list.js
@@ -60,3 +60,57 @@ function addItem (objectName, pos, value)
 }
 
 // ====================================================================
+
+// Adds an element to the end of the list
+function pushItem(objectName, value)
+{
+	var list = getGUIObjectByName(objectName).list;
+	list.push(value);
+	getGUIObjectByName(objectName).list = list;
+	// No need to update selection
+}
+
+// ====================================================================
+
+// Removes the last element
+function popItem(objectName)
+{
+	var selected = getGUIObjectByName(objectName).selected;
+	removeItem(objectName, getNumItems(objectName)-1);
+
+	If (selected == getNumItems(objectName)-1)
+	{
+		getGUIObjectByName(objectName).selected = -1;
+	}
+}
+
+// ====================================================================
+
+// Retrieves the number of elements in the list
+function getNumItems(objectName)
+{
+	var list = getGUIObjectByName(objectName).list;
+	return list.length;
+}
+
+// ====================================================================
+
+// Retrieves the value of the item at 'pos'
+function getItemValue(objectName, pos)
+{
+	var list = getGUIObjectByName(objectName).list;
+	return list[pos];
+}
+
+// ====================================================================
+
+// Retrieves the value of the currently selected item
+function getCurItemValue(objectName)
+{
+	if (getGUIObjectByName(objectName).selected == -1)
+		return "";
+	var list = getGUIObjectByName(objectName).list;
+	return list[getGUIObjectByName(objectName).selected];
+}
+
+// ====================================================================
