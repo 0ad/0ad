0f515fbb8735767828959d524840e26253474d45
matei <matei@3db68df2-c116-0410-a063-a993310a9797>
2005-07-23 08:19:15 +0000

    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@2520 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/official/maps/random/test.js
+++ binaries/data/mods/official/maps/random/test.js
@@ -1,12 +1,20 @@
 const SIZE = 64;
 
-init(SIZE, ["snow", "snow forest"], 0);
+init(SIZE, "grass dirt 75", 0);
 
 createAreas(
-	new ClumpPlacer(20.0, 0.01, 0.01), 
-	[new TerrainPainter("snow grass 2"), 
-	 new SmoothElevationPainter(ELEVATION_SET, 15.0, 3)],
-	new AvoidTextureConstraint("snow grass 2"),
+	new ClumpPlacer(23.0, 0.01, 0.01), 
+	[new TerrainPainter("cliff_greekb_moss"), 
+	 new SmoothElevationPainter(ELEVATION_SET, 30.0, 3)],
+	null,
+	20,
+	200
+);
+
+createAreas(
+	new ClumpPlacer(1.0, 1.0, 1.0), 
+	new TerrainPainter(["grass_dirt_75|wrld_flora_oak", "grass_dirt_75|bush_medit_me"]),
+	new AvoidTextureConstraint("cliff_greekb_moss"),
 	150,
-	500
+	200
 );
