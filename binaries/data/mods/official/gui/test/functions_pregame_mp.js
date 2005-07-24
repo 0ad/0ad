e07bf2ade0b930ea8142de6062f6c6d751a53f7c
malte <malte@3db68df2-c116-0410-a063-a993310a9797>
2005-07-24 01:13:50 +0000

    Broke networking code. Fixed messageBox() code.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@2529 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/official/gui/test/functions_pregame_mp.js
+++ binaries/data/mods/official/gui/test/functions_pregame_mp.js
@@ -1,37 +1,63 @@
 // ====================================================================
 
-function initMPSessionHost(playerName, mapName)
+function initMPSessionHost()
 {
-        GUIObjectHide("pregame_mp_ip");
-        GUIObjectHide("pregame_subwindow_bkg");
+	var gameName = getGUIObjectByName("pregame_mp_host_gamename").caption;
+	guiSwitch ("pregame_mp_host", "pregame_mp_setup_host");
+	guiHide("pregame_mainmenu_versionnumber");
+ 
+	// Set up the server
+	var server = createServer();
+	// Welcome message
+	server.welcomeMessage = getGUIObjectByName("pregame_mp_host_welcomemsg").caption;
+	// Server Name
+	server.serverName = gameName;
+                              	
+	// start listening
+	var success = server.open();
+	if(!success) {
+		messageBox(400, 200, "Failed to start server. Please review the logfile for more information on the problem.", "Problem", 2, new Array(), new Array());
+	}
 
-        var server = createServer();
+	// Set the title for the setup screen
+	getGUIObjectByName("pregame_mp_setup_host_titlebar").caption = "Hosting: " + getGUIObjectByName("pregame_mp_host_gamename").caption;
+
+	// Set the host player's name on the setup screen
+	getGUIObjectByName("pregame_mp_setup_host_p1_txt").caption = "P1: " + getGUIObjectByName("pregame_mp_modesel_playername").caption + " (Host)";
         
-        // Set the map to use
-        g_GameAttributes.mapFile = mapName;
+	// Set the map to use
+	//g_GameAttributes.mapFile = mapName;
+
+	// -------------------------
+	// INCOMING CONNECTIONS
+	// -------------------------
+	server.onClientConnect=function (event) {
+		console.write("Client connected.");
+		//console.write("A new client has successfully connected! ID: " + event.id + ", Name: " + event.name + ", Session: " + event.session);
+		//console.write("New client.");
+		//var playerSlot = g_GameAttributes.getOpenSlot();
+		// assign a slot
+		//playerSlot.assignToSession(event.session);
+		//console.write("slot: " + playerSlot.player);
+		//console.write("got here");
+		// need to refresh the dialog control data here
+	};
+	
+	server.onClientDisconnect=function (event) {
+		console.write("Client disconnected.");
+	};	
         
-        // Set basic server options, such as:
-        // server.port = 20595; // Default is 20595 - you can also explicitly set to -1 for default port
-        server.serverPlayerName=playerName;
-        server.serverName=playerName+"'s Server";
-        server.welcomeMessage="Welcome to "+server.serverName;
+	// CHAT
+	/*server.onChat=function (event) {
+		messageBox(400, 200, event.sender+" says: "+event.message, "Chat Message", 2, new Array(), new Array());
+	};*/
 
-        // Actually start listening for connections.. This should probably not be
-        // done until there's been a dialog for filling in the previous options ;-)
-        var success = server.open();
-        if(!success) {
-                messageBox(400, 200, "Failed to start server. Please review the logfile for more information on the problem.", "Problem", 2, new Array(), new Array());
-        }
+	// Need "waiting for more players to join and start game" code here
 
-        server.onChat=function (event) {
-                messageBox(400, 200, event.sender+" says: "+event.message, "Chat Message", 2, new Array(), new Array());
-        };
-
-        // Need "waiting for more players to join and start game" code here
-
-        btCaptions = new Array("OK");
-        btCode = new Array("startLoadingScreen();");
-        messageBox(400, 200, "Waiting for clients to join - Click OK to start the game.", "Ready", 0, btCaptions, btCode);
+	/*btCaptions = new Array("OK");
+	btCode = new Array("startLoadingScreen();");
+	messageBox(400, 200, "Waiting for clients to join - Click OK to start the game.", "Ready", 0, btCaptions, btCode);*/
+	
 }
 
 // ====================================================================
