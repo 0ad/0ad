41d522c9a17d716881481aa7622d799b7139f7de
stuart <stuart@3db68df2-c116-0410-a063-a993310a9797>
2005-07-28 02:36:48 +0000

    Made the framework of options, history, and campaign screens, mostly just for the hell of it, and fixed various things. Can't do much with them, mind, but Rome wasn't burnt in a day (thank you, Mr Nero).
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@2559 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/official/gui/test/functions_page_pregame.js
+++ binaries/data/mods/official/gui/test/functions_page_pregame.js
@@ -5,26 +5,60 @@
 
 // ====================================================================
 
+// Helper function that enables the dark background mask, then reveals a given subwindow object.
 function openMainMenuSubWindow (windowName)
 {
-	// Helper function that enables the dark background mask, then reveals a given subwindow object.
-
 	guiUnHide	("pgSubWindow");
 	guiUnHide	(windowName);
 }
 
 // ====================================================================
 
+// Helper function that disables the dark background mask, then hides a given subwindow object.
 function closeMainMenuSubWindow (windowName)
 {
-	// Helper function that disables the dark background mask, then hides a given subwindow object.
-
 	guiHide		("pgSubWindow");
 	guiHide		(windowName);
 }
 
 // ====================================================================
 
+// Switch to a given options tab window.
+function openOptionsTab(tabName)
+{
+	// Hide the other tabs.
+	for (i = 1; i <= 3; i++)
+	{
+		switch (i)
+		{
+			case 1:
+				tmpName = "pgOptionsAudio";
+			break;
+			case 2:
+				tmpName = "pgOptionsVideo";
+			break;
+			case 3:
+				tmpName = "pgOptionsGame";
+			break;
+			default:
+			break;
+		}
+
+		if (tmpName != tabName)
+		{
+			getGUIObjectByName (tmpName + "Window").hidden = true;
+			getGUIObjectByName (tmpName + "Button").enabled = true;
+		}
+	}
+
+	// Make given tab visible.
+	getGUIObjectByName (tabName + "Window").hidden = false;
+	getGUIObjectByName (tabName + "Button").enabled = false;
+}
+
+// ====================================================================
+
+// Move the credits up the screen.
 function updateCredits()
 {
 	// If there are still credit lines to remove, remove them.
