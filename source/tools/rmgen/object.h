9a882bda259ea6a5c91a065dedd124f082ee9b3e
matei <matei@3db68df2-c116-0410-a063-a993310a9797>
2005-07-30 19:22:16 +0000

    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@2564 3db68df2-c116-0410-a063-a993310a9797

---
+++ source/tools/rmgen/object.h
@@ -0,0 +1,15 @@
+#ifndef __ENTITY_H__
+#define __ENTITY_H__
+
+class Entity {
+public:
+	std::string type;   // called "template" in XML?
+	int player;
+	float x, y, z;
+	float orientation;
+
+	Entity();
+	Entity(const std::string& type, int player, float x, float y, float z, float orientation);
+};
+
+#endif
