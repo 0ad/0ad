16cca8b6d599cbd29c9fceec3bfa9df5b6b9cfa8
matei <matei@3db68df2-c116-0410-a063-a993310a9797>
2005-09-06 22:47:26 +0000

    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@2680 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/official/maps/random/test.js
+++ binaries/data/mods/official/maps/random/test.js
@@ -1,17 +1,4 @@
 const SIZE = 64;
 
-init(SIZE, ["grass dirt 50", "grass dirt 75"], 0);
+init(SIZE, "grass dirt 50", 0);
 
-var tc = createTileClass();
-var tc2 = createTileClass();
-
-createAreas(
-	new ClumpPlacer(4.0, 0.01, 0.01), 
-	[new TerrainPainter("dirt_forest|flora/wrld_flora_deci_1.xml"), new TileClassPainter(tc)],
-	new AvoidTileClassConstraint(tc, 5),
-	100);
-
-createObjectGroups(
-	new SimpleGroup([new SimpleObject("foliage/bush.xml", 1, 0)], tc2),
-	[new AvoidTileClassConstraint(tc, 3), new AvoidTileClassConstraint(tc2, 2)],
-	200);
