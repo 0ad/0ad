47d3340633b4148437fb5387aa669714d6bd3a43
nicolai <nicolai@3db68df2-c116-0410-a063-a993310a9797>
2005-11-06 18:13:01 +0000

    Enable key repeat on Linux.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@3111 3db68df2-c116-0410-a063-a993310a9797

--- source/ps/GameSetup/GameSetup.cpp
+++ source/ps/GameSetup/GameSetup.cpp
@@ -567,6 +567,10 @@ static void InitPs(bool setup_gui)
 
 static void InitInput()
 {
+#if !OS_WIN
+	SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
+#endif
+
 	// register input handlers
 	// This stack is constructed so the first added, will be the last
 	//  one called. This is important, because each of the handlers
