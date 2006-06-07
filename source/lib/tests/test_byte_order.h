3dd09b0bce7e20179aadf08bcc589e73dfd5233b
simon <simon@3db68df2-c116-0410-a063-a993310a9797>
2006-06-07 02:30:47 +0000

    The first test, and support file for it and future tests
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@3958 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/tests/test_byte_order.h
+++ source/lib/tests/test_byte_order.h
@@ -1,33 +1,37 @@
+#include "lib/precompiled.h"
+
+#include <lib/byte_order.h>
+
 #include <cxxtest/TestSuite.h>
 
 class TestByteOrder : public CxxTest::TestSuite 
 {
 public:
-	void test()
+	void test_()
 	{
 		const u32 x = 0x01234567u;
 		const u8 LS_byte = *(u8*)&x;
 		// little endian
-		if(LS_byte, 0x67)
+		if(LS_byte == 0x67)
 		{
-			TS_ASSERT_EQUAL(to_le16(0x0123u), 0x0123u);
-			TS_ASSERT_EQUAL(to_le32(0x01234567u), 0x01234567u);
-			TS_ASSERT_EQUAL(to_le64(0x0123456789ABCDEFu), 0x0123456789ABCDEFu);
+			TS_ASSERT_EQUALS(to_le16(0x0123u), 0x0123u);
+			TS_ASSERT_EQUALS(to_le32(0x01234567u), 0x01234567u);
+			TS_ASSERT_EQUALS(to_le64(0x0123456789ABCDEFULL), 0x0123456789ABCDEFULL);
 
-			TS_ASSERT_EQUAL(to_be16(0x0123u), 0x2301u);
-			TS_ASSERT_EQUAL(to_be32(0x01234567u), 0x67452301u);
-			TS_ASSERT_EQUAL(to_be64(0x0123456789ABCDEFu), 0xEFCDAB8967452301u);
+			TS_ASSERT_EQUALS(to_be16(0x0123u), 0x2301u);
+			TS_ASSERT_EQUALS(to_be32(0x01234567u), 0x67452301u);
+			TS_ASSERT_EQUALS(to_be64(0x0123456789ABCDEFull), 0xEFCDAB8967452301ull);
 		}
 		// big endian
-		else if(LS_byte, 0x01)
+		else if(LS_byte == 0x01)
 		{
-			TS_ASSERT_EQUAL(to_le16(0x0123u), 0x2301u);
-			TS_ASSERT_EQUAL(to_le32(0x01234567u), 0x67452301u);
-			TS_ASSERT_EQUAL(to_le64(0x0123456789ABCDEFu), 0xEFCDAB8967452301u);
+			TS_ASSERT_EQUALS(to_le16(0x0123u), 0x2301u);
+			TS_ASSERT_EQUALS(to_le32(0x01234567u), 0x67452301u);
+			TS_ASSERT_EQUALS(to_le64(0x0123456789ABCDEFull), 0xEFCDAB8967452301ull);
 
-			TS_ASSERT_EQUAL(to_be16(0x0123u), 0x0123u);
-			TS_ASSERT_EQUAL(to_be32(0x01234567u), 0x01234567u);
-			TS_ASSERT_EQUAL(to_be64(0x0123456789ABCDEFu), 0x0123456789ABCDEFu);
+			TS_ASSERT_EQUALS(to_be16(0x0123u), 0x0123u);
+			TS_ASSERT_EQUALS(to_be32(0x01234567u), 0x01234567u);
+			TS_ASSERT_EQUALS(to_be64(0x0123456789ABCDEFull), 0x0123456789ABCDEFull);
 		}
 		else
 			TS_FAIL("endian determination failed");
---
+++ source/test_root.cpp
@@ -0,0 +1,17 @@
+/* Generated file, do not edit */
+
+#ifndef CXXTEST_RUNNING
+#define CXXTEST_RUNNING
+#endif
+
+#define _CXXTEST_HAVE_STD
+#include <cxxtest/TestListener.h>
+#include <cxxtest/TestTracker.h>
+#include <cxxtest/TestRunner.h>
+#include <cxxtest/RealDescriptions.h>
+#include <cxxtest/ErrorPrinter.h>
+
+int main() {
+ return CxxTest::ErrorPrinter().run();
+}
+#include <cxxtest/Root.cpp>
