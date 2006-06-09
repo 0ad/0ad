605714c5118bad14c64878d0005f12eb8b5314ad
matei <matei@3db68df2-c116-0410-a063-a993310a9797>
2006-06-09 23:04:47 +0000

    Renamed tech and formation directories for better organization, and created a scripts directory for the game startup script, entity scripts, and eventually things like victory condition scripts for different game modes, or possibly triggers.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@3995 3db68df2-c116-0410-a063-a993310a9797

---
+++ binaries/data/mods/official/scripts/game_startup.js
@@ -0,0 +1,6 @@
+/*
+	DESCRIPTION	: Script called when the game is started, which should initialize the players and their units based on the game settings.
+	NOTES		: 
+*/
+
+console.write( "Game startup script done." );
