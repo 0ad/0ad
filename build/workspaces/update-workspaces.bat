150bfeb67072efc67041b3d8f35751f3136b57a3
jan <jan@3db68df2-c116-0410-a063-a993310a9797>
2006-06-09 20:01:25 +0000

    no longer create vc2002 project
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@3994 3db68df2-c116-0410-a063-a993310a9797

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -1,13 +1,14 @@
 @echo off
 rem ** Create Visual Studio Workspaces on Windows **
 
-mkdir vc2002
 mkdir vc2003
 mkdir vc2005
 
 cd ..\premake
 
-premake --target vs2002 --outpath ../workspaces/vc2002 %*
+rem VC2002 removed because no one is using it and generating it wastes time.
+rem it's entirely analogous to other cmdlines - just copy+paste if needed again.
+
 premake --target vs2003 --outpath ../workspaces/vc2003 %*
 premake --target vs2005 --outpath ../workspaces/vc2005 %*
 
