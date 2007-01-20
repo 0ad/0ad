31683cc2aeb35ec21516d125a1b79737ad45dcf2
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2007-01-20 14:20:31 +0000

    Singleton: removed "cunning trick", because someone pointed out that it is very undefined behaviour and is completely unnecessary nowadays
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@4800 3db68df2-c116-0410-a063-a993310a9797

--- source/ps/Singleton.h
+++ source/ps/Singleton.h
@@ -30,11 +30,7 @@ public:
 	Singleton()
 	{
 		debug_assert( !ms_singleton );
-
-		// use a cunning trick to get the singleton pointing to the start of
-		// the whole, rather than the start of the Singleton part of the object
-		uintptr_t offset = (uintptr_t)(T*)1 - (uintptr_t)(Singleton<T>*)(T*)1;
-		ms_singleton = (T*)((uintptr_t)this + offset);
+		ms_singleton = static_cast<T*>(this);
 	}
 
 	~Singleton()
