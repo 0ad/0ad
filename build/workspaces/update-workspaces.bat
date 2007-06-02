2d36f9785c3d125e95dbd1de5c5ca013ef016579
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2007-06-02 15:24:20 +0000

    Reverted update-workspaces.bat
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@5127 3db68df2-c116-0410-a063-a993310a9797

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -1,7 +1,7 @@
 @echo off
 rem ** Create Visual Studio Workspaces on Windows **
 
-rem mkdir vc2003
+mkdir vc2003
 mkdir vc2005
 
 cd ..\premake
@@ -9,7 +9,7 @@ cd ..\premake
 rem VC2002 removed because no one is using it and generating it wastes time.
 rem it's entirely analogous to other cmdlines - just copy+paste if needed again.
 
-rem premake --target vs2003 --outpath ../workspaces/vc2003 %*
-premake --target vs2005 --outpath ../workspaces/vc2005 --atlas %*
+premake --target vs2003 --outpath ../workspaces/vc2003 %*
+premake --target vs2005 --outpath ../workspaces/vc2005 %*
 
 cd ..\workspaces
