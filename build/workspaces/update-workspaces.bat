ab2fa07dcb54da9d7f7e3a65a07f3279c75c23e6
jan <jan@3db68df2-c116-0410-a063-a993310a9797>
2007-10-03 09:43:00 +0000

    avoid "directory already exists" warning. allow skipping generating the vc2003 or vc2005 projects by creating a file SKIP_PREMAKE_HERE in that workspace directory
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@5386 3db68df2-c116-0410-a063-a993310a9797

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -1,15 +1,15 @@
 @echo off
 rem ** Create Visual Studio Workspaces on Windows **
 
-mkdir vc2003
-mkdir vc2005
-
-cd ..\premake
+if not exist vc2003\pyrogenesis.sln mkdir vc2003
+if not exist vc2005\pyrogenesis.sln mkdir vc2005
 
 rem VC2002 removed because no one is using it and generating it wastes time.
 rem it's entirely analogous to other cmdlines - just copy+paste if needed again.
 
-premake --target vs2003 --outpath ../workspaces/vc2003 %*
-premake --target vs2005 --outpath ../workspaces/vc2005 %*
+cd ..\premake
+
+if not exist ..\workspaces\vc2003\SKIP_PREMAKE_HERE premake --target vs2003 --outpath ../workspaces/vc2003 %*
+if not exist ..\workspaces\vc2005\SKIP_PREMAKE_HERE premake --target vs2005 --outpath ../workspaces/vc2005 %*
 
 cd ..\workspaces
