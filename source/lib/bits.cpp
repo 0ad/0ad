625821d57016835f9aa86e7cb485f65b3cdb3a45
simon <simon@3db68df2-c116-0410-a063-a993310a9797>
2008-06-28 17:10:25 +0000

    GCC fix (*(u32*)&float_var confuses gcc)
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@6133 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/bits.cpp
+++ source/lib/bits.cpp
@@ -11,9 +11,16 @@
 #include "precompiled.h"
 #include "bits.h"
 
+static inline u32 get_float_bits(const float x)
+{
+	u32 ret;
+	memcpy(&ret, &x, 4);
+	return ret;
+}
+
 int floor_log2(const float x)
 {
-	const u32 i = *(u32*)&x;
-	u32 biased_exp = (i >> 23) & 0xFF;
+	const u32 i = get_float_bits(x);
+	const u32 biased_exp = (i >> 23) & 0xFF;
 	return (int)biased_exp - 127;
 }
