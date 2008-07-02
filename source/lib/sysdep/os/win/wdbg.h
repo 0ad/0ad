931dbefe8b6b67d79b17483aacc17c8f4024e025
jan <jan@3db68df2-c116-0410-a063-a993310a9797>
2008-07-02 06:21:06 +0000

    add header and include guard
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@6173 3db68df2-c116-0410-a063-a993310a9797

--- source/lib/sysdep/os/win/wdbg.h
+++ source/lib/sysdep/os/win/wdbg.h
@@ -1,4 +1,17 @@
 /**
+ * =========================================================================
+ * File        : wdbg.h
+ * Project     : 0 A.D.
+ * Description : Win32 debug support code.
+ * =========================================================================
+ */
+
+// license: GPL; see lib/license.txt
+
+#ifndef INCLUDED_WDBG
+#define INCLUDED_WDBG
+
+/**
  * same as debug_printf except that some type conversions aren't supported
  * (in particular, no floating point).
  *
@@ -12,3 +25,5 @@ LIB_API void wdbg_printf(const char* fmt, ...);
  * while under the heap or dbghelp locks.
  **/
 #define wdbg_assert(expr) STMT(if(!(expr)) debug_break();)
+
+#endif	// #ifndef INCLUDED_WDBG
