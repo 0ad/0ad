9f6c4588dc7b18cf2ada566ba58781c0411c3457
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2008-07-18 23:49:51 +0000

    Fixed one set of annoying DCDT warnings
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@6245 3db68df2-c116-0410-a063-a993310a9797

--- source/dcdt/se/sr_class_manager.h
+++ source/dcdt/se/sr_class_manager.h
@@ -17,9 +17,9 @@ class SrClassManagerBase : public SrSharedClass
     virtual void* alloc ()=0;
     virtual void* alloc ( const void* obj )=0;
     virtual void free ( void* obj )=0;
-    virtual void output ( SrOutput& o, const void* obj ) { }
-    virtual void input ( SrInput& i, void* obj ) { }
-    virtual int compare ( const void* obj1, const void* obj2 ) { return 0; }
+    virtual void output ( SrOutput& UNUSED(o), const void* UNUSED(obj) ) { }
+    virtual void input ( SrInput& UNUSED(i), void* UNUSED(obj) ) { }
+    virtual int compare ( const void* UNUSED(obj1), const void* UNUSED(obj2) ) { return 0; }
  };
 
 /*! Example of an implementation of a class to be automatically managed
