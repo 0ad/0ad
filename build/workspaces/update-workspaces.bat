9d309cce58b42a886c8de7764aacc4f220afb492
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2009-03-27 01:45:22 +0000

    Removed Collada_dbg.dll since it's outdated and not autobuilt. Added --collada option to default update-workspaces, so Collada_dbg.dll will get compiled automatically.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@6793 3db68df2-c116-0410-a063-a993310a9797

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -9,7 +9,7 @@ rem it's entirely analogous to other cmdlines - just copy+paste if needed again.
 
 cd ..\premake
 
-if not exist ..\workspaces\vc2005\SKIP_PREMAKE_HERE premake --target vs2005 --outpath ../workspaces/vc2005 %*
-if not exist ..\workspaces\vc2008\SKIP_PREMAKE_HERE premake --target vs2008 --outpath ../workspaces/vc2008 %*
+if not exist ..\workspaces\vc2005\SKIP_PREMAKE_HERE premake --collada --target vs2005 --outpath ../workspaces/vc2005 %*
+if not exist ..\workspaces\vc2008\SKIP_PREMAKE_HERE premake --collada --target vs2008 --outpath ../workspaces/vc2008 %*
 
 cd ..\workspaces
