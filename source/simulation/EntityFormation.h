3cc511c02cc30f5873d65ed1e717fb5d20aa36e4
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2009-04-18 17:00:33 +0000

    Add GPL header
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@6830 3db68df2-c116-0410-a063-a993310a9797

--- source/simulation/EntityFormation.h
+++ source/simulation/EntityFormation.h
@@ -1,3 +1,20 @@
+/* Copyright (C) 2009 Wildfire Games.
+ * This file is part of 0 A.D.
+ *
+ * 0 A.D. is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * 0 A.D. is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 //Instances of this class contain the actual information about in-game formations.
 //It is based off of Formation.cpp and uses it as a reference as to what can and cannot
 //be done in this formation.  This is represented as m_base.
