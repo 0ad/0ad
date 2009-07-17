df1a879b6b3ab5db46939e4342527745a2582efd
simon <simon@3db68df2-c116-0410-a063-a993310a9797>
2009-07-17 22:07:09 +0000

    Simplify the update-workspaces.sh script, remove some uses of absolute paths and allow the script to be run from anywhere (e.g. as ../update-workspaces.sh from the workspaces/gcc/ directory)
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@7011 3db68df2-c116-0410-a063-a993310a9797

--- build/workspaces/update-workspaces.sh
+++ build/workspaces/update-workspaces.sh
@@ -1,26 +1,12 @@
 #!/bin/sh
 
-# build/workspaces/
+cd "$(dirname $0)"
+# Now in build/workspaces/ (where we assume this script resides)
 
-start_dir=$(pwd)
-premake_dir=$(pwd)/../premake
-workspace_dir=$(pwd)/gcc
-mkdir "$workspace_dir" 2>/dev/null
+# Make sure workspaces/gcc exists.
+mkdir -p gcc
 
-cd "$premake_dir"
-
-# build/premake/
-
+# Now build premake and run it to create the makefiles
+cd ../premake
 make -C src
-HOSTTYPE=$HOSTTYPE ./premake --outpath "$workspace_dir" --atlas --collada "$@" --target gnu
-
-# These files need to be linked; premake makefiles assume that the
-# lua file is accessible from the makefile directory
-
-cd "$workspace_dir"
-ln -f -s "$premake_dir"/premake.lua "$premake_dir"/functions.lua .
-if [ -x "$premake_dir"/premake ]; then
-    ln -f -s "$premake_dir"/premake .
-fi
-
-cd "$start_dir"
+src/bin/premake --outpath ../workspaces/gcc --atlas --collada "$@" --target gnu
