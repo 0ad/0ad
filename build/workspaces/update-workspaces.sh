d4f19a80ae77682685f99ae122d919a065457267
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2009-07-17 21:15:01 +0000

    Fix update-workspaces.sh to handle paths with spaces (http://www.wildfiregames.com/forum/index.php?showtopic=12472)
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@7009 3db68df2-c116-0410-a063-a993310a9797

--- build/workspaces/update-workspaces.sh
+++ build/workspaces/update-workspaces.sh
@@ -5,22 +5,22 @@
 start_dir=$(pwd)
 premake_dir=$(pwd)/../premake
 workspace_dir=$(pwd)/gcc
-mkdir $workspace_dir 2>/dev/null
+mkdir "$workspace_dir" 2>/dev/null
 
-cd $premake_dir
+cd "$premake_dir"
 
 # build/premake/
 
 make -C src
-HOSTTYPE=$HOSTTYPE ./premake --outpath $workspace_dir --atlas --collada "$@" --target gnu
+HOSTTYPE=$HOSTTYPE ./premake --outpath "$workspace_dir" --atlas --collada "$@" --target gnu
 
 # These files need to be linked; premake makefiles assume that the
 # lua file is accessible from the makefile directory
 
-cd $workspace_dir
-ln -f -s $premake_dir/premake.lua $premake_dir/functions.lua .
-if [ -x $premake_dir/premake ]; then
-	ln -f -s $premake_dir/premake .
+cd "$workspace_dir"
+ln -f -s "$premake_dir"/premake.lua "$premake_dir"/functions.lua .
+if [ -x "$premake_dir"/premake ]; then
+    ln -f -s "$premake_dir"/premake .
 fi
 
-cd $start_dir
+cd "$start_dir"
