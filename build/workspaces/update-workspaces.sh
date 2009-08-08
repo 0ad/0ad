facc83ba40af20ec8ac446a86edc77fc4f70ce39
simon <simon@3db68df2-c116-0410-a063-a993310a9797>
2009-08-08 11:22:42 +0000

    Automatically build bundled external libraries and report errors if premake or an external library fails to build
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@7091 3db68df2-c116-0410-a063-a993310a9797

--- build/workspaces/update-workspaces.sh
+++ build/workspaces/update-workspaces.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+die()
+{
+	echo ERROR: $*
+	exit 1
+}
+
 cd "$(dirname $0)"
 # Now in build/workspaces/ (where we assume this script resides)
+
+# Build/update bundled external libraries
+(cd ../../libraries/fcollada/src && make) || die "FCollada build failed"
+(cd ../../libraries/spidermonkey/src && ./build.sh) || die "SpiderMonkey build failed"
 
 # Make sure workspaces/gcc exists.
 mkdir -p gcc
 
 # Now build premake and run it to create the makefiles
 cd ../premake
-make -C src
-src/bin/premake --outpath ../workspaces/gcc --atlas --collada "$@" --target gnu
+make -C src || die "Premake build failed"
+src/bin/premake --outpath ../workspaces/gcc --atlas --collada "$@" --target gnu || die "Premake failed"
