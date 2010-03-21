9f8ca7492b4be4b37da59704c02a874745a1198f
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2010-03-21 18:40:22 +0000

    Restrict rpath usage to Linux
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@7386 3db68df2-c116-0410-a063-a993310a9797

--- build/premake/premake.lua
+++ build/premake/premake.lua
@@ -206,10 +206,12 @@ function package_set_build_flags()
 			end
 		end
 
-		-- To use our local SpiderMonkey library, it needs to be part of the runtime dynamic linker
-		-- path. So add the executable path with -rpath:
-		-- (TODO: is this a sane way to do it?)
-		tinsert(package.linkoptions, {"-Wl,-rpath='$$ORIGIN'"}) -- use Makefile escaping of '$'
+		if OS == "linux" then
+			-- To use our local SpiderMonkey library, it needs to be part of the runtime dynamic linker
+			-- path. So add the executable path with -rpath:
+			-- (TODO: is this a sane way to do it?)
+			tinsert(package.linkoptions, {"-Wl,-rpath='$$ORIGIN'"}) -- use Makefile escaping of '$'
+		end
 
 		tinsert(package.buildoptions, {
 			-- Hide symbols in dynamic shared objects by default, for efficiency and for equivalence with
