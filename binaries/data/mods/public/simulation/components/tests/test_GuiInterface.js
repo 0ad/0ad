6c6009b30ad604a3df1e726b3d47f8573f7b3b86
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2010-07-06 20:30:22 +0000

    Fix simulation test
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@7707 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/tests/test_GuiInterface.js
+++ binaries/data/mods/public/simulation/components/tests/test_GuiInterface.js
@@ -22,19 +22,42 @@ AddMock(SYSTEM_ENTITY, IID_TemplateManager, {
 
 
 AddMock(100, IID_Player, {
+	GetName: function() { return "Player 1"; },
+	GetCiv: function() { return "gaia"; },
+	GetColour: function() { return { r: 1, g: 1, b: 1, a: 1}; },
 	GetPopulationCount: function() { return 10; },
 	GetPopulationLimit: function() { return 20; },
-	GetResourceCounts: function() { return { "food": 100 }; }
+	GetResourceCounts: function() { return { food: 100 }; }
 });
 
 AddMock(101, IID_Player, {
+	GetName: function() { return "Player 2"; },
+	GetCiv: function() { return "celt"; },
+	GetColour: function() { return { r: 1, g: 0, b: 0, a: 1}; },
 	GetPopulationCount: function() { return 40; },
 	GetPopulationLimit: function() { return 30; },
-	GetResourceCounts: function() { return { "food": 200 }; }
+	GetResourceCounts: function() { return { food: 200 }; }
 });
 
 TS_ASSERT_UNEVAL_EQUALS(cmp.GetSimulationState(), {
-	players: [{popCount:10, popLimit:20, resourceCounts:{food:100}}, {popCount:40, popLimit:30, resourceCounts:{food:200}}]
+	players: [
+		{
+			name: "Player 1",
+			civ: "gaia",
+			color: { r:1, g:1, b:1, a:1 },
+			popCount: 10,
+			popLimit: 20,
+			resourceCounts: { food: 100 }
+		},
+		{
+			name: "Player 2",
+			civ: "celt",
+			color: { r:1, g:0, b:0, a:1 },
+			popCount: 40,
+			popLimit: 30,
+			resourceCounts: { food: 200 }
+		}
+	]
 });
 
 
