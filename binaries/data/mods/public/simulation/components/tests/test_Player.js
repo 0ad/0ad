b6e50a44307539ac140eaf681faa6c135aa1c5da
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2010-09-07 09:29:23 +0000

    Fix #566 (test failure)
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@8088 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/tests/test_Player.js
+++ binaries/data/mods/public/simulation/components/tests/test_Player.js
@@ -3,4 +3,4 @@ Engine.LoadComponentScript("Player.js");
 var cmp = ConstructComponent(10, "Player");
 
 TS_ASSERT_EQUALS(cmp.GetPopulationCount(), 0);
-TS_ASSERT_EQUALS(cmp.GetPopulationLimit(), 50);
+TS_ASSERT_EQUALS(cmp.GetPopulationLimit(), 0);
