87df4951b7a34a4a057d08e46d9e3dd5f37e814f
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2010-11-17 00:50:38 +0000

    Add a script to clean up broken builds of bundled libraries
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@8632 3db68df2-c116-0410-a063-a993310a9797

---
+++ build/workspaces/clean-workspaces.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# (We don't attempt to clean up every last file here - output in
+# binaries/system/ will still be there, etc. This is mostly just
+# to quickly fix problems in the bundled dependencies.)
+
+cd "$(dirname $0)"
+# Now in build/workspaces/ (where we assume this script resides)
+
+echo "Cleaning bundled third-party dependencies..."
+
+(cd ../../libraries/fcollada/src && rm -rf ./output)
+(cd ../../libraries/spidermonkey-tip/src && rm -rf ./build-debug)
+(cd ../../libraries/spidermonkey-tip/src && rm -rf ./build-release)
+(cd ../../libraries/nvtt/src && rm -rf ./build)
+
+(cd ../premake/src && make clean)
+
+echo "Cleaning build output..."
+
+# Remove workspaces/gcc
+rm -rf ./gcc
+
+echo
+echo "Done. Try running update-workspaces.sh again now."
