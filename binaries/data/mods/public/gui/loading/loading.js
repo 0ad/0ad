9b52c99663fb5756d219debb42b63276ec8744fe
brian <brian@3db68df2-c116-0410-a063-a993310a9797>
2010-11-27 03:07:48 +0000

    New loading screen style (may need more polishing)
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@8699 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/loading/loading.js
+++ binaries/data/mods/public/gui/loading/loading.js
@@ -11,38 +11,58 @@ function init(data)
 	// Set to "hourglass" cursor.
 	setCursor("cursor-wait");
 
-	// Choose random concept art for loading screen background (should depend on the selected civ later when this is specified).
+	// Get tip image and corresponding tip text
 	var sprite = "";
-	var loadingBkgArray = buildDirEntList("art/textures/ui/loading/", "*.dds", false);
-	if (loadingBkgArray.length == 0)
-		error("Failed to find any matching textures for the loading screen background.");
+	var tipText = "";
+	var tipImageLoadingArray = buildDirEntList("art/textures/ui/loading/tips/", "*.png", false);
+	var tipTextLoadingArray = buildDirEntList("gui/text/tips/", "*.txt", false);
+	
+	if (tipImageLoadingArray.length == 0)
+		error("Failed to find any matching tip textures for the loading screen.");
+	else if (tipTextLoadingArray.length == 0)
+		error("Failed to find any matching tip text files for the loading screen.");
+	else if (tipImageLoadingArray.length != tipTextLoadingArray.length)
+		error("The there are different amounts of tip images and tip text files.");
 	else
 	{
-		// Get a random index from the list of loading screen backgrounds.
-		sprite = "stretched:" + loadingBkgArray[getRandom (0, loadingBkgArray.length-1)];
+		var randomIndex = getRandom (0, tipImageLoadingArray.length-1);
+
+		sprite = "stretched:" + tipImageLoadingArray[randomIndex];
 		sprite = sprite.replace("art/textures/ui/", "");
 		sprite = sprite.replace(".cached.dds", ""); // cope with pre-cached textures
+		
+		tipText = readFile(tipTextLoadingArray[randomIndex]);
 	}
-	getGUIObjectByName ("ldConcept").sprite = sprite;
+
+	// Set tip image
+	getGUIObjectByName("loadingTipImage").sprite = sprite;
+
+	// Set tip text
+	if (tipText)
+	{
+		var index = tipText.indexOf("\n");
+		tipTextTitle = tipText.substring(0, index);
+		tipTextMessage = tipText.substring(index);
+		getGUIObjectByName("loadingTipTitle").caption = tipTextTitle? tipTextTitle : "";
+		getGUIObjectByName("loadingTipText").caption = tipTextMessage? tipTextMessage : "";
+	}
 
 	// janwas: main loop now sets progress / description, but that won't
 	// happen until the first timeslice completes, so set initial values.
-	
-	var ldTitleBar = getGUIObjectByName ("ldTitleBar");
-	var ldText = getGUIObjectByName ("ldText");
+	var loadingMapName = getGUIObjectByName ("loadingMapName");
 	
 	if (data)
 	{
 		switch (data.attribs.mapType)
 		{
 		case "scenario":
-			ldTitleBar.caption = "Loading Scenario";
-			ldText.caption = "Loading " + mapName + "\nPlease wait...";
+			//ldTitleBar.caption = "Loading Scenario";
+			loadingMapName.caption = "Loading \"" + mapName + "\"";
 			break;
 
 		case "random":
-			ldTitleBar.caption = "Loading Random Map";
-			ldText.caption = "Generating " + mapName + "\nPlease wait...";
+			//ldTitleBar.caption = "Loading Random Map";
+			loadingMapName.caption = "Generating \"" + mapName + "\"";
 			break;
 
 		default:
@@ -50,17 +70,34 @@ function init(data)
 		}
 	}
 
-	getGUIObjectByName("ldProgressBarText").caption = "";
-	getGUIObjectByName("ldProgressBar").caption = 0;
+	getGUIObjectByName("progressText").caption = "";
+	getGUIObjectByName("progressBar").caption = 0;
 
-	// Pick a random tip of the day (each line is a separate tip).
-	var tipArray = readFileLines("gui/text/tips.txt");
-	// Set tip string.
-	getGUIObjectByName("ldTip").caption = tipArray[getRandom(0, tipArray.length-1)];
+	// Pick a random quote of the day (each line is a separate tip).
+	var quoteArray = readFileLines("gui/text/quotes.txt");
+	getGUIObjectByName("loadingQuoteText").caption = quoteArray[getRandom(0, quoteArray.length-1)];
 }
 
 // ====================================================================
+function displayProgress()
+{
+	getGUIObjectByName("progressBar").caption = g_Progress; // display current progress
+	getGUIObjectByName("progressText").caption = g_LoadDescription; // display current progess details
+	
+	// Keep curved right edge of progress bar in sync with the rest of the progress bar
+	var middle = getGUIObjectByName("progressBar");
+	var rightSide = getGUIObjectByName("loadingProgressbar_right");
+	
+	var middleLength = middle.size.right - middle.size.left;
+	var increment = Math.round(g_Progress*middleLength/100);
 
+	var size = rightSide.size;
+	size.left = increment;
+	size.right = increment+12;
+	rightSide.size = size;
+}
+
+// ====================================================================
 function reallyStartGame()
 {
 	// Stop the music
