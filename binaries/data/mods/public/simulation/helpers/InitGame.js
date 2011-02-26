d6cc2fdd94d3069debded942d8190f883bc83a02
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2011-02-26 00:19:02 +0000

    Fix script error message when starting Atlas
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@8989 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/helpers/InitGame.js
+++ binaries/data/mods/public/simulation/helpers/InitGame.js
@@ -3,6 +3,10 @@ function InitGame(settings)
 	// This will be called after the map settings have been loaded,
 	// before the simulation has started.
 
+	// No settings when loading a map in Atlas, so do nothing
+	if (!settings)
+		return;
+
 	var cmpAIManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_AIManager);
 	for (var i = 0; i < settings.AIs.length; ++i)
 	{
