8f677abcd2f65f7d1f0ff32178122164887f5410
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2011-03-14 02:50:43 +0000

    Attempt to fix shadowAlphaFix to generate FBOs in a more widely supported format
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@9062 3db68df2-c116-0410-a063-a993310a9797

--- source/renderer/ShadowMap.cpp
+++ source/renderer/ShadowMap.cpp
@@ -362,7 +362,7 @@ void ShadowMapInternals::CreateTexture()
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA8, Width, Height, 0, GL_ALPHA, GL_UNSIGNED_BYTE, NULL);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, Width, Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 		}
 
 		glGenTextures(1, &Texture);
