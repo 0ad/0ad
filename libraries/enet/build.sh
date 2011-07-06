97831e038b9832e807d184c6fd9b2d089b682675
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2011-07-06 20:44:35 +0000

    Attempt to fix libenet build on OS X
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@9767 3db68df2-c116-0410-a063-a993310a9797

--- libraries/enet/build.sh
+++ libraries/enet/build.sh
@@ -17,9 +17,10 @@ cd ../
 
 if [ "`uname -s`" = "Darwin" ]
 then
-  # TODO: this is untested
+  # Fix libtool's use of an absolute path
+  install_name_tool -id @executable_path/libenet.1.dylib src/.libs/libenet.1.dylib
   cp src/.libs/libenet.dylib lib/
-  cp src/.libs/libenet.dylib ../../binaries/system/
+  cp src/.libs/libenet.1.dylib ../../binaries/system/
 else
   cp src/.libs/libenet.so lib/
   cp src/.libs/libenet.so.1 ../../binaries/system/
