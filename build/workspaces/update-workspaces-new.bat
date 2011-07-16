adf6641b42ed8377288823d6cd49a1bb61138590
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2011-07-16 22:17:47 +0000

    Fix handling of update-workspaces arguments on Windows
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@9849 3db68df2-c116-0410-a063-a993310a9797

--- build/workspaces/update-workspaces-new.bat
+++ build/workspaces/update-workspaces-new.bat
@@ -2,7 +2,7 @@
 rem ** Create Visual Studio Workspaces on Windows **
 
 cd ..\premake
-if not exist ..\workspaces\vc2005\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2005" --collada vs2005 %*
-if not exist ..\workspaces\vc2008\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2008" --collada vs2008 %*
-if not exist ..\workspaces\vc2010\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2010" --collada vs2010 %*
+if not exist ..\workspaces\vc2005\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2005" --collada %* vs2005
+if not exist ..\workspaces\vc2008\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2008" --collada %* vs2008
+if not exist ..\workspaces\vc2010\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2010" --collada %* vs2010
 cd ..\workspaces
