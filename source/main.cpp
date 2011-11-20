9c9b25e9fc061a705b5b3d10e901dfddc7d2d049
ben <ben@3db68df2-c116-0410-a063-a993310a9797>
2011-11-20 02:00:54 +0000

    Fixes simulation replay mode in Windows.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@10563 3db68df2-c116-0410-a063-a993310a9797

--- source/main.cpp
+++ source/main.cpp
@@ -453,7 +453,7 @@ static void RunGameOrAtlas(int argc, const char* argv[])
 		Paths paths(args);
 		g_VFS = CreateVfs(20 * MiB);
 		g_VFS->Mount(L"cache/", paths.Cache(), VFS_MOUNT_ARCHIVABLE);
-		g_VFS->Mount(L"", paths.RData()/"mods/public", VFS_MOUNT_MUST_EXIST);
+		g_VFS->Mount(L"", paths.RData()/"mods"/"public", VFS_MOUNT_MUST_EXIST);
 
 		{
 			CReplayPlayer replay;
