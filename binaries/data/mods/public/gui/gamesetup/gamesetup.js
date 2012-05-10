453f2e85fdbc35f9fd563c5a41950a4b278ac817
leper <leper@3db68df2-c116-0410-a063-a993310a9797>
2012-05-10 22:23:09 +0000

    Really set Oasis 11 as the default multiplayer map.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@11829 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/gamesetup/gamesetup.js
+++ binaries/data/mods/public/gui/gamesetup/gamesetup.js
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // Constants
-const DEFAULT_NETWORKED_MAP = "Oasis 11";
+const DEFAULT_NETWORKED_MAP = "Oasis XI";
 const DEFAULT_OFFLINE_MAP = "Punjab III";
 
 // TODO: Move these somewhere like simulation\data\game_types.json, Atlas needs them too
