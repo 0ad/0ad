f7f0a510135cadca5caa7859b21e22da000121d0
myconid <myconid@3db68df2-c116-0410-a063-a993310a9797>
2012-09-17 12:22:21 +0000

    First attempt at adding waterfall splashes...
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@12686 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/shaders/glsl/model_waterfall.fs
+++ binaries/data/mods/public/shaders/glsl/model_waterfall.fs
@@ -31,7 +31,7 @@ varying vec4 v_shadow;
 varying vec2 v_los;
 varying vec3 v_half;
 varying vec3 v_normal;
-
+varying float v_transp;
 varying vec3 v_lighting;
 
 float get_shadow()
@@ -65,6 +65,11 @@ void main()
 {
 	//vec4 texdiffuse = textureGrad(baseTex, vec3(fract(v_tex.xy), v_tex.z), dFdx(v_tex.xy), dFdy(v_tex.xy));
 	vec4 texdiffuse = texture2D(baseTex, fract(v_tex.xy));
+
+	if (texdiffuse.a < 0.25)
+		discard;
+
+	texdiffuse.a *= v_transp;
 
 	vec3 specular = sunColor * specularColor * pow(max(0.0, dot(normalize(v_normal), v_half)), specularPower);
 
