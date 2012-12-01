0ad/0ad

commit b1b02c3b0a57f698cd4ac626307cc006a7e8cb76

merge with local codebase: fix PopulationCount
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@12907 3db68df2-c116-0410-a063-a993310a9797

source/lib/bits.h
@@ -147,11 +147,12 @@ inline size_t SparsePopulationCount(T mask)
 template<typename T>
 static inline size_t PopulationCount(T x)
 {
+    cassert(!std::numeric_limits<T>::is_signed);
 	const T mask = T(~T(0));
 	x -= (x >> 1) & (mask/3);	// count 2 bits
 	x = (x & (mask/15*3)) + ((x >> 2) & (mask/15*3));	// count 4 bits
 	x = (x + (x >> 4)) & (mask/255*15);	// count 8 bits
-	return (x * (mask/255)) >> ((sizeof(T)-1)*CHAR_BIT);
+	return T(x * (mask/255)) >> ((sizeof(T)-1)*CHAR_BIT);
 }
 
 

commit 845b31adf29359a016cc9c2b62e2ecc5c22572c0

Fixes compile error introduced in r12907
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@12909 3db68df2-c116-0410-a063-a993310a9797

source/lib/bits.h
@@ -147,7 +147,7 @@ inline size_t SparsePopulationCount(T mask)
 template<typename T>
 static inline size_t PopulationCount(T x)
 {
-    cassert(!std::numeric_limits<T>::is_signed);
+    cassert_dependent(!std::numeric_limits<T>::is_signed);
 	const T mask = T(~T(0));
 	x -= (x >> 1) & (mask/3);	// count 2 bits
 	x = (x & (mask/15*3)) + ((x >> 2) & (mask/15*3));	// count 4 bits