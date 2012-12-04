0ad/0ad

commit fe3af63b34863ffc0224e051cafcfcad826b03ba

Factors out common properties from siege templates, refs #1718. Simplifies pack-related tooltips.
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@12937 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/gui/session/selection_details.js
@@ -16,6 +16,9 @@ function displaySingle(entState, template)
 	// Get general unit and player data
 	var specificName = template.name.specific;
 	var genericName = template.name.generic != template.name.specific? template.name.generic : "";
+	// If packed, add that to the generic name (reduces template clutter)
+	if (genericName && template.pack.state == "packed")
+		genericName += " -- Packed";
 	var playerState = g_Players[entState.player];
 
 	var civName = g_CivData[playerState.civ].Name;

binaries/data/mods/public/gui/session/unit_commands.js
@@ -1111,13 +1111,13 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 				}
 			}
 			if (packButton)
-				items.push({ "packing": false, "packed": false, "tooltip": "Pack this unit", "callback": function() { packUnit(true); } });
+				items.push({ "packing": false, "packed": false, "tooltip": "Pack", "callback": function() { packUnit(true); } });
 			if (unpackButton)
-				items.push({ "packing": false, "packed": true, "tooltip": "Unpack this unit", "callback": function() { packUnit(false); } });
+				items.push({ "packing": false, "packed": true, "tooltip": "Unpack", "callback": function() { packUnit(false); } });
 			if (packCancelButton)
-				items.push({ "packing": true, "packed": false, "tooltip": "Stop packing this unit", "callback": function() { cancelPackUnit(true); } });
+				items.push({ "packing": true, "packed": false, "tooltip": "Cancel packing", "callback": function() { cancelPackUnit(true); } });
 			if (unpackCancelButton)
-				items.push({ "packing": true, "packed": true, "tooltip": "Stop unpacking this unit", "callback": function() { cancelPackUnit(false); } });
+				items.push({ "packing": true, "packed": true, "tooltip": "Cancel unpacking", "callback": function() { cancelPackUnit(false); } });
 
 			if (items.length)
 				setupUnitPanel(PACK, usedPanels, entState, playerState, items);

binaries/data/mods/public/simulation/templates/template_unit_mechanical_siege_ballista.xml
@@ -57,11 +57,6 @@
     <stone>0</stone>
     <metal>10</metal>
   </Loot>
-  <Pack>
-    <Entity>template_unit_mechanical_siege_ballista_packed</Entity>
-    <Time>10000</Time>
-    <State>unpacked</State>
-  </Pack>
   <Selectable>
     <Overlay>
       <Texture>
@@ -71,9 +66,9 @@
     </Overlay>
   </Selectable>
   <UnitMotion>
-    <WalkSpeed>0.001</WalkSpeed>
+    <WalkSpeed>6.5</WalkSpeed>
     <Run>
-      <Speed>0.001</Speed>
+      <Speed>12.0</Speed>
     </Run>
   </UnitMotion>
   <Vision>

binaries/data/mods/public/simulation/templates/template_unit_mechanical_siege_ballista_packed.xml
@@ -1,85 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<Entity parent="template_unit_mechanical_siege">
-  <Armour>
-    <Hack>4.0</Hack>
-    <Crush>2.0</Crush>
-  </Armour>
-  <Attack>
-    <Ranged>
-      <Hack>0.0</Hack>
-      <Pierce>20.0</Pierce>
-      <Crush>20.0</Crush>
-      <MaxRange>60</MaxRange>
-      <MinRange>8.0</MinRange>
-      <Spread>6.0</Spread>
-      <ProjectileSpeed>60.0</ProjectileSpeed>
-      <PrepareTime>5000</PrepareTime>
-      <RepeatTime>5000</RepeatTime>
-      <Bonuses>
-        <BonusOrganic>
-        <Classes>Organic</Classes>
-        <Multiplier>2.0</Multiplier>
-        </BonusOrganic>
-      </Bonuses>
-      <Splash>
-        <Shape>Linear</Shape>
-        <Range>12</Range>
-        <FriendlyFire>false</FriendlyFire>
-        <Hack>0.0</Hack>
-        <Pierce>30.0</Pierce>
-        <Crush>30.0</Crush>
-      </Splash>
-    </Ranged>
-  </Attack>
-  <Cost>
-    <BuildTime>20</BuildTime>
-    <Resources>
-      <wood>100</wood>
-      <metal>100</metal>
-    </Resources>
-    <Population>2</Population>
-  </Cost>
-  <Footprint replace="">
-    <Square width="8.0" depth="9.0"/>
-    <Height>2.0</Height>
-  </Footprint>
-  <Health>
-    <Max>100</Max>
-  </Health>
-  <Identity>
-    <GenericName>Bolt Shooter -- Packed</GenericName>
-    <Tooltip>Bonused vs. Infantry and Cavalry units.</Tooltip>
-  </Identity>
-  <Loot>
-    <xp>200</xp>
-    <food>0</food>
-    <wood>10</wood>
-    <stone>0</stone>
-    <metal>10</metal>
-  </Loot>
-  <Pack>
-    <Entity>template_unit_mechanical_siege_ballista_unpacked</Entity>
-    <Time>10000</Time>
-    <State>packed</State>
-  </Pack>
-  <Selectable>
-    <Overlay>
-      <Texture>
-        <MainTexture>circle/256x256.png</MainTexture>
-        <MainTextureMask>circle/256x256_mask.png</MainTextureMask>
-      </Texture>
-    </Overlay>
-  </Selectable>
-  <UnitAI>
-    <DefaultStance>passive</DefaultStance>
-  </UnitAI>
-  <UnitMotion>
-    <WalkSpeed>6.5</WalkSpeed>
-    <Run>
-      <Speed>12.0</Speed>
-    </Run>
-  </UnitMotion>
-  <Vision>
-    <Range>70</Range>
-  </Vision>
-</Entity>

binaries/data/mods/public/simulation/templates/template_unit_mechanical_siege_onager.xml
@@ -50,7 +50,7 @@
     <Max>150</Max>
   </Health>
   <Identity>
-    <GenericName>Siege Catapult -- Packed</GenericName>
+    <GenericName>Siege Catapult</GenericName>
     <Tooltip>Bonused vs. Structures and Massed Infantry.</Tooltip>
   </Identity>
   <Loot>
@@ -60,11 +60,6 @@
     <stone>10</stone>
     <metal>0</metal>
   </Loot>
-  <Pack>
-    <Entity>template_unit_mechanical_siege_onager_unpacked</Entity>
-    <Time>10000</Time>
-    <State>packed</State>
-  </Pack>
   <Selectable>
     <Overlay>
       <Texture>

binaries/data/mods/public/simulation/templates/template_unit_mechanical_siege_onager_unpacked.xml
@@ -1,97 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<Entity parent="template_unit_mechanical_siege">
-  <Armour>
-    <Hack>4.0</Hack>
-    <Crush>10.0</Crush>
-  </Armour>
-  <Attack>
-    <Ranged>
-      <Hack>30.0</Hack>
-      <Pierce>0.0</Pierce>
-      <Crush>30.0</Crush>
-      <MaxRange>68</MaxRange>
-      <MinRange>12.0</MinRange>
-      <ProjectileSpeed>28.0</ProjectileSpeed>
-      <PrepareTime>5000</PrepareTime>
-      <RepeatTime>5000</RepeatTime>
-      <Spread>6.0</Spread>
-      <Bonuses>
-        <BonusStruct>
-          <Classes>Structure</Classes>
-          <Multiplier>2.0</Multiplier>
-        </BonusStruct>
-        <NerfWall>
-          <Classes>StoneWall</Classes>
-          <Multiplier>0.75</Multiplier>
-        </NerfWall>
-      </Bonuses>
-      <Splash>
-        <Shape>Circular</Shape>
-        <Range>8</Range>
-        <FriendlyFire>true</FriendlyFire>
-        <Hack>40.0</Hack>
-        <Pierce>0.0</Pierce>
-        <Crush>40.0</Crush>
-      </Splash>
-    </Ranged>
-  </Attack>
-  <Cost>
-    <BuildTime>30</BuildTime>
-    <Resources>
-      <wood>200</wood>
-      <stone>100</stone>
-    </Resources>
-  </Cost>
-  <Footprint replace="">
-    <Square width="10.0" depth="15.0"/>
-    <Height>4.5</Height>
-  </Footprint>
-  <Health>
-    <Max>150</Max>
-  </Health>
-  <Identity>
-    <GenericName>Siege Catapult</GenericName>
-    <Tooltip>Bonused vs. Structures and Massed Infantry.</Tooltip>
-  </Identity>
-  <Loot>
-    <xp>300</xp>
-    <food>0</food>
-    <wood>20</wood>
-    <stone>10</stone>
-    <metal>0</metal>
-  </Loot>
-  <Pack>
-    <Entity>template_unit_mechanical_siege_onager_packed</Entity>
-    <Time>10000</Time>
-    <State>unpacked</State>
-  </Pack>
-  <Selectable>
-    <Overlay>
-      <Texture>
-        <MainTexture>circle/256x256.png</MainTexture>
-        <MainTextureMask>circle/256x256_mask.png</MainTextureMask>
-      </Texture>
-    </Overlay>
-  </Selectable>
-  <Sound>
-    <SoundGroups>
-      <attack>attack/siege/ballist_attack.xml</attack>
-    </SoundGroups>
-  </Sound>
-  <StatusBars>
-    <BarWidth>4.0</BarWidth>
-    <BarHeight>0.5</BarHeight>
-  </StatusBars>
-  <UnitAI>
-    <DefaultStance>standground</DefaultStance>
-  </UnitAI>
-  <UnitMotion>
-    <WalkSpeed>0.001</WalkSpeed>
-    <Run>
-      <Speed>0.001</Speed>
-    </Run>
-  </UnitMotion>
-  <Vision>
-    <Range>80</Range>
-  </Vision>
-</Entity>

binaries/data/mods/public/simulation/templates/units/athen_mechanical_siege_lithobolos_common.xml
@@ -1,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<Entity parent="template_unit_mechanical_siege_onager">
+  <Identity>
+    <Civ>athen</Civ>
+    <SpecificName>Lithóbolos</SpecificName>
+  </Identity>
+  <Obstruction>
+    <Unit radius="3.5"/>
+  </Obstruction>
+  <Vision>
+    <Range>88</Range>
+  </Vision>
+</Entity>

binaries/data/mods/public/simulation/templates/units/athen_mechanical_siege_lithobolos_packed.xml
@@ -1,19 +1,13 @@
 <?xml version="1.0" encoding="utf-8"?>
-<Entity parent="template_unit_mechanical_siege_onager_packed">
+<Entity parent="units/athen_mechanical_siege_lithobolos_common">
   <Identity>
-    <Civ>athen</Civ>
-    <SpecificName>Lithóbolos</SpecificName>
     <Icon>units/hele_mechanical_siege_lithobolos_packed.png</Icon>
   </Identity>
-  <Obstruction>
-    <Unit radius="3.5"/>
-  </Obstruction>
   <Pack>
     <Entity>units/athen_mechanical_siege_lithobolos_unpacked</Entity>
+    <Time>10000</Time>
+    <State>packed</State>
   </Pack>
-  <Vision>
-    <Range>88</Range>
-  </Vision>
   <VisualActor>
     <Actor>units/athenians/siege_rock_packed.xml</Actor>
   </VisualActor>

commit 055cd2a63eccae269c39750ea78844c5df8a94ef

Fixes small bug introduced by r12937. Clarifies new serialization test options in readme.txt.
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@12938 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/gui/session/selection_details.js
@@ -17,7 +17,7 @@ function displaySingle(entState, template)
 	var specificName = template.name.specific;
 	var genericName = template.name.generic != template.name.specific? template.name.generic : "";
 	// If packed, add that to the generic name (reduces template clutter)
-	if (genericName && template.pack.state == "packed")
+	if (genericName && template.pack && template.pack.state == "packed")
 		genericName += " -- Packed";
 	var playerState = g_Players[entState.player];
 