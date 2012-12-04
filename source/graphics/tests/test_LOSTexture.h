567e681ae61efded93810974fde5c465a9b71ad5
leper <leper@3db68df2-c116-0410-a063-a993310a9797>
2012-12-04 13:21:52 +0000

    Fix a test failure caused by r12930.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@12940 3db68df2-c116-0410-a063-a993310a9797

--- source/graphics/tests/test_LOSTexture.h
+++ source/graphics/tests/test_LOSTexture.h
@@ -19,6 +19,8 @@
 
 #include "graphics/LOSTexture.h"
 #include "lib/timer.h"
+#include "ps/ConfigDB.h"
+#include "scripting/ScriptingHost.h"
 #include "simulation2/Simulation2.h"
 
 class TestLOSTexture : public CxxTest::TestSuite
@@ -26,6 +28,10 @@ class TestLOSTexture : public CxxTest::TestSuite
 public:
 	void test_basic()
 	{
+		// Needed for CFG_GET_USER_VAL
+		new ScriptingHost;
+		new CConfigDB;
+
 		CSimulation2 sim(NULL, NULL);
 		CLOSTexture tex(sim);
 
