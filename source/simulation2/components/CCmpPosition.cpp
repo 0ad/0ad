020653a923a8689ed689de2d81fbe9df0361ce68
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2013-03-10 20:31:53 +0000

    Simplify construction of the transformation matrix in GetInterpolatedTransform. Patch by sbte. Fixes #1854
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@13252 3db68df2-c116-0410-a063-a993310a9797

--- source/simulation2/components/CCmpPosition.cpp
+++ source/simulation2/components/CCmpPosition.cpp
@@ -407,25 +407,13 @@ public:
 
 		// TODO: do something with m_AnchorType
 
-		CMatrix3D m;
-		CMatrix3D mXZ;
-		float Cos = cosf(rotY);
-		float Sin = sinf(rotY);
-
-		m.SetIdentity();
-		m._11 = -Cos;
-		m._13 = -Sin;
-		m._31 = Sin;
-		m._33 = -Cos;
-
-		mXZ.SetIdentity();
-		mXZ.SetXRotation(m_RotX.ToFloat());
-		mXZ.RotateZ(m_RotZ.ToFloat());
-		// TODO: is this all done in the correct order?
-		mXZ = m * mXZ;
-		mXZ.Translate(CVector3D(x, y, z));
-
-		return mXZ;
+        CMatrix3D m;
+		m.SetXRotation(m_RotX.ToFloat());
+		m.RotateZ(m_RotZ.ToFloat());
+		m.RotateY(rotY + (float)M_PI);
+		m.Translate(CVector3D(x, y, z));
+		
+		return m;
 	}
 
 	virtual void HandleMessage(const CMessage& msg, bool UNUSED(global))
