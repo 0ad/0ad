590a2b3aadf4e22c5261fc1f42570c174efcd289
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2013-03-15 10:26:46 +0000

    Improve workers' reactions when they find out that the resource they gathered from is full. Refs #1387
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@13288 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/UnitAI.js
+++ binaries/data/mods/public/simulation/components/UnitAI.js
@@ -1570,14 +1570,27 @@ var UnitFsmSpec = {
 							this.PerformGather(nearby, false, false);
 							return true;
 						}
-
-						var nearby = this.FindNearestDropsite(oldType.generic);
-						if (nearby)
+						else
 						{
-							this.PushOrderFront("ReturnResource", { "target": nearby, "force": false });
-							return true;
+							// It's probably better in this case, to avoid units getting stuck around a dropsite
+							// in a "Target is far away, full, nearby are no good resources, return to dropsite" loop
+							// to order it to GatherNear the resource position.
+							var cmpPosition = Engine.QueryInterface(this.gatheringTarget, IID_Position);
+							if (cmpPosition)
+							{
+								var pos = cmpPosition.GetPosition();
+								this.GatherNearPosition(pos.x, pos.z, oldType, oldTemplate);
+								return true;
+							} else {
+								// we're kind of stuck here. Return resource.
+								var nearby = this.FindNearestDropsite(oldType.generic);
+								if (nearby)
+								{
+									this.PushOrderFront("ReturnResource", { "target": nearby, "force": false });
+									return true;
+								}
+							}
 						}
-						
 						return true;
 					}
 					return false;
