0ad/0ad

commit 276f6e42c75a945078f53e373f4dd76c79f4e99b

Fix errors when using autostart to launch a game.  Use setters instead of accessing cmpPlayers properties directly. Tidy the formatting in Player.js
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@13044 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/simulation/helpers/InitGame.js
@@ -22,11 +22,15 @@ function InitGame(settings)
 			cmpPlayer.SetAI(true);
 			cmpPlayer.SetCheatEnabled(true);
 		}
-		cmpPlayer.maxPop = settings.PopulationCap;
+		if (settings.PopulationCap)
+			cmpPlayer.SetMaxPopulation(settings.PopulationCap);
 
-		if (settings.mapType !== "scenario")
-			for (var resouces in cmpPlayer.resourceCount)
-				cmpPlayer.resourceCount[resouces] = settings.StartingResources;
+		if (settings.mapType !== "scenario" && settings.StartingResources)
+			var resourceCounts = cmpPlayer.GetResourceCounts();
+			var newResourceCounts = {};
+			for (var resouces in resourceCounts)
+				newResourceCounts[resouces] = settings.StartingResources;
+			cmpPlayer.SetResourceCounts(newResourceCounts);
 	}
 }
 

binaries/data/mods/public/simulation/helpers/Player.js
@@ -37,7 +37,9 @@ function LoadPlayerSettings(settings, newPlayers)
 			numPlayers = settings.PlayerData.length + 1;
 		}
 		else
+		{
 			warn("Player.js: Setup has no player data - using defaults");
+		}
 
 		for (var i = 0; i < numPlayers; ++i)
 		{

commit 9acb2bf151d62b2047a026b8bbce0ebf410453d3

Split up cheats into multiple files. Patch by lsdh/ldsh. Fixes #2019. Replace buggy hasAllies with the inlined check if we have a team at all. Fix setting of starting resources (broken in r13044).
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@13620 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/gui/session/messages.js
@@ -9,7 +9,22 @@ const NOTIFICATION_TIMEOUT = 10000;
 const MAX_NUM_NOTIFICATION_LINES = 3;
 var notifications = [];
 var notificationsTimers = [];
-var cheatList = parseJSONData("simulation/data/cheats.json").Cheats;
+var cheats = getCheatsData();
+
+function getCheatsData()
+{
+	var cheats = {};
+	var cheatFileList = getJSONFileList("simulation/data/cheats/");
+	for each (var fileName in cheatFileList)
+	{
+		var currentCheat = parseJSONData("simulation/data/cheats/"+fileName+".json");
+		if (Object.keys(cheats).indexOf(currentCheat.Name) !== -1)
+			warn("Cheat name '"+currentCheat.Name+"' is already present");
+		else
+			cheats[currentCheat.Name] = currentCheat.Data;
+	}
+	return cheats;
+}
 
 // Notifications
 function handleNotifications()
@@ -219,32 +234,33 @@ function submitChatInput()
 	var isCheat = false;
 	if (text.length)
 	{
-		for (var i = 0; i < cheatList.length; i++)
+		if (g_Players[Engine.GetPlayerID()].cheatsEnabled)
 		{
-			var cheat = cheatList[i];
-
-			// Line must start with the cheat.
-			if (text.indexOf(cheat.Name) == 0)
+			for each (var cheat in Object.keys(cheats))
 			{
+				// Line must start with the cheat.
+				if (text.indexOf(cheat) !== 0)
+					continue
+
 				var number;
-				if (cheat.IsNumeric)
+				if (cheats[cheat].DefaultNumber !== undefined)
 				{
 					// Match the first word in the substring.
-					var match = text.substr(cheat.Name.length).match(/\S+/);
+					var match = text.substr(cheat.length).match(/\S+/);
 					if (match && match[0])
 						number = Math.floor(match[0]);
 
 					if (number <= 0 || isNaN(number))
-						number = cheat.DefaultNumber;
+						number = cheats[cheat].DefaultNumber;
 				}
 
 				Engine.PostNetworkCommand({
 					"type": "cheat",
-					"action": cheat.Action,
+					"action": cheats[cheat].Action,
 					"number": number,
-					"text": cheat.Type,
+					"text": cheats[cheat].Type,
 					"selected": g_Selection.toList(),
-					"templates": cheat.Templates,
+					"templates": cheats[cheat].Templates,
 					"player": Engine.GetPlayerID()});
 				isCheat = true;
 				break;
@@ -420,62 +436,62 @@ function parseChatCommands(msg, playerAssignments)
 	// Parse commands embedded in the message.
 	switch (split[0])
 	{
-		case "/all":
-			// Resets values that 'team' or 'enemy' may have set.
-			msg.prefix = "";
-			msg.hide = false;
-			recurse = true;
-			break;
-		case "/team":
-			var playerData = getPlayerData();
-			if (hasAllies(sender, playerData))
-			{
-				if (playerData[Engine.GetPlayerID()].team != playerData[sender].team)
-					msg.hide = true;
-				else
-					msg.prefix = "(Team) ";
-			} else {
+	case "/all":
+		// Resets values that 'team' or 'enemy' may have set.
+		msg.prefix = "";
+		msg.hide = false;
+		recurse = true;
+		break;
+	case "/team":
+		// Check if we are in a team.
+		if (g_Players[Engine.GetPlayerID()] && g_Players[Engine.GetPlayerID()].team != -1)
+		{
+			if (g_Players[Engine.GetPlayerID()].team != g_Players[sender].team)
 				msg.hide = true;
-			}
-			recurse = true;
-			break;
-		case "/enemy":
-			var playerData = getPlayerData();
-			if (hasAllies(sender, playerData))
-			{
-				if (playerData[Engine.GetPlayerID()].team == playerData[sender].team && sender != Engine.GetPlayerID())
-					msg.hide = true;
-				else
-					msg.prefix = "(Enemy) ";
-			}
-			recurse = true;
-			break;
-		case "/me":
-			msg.action = true;
-			break;
-		case "/msg":
-			var trimmed = msg.text.substr(split[0].length + 1);
-			var matched = "";
-
-			// Reject names which don't match or are a superset of the intended name.
-			for each (var player in playerAssignments)
-				if (trimmed.indexOf(player.name + " ") == 0 && player.name.length > matched.length)
-					matched = player.name;
-
-			// If the local player's name was the longest one matched, show the message.
-			var playerName = g_Players[Engine.GetPlayerID()].name;
-			if (matched.length && (matched == playerName || sender == Engine.GetPlayerID()))
-			{
-				msg.prefix = "(Private) ";
-				msg.text = trimmed.substr(matched.length + 1);
-				msg.hide = false; // Might override team message hiding.
-				return;
-			}
 			else
+				msg.prefix = "(Team) ";
+		}
+		else
+			msg.hide = true;
+		recurse = true;
+		break;
+	case "/enemy":
+		// Check if we are in a team.
+		if (g_Players[Engine.GetPlayerID()] && g_Players[Engine.GetPlayerID()].team != -1)
+		{
+			if (g_Players[Engine.GetPlayerID()].team == g_Players[sender].team && sender != Engine.GetPlayerID())
 				msg.hide = true;
-			break;
-		default:
+			else
+				msg.prefix = "(Enemy) ";
+		}
+		recurse = true;
+		break;
+	case "/me":
+		msg.action = true;
+		break;
+	case "/msg":
+		var trimmed = msg.text.substr(split[0].length + 1);
+		var matched = "";
+
+		// Reject names which don't match or are a superset of the intended name.
+		for each (var player in playerAssignments)
+			if (trimmed.indexOf(player.name + " ") == 0 && player.name.length > matched.length)
+				matched = player.name;
+
+		// If the local player's name was the longest one matched, show the message.
+		var playerName = g_Players[Engine.GetPlayerID()].name;
+		if (matched.length && (matched == playerName || sender == Engine.GetPlayerID()))
+		{
+			msg.prefix = "(Private) ";
+			msg.text = trimmed.substr(matched.length + 1);
+			msg.hide = false; // Might override team message hiding.
 			return;
+		}
+		else
+			msg.hide = true;
+		break;
+	default:
+		return;
 	}
 
 	msg.text = msg.text.substr(split[0].length + 1);

binaries/data/mods/public/gui/session/utility_functions.js
@@ -60,6 +60,7 @@ function getPlayerData(playerAssignments)
 		    "color": color,
 		    "team": playerState.team,
 		    "teamsLocked": playerState.teamsLocked,
+		    "cheatsEnabled": playerState.cheatsEnabled,
 		    "state": playerState.state,
 		    "isAlly": playerState.isAlly,
 		    "isMutualAlly": playerState.isMutualAlly,
@@ -86,18 +87,6 @@ function getPlayerData(playerAssignments)
 	}
 
 	return players;
-}
-
-// Returns whether a player has physical allies.
-function hasAllies(playerID, playerData)
-{
-	if (playerData[playerID] && playerData[playerID].team != -1)
-	{
-		for (var i = 0; i < playerData.length; i++)
-			if (playerData[i].team == playerData[playerID].team)
-				return true;
-	}
-	return false;
 }
 
 function findGuidForPlayerID(playerAssignments, player)

binaries/data/mods/public/simulation/components/GuiInterface.js
@@ -87,6 +87,7 @@ GuiInterface.prototype.GetSimulationState = function(player)
 			"state": cmpPlayer.GetState(),
 			"team": cmpPlayer.GetTeam(),
 			"teamsLocked": cmpPlayer.GetLockTeams(),
+			"cheatsEnabled": cmpPlayer.GetCheatsEnabled(),
 			"phase": phase,
 			"isAlly": allies,
 			"isMutualAlly": mutualAllies,

binaries/data/mods/public/simulation/components/Player.js
@@ -31,7 +31,7 @@ Player.prototype.Init = function()
 	this.controlAllUnits = false;
 	this.isAI = false;
 	this.gatherRateMultiplier = 1;
-	this.cheatsEnabled = true;
+	this.cheatsEnabled = false;
 	this.cheatTimeMultiplier = 1;
 	this.heroes = [];
 };
@@ -550,12 +550,12 @@ Player.prototype.OnDiplomacyChanged = function()
 	this.UpdateSharedLos();
 };
 
-Player.prototype.SetCheatEnabled = function(flag)
+Player.prototype.SetCheatsEnabled = function(flag)
 {
 	this.cheatsEnabled = flag;
 };
 
-Player.prototype.GetCheatEnabled = function()
+Player.prototype.GetCheatsEnabled = function()
 {
 	return this.cheatsEnabled;
 };

binaries/data/mods/public/simulation/helpers/Cheat.js
@@ -11,7 +11,7 @@ function Cheat(input)
 		return;
 
 	var cmpGuiInterface = Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface);
-	if (!cmpPlayer.GetCheatEnabled())
+	if (!cmpPlayer.GetCheatsEnabled())
 	{
 		cmpGuiInterface.PushNotification({"type": "chat", "player": input.player, "message": "Cheats are disbaled in this match"});
 		return;

binaries/data/mods/public/simulation/helpers/InitGame.js
@@ -14,24 +14,24 @@ function InitGame(settings)
 	for (var i = 0; i < settings.PlayerData.length; ++i)
 	{
 		var cmpPlayer = Engine.QueryInterface(cmpPlayerManager.GetPlayerByID(i+1), IID_Player);
-		if (!settings.CheatsEnabled)
-			cmpPlayer.SetCheatEnabled(false);
+		cmpPlayer.SetCheatsEnabled(!!settings.CheatsEnabled);
 		if (settings.PlayerData[i] && settings.PlayerData[i].AI && settings.PlayerData[i].AI != "")
 		{
 			cmpAIManager.AddPlayer(settings.PlayerData[i].AI, i+1, settings.PlayerData[i].AIDiff);
 			cmpPlayer.SetAI(true);
 			cmpPlayer.SetGatherRateMultiplier((+settings.PlayerData[i].AIDiff+2)/3.0)	// Medium is 1, easy is 66%, hard is 133%, very hard 166%
-			cmpPlayer.SetCheatEnabled(true);
 		}
 		if (settings.PopulationCap)
 			cmpPlayer.SetMaxPopulation(settings.PopulationCap);
 
 		if (settings.mapType !== "scenario" && settings.StartingResources)
+		{
 			var resourceCounts = cmpPlayer.GetResourceCounts();
 			var newResourceCounts = {};
 			for (var resouces in resourceCounts)
 				newResourceCounts[resouces] = settings.StartingResources;
 			cmpPlayer.SetResourceCounts(newResourceCounts);
+		}
 	}
 	cmpAIManager.TryLoadSharedComponent();
 	cmpAIManager.RunGamestateInit();

binaries/data/mods/public/simulation/components/tests/test_GuiInterface.js
@@ -64,6 +64,7 @@ AddMock(100, IID_Player, {
 	GetState: function() { return "active"; },
 	GetTeam: function() { return -1; },
 	GetLockTeams: function() { return false; },
+	GetCheatsEnabled: function() { return false; },
 	GetDiplomacy: function() { return [-1, 1]; },
 	GetConquestCriticalEntitiesCount: function() { return 1; },
 	IsAlly: function() { return false; },
@@ -119,6 +120,7 @@ AddMock(101, IID_Player, {
 	GetState: function() { return "active"; },
 	GetTeam: function() { return -1; },
 	GetLockTeams: function() {return false; },
+	GetCheatsEnabled: function() { return false; },
 	GetDiplomacy: function() { return [-1, 1]; },
 	GetConquestCriticalEntitiesCount: function() { return 1; },
 	IsAlly: function() { return true; },
@@ -217,6 +219,7 @@ TS_ASSERT_UNEVAL_EQUALS(cmp.GetSimulationState(), {
 			state: "active",
 			team: -1,
 			teamsLocked: false,
+			cheatsEnabled: false,
 			phase: "village",
 			isAlly: [false, false],
 			isMutualAlly: [false, false],
@@ -244,6 +247,7 @@ TS_ASSERT_UNEVAL_EQUALS(cmp.GetSimulationState(), {
 			state: "active",
 			team: -1,
 			teamsLocked: false,
+			cheatsEnabled: false,
 			phase: "village",
 			isAlly: [true, true],
 			isMutualAlly: [false, false],
@@ -278,6 +282,7 @@ TS_ASSERT_UNEVAL_EQUALS(cmp.GetExtendedSimulationState(), {
 			state: "active",
 			team: -1,
 			teamsLocked: false,
+			cheatsEnabled: false,
 			phase: "village",
 			isAlly: [false, false],
 			isMutualAlly: [false, false],
@@ -321,6 +326,7 @@ TS_ASSERT_UNEVAL_EQUALS(cmp.GetExtendedSimulationState(), {
 			state: "active",
 			team: -1,
 			teamsLocked: false,
+			cheatsEnabled: false,
 			phase: "village",
 			isAlly: [true, true],
 			isMutualAlly: [false, false],