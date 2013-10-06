85dd9b01b67abfa04ff026b93484d52d9ec005e6
ben <ben@3db68df2-c116-0410-a063-a993310a9797>
2013-10-06 03:13:46 +0000

    Fixes some erroneous documentation in EntityLimits. Removes previously implemented LimitsPerCivCentre from AI states. Removes hardcoded categories from TrainingRestrictions
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@13950 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/TrainingRestrictions.js
+++ binaries/data/mods/public/simulation/components/TrainingRestrictions.js
@@ -1,18 +1,14 @@
 function TrainingRestrictions() {}
 
 TrainingRestrictions.prototype.Schema =
-	"<a:help>Specifies unit training restrictions, currently only unit category.</a:help>" +
+	"<a:help>Specifies unit training restrictions, currently only unit category</a:help>" +
 	"<a:example>" +
 		"<TrainingRestrictions>" +
 			"<Category>Hero</Category>" +
 		"</TrainingRestrictions>" +
 	"</a:example>" +
-	"<element name='Category' a:help='Specifies the category of this unit, for satisfying special constraints.'>" +
-		"<choice>" +
-			"<value>Hero</value>" +
-			"<value>FemaleCitizen</value>" +
-			"<value>WarDog</value>" +
-		"</choice>" +
+	"<element name='Category' a:help='Specifies the category of this unit, for satisfying special constraints. Choices include: Hero, FemaleCitizen, WarDog'>" +
+		"<text/>" +
 	"</element>";
 
 TrainingRestrictions.prototype.GetCategory = function()
