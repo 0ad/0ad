0dbcf3e8652140b6785318cfac56fbed0ada20d7
RedFox <RedFox@3db68df2-c116-0410-a063-a993310a9797>
2013-10-07 22:39:13 +0000

    Adds EnableSSE2 for Visual Studio builds on windows. Adds optional "--with-c++11" flag for GCC/ICC builds. For possible future C++11 migration. refs #2191
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@13971 3db68df2-c116-0410-a063-a993310a9797

--- build/premake/premake4.lua
+++ build/premake/premake4.lua
@@ -13,6 +13,7 @@ newoption { trigger = "without-pch", description = "Disable generation and usage
 newoption { trigger = "with-system-nvtt", description = "Search standard paths for nvidia-texture-tools library, instead of using bundled copy" }
 newoption { trigger = "with-system-enet", description = "Search standard paths for libenet, instead of using bundled copy" }
 newoption { trigger = "with-system-mozjs185", description = "Search standard paths for libmozjs185, instead of using bundled copy" }
+newoption { trigger = "with-c++11", description = "Enable C++11 on GCC" }
 newoption { trigger = "sysroot", description = "Set compiler system root path, used for building against a non-system SDK. For example /usr/local becomes SYSROOT/user/local" }
 newoption { trigger = "macosx-version-min", description = "Set minimum required version of the OS X API, the build will possibly fail if an older SDK is used, while newer API functions will be weakly linked (i.e. resolved at runtime)" }
 newoption { trigger = "macosx-bundle", description = "Enable OSX bundle, the argument is the bundle identifier string (e.g. com.wildfiregames.0ad)" }
@@ -182,6 +183,7 @@ function project_set_build_flags()
 
 		-- use native wchar_t type (not typedef to unsigned short)
 		flags { "NativeWChar" }
+		flags { "EnableSSE2" } -- Enable SSE2 code generation for VS
 
 		-- VC++ 2008 has implied FPO as the default (newer versions default to /Oy-)
 		-- disable it explicitly since it breaks our stack walker in release build
@@ -270,7 +272,14 @@ function project_set_build_flags()
 						"-march=i686"
 					}
 				end
-
+			end
+			
+			if _OPTIONS["with-c++11"] then
+				buildoptions {
+					-- Enable C++11 standard. VS2010 and higher automatically support C++11
+					-- but we have to enable it manually on GNU C++ and Intel C++
+					"-std=c++0x"
+				}
 			end
 
 			if arch == "arm" then
@@ -1255,6 +1264,8 @@ function setup_tests()
 
 		-- see wstartup.h
 		linkoptions { "/INCLUDE:_wstartup_InitAndRegisterShutdown" }
+		-- Enables console for the TEST project on Windows
+		linkoptions { "/SUBSYSTEM:CONSOLE" }
 
 		project_add_manifest()
 
