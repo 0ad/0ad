d00887e6fa5a10ab0f862a4ad692f21a9fc0d86c
JoshuaJB <JoshuaJB@3db68df2-c116-0410-a063-a993310a9797>
2013-11-07 19:38:09 +0000

    glooxwrapper by Philip to support building gloox on Windows.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@14097 3db68df2-c116-0410-a063-a993310a9797

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -2,7 +2,7 @@
 rem ** Create Visual Studio Workspaces on Windows **
 
 cd ..\premake
-if not exist ..\workspaces\vc2008\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2008" --collada %* vs2008
-if not exist ..\workspaces\vc2010\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2010" --collada %* vs2010
-if not exist ..\workspaces\vc2012\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2012" --collada %* vs2012
+if not exist ..\workspaces\vc2008\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2008" --collada --use-shared-glooxwrapper %* vs2008
+if not exist ..\workspaces\vc2010\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2010" --collada --use-shared-glooxwrapper %* vs2010
+if not exist ..\workspaces\vc2012\SKIP_PREMAKE_HERE premake4\bin\release\premake4 --outpath="../workspaces/vc2012" --collada --use-shared-glooxwrapper %* vs2012
 cd ..\workspaces
