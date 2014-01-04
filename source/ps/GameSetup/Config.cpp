0ad/0ad

commit 233bee2357112653eaf60aa3f7092ab3bf53a119

Removes g_ScriptingHost and implements global to compartment 1 to 1 relation. Each GUI Page gets its own compartment and all ScriptInterfaces in the same thread should now use the same JS Runtime. This is required for the SpiderMonkey upgrade. Check the ticket for details.
Closes #2241
Refs #1886
Refs #1966
 


git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@14496 3db68df2-c116-0410-a063-a993310a9797

source/ps/GameSetup/GameSetup.cpp
@@ -44,8 +44,6 @@
 #include "graphics/TerrainTextureManager.h"
 #include "gui/GUI.h"
 #include "gui/GUIManager.h"
-#include "gui/scripting/JSInterface_IGUIObject.h"
-#include "gui/scripting/JSInterface_GUITypes.h"
 #include "gui/scripting/ScriptFunctions.h"
 #include "maths/MathUtil.h"
 #include "network/NetServer.h"
@@ -81,8 +79,6 @@
 #include "renderer/Renderer.h"
 #include "renderer/VertexBufferManager.h"
 #include "renderer/ModelRenderer.h"
-#include "scripting/ScriptingHost.h"
-#include "scripting/ScriptGlue.h"
 #include "scriptinterface/DebuggingServer.h"
 #include "scriptinterface/ScriptInterface.h"
 #include "scriptinterface/ScriptStats.h"
@@ -114,6 +110,8 @@ ERROR_TYPE(System, RequiredExtensionsMissing);
 bool g_DoRenderGui = true;
 bool g_DoRenderLogger = true;
 bool g_DoRenderCursor = true;
+
+shared_ptr<ScriptRuntime> g_ScriptRuntime;
 
 static const int SANE_TEX_QUALITY_DEFAULT = 5;	// keep in sync with code
 
@@ -182,9 +180,9 @@ retry:
 // display progress / description in loading screen
 void GUI_DisplayLoadProgress(int percent, const wchar_t* pending_task)
 {
-	g_ScriptingHost.GetScriptInterface().SetGlobal("g_Progress", percent, true);
-	g_ScriptingHost.GetScriptInterface().SetGlobal("g_LoadDescription", pending_task, true);
-	g_GUI->SendEventToAll("progress");
+	g_GUI->GetActiveGUI()->GetScriptInterface()->SetGlobal("g_Progress", percent, true);
+	g_GUI->GetActiveGUI()->GetScriptInterface()->SetGlobal("g_LoadDescription", pending_task, true);
+	g_GUI->GetActiveGUI()->SendEventToAll("progress");
 }
 
 
@@ -317,28 +315,6 @@ void Render()
 	g_Profiler2.RecordGPUFrameEnd();
 
 	ogl_WarnIfError();
-}
-
-
-static void RegisterJavascriptInterfaces()
-{
-	// GUI
-	CGUI::ScriptingInit();
-
-	GuiScriptingInit(g_ScriptingHost.GetScriptInterface());
-	JSI_Sound::RegisterScriptFunctions(g_ScriptingHost.GetScriptInterface());
-}
-
-
-static void InitScripting()
-{
-	TIMER(L"InitScripting");
-
-	// Create the scripting host.  This needs to be done before the GUI is created.
-	// [7ms]
-	new ScriptingHost;
-	
-	RegisterJavascriptInterfaces();
 }
 
 
@@ -499,7 +475,7 @@ static void InitVfs(const CmdLineArgs& args, int flags)
 }
 
 
-static void InitPs(bool setup_gui, const CStrW& gui_page, CScriptVal initData)
+static void InitPs(bool setup_gui, const CStrW& gui_page, ScriptInterface* srcScriptInterface, CScriptVal initData)
 {
 	{
 		// console
@@ -530,12 +506,12 @@ static void InitPs(bool setup_gui, const CStrW& gui_page, CScriptVal initData)
 	{
 		// We do actually need *some* kind of GUI loaded, so use the
 		// (currently empty) Atlas one
-		g_GUI->SwitchPage(L"page_atlas.xml", initData);
+		g_GUI->SwitchPage(L"page_atlas.xml", srcScriptInterface, initData);
 		return;
 	}
 
 	// GUI uses VFS, so this must come after VFS init.
-	g_GUI->SwitchPage(gui_page, initData);
+	g_GUI->SwitchPage(gui_page, srcScriptInterface, initData);
 }
 
 
@@ -687,7 +663,7 @@ void Shutdown(int UNUSED(flags))
 
 	SAFE_DELETE(g_XmppClient);
 
-	ShutdownPs(); // Must delete g_GUI before g_ScriptingHost
+	ShutdownPs();
 
 	in_reset_handlers();
 
@@ -718,10 +694,9 @@ void Shutdown(int UNUSED(flags))
 	g_UserReporter.Deinitialize();
 	TIMER_END(L"shutdown UserReporter");
 
-	TIMER_BEGIN(L"shutdown ScriptingHost");
-	delete &g_ScriptingHost;
+	TIMER_BEGIN(L"shutdown DebuggingServer (if active)");
 	delete g_DebuggingServer;
-	TIMER_END(L"shutdown ScriptingHost");
+	TIMER_END(L"shutdown DebuggingServer (if active)");
 
 	TIMER_BEGIN(L"shutdown ConfigDB");
 	delete &g_ConfigDB;
@@ -875,13 +850,16 @@ void Init(const CmdLineArgs& args, int flags)
 	// This must come after VFS init, which sets the current directory
 	// (required for finding our output log files).
 	g_Logger = new CLogger;
+	
+	// Workaround until Simulation and AI use their own threads and also their own runtimes
+	g_ScriptRuntime = ScriptInterface::CreateRuntime(128 * 1024 * 1024);
 
 	// Special command-line mode to dump the entity schemas instead of running the game.
 	// (This must be done after loading VFS etc, but should be done before wasting time
 	// on anything else.)
 	if (args.Has("dumpSchema"))
 	{
-		CSimulation2 sim(NULL, NULL);
+		CSimulation2 sim(NULL, g_ScriptRuntime, NULL);
 		sim.LoadDefaultScripts();
 		std::ofstream f("entity.rng", std::ios_base::out | std::ios_base::trunc);
 		f << sim.GenerateSchema();
@@ -919,8 +897,6 @@ void Init(const CmdLineArgs& args, int flags)
 	// before scripting 
 	if (g_JSDebuggerEnabled)
 		g_DebuggingServer = new CDebuggingServer();
-
-	InitScripting();	// before GUI
 
 	// Optionally start profiler HTTP output automatically
 	// (By default it's only enabled by a hotkey, for security/performance)
@@ -977,7 +953,7 @@ void InitGraphics(const CmdLineArgs& args, int flags)
 	if(g_DisableAudio)
 		ISoundManager::SetEnabled(false);
 
-	g_GUI = new CGUIManager(g_ScriptingHost.GetScriptInterface());
+	g_GUI = new CGUIManager();
 
 	// (must come after SetVideoMode, since it calls ogl_Init)
 	if (ogl_HaveExtensions(0, "GL_ARB_vertex_program", "GL_ARB_fragment_program", NULL) != 0 // ARB
@@ -1036,11 +1012,11 @@ void InitGraphics(const CmdLineArgs& args, int flags)
 			CScriptValRooted data;
 			if (g_GUI)
 			{
-				ScriptInterface& scriptInterface = g_GUI->GetScriptInterface();
-				scriptInterface.Eval("({})", data);
-				scriptInterface.SetProperty(data.get(), "isStartup", true);
+				shared_ptr<ScriptInterface> scriptInterface = g_GUI->GetScriptInterface();
+				scriptInterface->Eval("({})", data);
+				scriptInterface->SetProperty(data.get(), "isStartup", true);
 			}
-			InitPs(setup_gui, L"page_pregame.xml", data.get());
+			InitPs(setup_gui, L"page_pregame.xml", g_GUI->GetScriptInterface().get(), data.get());
 		}
 	}
 	catch (PSERROR_Game_World_MapLoadFailed& e)
@@ -1048,7 +1024,7 @@ void InitGraphics(const CmdLineArgs& args, int flags)
 		// Map Loading failed
 
 		// Start the engine so we have a GUI
-		InitPs(true, L"page_pregame.xml", JSVAL_VOID);
+		InitPs(true, L"page_pregame.xml", NULL, JSVAL_VOID);
 
 		// Call script function to do the actual work
 		//	(delete game data, switch GUI page, show error, etc.)
@@ -1262,9 +1238,8 @@ bool Autostart(const CmdLineArgs& args)
 	scriptInterface.SetProperty(attrs.get(), "settings", settings);
 
 	CScriptVal mpInitData;
-	g_GUI->GetScriptInterface().Eval("({isNetworked:true, playerAssignments:{}})", mpInitData);
-	g_GUI->GetScriptInterface().SetProperty(mpInitData.get(), "attribs",
-			CScriptVal(g_GUI->GetScriptInterface().CloneValueFromOtherContext(scriptInterface, attrs.get())));
+	scriptInterface.Eval("({isNetworked:true, playerAssignments:{}})", mpInitData);
+	scriptInterface.SetProperty(mpInitData.get(), "attribs", attrs);
 
 	// Get optional playername
 	CStrW userName = L"anonymous";
@@ -1275,7 +1250,7 @@ bool Autostart(const CmdLineArgs& args)
 
 	if (args.Has("autostart-host"))
 	{
-		InitPs(true, L"page_loading.xml", mpInitData.get());
+		InitPs(true, L"page_loading.xml", &scriptInterface, mpInitData.get());
 
 		size_t maxPlayers = 2;
 		if (args.Has("autostart-players"))
@@ -1296,7 +1271,7 @@ bool Autostart(const CmdLineArgs& args)
 	}
 	else if (args.Has("autostart-client"))
 	{
-		InitPs(true, L"page_loading.xml", mpInitData.get());
+		InitPs(true, L"page_loading.xml", &scriptInterface, mpInitData.get());
 
 		g_NetClient = new CNetClient(g_Game);
 		g_NetClient->SetUserName(userName);
@@ -1320,7 +1295,7 @@ bool Autostart(const CmdLineArgs& args)
 		PSRETURN ret = g_Game->ReallyStartGame();
 		ENSURE(ret == PSRETURN_OK);
 
-		InitPs(true, L"page_session.xml", JSVAL_VOID);
+		InitPs(true, L"page_session.xml", NULL, JSVAL_VOID);
 	}
 
 	return true;
@@ -1335,14 +1310,7 @@ void CancelLoad(const CStrW& message)
 	// So all GUI pages that load games should include this script
 	if (g_GUI && g_GUI->HasPages())
 	{
-		JSContext* cx = g_ScriptingHost.getContext();
-		jsval fval, rval;
-		JSBool ok = JS_GetProperty(cx, g_GUI->GetScriptObject(), "cancelOnError", &fval);
-		ENSURE(ok);
-
-		jsval msgval = ScriptInterface::ToJSVal(cx, message);
-
-		if (ok && !JSVAL_IS_VOID(fval))
-			JS_CallFunctionValue(cx, g_GUI->GetScriptObject(), fval, 1, &msgval, &rval);
+		if (g_GUI->GetActiveGUI()->GetScriptInterface()->HasProperty(g_GUI->GetActiveGUI()->GetGlobalObject(), "cancelOnError" ))
+			g_GUI->GetActiveGUI()->GetScriptInterface()->CallFunctionVoid(g_GUI->GetActiveGUI()->GetGlobalObject(), "cancelOnError", message);
 	}
 }

source/scriptinterface/ScriptInterface.cpp
@@ -69,7 +69,7 @@ class ScriptRuntime
 {
 public:
 	ScriptRuntime(int runtimeSize) :
-		m_rooter(NULL), m_compartmentGlobal(NULL)
+		m_rooter(NULL)
 	{
 		m_rt = JS_NewRuntime(runtimeSize);
 		ENSURE(m_rt); // TODO: error handling
@@ -97,8 +97,6 @@ public:
 
 	JSRuntime* m_rt;
 	AutoGCRooter* m_rooter;
-
-	JSObject* m_compartmentGlobal;
 
 private:
 
@@ -241,7 +239,6 @@ struct ScriptInterface_impl
 	JSContext* m_cx;
 	JSObject* m_glob; // global scope object
 	JSObject* m_nativeScope; // native function scope object
-	JSCrossCompartmentCall* m_call;
 };
 
 namespace
@@ -507,24 +504,9 @@ ScriptInterface_impl::ScriptInterface_impl(const char* nativeScopeName, const sh
 	}
 
 	JS_SetOptions(m_cx, options);
-
 	JS_SetVersion(m_cx, JSVERSION_LATEST);
 
-	// Threadsafe SpiderMonkey requires that we have a request before doing anything much
-	JS_BeginRequest(m_cx);
-
-	// We only want a single compartment per runtime
-	if (m_runtime->m_compartmentGlobal)
-	{
-		m_call = JS_EnterCrossCompartmentCall(m_cx, m_runtime->m_compartmentGlobal);
-		m_glob = JS_NewGlobalObject(m_cx, &global_class);
-	}
-	else
-	{
-		m_call = NULL;
-		m_glob = JS_NewCompartmentAndGlobalObject(m_cx, &global_class, NULL);
-		m_runtime->m_compartmentGlobal = m_glob;
-	}
+	m_glob = JS_NewCompartmentAndGlobalObject(m_cx, &global_class, NULL);
 
 	ok = JS_InitStandardClasses(m_cx, m_glob);
 	ENSURE(ok);
@@ -547,9 +529,6 @@ ScriptInterface_impl::ScriptInterface_impl(const char* nativeScopeName, const sh
 
 ScriptInterface_impl::~ScriptInterface_impl()
 {
-	if (m_call)
-		JS_LeaveCrossCompartmentCall(m_call);
-	JS_EndRequest(m_cx);
 	JS_DestroyContext(m_cx);
 }
 
@@ -594,6 +573,9 @@ ScriptInterface::ScriptInterface(const char* nativeScopeName, const char* debugN
 		else
 			g_DebuggingServer->RegisterScriptinterface(debugName, this);
 	}
+
+	m_CxPrivate.pScriptInterface = this;
+	JS_SetContextPrivate(m->m_cx, (void*)&m_CxPrivate);
 }
 
 ScriptInterface::~ScriptInterface()
@@ -614,14 +596,15 @@ void ScriptInterface::ShutDown()
 	JS_ShutDown();
 }
 
-void ScriptInterface::SetCallbackData(void* cbdata)
+void ScriptInterface::SetCallbackData(void* pCBData)
 {
-	JS_SetContextPrivate(m->m_cx, cbdata);
+	m_CxPrivate.pCBData = pCBData;
 }
 
-void* ScriptInterface::GetCallbackData(JSContext* cx)
+ScriptInterface::CxPrivate* ScriptInterface::GetScriptInterfaceAndCBData(JSContext* cx)
 {
-	return JS_GetContextPrivate(cx);
+	CxPrivate* pCxPrivate = (CxPrivate*)JS_GetContextPrivate(cx);
+	return pCxPrivate;
 }
 
 bool ScriptInterface::LoadGlobalScripts()
@@ -674,9 +657,14 @@ JSContext* ScriptInterface::GetContext() const
 	return m->m_cx;
 }
 
-JSRuntime* ScriptInterface::GetRuntime() const
+JSRuntime* ScriptInterface::GetJSRuntime() const
 {
 	return m->m_runtime->m_rt;
+}
+
+shared_ptr<ScriptRuntime> ScriptInterface::GetRuntime() const
+{
+	return m->m_runtime;
 }
 
 AutoGCRooter* ScriptInterface::ReplaceAutoGCRooter(AutoGCRooter* rooter)
@@ -733,6 +721,47 @@ jsval ScriptInterface::NewObjectFromConstructor(jsval ctor)
 
 	return OBJECT_TO_JSVAL(obj);
 }
+
+void ScriptInterface::DefineCustomObjectType(JSClass *clasp, JSNative constructor, uint minArgs, JSPropertySpec *ps, JSFunctionSpec *fs, JSPropertySpec *static_ps, JSFunctionSpec *static_fs)
+{
+	std::string typeName = clasp->name;
+
+	if (m_CustomObjectTypes.find(typeName) != m_CustomObjectTypes.end())
+	{
+		// This type already exists
+		throw PSERROR_Scripting_DefineType_AlreadyExists();
+	}
+
+	JSObject * obj = JS_InitClass(	m->m_cx, JSVAL_TO_OBJECT(GetGlobalObject()), 0,
+									clasp,
+									constructor, minArgs,				// Constructor, min args
+									ps, fs,								// Properties, methods
+									static_ps, static_fs);				// Constructor properties, methods
+
+	if (obj == NULL)
+		throw PSERROR_Scripting_DefineType_CreationFailed();
+
+	CustomType type;
+
+	type.m_Object = obj;
+	type.m_Class = clasp;
+	type.m_Constructor = constructor;
+
+	m_CustomObjectTypes[typeName] = type;
+}
+
+JSObject* ScriptInterface::CreateCustomObject(const std::string & typeName)
+{
+	std::map < std::string, CustomType > ::iterator it = m_CustomObjectTypes.find(typeName);
+
+	if (it == m_CustomObjectTypes.end())
+		throw PSERROR_Scripting_TypeDoesNotExist();
+
+	JSFunction* ctor = JS_NewFunction(m->m_cx, (*it).second.m_Constructor, 0, 0,
+               NULL, "ctor_fun");
+	return JS_New(m->m_cx, JS_GetFunctionObject(ctor), 0, NULL);
+}
+
 
 bool ScriptInterface::CallFunctionVoid(jsval val, const char* name)
 {
@@ -951,11 +980,11 @@ bool ScriptInterface::LoadScript(const VfsPath& filename, const std::string& cod
 	return ok ? true : false;
 }
 
-bool ScriptInterface::LoadGlobalScript(const VfsPath& filename, const std::string& code)
+bool ScriptInterface::LoadGlobalScript(const VfsPath& filename, const std::wstring& code)
 {
 	// Compile the code in strict mode, to encourage better coding practices and
 	// to possibly help SpiderMonkey with optimisations
-	std::wstring codeStrict = L"\"use strict\";\n" + wstring_from_utf8(code);
+	std::wstring codeStrict = L"\"use strict\";\n" + code;
 	utf16string codeUtf16(codeStrict.begin(), codeStrict.end());
 	uintN lineNo = 0; // put the automatic 'use strict' on line 0, so the real code starts at line 1
 
@@ -1187,9 +1216,9 @@ void ScriptInterface::DumpHeap()
 #if MOZJS_DEBUG_ABI
 	JS_DumpHeap(m->m_cx, stderr, NULL, 0, NULL, (size_t)-1, NULL);
 #endif
-	fprintf(stderr, "# Bytes allocated: %u\n", JS_GetGCParameter(GetRuntime(), JSGC_BYTES));
+	fprintf(stderr, "# Bytes allocated: %u\n", JS_GetGCParameter(GetJSRuntime(), JSGC_BYTES));
 	JS_GC(m->m_cx);
-	fprintf(stderr, "# Bytes allocated after GC: %u\n", JS_GetGCParameter(GetRuntime(), JSGC_BYTES));
+	fprintf(stderr, "# Bytes allocated after GC: %u\n", JS_GetGCParameter(GetJSRuntime(), JSGC_BYTES));
 }
 
 void ScriptInterface::MaybeGC()
@@ -1337,9 +1366,10 @@ shared_ptr<ScriptInterface::StructuredClone> ScriptInterface::WriteStructuredClo
 	uint64* data = NULL;
 	size_t nbytes = 0;
 	if (!JS_WriteStructuredClone(m->m_cx, v, &data, &nbytes, NULL, NULL))
+	{
+		debug_warn(L"Writing a structured clone with JS_WriteStructuredClone failed!");
 		return shared_ptr<StructuredClone>();
-	// TODO: should we have better error handling?
-	// Currently we'll probably continue and then crash in ReadStructuredClone
+	}
 
 	shared_ptr<StructuredClone> ret (new StructuredClone);
 	ret->m_Context = m->m_cx;

source/scriptinterface/ThreadDebugger.cpp
@@ -289,10 +289,10 @@ CThreadDebugger::~CThreadDebugger()
 	ReturnActiveBreakPoints(NULL);
 
 	// Remove all the hooks because they store a pointer to this object
-	JS_SetExecuteHook(m->m_pScriptInterface->GetRuntime(), NULL, NULL);
-	JS_SetCallHook(m->m_pScriptInterface->GetRuntime(), NULL, NULL);
-	JS_SetNewScriptHook(m->m_pScriptInterface->GetRuntime(), NULL, NULL);
-	JS_SetDestroyScriptHook(m->m_pScriptInterface->GetRuntime(), NULL, NULL);
+	JS_SetExecuteHook(m->m_pScriptInterface->GetJSRuntime(), NULL, NULL);
+	JS_SetCallHook(m->m_pScriptInterface->GetJSRuntime(), NULL, NULL);
+	JS_SetNewScriptHook(m->m_pScriptInterface->GetJSRuntime(), NULL, NULL);
+	JS_SetDestroyScriptHook(m->m_pScriptInterface->GetJSRuntime(), NULL, NULL);
 }
 
 void CThreadDebugger::ReturnActiveBreakPoints(jsbytecode* pBytecode)
@@ -329,16 +329,16 @@ void CThreadDebugger::Initialize(uint id, std::string name, ScriptInterface* pSc
 	m->m_Name = name;
 	m->m_pScriptInterface = pScriptInterface;
 	m->m_pDebuggingServer = pDebuggingServer;
-	JS_SetExecuteHook(m->m_pScriptInterface->GetRuntime(), CallHook_, (void*)this);
-	JS_SetCallHook(m->m_pScriptInterface->GetRuntime(), CallHook_, (void*)this);
-	JS_SetNewScriptHook(m->m_pScriptInterface->GetRuntime(), NewScriptHook_, (void*)this);
-	JS_SetDestroyScriptHook(m->m_pScriptInterface->GetRuntime(), DestroyScriptHook_, (void*)this);
-	JS_SetThrowHook(m->m_pScriptInterface->GetRuntime(), ThrowHandler_, (void*)this);
+	JS_SetExecuteHook(m->m_pScriptInterface->GetJSRuntime(), CallHook_, (void*)this);
+	JS_SetCallHook(m->m_pScriptInterface->GetJSRuntime(), CallHook_, (void*)this);
+	JS_SetNewScriptHook(m->m_pScriptInterface->GetJSRuntime(), NewScriptHook_, (void*)this);
+	JS_SetDestroyScriptHook(m->m_pScriptInterface->GetJSRuntime(), DestroyScriptHook_, (void*)this);
+	JS_SetThrowHook(m->m_pScriptInterface->GetJSRuntime(), ThrowHandler_, (void*)this);
 	
 	if (m->m_pDebuggingServer->GetSettingSimultaneousThreadBreak())
 	{
 		// Setup a handler to check for break-requests from the DebuggingServer regularly
-		JS_SetInterrupt(m->m_pScriptInterface->GetRuntime(), CheckForBreakRequestHandler_, (void*)this);
+		JS_SetInterrupt(m->m_pScriptInterface->GetJSRuntime(), CheckForBreakRequestHandler_, (void*)this);
 	}
 }
 
@@ -456,7 +456,7 @@ JSTrapStatus CThreadDebugger::BreakHandler(JSContext* cx, JSScript* script, jsby
 	
 	if (breakSrc == BREAK_SRC_INTERRUP)
 	{
-		JS_ClearInterrupt(m->m_pScriptInterface->GetRuntime(), NULL, NULL);
+		JS_ClearInterrupt(m->m_pScriptInterface->GetJSRuntime(), NULL, NULL);
 		JS_SetSingleStepMode(cx, script, false);
 	}
 	
@@ -496,17 +496,17 @@ JSTrapStatus CThreadDebugger::BreakHandler(JSContext* cx, JSScript* script, jsby
 			{
 				if (nextDbgCmd == DBG_CMD_SINGLESTEP)
 				{
-					JS_SetInterrupt(m->m_pScriptInterface->GetRuntime(), StepHandler_, this);
+					JS_SetInterrupt(m->m_pScriptInterface->GetJSRuntime(), StepHandler_, this);
 					break;
 				}
 				else if (nextDbgCmd == DBG_CMD_STEPINTO)
 				{
-					JS_SetInterrupt(m->m_pScriptInterface->GetRuntime(), StepIntoHandler_, this);
+					JS_SetInterrupt(m->m_pScriptInterface->GetJSRuntime(), StepIntoHandler_, this);
 					break;
 				}
 				else if (nextDbgCmd == DBG_CMD_STEPOUT)
 				{
-					JS_SetInterrupt(m->m_pScriptInterface->GetRuntime(), StepOutHandler_, this);
+					JS_SetInterrupt(m->m_pScriptInterface->GetJSRuntime(), StepOutHandler_, this);
 					break;
 				}
 			}
@@ -518,7 +518,7 @@ JSTrapStatus CThreadDebugger::BreakHandler(JSContext* cx, JSScript* script, jsby
 			else
 			{
 				// Setup a handler to check for break-requests from the DebuggingServer regularly
-				JS_SetInterrupt(m->m_pScriptInterface->GetRuntime(), CheckForBreakRequestHandler_, this);
+				JS_SetInterrupt(m->m_pScriptInterface->GetJSRuntime(), CheckForBreakRequestHandler_, this);
 			}
 			break;
 		}

commit 834133b3b04f9c414869c26455cabbd797ea7b89

Disables the JS debugger. It's completely broken since r14496 and will have to be updated for the new SpiderMonkey API. I only uncomment it at the moment because I plan to fix/reimplement it soon after the upgrade.
Closes #2348
Refs #2241
Refs #1886
 


git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@14506 3db68df2-c116-0410-a063-a993310a9797

binaries/data/config/default.cfg
@@ -330,7 +330,7 @@ profiler2.gpu.ext.enable = true             ; Allow GL_EXT_timer_query timing mo
 profiler2.gpu.intel.enable = true           ; Allow GL_INTEL_performance_queries timing mode when available
 
 ; > JS DEBUGGER
-jsdebugger.enable = false                   ; Enable Javascript debugging (default off for security/performance)
+jsdebugger.enable = false                   ; Enable Javascript debugging. The Debugger is currently broken and can't be enabled until the SpiderMonkey upgrade is done and the debugger is updated for the new API.
 
 ; > QUICKSAVE
 hotkey.quicksave = "Shift+F5"

source/ps/GameSetup/Config.cpp
@@ -134,6 +134,8 @@ static void LoadGlobals()
 	CFG_GET_VAL("jsdebugger.enable", Bool, g_JSDebuggerEnabled);
 	CFG_GET_VAL("profiler2.script.enable", Bool, g_ScriptProfilingEnabled);
 
+	if (g_JSDebuggerEnabled)
+		LOGERROR(L"JS debugger temporarily disabled during the SpiderMonkey upgrade (check trac ticket #2348 for details)");
 	// Script Debugging and profiling does not make sense together because of the hooks
 	// that reduce performance a lot - and it wasn't tested if it even works together.
 	if (g_JSDebuggerEnabled && g_ScriptProfilingEnabled)

source/ps/GameSetup/GameSetup.cpp
@@ -694,9 +694,10 @@ void Shutdown(int UNUSED(flags))
 	g_UserReporter.Deinitialize();
 	TIMER_END(L"shutdown UserReporter");
 
-	TIMER_BEGIN(L"shutdown DebuggingServer (if active)");
-	delete g_DebuggingServer;
-	TIMER_END(L"shutdown DebuggingServer (if active)");
+	// JS debugger temporarily disabled during the SpiderMonkey upgrade (check trac ticket #2348 for details)
+	//TIMER_BEGIN(L"shutdown DebuggingServer (if active)");
+	//delete g_DebuggingServer;
+	//TIMER_END(L"shutdown DebuggingServer (if active)");
 
 	TIMER_BEGIN(L"shutdown ConfigDB");
 	delete &g_ConfigDB;
@@ -895,8 +896,9 @@ void Init(const CmdLineArgs& args, int flags)
 	CONFIG_Init(args);
 
 	// before scripting 
-	if (g_JSDebuggerEnabled)
-		g_DebuggingServer = new CDebuggingServer();
+	// JS debugger temporarily disabled during the SpiderMonkey upgrade (check trac ticket #2348 for details)
+	//if (g_JSDebuggerEnabled)
+	//	g_DebuggingServer = new CDebuggingServer();
 
 	// Optionally start profiler HTTP output automatically
 	// (By default it's only enabled by a hotkey, for security/performance)

source/scriptinterface/DebuggingServer.cpp
@@ -14,7 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+ 
+// JS debugger temporarily disabled during the SpiderMonkey upgrade (check trac ticket #2348 for details)
+/* 
 #include "precompiled.h"
 
 #include "DebuggingServer.h"
@@ -566,3 +568,4 @@ void CDebuggingServer::ReleaseBreakPointAccess(double breakPointsLockID)
 	ENSURE(m_BreakPointsLockID == breakPointsLockID); 
 	SDL_SemPost(m_BreakPointsSem);
 }
+*/

source/scriptinterface/ScriptInterface.cpp
@@ -566,13 +566,15 @@ ScriptInterface::ScriptInterface(const char* nativeScopeName, const char* debugN
 			g_ScriptStatsTable->Add(this, debugName);
 	}
 	
+	// JS debugger temporarily disabled during the SpiderMonkey upgrade (check trac ticket #2348 for details)
+	/*
 	if (g_JSDebuggerEnabled && g_DebuggingServer != NULL)
 	{
 		if(!JS_SetDebugMode(GetContext(), true))
 			LOGERROR(L"Failed to set Spidermonkey to debug mode!");
 		else
 			g_DebuggingServer->RegisterScriptinterface(debugName, this);
-	}
+	} */
 
 	m_CxPrivate.pScriptInterface = this;
 	JS_SetContextPrivate(m->m_cx, (void*)&m_CxPrivate);
@@ -587,8 +589,9 @@ ScriptInterface::~ScriptInterface()
 	}
 	
 	// Unregister from the Debugger class
-	if (g_JSDebuggerEnabled && g_DebuggingServer != NULL)
-		g_DebuggingServer->UnRegisterScriptinterface(this);
+	// JS debugger temporarily disabled during the SpiderMonkey upgrade (check trac ticket #2348 for details)
+	//if (g_JSDebuggerEnabled && g_DebuggingServer != NULL)
+	//	g_DebuggingServer->UnRegisterScriptinterface(this);
 }
 
 void ScriptInterface::ShutDown()

source/scriptinterface/ThreadDebugger.cpp
@@ -15,6 +15,8 @@
  * along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
  */
  
+ // JS debugger temporarily disabled during the SpiderMonkey upgrade (check trac ticket #2348 for details)
+ /*
 #include "precompiled.h"
 
 #include "ThreadDebugger.h"
@@ -757,7 +759,7 @@ void CThreadDebugger::SaveStackFrameData(STACK_INFO stackInfo, uint nestingLevel
 		}
 	}
 }
-
+*/
 
 /*
  * TODO: This is very hacky and ugly and should be improved. 
@@ -772,6 +774,7 @@ void CThreadDebugger::SaveStackFrameData(STACK_INFO stackInfo, uint nestingLevel
  *    the rest of the game and because this part of code should be replaced anyway in the future.
  */
  
+ /*
 namespace CyclicRefWorkaround
 {
 	std::set<JSObject*> g_ProcessedObjects;
@@ -933,4 +936,4 @@ uint CThreadDebugger::GetID()
 	CScopeLock lock(m->m_Mutex);
 	return m->m_ID;
 }
-
+*/