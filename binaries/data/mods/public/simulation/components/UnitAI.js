0ad/0ad

commit afda0ff082fa9a9a249e29f0d1cb13f14121ad76

fix units attacking harmless gaia buildings
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@14494 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -5031,14 +5031,17 @@ UnitAI.prototype.GetTargetsFromUnit = function()
 	if (!cmpAttack)
 		return [];
 
-	const animalfilter = function(e) {
+	const attackfilter = function(e) {
+		var cmpPlayer = Engine.QueryInterface(e, IID_Player);
+		if (cmpPlayer && cmpPlayer.GetPlayerID() > 0)
+			return true;
 		var cmpUnitAI = Engine.QueryInterface(e, IID_UnitAI);
-		return !cmpUnitAI || !cmpUnitAI.IsAnimal() || cmpUnitAI.IsDangerousAnimal();
+		return cmpUnitAI && (!cmpUnitAI.IsAnimal() || cmpUnitAI.IsDangerousAnimal());
 	};
 
 	var rangeMan = Engine.QueryInterface(SYSTEM_ENTITY, IID_RangeManager);
 	var entities = rangeMan.ResetActiveQuery(this.losRangeQuery);
-	var targets = entities.filter(function (v, i, a) { return cmpAttack.CanAttack(v) && animalfilter(v); })
+	var targets = entities.filter(function (v, i, a) { return cmpAttack.CanAttack(v) && attackfilter(v); })
 		.sort(function (a, b) { return cmpAttack.CompareEntitiesByPreference(a, b); });
 
 	return targets;
@@ -5438,13 +5441,16 @@ UnitAI.prototype.AttackEntitiesByPreference = function(ents)
 	if (!cmpAttack)
 		return false;
 
-	const animalfilter = function(e) {
+	const attackfilter = function(e) {
+		var cmpPlayer = Engine.QueryInterface(e, IID_Player);
+		if (cmpPlayer && cmpPlayer.GetPlayerID() > 0)
+			return true;
 		var cmpUnitAI = Engine.QueryInterface(e, IID_UnitAI);
-		return !cmpUnitAI || !cmpUnitAI.IsAnimal() || cmpUnitAI.IsDangerousAnimal();
+		return cmpUnitAI && (!cmpUnitAI.IsAnimal() || cmpUnitAI.IsDangerousAnimal());
 	};
 
 	return this.RespondToTargetedEntities(
-		ents.filter(function (v, i, a) { return cmpAttack.CanAttack(v) && animalfilter(v); })
+		ents.filter(function (v, i, a) { return cmpAttack.CanAttack(v) && attackfilter(v); })
 		.sort(function (a, b) { return cmpAttack.CompareEntitiesByPreference(a, b); })
 	);
 };

commit 308679b70a079b906ad95147633c468331159ebc

fix losRangeQuery broken in r14494
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@14526 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -5058,8 +5058,8 @@ UnitAI.prototype.GetTargetsFromUnit = function()
 		return [];
 
 	const attackfilter = function(e) {
-		var cmpPlayer = Engine.QueryInterface(e, IID_Player);
-		if (cmpPlayer && cmpPlayer.GetPlayerID() > 0)
+		var cmpOwnership = Engine.QueryInterface(e, IID_Ownership);
+		if (cmpOwnership && cmpOwnership.GetOwner() > 0)
 			return true;
 		var cmpUnitAI = Engine.QueryInterface(e, IID_UnitAI);
 		return cmpUnitAI && (!cmpUnitAI.IsAnimal() || cmpUnitAI.IsDangerousAnimal());
@@ -5067,7 +5067,7 @@ UnitAI.prototype.GetTargetsFromUnit = function()
 
 	var rangeMan = Engine.QueryInterface(SYSTEM_ENTITY, IID_RangeManager);
 	var entities = rangeMan.ResetActiveQuery(this.losRangeQuery);
-	var targets = entities.filter(function (v, i, a) { return cmpAttack.CanAttack(v) && attackfilter(v); })
+	var targets = entities.filter(function (v) { return cmpAttack.CanAttack(v) && attackfilter(v); })
 		.sort(function (a, b) { return cmpAttack.CompareEntitiesByPreference(a, b); });
 
 	return targets;
@@ -5468,15 +5468,15 @@ UnitAI.prototype.AttackEntitiesByPreference = function(ents)
 		return false;
 
 	const attackfilter = function(e) {
-		var cmpPlayer = Engine.QueryInterface(e, IID_Player);
-		if (cmpPlayer && cmpPlayer.GetPlayerID() > 0)
+		var cmpOwnership = Engine.QueryInterface(e, IID_Ownership);
+		if (cmpOwnership && cmpOwnership.GetOwner() > 0)
 			return true;
 		var cmpUnitAI = Engine.QueryInterface(e, IID_UnitAI);
 		return cmpUnitAI && (!cmpUnitAI.IsAnimal() || cmpUnitAI.IsDangerousAnimal());
 	};
 
 	return this.RespondToTargetedEntities(
-		ents.filter(function (v, i, a) { return cmpAttack.CanAttack(v) && attackfilter(v); })
+		ents.filter(function (v) { return cmpAttack.CanAttack(v) && attackfilter(v); })
 		.sort(function (a, b) { return cmpAttack.CompareEntitiesByPreference(a, b); })
 	);
 };