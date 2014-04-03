c09c9f6687b1f2704e6d258d36dbbc49f84de009
mimo <mimo@3db68df2-c116-0410-a063-a993310a9797>
2014-04-03 20:31:59 +0000

    supplement the ai common-api
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@14895 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/ai/common-api/filters.js
+++ binaries/data/mods/public/simulation/ai/common-api/filters.js
@@ -141,7 +141,7 @@ m.Filters = {
 	
 	isGarrisoned: function(){
 		return {"func" : function(ent){
-			return ent.position() == -1;	// assumes garrisoned
+			return ent.position() === undefined;
 		},
 			"dynamicProperties": []};
 	},
