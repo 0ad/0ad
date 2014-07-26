0ad/0ad

commit d1b86add396ef80327bbb57d7830ceb2c7ca6cdb

Quite a lot of stack rooting related changes.
Changes GetProperty, SetProperty and HasProperty and a few other functions to take handles. The conversions to CScriptVal or CScriptValRooted at some places should be removed in the future. I've done that to avoid an even larger patch.

Refs #2415
Refs #2462  


git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@15568 3db68df2-c116-0410-a063-a993310a9797

source/graphics/MapGenerator.cpp
@@ -79,6 +79,9 @@ void* CMapGeneratorWorker::RunThread(void *data)
 
 bool CMapGeneratorWorker::Run()
 {
+	JSContext* cx = m_ScriptInterface->GetContext();
+	JSAutoRequest rq(cx);
+	
 	m_ScriptInterface->SetCallbackData(static_cast<void*> (this));
 
 	// Replace RNG with a seeded deterministic function
@@ -95,52 +98,44 @@ bool CMapGeneratorWorker::Run()
 	m_ScriptInterface->RegisterFunction<std::vector<std::string>, std::string, bool, CMapGeneratorWorker::FindTemplates>("FindTemplates");
 	m_ScriptInterface->RegisterFunction<std::vector<std::string>, std::string, bool, CMapGeneratorWorker::FindActorTemplates>("FindActorTemplates");
 
-	// TODO: This code is a bit ugly because we have to ensure that CScriptValRooted gets destroyed before the ScriptInterface.
-	// In the future we should work more with the standard JSAPI types for rooting on the stack, which should avoid such problems.
-	bool ret = true;
+	// Parse settings
+	JS::RootedValue settingsVal(cx, m_ScriptInterface->ParseJSON(m_Settings).get());
+	if (settingsVal.isUndefined())
 	{
-		// Parse settings
-		CScriptValRooted settingsVal = m_ScriptInterface->ParseJSON(m_Settings);
-		if (settingsVal.undefined())
-		{
-			LOGERROR(L"CMapGeneratorWorker::Run: Failed to parse settings");
-			ret = false;
-		} 
-		else
-		{
-			// Init RNG seed
-			u32 seed;
-			if (!m_ScriptInterface->GetProperty(settingsVal.get(), "Seed", seed))
-			{	// No seed specified
-				LOGWARNING(L"CMapGeneratorWorker::Run: No seed value specified - using 0");
-				seed = 0;
-			}
+		LOGERROR(L"CMapGeneratorWorker::Run: Failed to parse settings");
+		return false;
+	}
+	
+	// Init RNG seed
+	u32 seed;
+	if (!m_ScriptInterface->GetProperty(settingsVal, "Seed", seed))
+	{	// No seed specified
+		LOGWARNING(L"CMapGeneratorWorker::Run: No seed value specified - using 0");
+		seed = 0;
+	}
 
-			m_MapGenRNG.seed(seed);
+	m_MapGenRNG.seed(seed);
 
-			// Copy settings to global variable
-			if (!m_ScriptInterface->SetProperty(m_ScriptInterface->GetGlobalObject(), "g_MapSettings", settingsVal))
-			{
-				LOGERROR(L"CMapGeneratorWorker::Run: Failed to define g_MapSettings");
-				ret = false;
-			}
-			else
-			{
-				// Load RMS
-				LOGMESSAGE(L"Loading RMS '%ls'", m_ScriptPath.string().c_str());
-				if (!m_ScriptInterface->LoadGlobalScriptFile(m_ScriptPath))
-				{
-					LOGERROR(L"CMapGeneratorWorker::Run: Failed to load RMS '%ls'", m_ScriptPath.string().c_str());
-					ret = false;
-				}
-			}
-		}
+	// Copy settings to global variable
+	JS::RootedValue global(cx, m_ScriptInterface->GetGlobalObject());
+	if (!m_ScriptInterface->SetProperty(global, "g_MapSettings", settingsVal))
+	{
+		LOGERROR(L"CMapGeneratorWorker::Run: Failed to define g_MapSettings");
+		return false;
+	}
+
+	// Load RMS
+	LOGMESSAGE(L"Loading RMS '%ls'", m_ScriptPath.string().c_str());
+	if (!m_ScriptInterface->LoadGlobalScriptFile(m_ScriptPath))
+	{
+		LOGERROR(L"CMapGeneratorWorker::Run: Failed to load RMS '%ls'", m_ScriptPath.string().c_str());
+		return false;
 	}
 	
 	// We must destroy the ScriptInterface in the same thread because the JSAPI requires that!
 	SAFE_DELETE(m_ScriptInterface);
 
-	return ret;
+	return true;
 }
 
 int CMapGeneratorWorker::GetProgress()

source/ps/GameSetup/GameSetup.cpp
@@ -1020,12 +1020,14 @@ void InitGraphics(const CmdLineArgs& args, int flags)
 		{
 			const bool setup_gui = ((flags & INIT_NO_GUI) == 0);
 			// We only want to display the splash screen at startup
-			CScriptValRooted data;
+			shared_ptr<ScriptInterface> scriptInterface = g_GUI->GetScriptInterface();
+			JSContext* cx = scriptInterface->GetContext();
+			JSAutoRequest rq(cx);
+			JS::RootedValue data(cx);
 			if (g_GUI)
 			{
-				shared_ptr<ScriptInterface> scriptInterface = g_GUI->GetScriptInterface();
-				scriptInterface->Eval("({})", data);
-				scriptInterface->SetProperty(data.get(), "isStartup", true);
+				scriptInterface->Eval("({})", &data);
+				scriptInterface->SetProperty(data, "isStartup", true);
 			}
 			InitPs(setup_gui, L"page_pregame.xml", g_GUI->GetScriptInterface().get(), data.get());
 		}
@@ -1142,13 +1144,15 @@ bool Autostart(const CmdLineArgs& args)
 	g_Game = new CGame();
 
 	ScriptInterface& scriptInterface = g_Game->GetSimulation2()->GetScriptInterface();
+	JSContext* cx = scriptInterface.GetContext();
+	JSAutoRequest rq(cx);
 
-	CScriptValRooted attrs;
-	scriptInterface.Eval("({})", attrs);
-	CScriptVal settings;
-	scriptInterface.Eval("({})", settings);
-	CScriptVal playerData;
-	scriptInterface.Eval("([])", playerData);
+	JS::RootedValue attrs(cx);
+	scriptInterface.Eval("({})", &attrs);
+	JS::RootedValue settings(cx);
+	scriptInterface.Eval("({})", &settings);
+	JS::RootedValue playerData(cx);
+	scriptInterface.Eval("([])", &playerData);
 
 	// The directory in front of the actual map name indicates which type
 	// of map is being loaded. Drawback of this approach is the association
@@ -1180,13 +1184,13 @@ bool Autostart(const CmdLineArgs& args)
 		
 		// Random map definition will be loaded from JSON file, so we need to parse it
 		std::wstring scriptPath = L"maps/" + autoStartName.FromUTF8() + L".json";
-		CScriptValRooted scriptData = scriptInterface.ReadJSONFile(scriptPath);
-		if (!scriptData.undefined() && scriptInterface.GetProperty(scriptData.get(), "settings", settings))
+		JS::RootedValue scriptData(cx, scriptInterface.ReadJSONFile(scriptPath).get());
+		if (!scriptData.isUndefined() && scriptInterface.GetProperty(scriptData, "settings", &settings))
 		{
 			// JSON loaded ok - copy script name over to game attributes
 			std::wstring scriptFile;
-			scriptInterface.GetProperty(settings.get(), "Script", scriptFile);
-			scriptInterface.SetProperty(attrs.get(), "script", scriptFile);				// RMS filename
+			scriptInterface.GetProperty(settings, "Script", scriptFile);
+			scriptInterface.SetProperty(attrs, "script", scriptFile);				// RMS filename
 		}
 		else
 		{
@@ -1203,8 +1207,8 @@ bool Autostart(const CmdLineArgs& args)
 			mapSize = size.ToUInt();
 		}
 
-		scriptInterface.SetProperty(settings.get(), "Seed", seed);									// Random seed
-		scriptInterface.SetProperty(settings.get(), "Size", mapSize);								// Random map size (in patches)
+		scriptInterface.SetProperty(settings, "Seed", seed);									// Random seed
+		scriptInterface.SetProperty(settings, "Size", mapSize);								// Random map size (in patches)
 
 		// Get optional number of players (default 2)
 		size_t numPlayers = 2;
@@ -1217,13 +1221,13 @@ bool Autostart(const CmdLineArgs& args)
 		// Set up player data
 		for (size_t i = 0; i < numPlayers; ++i)
 		{
-			CScriptVal player;
-			scriptInterface.Eval("({})", player);
+			JS::RootedValue player(cx);
+			scriptInterface.Eval("({})", &player);
 
 			// We could load player_defaults.json here, but that would complicate the logic
 			//	even more and autostart is only intended for developers anyway
-			scriptInterface.SetProperty(player.get(), "Civ", std::string("athen"));
-			scriptInterface.SetPropertyInt(playerData.get(), i, player);
+			scriptInterface.SetProperty(player, "Civ", std::string("athen"));
+			scriptInterface.SetPropertyInt(playerData, i, player);
 		}
 		mapType = "random";
 	}
@@ -1255,7 +1259,7 @@ bool Autostart(const CmdLineArgs& args)
 		// ...and initialize the playerData array being edited by
 		// autostart-civ et.al. with the real map data, so sensible values
 		// are always present:
-		scriptInterface.GetProperty(settings.get(), "PlayerData", playerData);
+		scriptInterface.GetProperty(settings, "PlayerData", &playerData);
 		mapType = "skirmish";
 	}
 	if (mapType.empty())
@@ -1263,10 +1267,10 @@ bool Autostart(const CmdLineArgs& args)
 		LOGERROR(L"Unrecognized map type '%ls' detected", mapType.c_str());
 		throw PSERROR_Game_World_MapLoadFailed("Unrecognized map type.\nConsult GameSetup.cpp for the currently supported types.");
 	}
-	scriptInterface.SetProperty(attrs.get(), "mapType", mapType);
-	scriptInterface.SetProperty(attrs.get(), "map", std::string("maps/" + autoStartName));
+	scriptInterface.SetProperty(attrs, "mapType", mapType);
+	scriptInterface.SetProperty(attrs, "map", std::string("maps/" + autoStartName));
 
-	scriptInterface.SetProperty(settings.get(), "mapType", mapType);
+	scriptInterface.SetProperty(settings, "mapType", mapType);
 
 	// Set player data for AIs
 	//		attrs.settings = { PlayerData: [ { AI: ... }, ... ] }:
@@ -1276,18 +1280,18 @@ bool Autostart(const CmdLineArgs& args)
 		for (size_t i = 0; i < aiArgs.size(); ++i)
 		{
 			// Instead of overwriting existing player data, modify the array
-			CScriptVal player;
-			if (!scriptInterface.GetPropertyInt(playerData.get(), i, player) || player.undefined())
+			JS::RootedValue player(cx);
+			if (!scriptInterface.GetPropertyInt(playerData, i, &player) || player.isUndefined())
 			{
-				scriptInterface.Eval("({})", player);
+				scriptInterface.Eval("({})", &player);
 			}
 
 			int playerID = aiArgs[i].BeforeFirst(":").ToInt();
 			CStr name = aiArgs[i].AfterFirst(":");
 
-			scriptInterface.SetProperty(player.get(), "AI", std::string(name));
-			scriptInterface.SetProperty(player.get(), "AIDiff", 2);
-			scriptInterface.SetPropertyInt(playerData.get(), playerID-1, player);
+			scriptInterface.SetProperty(player, "AI", std::string(name));
+			scriptInterface.SetProperty(player, "AIDiff", 2);
+			scriptInterface.SetPropertyInt(playerData, playerID-1, player);
 		}
 	}
 	// Set AI difficulty
@@ -1297,17 +1301,17 @@ bool Autostart(const CmdLineArgs& args)
 		for (size_t i = 0; i < civArgs.size(); ++i)
 		{
 			// Instead of overwriting existing player data, modify the array
-			CScriptVal player;
-			if (!scriptInterface.GetPropertyInt(playerData.get(), i, player) || player.undefined())
+			JS::RootedValue player(cx);
+			if (!scriptInterface.GetPropertyInt(playerData, i, &player) || player.isUndefined())
 			{
-				scriptInterface.Eval("({})", player);
+				scriptInterface.Eval("({})", &player);
 			}
 			
 			int playerID = civArgs[i].BeforeFirst(":").ToInt();
 			int difficulty = civArgs[i].AfterFirst(":").ToInt();
 			
-			scriptInterface.SetProperty(player.get(), "AIDiff", difficulty);
-			scriptInterface.SetPropertyInt(playerData.get(), playerID-1, player);
+			scriptInterface.SetProperty(player, "AIDiff", difficulty);
+			scriptInterface.SetPropertyInt(playerData, playerID-1, player);
 		}
 	}
 	// Set player data for Civs
@@ -1317,29 +1321,29 @@ bool Autostart(const CmdLineArgs& args)
 		for (size_t i = 0; i < civArgs.size(); ++i)
 		{
 			// Instead of overwriting existing player data, modify the array
-			CScriptVal player;
-			if (!scriptInterface.GetPropertyInt(playerData.get(), i, player) || player.undefined())
+			JS::RootedValue player(cx);
+			if (!scriptInterface.GetPropertyInt(playerData, i, &player) || player.isUndefined())
 			{
-				scriptInterface.Eval("({})", player);
+				scriptInterface.Eval("({})", &player);
 			}
 			
 			int playerID = civArgs[i].BeforeFirst(":").ToInt();
 			CStr name = civArgs[i].AfterFirst(":");
 			
-			scriptInterface.SetProperty(player.get(), "Civ", std::string(name));
-			scriptInterface.SetPropertyInt(playerData.get(), playerID-1, player);
+			scriptInterface.SetProperty(player, "Civ", std::string(name));
+			scriptInterface.SetPropertyInt(playerData, playerID-1, player);
 		}
 	}
 
 	// Add player data to map settings
-	scriptInterface.SetProperty(settings.get(), "PlayerData", playerData);
+	scriptInterface.SetProperty(settings, "PlayerData", playerData);
 
 	// Add map settings to game attributes
-	scriptInterface.SetProperty(attrs.get(), "settings", settings);
+	scriptInterface.SetProperty(attrs, "settings", settings);
 
-	CScriptVal mpInitData;
-	scriptInterface.Eval("({isNetworked:true, playerAssignments:{}})", mpInitData);
-	scriptInterface.SetProperty(mpInitData.get(), "attribs", attrs);
+	JS::RootedValue mpInitData(cx);
+	scriptInterface.Eval("({isNetworked:true, playerAssignments:{}})", &mpInitData);
+	scriptInterface.SetProperty(mpInitData, "attribs", attrs);
 
 	// Get optional playername
 	CStrW userName = L"anonymous";
@@ -1388,7 +1392,7 @@ bool Autostart(const CmdLineArgs& args)
 	else
 	{
 		g_Game->SetPlayerID(1);
-		g_Game->StartGame(attrs, "");
+		g_Game->StartGame(CScriptValRooted(cx, attrs), "");
 
 		LDR_NonprogressiveLoad();
 
@@ -1403,6 +1407,11 @@ bool Autostart(const CmdLineArgs& args)
 
 void CancelLoad(const CStrW& message)
 {
+	shared_ptr<ScriptInterface> pScriptInterface = g_GUI->GetActiveGUI()->GetScriptInterface();
+	JSContext* cx = pScriptInterface->GetContext();
+	JSAutoRequest rq(cx);
+	
+	JS::RootedValue global(cx, pScriptInterface->GetGlobalObject());
 	// Cancel loader
 	LDR_Cancel();
 
@@ -1410,8 +1419,8 @@ void CancelLoad(const CStrW& message)
 	// So all GUI pages that load games should include this script
 	if (g_GUI && g_GUI->HasPages())
 	{
-		if (g_GUI->GetActiveGUI()->GetScriptInterface()->HasProperty(g_GUI->GetActiveGUI()->GetGlobalObject(), "cancelOnError" ))
-			g_GUI->GetActiveGUI()->GetScriptInterface()->CallFunctionVoid(g_GUI->GetActiveGUI()->GetGlobalObject(), "cancelOnError", message);
+		if (pScriptInterface->HasProperty(global, "cancelOnError" ))
+			pScriptInterface->CallFunctionVoid(global, "cancelOnError", message);
 	}
 }
 

source/graphics/MapReader.cpp
@@ -386,11 +386,14 @@ PSRETURN CMapSummaryReader::LoadMap(const VfsPath& pathname)
 
 CScriptValRooted CMapSummaryReader::GetMapSettings(ScriptInterface& scriptInterface)
 {
-	CScriptValRooted data;
-	scriptInterface.Eval("({})", data);
+	JSContext* cx = scriptInterface.GetContext();
+	JSAutoRequest rq(cx);
+	
+	JS::RootedValue data(cx);
+	scriptInterface.Eval("({})", &data);
 	if (!m_ScriptSettings.empty())
-		scriptInterface.SetProperty(data.get(), "settings", scriptInterface.ParseJSON(m_ScriptSettings), false);
-	return data;
+		scriptInterface.SetProperty(data, "settings", scriptInterface.ParseJSON(m_ScriptSettings), false);
+	return CScriptValRooted(cx, data);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -1311,7 +1314,9 @@ int CMapReader::GenerateMap()
 int CMapReader::ParseTerrain()
 {
 	TIMER(L"ParseTerrain");
-
+	JSContext* cx = pSimulation2->GetScriptInterface().GetContext();
+	JSAutoRequest rq(cx);
+	
 	// parse terrain from map data
 	//	an error here should stop the loading process
 #define GET_TERRAIN_PROPERTY(val, prop, out)\
@@ -1319,17 +1324,18 @@ int CMapReader::ParseTerrain()
 		{	LOGERROR(L"CMapReader::ParseTerrain() failed to get '%hs' property", #prop);\
 			throw PSERROR_Game_World_MapLoadFailed("Error parsing terrain data.\nCheck application log for details"); }
 
+	JS::RootedValue tmpMapData(cx, m_MapData.get()); // TODO: Check if this temporary root can be removed after SpiderMonkey 31 upgrade 
 	u32 size;
-	GET_TERRAIN_PROPERTY(m_MapData.get(), size, size)
+	GET_TERRAIN_PROPERTY(tmpMapData, size, size)
 
 	m_PatchesPerSide = size / PATCH_SIZE;
 
 	// flat heightmap of u16 data
-	GET_TERRAIN_PROPERTY(m_MapData.get(), height, m_Heightmap)
+	GET_TERRAIN_PROPERTY(tmpMapData, height, m_Heightmap)
 
 	// load textures
 	std::vector<std::string> textureNames;
-	GET_TERRAIN_PROPERTY(m_MapData.get(), textureNames, textureNames)
+	GET_TERRAIN_PROPERTY(tmpMapData, textureNames, textureNames)
 	num_terrain_tex = textureNames.size();
 
 	while (cur_terrain_tex < num_terrain_tex)
@@ -1344,14 +1350,14 @@ int CMapReader::ParseTerrain()
 	// build tile data
 	m_Tiles.resize(SQR(size));
 
-	CScriptValRooted tileData;
-	GET_TERRAIN_PROPERTY(m_MapData.get(), tileData, tileData)
+	JS::RootedValue tileData(cx);
+	GET_TERRAIN_PROPERTY(tmpMapData, tileData, &tileData)
 
 	// parse tile data object into flat arrays
 	std::vector<u16> tileIndex;
 	std::vector<u16> tilePriority;
-	GET_TERRAIN_PROPERTY(tileData.get(), index, tileIndex);
-	GET_TERRAIN_PROPERTY(tileData.get(), priority, tilePriority);
+	GET_TERRAIN_PROPERTY(tileData, index, tileIndex);
+	GET_TERRAIN_PROPERTY(tileData, priority, tilePriority);
 
 	ENSURE(SQR(size) == tileIndex.size() && SQR(size) == tilePriority.size());
 
@@ -1385,11 +1391,15 @@ int CMapReader::ParseTerrain()
 int CMapReader::ParseEntities()
 {
 	TIMER(L"ParseEntities");
+	JSContext* cx = pSimulation2->GetScriptInterface().GetContext();
+	JSAutoRequest rq(cx);
+	
+	JS::RootedValue tmpMapData(cx, m_MapData.get()); // TODO: Check if this temporary root can be removed after SpiderMonkey 31 upgrade 
 
 	// parse entities from map data
 	std::vector<Entity> entities;
 
-	if (!pSimulation2->GetScriptInterface().GetProperty(m_MapData.get(), "entities", entities))
+	if (!pSimulation2->GetScriptInterface().GetProperty(tmpMapData, "entities", entities))
 		LOGWARNING(L"CMapReader::ParseEntities() failed to get 'entities' property");
 
 	CSimulation2& sim = *pSimulation2;
@@ -1448,15 +1458,19 @@ int CMapReader::ParseEntities()
 int CMapReader::ParseEnvironment()
 {
 	// parse environment settings from map data
+	JSContext* cx = pSimulation2->GetScriptInterface().GetContext();
+	JSAutoRequest rq(cx);
+	
+	JS::RootedValue tmpMapData(cx, m_MapData.get()); // TODO: Check if this temporary root can be removed after SpiderMonkey 31 upgrade 
 
 #define GET_ENVIRONMENT_PROPERTY(val, prop, out)\
 	if (!pSimulation2->GetScriptInterface().GetProperty(val, #prop, out))\
 		LOGWARNING(L"CMapReader::ParseEnvironment() failed to get '%hs' property", #prop);
 
-	CScriptValRooted envObj;
-	GET_ENVIRONMENT_PROPERTY(m_MapData.get(), Environment, envObj)
+	JS::RootedValue envObj(cx);
+	GET_ENVIRONMENT_PROPERTY(tmpMapData, Environment, &envObj)
 
-	if (envObj.undefined())
+	if (envObj.isUndefined())
 	{
 		LOGWARNING(L"CMapReader::ParseEnvironment(): Environment settings not found");
 		return 0;
@@ -1467,35 +1481,35 @@ int CMapReader::ParseEnvironment()
 		pPostproc->SetPostEffect(L"default");
 
 	std::wstring skySet;
-	GET_ENVIRONMENT_PROPERTY(envObj.get(), SkySet, skySet)
+	GET_ENVIRONMENT_PROPERTY(envObj, SkySet, skySet)
 	if (pSkyMan)
 		pSkyMan->SetSkySet(skySet);
 
 	CColor sunColor;
-	GET_ENVIRONMENT_PROPERTY(envObj.get(), SunColour, sunColor)
+	GET_ENVIRONMENT_PROPERTY(envObj, SunColour, sunColor)
 	m_LightEnv.m_SunColor = RGBColor(sunColor.r, sunColor.g, sunColor.b);
 
-	GET_ENVIRONMENT_PROPERTY(envObj.get(), SunElevation, m_LightEnv.m_Elevation)
-	GET_ENVIRONMENT_PROPERTY(envObj.get(), SunRotation, m_LightEnv.m_Rotation)
+	GET_ENVIRONMENT_PROPERTY(envObj, SunElevation, m_LightEnv.m_Elevation)
+	GET_ENVIRONMENT_PROPERTY(envObj, SunRotation, m_LightEnv.m_Rotation)
 	
 	CColor terrainAmbientColor;
-	GET_ENVIRONMENT_PROPERTY(envObj.get(), TerrainAmbientColour, terrainAmbientColor)
+	GET_ENVIRONMENT_PROPERTY(envObj, TerrainAmbientColour, terrainAmbientColor)
 	m_LightEnv.m_TerrainAmbientColor = RGBColor(terrainAmbientColor.r, terrainAmbientColor.g, terrainAmbientColor.b);
 
 	CColor unitsAmbientColor;
-	GET_ENVIRONMENT_PROPERTY(envObj.get(), UnitsAmbientColour, unitsAmbientColor)
+	GET_ENVIRONMENT_PROPERTY(envObj, UnitsAmbientColour, unitsAmbientColor)
 	m_LightEnv.m_UnitsAmbientColor = RGBColor(unitsAmbientColor.r, unitsAmbientColor.g, unitsAmbientColor.b);
 
 	// Water properties
-	CScriptValRooted waterObj;
-	GET_ENVIRONMENT_PROPERTY(envObj.get(), Water, waterObj)
+	JS::RootedValue waterObj(cx);
+	GET_ENVIRONMENT_PROPERTY(envObj, Water, &waterObj)
 
-	CScriptValRooted waterBodyObj;
-	GET_ENVIRONMENT_PROPERTY(waterObj.get(), WaterBody, waterBodyObj)
+	JS::RootedValue waterBodyObj(cx);
+	GET_ENVIRONMENT_PROPERTY(waterObj, WaterBody, &waterBodyObj)
 
 	// Water level - necessary
 	float waterHeight;
-	GET_ENVIRONMENT_PROPERTY(waterBodyObj.get(), Height, waterHeight)
+	GET_ENVIRONMENT_PROPERTY(waterBodyObj, Height, waterHeight)
 
 	CmpPtr<ICmpWaterManager> cmpWaterManager(*pSimulation2, SYSTEM_ENTITY);
 	ENSURE(cmpWaterManager);
@@ -1504,39 +1518,39 @@ int CMapReader::ParseEnvironment()
 	// If we have graphics, get rest of settings
 	if (pWaterMan)
 	{
-		GET_ENVIRONMENT_PROPERTY(waterBodyObj.get(), Type, pWaterMan->m_WaterType)
+		GET_ENVIRONMENT_PROPERTY(waterBodyObj, Type, pWaterMan->m_WaterType)
 		if (pWaterMan->m_WaterType == L"default")
 			pWaterMan->m_WaterType = L"ocean";
-		GET_ENVIRONMENT_PROPERTY(waterBodyObj.get(), Colour, pWaterMan->m_WaterColor)
-		GET_ENVIRONMENT_PROPERTY(waterBodyObj.get(), Tint, pWaterMan->m_WaterTint)
-		GET_ENVIRONMENT_PROPERTY(waterBodyObj.get(), Waviness, pWaterMan->m_Waviness)
-		GET_ENVIRONMENT_PROPERTY(waterBodyObj.get(), Murkiness, pWaterMan->m_Murkiness)
-		GET_ENVIRONMENT_PROPERTY(waterBodyObj.get(), WindAngle, pWaterMan->m_WindAngle)
+		GET_ENVIRONMENT_PROPERTY(waterBodyObj, Colour, pWaterMan->m_WaterColor)
+		GET_ENVIRONMENT_PROPERTY(waterBodyObj, Tint, pWaterMan->m_WaterTint)
+		GET_ENVIRONMENT_PROPERTY(waterBodyObj, Waviness, pWaterMan->m_Waviness)
+		GET_ENVIRONMENT_PROPERTY(waterBodyObj, Murkiness, pWaterMan->m_Murkiness)
+		GET_ENVIRONMENT_PROPERTY(waterBodyObj, WindAngle, pWaterMan->m_WindAngle)
 	}
 
-	CScriptValRooted fogObject;
-	GET_ENVIRONMENT_PROPERTY(envObj.get(), Fog, fogObject);
+	JS::RootedValue fogObject(cx);
+	GET_ENVIRONMENT_PROPERTY(envObj, Fog, &fogObject);
 
-	GET_ENVIRONMENT_PROPERTY(fogObject.get(), FogFactor, m_LightEnv.m_FogFactor);
-	GET_ENVIRONMENT_PROPERTY(fogObject.get(), FogThickness, m_LightEnv.m_FogMax);
+	GET_ENVIRONMENT_PROPERTY(fogObject, FogFactor, m_LightEnv.m_FogFactor);
+	GET_ENVIRONMENT_PROPERTY(fogObject, FogThickness, m_LightEnv.m_FogMax);
 
 	CColor fogColor;
-	GET_ENVIRONMENT_PROPERTY(fogObject.get(), FogColor, fogColor);
+	GET_ENVIRONMENT_PROPERTY(fogObject, FogColor, fogColor);
 	m_LightEnv.m_FogColor = RGBColor(fogColor.r, fogColor.g, fogColor.b);
 
-	CScriptValRooted postprocObject;
-	GET_ENVIRONMENT_PROPERTY(envObj.get(), Postproc, postprocObject);
+	JS::RootedValue postprocObject(cx);
+	GET_ENVIRONMENT_PROPERTY(envObj, Postproc, &postprocObject);
 
 	std::wstring postProcEffect;
-	GET_ENVIRONMENT_PROPERTY(postprocObject.get(), PostprocEffect, postProcEffect);
+	GET_ENVIRONMENT_PROPERTY(postprocObject, PostprocEffect, postProcEffect);
 
 	if (pPostproc)
 		pPostproc->SetPostEffect(postProcEffect);
 
-	GET_ENVIRONMENT_PROPERTY(postprocObject.get(), Brightness, m_LightEnv.m_Brightness);
-	GET_ENVIRONMENT_PROPERTY(postprocObject.get(), Contrast, m_LightEnv.m_Contrast);
-	GET_ENVIRONMENT_PROPERTY(postprocObject.get(), Saturation, m_LightEnv.m_Saturation);
-	GET_ENVIRONMENT_PROPERTY(postprocObject.get(), Bloom, m_LightEnv.m_Bloom);
+	GET_ENVIRONMENT_PROPERTY(postprocObject, Brightness, m_LightEnv.m_Brightness);
+	GET_ENVIRONMENT_PROPERTY(postprocObject, Contrast, m_LightEnv.m_Contrast);
+	GET_ENVIRONMENT_PROPERTY(postprocObject, Saturation, m_LightEnv.m_Saturation);
+	GET_ENVIRONMENT_PROPERTY(postprocObject, Bloom, m_LightEnv.m_Bloom);
 
 	m_LightEnv.CalculateSunDirection();
 
@@ -1547,6 +1561,8 @@ int CMapReader::ParseEnvironment()
 
 int CMapReader::ParseCamera()
 {
+	JSContext* cx = pSimulation2->GetScriptInterface().GetContext();
+	JSAutoRequest rq(cx);
 	// parse camera settings from map data
 	// defaults if we don't find player starting camera
 	float declination = DEGTORAD(30.f), rotation = DEGTORAD(-45.f);
@@ -1556,17 +1572,18 @@ int CMapReader::ParseCamera()
 	if (!pSimulation2->GetScriptInterface().GetProperty(val, #prop, out))\
 		LOGWARNING(L"CMapReader::ParseCamera() failed to get '%hs' property", #prop);
 
-	CScriptValRooted cameraObj;
-	GET_CAMERA_PROPERTY(m_MapData.get(), Camera, cameraObj)
+	JS::RootedValue tmpMapData(cx, m_MapData.get()); // TODO: Check if this temporary root can be removed after SpiderMonkey 31 upgrade 
+	JS::RootedValue cameraObj(cx);
+	GET_CAMERA_PROPERTY(tmpMapData, Camera, &cameraObj)
 
-	if (!cameraObj.undefined())
+	if (!cameraObj.isUndefined())
 	{	// If camera property exists, read values
 		CFixedVector3D pos;
-		GET_CAMERA_PROPERTY(cameraObj.get(), Position, pos)
+		GET_CAMERA_PROPERTY(cameraObj, Position, pos)
 		translation = pos;
 
-		GET_CAMERA_PROPERTY(cameraObj.get(), Rotation, rotation)
-		GET_CAMERA_PROPERTY(cameraObj.get(), Declination, declination)
+		GET_CAMERA_PROPERTY(cameraObj, Rotation, rotation)
+		GET_CAMERA_PROPERTY(cameraObj, Declination, declination)
 	}
 #undef GET_CAMERA_PROPERTY
 

source/gui/GUIManager.cpp
@@ -109,38 +109,41 @@ void CGUIManager::PopPageCB(shared_ptr<ScriptInterface::StructuredClone> args)
 	PopPage();
 	
 	shared_ptr<ScriptInterface> scriptInterface = m_PageStack.back().gui->GetScriptInterface();
-	CScriptVal initDataVal;
+	JSContext* cx = scriptInterface->GetContext();
+	JS::RootedValue initDataVal(cx);
+	
 	if (initDataClone)
-		initDataVal = scriptInterface->ReadStructuredClone(initDataClone);
+		initDataVal.set(scriptInterface->ReadStructuredClone(initDataClone));
 	else
 	{
 		LOGERROR(L"Called PopPageCB when initData (which should contain the callback function name) isn't set!");
 		return;
 	}
 	
-	if (!scriptInterface->HasProperty(initDataVal.get(), "callback"))
+	if (!scriptInterface->HasProperty(initDataVal, "callback"))
 	{
 		LOGERROR(L"Called PopPageCB when the callback function name isn't set!");
 		return;
 	}
 	
 	std::string callback;
-	if (!scriptInterface->GetProperty(initDataVal.get(), "callback", callback))
+	if (!scriptInterface->GetProperty(initDataVal, "callback", callback))
 	{
 		LOGERROR(L"Failed to get the callback property as a string from initData in PopPageCB!");
 		return;
 	}
 	
-	if (!scriptInterface->HasProperty(scriptInterface->GetGlobalObject(), callback.c_str()))
+	JS::RootedValue global(cx, scriptInterface->GetGlobalObject());
+	if (!scriptInterface->HasProperty(global, callback.c_str()))
 	{
 		LOGERROR(L"The specified callback function %hs does not exist in the page %ls", callback.c_str(), m_PageStack.back().name.c_str());
 		return;
 	}
 
-	CScriptVal argVal;
+	JS::RootedValue argVal(cx);
 	if (args)
-		argVal = scriptInterface->ReadStructuredClone(args);
-	if (!scriptInterface->CallFunctionVoid(scriptInterface->GetGlobalObject(), callback.c_str(), argVal))
+		argVal.set(scriptInterface->ReadStructuredClone(args));
+	if (!scriptInterface->CallFunctionVoid(global, callback.c_str(), argVal))
 	{
 		LOGERROR(L"Failed to call the callback function %hs in the page %ls", callback.c_str(), m_PageStack.back().name.c_str());
 		return;
@@ -149,17 +152,19 @@ void CGUIManager::PopPageCB(shared_ptr<ScriptInterface::StructuredClone> args)
 
 void CGUIManager::DisplayMessageBox(int width, int height, const CStrW& title, const CStrW& message)
 {
+	JSContext* cx = m_ScriptInterface->GetContext();
+	JSAutoRequest rq(cx);
 	// Set up scripted init data for the standard message box window
-	CScriptValRooted data;
-	m_ScriptInterface->Eval("({})", data);
-	m_ScriptInterface->SetProperty(data.get(), "width", width, false);
-	m_ScriptInterface->SetProperty(data.get(), "height", height, false);
-	m_ScriptInterface->SetProperty(data.get(), "mode", 2, false);
-	m_ScriptInterface->SetProperty(data.get(), "title", std::wstring(title), false);
-	m_ScriptInterface->SetProperty(data.get(), "message", std::wstring(message), false);
+	JS::RootedValue data(cx);
+	m_ScriptInterface->Eval("({})", &data);
+	m_ScriptInterface->SetProperty(data, "width", width, false);
+	m_ScriptInterface->SetProperty(data, "height", height, false);
+	m_ScriptInterface->SetProperty(data, "mode", 2, false);
+	m_ScriptInterface->SetProperty(data, "title", std::wstring(title), false);
+	m_ScriptInterface->SetProperty(data, "message", std::wstring(message), false);
 
 	// Display the message box
-	PushPage(L"page_msgbox.xml", m_ScriptInterface->WriteStructuredClone(data.get()));
+	PushPage(L"page_msgbox.xml", m_ScriptInterface->WriteStructuredClone(data));
 }
 
 void CGUIManager::LoadPage(SGUIPage& page)

commit dd02742f0f4642ec5452c2f00001bb4adead0191

Fixes small memory leak introduced in r15568.
Thanks to leper for pointing it out.


git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@15569 3db68df2-c116-0410-a063-a993310a9797

source/graphics/MapGenerator.cpp
@@ -82,6 +82,13 @@ bool CMapGeneratorWorker::Run()
 	JSContext* cx = m_ScriptInterface->GetContext();
 	JSAutoRequest rq(cx);
 	
+	// We must destroy the ScriptInterface in the same thread because the JSAPI requires that!
+	struct AutoFree {
+		AutoFree(ScriptInterface* p) : m_p(p) {}
+		~AutoFree() { SAFE_DELETE(m_p); }
+		ScriptInterface* m_p;
+	} autoFree(m_ScriptInterface);
+	
 	m_ScriptInterface->SetCallbackData(static_cast<void*> (this));
 
 	// Replace RNG with a seeded deterministic function
@@ -131,9 +138,6 @@ bool CMapGeneratorWorker::Run()
 		LOGERROR(L"CMapGeneratorWorker::Run: Failed to load RMS '%ls'", m_ScriptPath.string().c_str());
 		return false;
 	}
-	
-	// We must destroy the ScriptInterface in the same thread because the JSAPI requires that!
-	SAFE_DELETE(m_ScriptInterface);
 
 	return true;
 }

source/ps/GameSetup/GameSetup.cpp
@@ -1207,8 +1207,8 @@ bool Autostart(const CmdLineArgs& args)
 			mapSize = size.ToUInt();
 		}
 
-		scriptInterface.SetProperty(settings, "Seed", seed);									// Random seed
-		scriptInterface.SetProperty(settings, "Size", mapSize);								// Random map size (in patches)
+		scriptInterface.SetProperty(settings, "Seed", seed);		// Random seed
+		scriptInterface.SetProperty(settings, "Size", mapSize);		// Random map size (in patches)
 
 		// Get optional number of players (default 2)
 		size_t numPlayers = 2;