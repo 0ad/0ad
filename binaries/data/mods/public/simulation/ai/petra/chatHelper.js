839e71bba4273205403527d28634395deb8e1bad
mimo <mimo@3db68df2-c116-0410-a063-a993310a9797>
2014-10-14 19:43:19 +0000

    internationalization of ai chat messages
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@15866 3db68df2-c116-0410-a063-a993310a9797

---
+++ binaries/data/mods/public/simulation/ai/petra/chatHelper.js
@@ -0,0 +1,31 @@
+var PETRA = function(m)
+{
+
+m.chatLaunchAttack = function(gameState, player)
+{
+	var name = gameState.sharedScript.playersData[player].name;
+	var proba = Math.random();
+	if (proba < 0.5)
+		var message = "/team " + markForTranslation("I am launching an attack against %(name)s.");
+	else
+		var message = "/team " + markForTranslation("I have just sent an army against %(name)s.");
+
+	var chat = { "type": "aichat", "message": message, "translateMessage": true, "translateParameters": ["name"], "parameters": { "name": name } };
+	Engine.PostCommand(PlayerID, chat);
+};
+
+m.chatSentTribute = function(gameState, player)
+{
+	var name = gameState.sharedScript.playersData[player].name;
+	var proba = Math.random();
+	if (proba < 0.5)
+		var message = "/team " + markForTranslation("Here is a gift for %(name)s, make a good use of it.");
+	else
+		var message = "/team " + markForTranslation("I see you are in a bad situation %(name)s, hope this will help."); 
+
+	var chat = { "type": "aichat", "message": message, "translateMessage": true, "translateParameters": ["name"], "parameters": { "name": name } };
+	Engine.PostCommand(PlayerID, chat);
+};
+
+return m;
+}(PETRA);
