b3f416f683fd1318fac0d07e8272eedc2ea64cfe
leper <leper@3db68df2-c116-0410-a063-a993310a9797>
2014-10-27 03:09:36 +0000

    Convert path separator after building the full path.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@15908 3db68df2-c116-0410-a063-a993310a9797

--- source/tools/i18n/extractors/extractors.py
+++ source/tools/i18n/extractors/extractors.py
@@ -79,7 +79,7 @@ class Extractor(object):
             folders.sort()
             filenames.sort()
             for filename in filenames:
-                filename = os.path.relpath(os.path.join(root, filename).replace(os.sep, '/'), self.directoryPath)
+                filename = os.path.relpath(os.path.join(root, filename), self.directoryPath).replace(os.sep, '/')
                 for filemask in self.excludeMasks:
                     if pathmatch(filemask, filename):
                         break
