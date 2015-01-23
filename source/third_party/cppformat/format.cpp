062de53845ace7f62f8031b756f0801fa5360181
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2015-01-23 21:15:48 +0000

    cppformat: Fix Android build.
    The NDK only exposes the BSD-style strerror_r, not the GNU-style, so select the appropriate code path.
    
    
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@16208 3db68df2-c116-0410-a063-a993310a9797

--- source/third_party/cppformat/format.cpp
+++ source/third_party/cppformat/format.cpp
@@ -431,7 +431,7 @@ int fmt::internal::safe_strerror(
     int error_code, char *&buffer, std::size_t buffer_size) FMT_NOEXCEPT(true) {
   assert(buffer != 0 && buffer_size != 0);
   int result = 0;
-#ifdef _GNU_SOURCE
+#if defined(_GNU_SOURCE) && !defined(__BIONIC__)
   char *message = strerror_r(error_code, buffer, buffer_size);
   // If the buffer is full then the message is probably truncated.
   if (message == buffer && strlen(buffer) == buffer_size - 1)
