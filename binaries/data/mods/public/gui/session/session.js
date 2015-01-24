1f2f96a3eefff1dbef25cb34dfbd427f9bdaf114
philip <philip@3db68df2-c116-0410-a063-a993310a9797>
2015-01-24 14:29:05 +0000

    Disable online reporting of profiler data.
    Nobody ever looks at the data, and it takes up a lot of server disk space. The feature should probably be restored in the future once we've fixed all the obvious performance problems, since a wider range of data lets us search for system-dependent issues (e.g. some GL drivers are particularly slow with particular features), but it's not worthwhile now.
    
    
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@16213 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/session/session.js
+++ binaries/data/mods/public/gui/session/session.js
@@ -237,8 +237,11 @@ function init(initData, hotloadData)
 	// the initial camera view) and a minute (when the profiler will
 	// have settled down if framerates as very low), to give some
 	// extremely rough indications of performance
-	setTimeout(function() { reportPerformance(5); }, 5000);
-	setTimeout(function() { reportPerformance(60); }, 60000);
+	//
+	// DISABLED: this information isn't currently useful for anything much,
+	// and it generates a massive amount of data to transmit and store
+	//setTimeout(function() { reportPerformance(5); }, 5000);
+	//setTimeout(function() { reportPerformance(60); }, 60000);
 }
 
 function selectViewPlayer(playerID)
