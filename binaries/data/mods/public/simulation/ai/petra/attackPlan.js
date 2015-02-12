7d91984080dfc2cdc6abc73421793312e7a66083
mimo <mimo@3db68df2-c116-0410-a063-a993310a9797>
2015-02-12 21:04:29 +0000

    Petra: really fix the bug supposed to be fixed in r16317
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@16327 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/ai/petra/attackPlan.js
+++ binaries/data/mods/public/simulation/ai/petra/attackPlan.js
@@ -475,7 +475,6 @@ m.AttackPlan.prototype.updatePreparation = function(gameState, events)
 	{
 		if (this.canStart())
 		{
-			this.removeTrainingUnitsFromPlan(gameState);
 			this.queue.empty();
 			this.queueChamp.empty();
 			this.queueSiege.empty();
@@ -1695,7 +1694,6 @@ m.AttackPlan.prototype.Abort = function(gameState)
 	for (let unitCat in this.unitStat)
 		this.unit[unitCat].unregister();
 
-	this.removeTrainingUnitsFromPlan(gameState);
 	gameState.ai.queueManager.removeQueue("plan_" + this.name);
 	gameState.ai.queueManager.removeQueue("plan_" + this.name + "_champ");
 	gameState.ai.queueManager.removeQueue("plan_" + this.name + "_siege");
@@ -1786,14 +1784,6 @@ m.AttackPlan.prototype.debugAttack = function()
 		API3.warn(unitCat + " num=" + this.unit[unitCat].length + " min=" + Unit["minSize"] + " need=" + Unit["targetSize"]);
 	}
 	API3.warn("------------------------------");
-};
-
-m.AttackPlan.prototype.removeTrainingUnitsFromPlan = function(gameState)
-{
-	for (let trainer of gameState.getOwnTrainingFacilities().values())
-		for (let item of trainer.trainingQueue())
-			if (item.metadata && item.metadata.plan && item.metadata.plan == this.name)
-				item.metadata.plan = undefined;
 };
 
 m.AttackPlan.prototype.Serialize = function()
