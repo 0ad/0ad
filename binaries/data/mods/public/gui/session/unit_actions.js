35d902031a4379e0b5d174e0e70133601edac6b9
leper <leper@3db68df2-c116-0410-a063-a993310a9797>
2015-04-22 22:10:18 +0000

    Pass some properties as null instead of undefined to circumvent serializer limitations. Based on patch by elexis. Fixes #3187.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@16566 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/session/unit_actions.js
+++ binaries/data/mods/public/gui/session/unit_actions.js
@@ -222,7 +222,7 @@ var unitActions =
 				return false;
 			if (getActionInfo("repair", target).possible)
 				return {"type": "repair", "cursor": "action-repair", "target": target};
-			return {"type": "none", "cursor": "action-repair-disabled", "target": undefined};
+			return {"type": "none", "cursor": "action-repair-disabled", "target": null};
 		},
 		"actionCheck": function(target)
 		{
@@ -295,7 +295,7 @@ var unitActions =
 	{
 		"execute": function(target, action, selection, queued)
 		{
-			Engine.PostNetworkCommand({"type": "setup-trade-route", "entities": selection, "target": action.target, "source": undefined, "route": undefined, "queued": queued});
+			Engine.PostNetworkCommand({"type": "setup-trade-route", "entities": selection, "target": action.target, "source": null, "route": null, "queued": queued});
 			Engine.GuiInterfaceCall("PlaySound", { "name": "order_trade", "entity": selection[0] });
 			return true;
 		},
@@ -390,7 +390,7 @@ var unitActions =
 			var actionInfo =  getActionInfo("garrison", target);
 			if (actionInfo.possible)
 				return {"type": "garrison", "cursor": "action-garrison", "tooltip": actionInfo.tooltip, "target": target};
-			return {"type": "none", "cursor": "action-garrison-disabled", "target": undefined};
+			return {"type": "none", "cursor": "action-garrison-disabled", "target": null};
 		},
 		"hotkeyActionCheck": function(target)
 		{
@@ -428,7 +428,7 @@ var unitActions =
 				return false;
 			if (getActionInfo("guard", target).possible)
 				return {"type": "guard", "cursor": "action-guard", "target": target};
-			return {"type": "none", "cursor": "action-guard-disabled", "target": undefined};
+			return {"type": "none", "cursor": "action-guard-disabled", "target": null};
 		},
 		"hotkeyActionCheck": function(target)
 		{
