3aa84113c5835ce4ff6501324ce6f7b0788e89fc
sanderd17 <sanderd17@3db68df2-c116-0410-a063-a993310a9797>
2015-05-02 20:01:03 +0000

    Flip aura icons correctly
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@16612 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/StatusBars.js
+++ binaries/data/mods/public/simulation/components/StatusBars.js
@@ -117,8 +117,8 @@ StatusBars.prototype.AddAuraIcons = function(cmpOverlayRenderer, yoffset)
 	{ 
 		cmpOverlayRenderer.AddSprite( 
 			icon, 
-			{ "x": xoffset - iconSize/2, "y": yoffset }, 
 			{ "x": xoffset + iconSize/2, "y": yoffset + iconSize }, 
+			{ "x": xoffset - iconSize/2, "y": yoffset }, 
 			offset
 		); 
 		xoffset += iconSize * 1.2;
