339af096cf210959c787625412f20e8bfba859ef
sanderd17 <sanderd17@3db68df2-c116-0410-a063-a993310a9797>
2015-06-04 13:55:45 +0000

    Split up different status bars for even easier moddability + add a capture bar (thanks again to Vladislav for making this possible). Refs #3233
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@16716 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/StatusBars.js
+++ binaries/data/mods/public/simulation/components/StatusBars.js
@@ -13,6 +13,22 @@ StatusBars.prototype.Schema =
 		"<data type='decimal'/>" +
 	"</element>";
 
+/**
+ * For every sprite, the code will call their "Add" method when regenerating
+ * the sprites. Every sprite adder should return the height it needs.
+ * 
+ * Modders who need extra sprites can just modify this array, and
+ * provide the right methods.
+ */
+StatusBars.prototype.Sprites =
+	[
+		"PackBar",
+		"ResourceSupplyBar",
+		"CaptureBar",
+		"HealthBar",
+		"AuraIcons",
+	];
+
 StatusBars.prototype.Init = function()
 {
 	this.enabled = false;
@@ -67,6 +83,12 @@ StatusBars.prototype.OnHealthChanged = function(msg)
 		this.RegenerateSprites();
 };
 
+StatusBars.prototype.OnCapturePointsChanged = function(msg)
+{
+	if (this.enabled)
+		this.RegenerateSprites();
+};
+
 StatusBars.prototype.OnResourceSupplyChanged = function(msg)
 {
 	if (this.enabled)
@@ -85,20 +107,137 @@ StatusBars.prototype.RegenerateSprites = function()
 	cmpOverlayRenderer.Reset();
 
 	let yoffset = 0;
-	if (this.enabled)
-		yoffset = this.AddBars(cmpOverlayRenderer, yoffset);
-	yoffset = this.AddAuraIcons(cmpOverlayRenderer, yoffset);
-	return yoffset;
+	for (let sprite of this.Sprites)
+		yoffset += this["Add"+sprite](cmpOverlayRenderer, yoffset);
 };
 
 // Internal helper functions
+/**
+ * Generic piece of code to add a bar
+ */
+StatusBars.prototype.AddBar = function(cmpOverlayRenderer, yoffset, type, amount)
+{
+	// Size of health bar (in world-space units)
+	let width = +this.template.BarWidth;
+	let height = +this.template.BarHeight;
+
+	// World-space offset from the unit's position
+	let offset = { "x": 0, "y": +this.template.HeightOffset, "z": 0 };
+
+	// background
+	cmpOverlayRenderer.AddSprite(
+		"art/textures/ui/session/icons/"+type+"_bg.png",
+		{ "x": -width/2, "y": yoffset },
+		{ "x": width/2, "y": height + yoffset},
+		offset,
+		NATURAL_COLOR
+	);
+
+	// foreground
+	cmpOverlayRenderer.AddSprite(
+		"art/textures/ui/session/icons/"+type+"_fg.png",
+		{ "x": -width/2, "y": yoffset},
+		{ "x": width*(amount - 0.5), "y": height + yoffset},
+		offset,
+		NATURAL_COLOR
+	);
+
+	return height * 1.2;
+};
+
+StatusBars.prototype.AddPackBar = function(cmpOverlayRenderer, yoffset)
+{
+	if (!this.enabled)
+		return 0;
+
+	let cmpPack = Engine.QueryInterface(this.entity, IID_Pack);
+	if (!cmpPack || !cmpPack.IsPacking())
+		return 0;
+
+	return this.AddBar(cmpOverlayRenderer, yoffset, "pack", cmpPack.GetProgress());
+};
+
+StatusBars.prototype.AddHealthBar = function(cmpOverlayRenderer, yoffset)
+{
+	if (!this.enabled)
+		return 0;
+
+	let cmpHealth = QueryMiragedInterface(this.entity, IID_Health);
+	if (!cmpHealth || !cmpHealth.GetHitpoints() > 0)
+		return 0;
+
+	return this.AddBar(cmpOverlayRenderer, yoffset, "health", cmpHealth.GetHitpoints() / cmpHealth.GetMaxHitpoints()
+	);
+}
+
+StatusBars.prototype.AddResourceSupplyBar = function(cmpOverlayRenderer, yoffset)
+{
+	if (!this.enabled)
+		return 0;
+
+	let cmpResourceSupply = QueryMiragedInterface(this.entity, IID_ResourceSupply);
+	if (!cmpResourceSupply)
+		return 0;
+	let value = cmpResourceSupply.IsInfinite() ? 1 : cmpResourceSupply.GetCurrentAmount() / cmpResourceSupply.GetMaxAmount()
+	return this.AddBar(cmpOverlayRenderer, yoffset, "supply", value);
+};
+
+StatusBars.prototype.AddCaptureBar = function(cmpOverlayRenderer, yoffset)
+{
+	if (!this.enabled)
+		return 0;
+
+	let cmpCapturable = QueryMiragedInterface(this.entity, IID_Capturable);
+	if (!cmpCapturable)
+		return 0;
+
+	let cmpOwnership = QueryMiragedInterface(this.entity, IID_Ownership);
+	if (!cmpOwnership)
+		return 0;
+
+	let owner = cmpOwnership.GetOwner();
+	let cp = cmpCapturable.GetCapturePoints()
+
+	// Size of health bar (in world-space units)
+	let width = +this.template.BarWidth;
+	let height = +this.template.BarHeight;
+
+	// World-space offset from the unit's position
+	let offset = { "x": 0, "y": +this.template.HeightOffset, "z": 0 };
+
+	function setCaptureBarPart(playerID, startSize)
+	{
+		let c = QueryPlayerIDInterface(playerID).GetColor();
+		let strColor = (c.r * 255) + " " + (c.g * 255) + " " + (c.b * 255) + " 255";
+		let size = width * cp[playerID] / cmpCapturable.GetMaxCapturePoints();
+
+		cmpOverlayRenderer.AddSprite(
+			"art/textures/ui/session/icons/capture_bar.png",
+			{ "x": startSize, "y": yoffset },
+			{ "x": startSize + size, "y": height + yoffset},
+			offset,
+			strColor
+		);
+
+		return size;
+	};
+
+	// first handle the owner's points, to keep those points on the left for clarity
+	let size = setCaptureBarPart(owner, -width / 2);
+	for (let i in cp)
+		if (i != owner && cp[i] > 0)
+			size += setCaptureBarPart(i, size);
+
+	return height * 1.2;
+}
+
 StatusBars.prototype.AddAuraIcons = function(cmpOverlayRenderer, yoffset)
 {
 	let cmpGuiInterface = Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface);
 	let sources = cmpGuiInterface.GetEntitiesWithStatusBars().filter(e => this.auraSources[e] && this.auraSources[e].length);
 
 	if (!sources.length)
-		return yoffset;
+		return 0;
 
 	let iconSet = new Set();
 	for (let ent of sources)
@@ -111,7 +250,7 @@ StatusBars.prototype.AddAuraIcons = function(cmpOverlayRenderer, yoffset)
 	}
 
 	// World-space offset from the unit's position
-	var offset = { "x": 0, "y": +this.template.HeightOffset, "z": 0 };
+	let offset = { "x": 0, "y": +this.template.HeightOffset + yoffset, "z": 0 };
 
 	let iconSize = +this.template.BarWidth / 2; 
 	let xoffset = -iconSize * (iconSet.size - 1) * 0.6
@@ -120,87 +259,16 @@ StatusBars.prototype.AddAuraIcons = function(cmpOverlayRenderer, yoffset)
 		cmpOverlayRenderer.AddSprite( 
 			icon, 
 			{ "x": xoffset - iconSize/2, "y": yoffset }, 
-			{ "x": xoffset + iconSize/2, "y": yoffset + iconSize }, 
+			{ "x": xoffset + iconSize/2, "y": iconSize + yoffset }, 
 			offset,
 			NATURAL_COLOR
 		); 
 		xoffset += iconSize * 1.2;
 	} 
 
-	return yoffset + iconSize + this.template.BarHeight / 2;
+	return iconSize + this.template.BarHeight / 2;
 };
 
-StatusBars.prototype.AddBars = function(cmpOverlayRenderer, yoffset)
-{
-	// Size of health bar (in world-space units)
-	var width = +this.template.BarWidth;
-	var height = +this.template.BarHeight;
 
-	// World-space offset from the unit's position
-	var offset = { "x": 0, "y": +this.template.HeightOffset, "z": 0 };
-
-	var AddBar = function(type, amount)
-	{
-		cmpOverlayRenderer.AddSprite(
-			"art/textures/ui/session/icons/"+type+"_bg.png",
-			{ "x": -width/2, "y":yoffset },
-			{ "x": width/2, "y": height + yoffset },
-			offset,
-			NATURAL_COLOR
-		);
-
-		cmpOverlayRenderer.AddSprite(
-			"art/textures/ui/session/icons/"+type+"_fg.png",
-			{ "x": -width/2, "y": yoffset },
-			{ "x": width*(amount - 0.5), "y": height + yoffset },
-			offset,
-			NATURAL_COLOR
-		);
-
-		yoffset += height * 1.2;
-	};
-
-	var cmpPack = Engine.QueryInterface(this.entity, IID_Pack);
-	if (cmpPack && cmpPack.IsPacking())
-		AddBar("pack", cmpPack.GetProgress());
-
-	var cmpHealth = QueryMiragedInterface(this.entity, IID_Health);
-	if (cmpHealth && cmpHealth.GetHitpoints() > 0)
-		AddBar("health", cmpHealth.GetHitpoints() / cmpHealth.GetMaxHitpoints());
-
-	var cmpResourceSupply = QueryMiragedInterface(this.entity, IID_ResourceSupply);
-	if (cmpResourceSupply)
-		AddBar("supply", cmpResourceSupply.IsInfinite() ? 1 : cmpResourceSupply.GetCurrentAmount() / cmpResourceSupply.GetMaxAmount());
-
-	return yoffset;
-	/*
-	// Rank icon disabled for now - see discussion around
-	// http://www.wildfiregames.com/forum/index.php?s=&showtopic=13608&view=findpost&p=212154
-
-	var cmpIdentity = Engine.QueryInterface(this.entity, IID_Identity);
-	if (cmpIdentity)
-	{
-		var rank = cmpIdentity.GetRank();
-		if (rank == "Advanced" || rank == "Elite")
-		{
-			var icon;
-			if (rank == "Advanced")
-				icon = "art/textures/ui/session/icons/advanced.dds";
-			else
-				icon = "art/textures/ui/session/icons/elite.dds";
-
-			var rankSize = 0.666;
-			var xoffset = -width/2 - rankSize/2;
-			cmpOverlayRenderer.AddSprite(
-				icon,
-				{ "x": -rankSize/2 + xoffset, "y": -rankSize/2 + yoffset },
-				{ "x": rankSize/2 + xoffset, "y": rankSize/2 + yoffset },
-				offset,
-				"255 255 255 255"
-			);
-		}
-	}
-	*/
-};
 
 Engine.RegisterComponentType(IID_StatusBars, "StatusBars", StatusBars);
