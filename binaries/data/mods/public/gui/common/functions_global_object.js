f02ebc233baa24ab95069517f16ca98ce9193799
mimo <mimo@3db68df2-c116-0410-a063-a993310a9797>
2015-06-07 20:58:51 +0000

    improve display of timing options, patch by Vladislav, fixes #2894
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@16732 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/common/functions_global_object.js
+++ binaries/data/mods/public/gui/common/functions_global_object.js
@@ -74,7 +74,58 @@ function openURL(url)
 	messageBox(600, 200, sprintf(translate("Opening %(url)s\n in default web browser. Please wait...."), { url: url }), translate("Opening page"), 2);
 }
 
-function updateFPS()
-{	
-	Engine.GetGUIObjectByName("fpsCounter").caption = sprintf(translate("FPS: %(fps)s"), { fps: Engine.GetFPS() });
+function updateCounters()
+{
+	var caption = "";
+	var linesCount = 0;
+	var researchCount = 0;
+
+	if (Engine.ConfigDB_GetValue("user", "overlay.fps") === "true")
+	{
+		caption += sprintf(translate("FPS: %(fps)4s"), { fps: Engine.GetFPS() }) + "\n";
+		++linesCount;
+	}
+	if (Engine.ConfigDB_GetValue("user", "overlay.realtime") === "true")
+	{
+		caption += (new Date()).toLocaleTimeString() + "\n";
+		++linesCount;
+	}
+
+	// If game has been started
+	if (typeof g_SimState != "undefined")
+	{
+		if (Engine.ConfigDB_GetValue("user", "gui.session.timeelapsedcounter") === "true")
+		{
+			var currentSpeed = Engine.GetSimRate();
+			if (currentSpeed != 1.0)
+				caption += sprintf(
+					translate("%(time)s (%(speed)sx)"),
+					{
+						time: timeToString(g_SimState.timeElapsed),
+						speed: Engine.FormatDecimalNumberIntoString(currentSpeed)
+					}
+				);
+			else
+				caption += timeToString(g_SimState.timeElapsed);
+			caption += "\n";
+			++linesCount;
+		}
+		if (Engine.ConfigDB_GetValue("user", "gui.session.ceasefirecounter") === "true" && g_SimState.ceasefireActive)
+		{
+			var remainingTimeString = timeToString(g_SimState.ceasefireTimeRemaining);
+			caption += remainingTimeString + "\n";
+			
+			var diplomacyCeasefireCounter = Engine.GetGUIObjectByName("diplomacyCeasefireCounter");
+			diplomacyCeasefireCounter.caption = sprintf(translateWithContext("ceasefire", "Time remaining until ceasefire is over: %(time)s."), {"time": remainingTimeString});
+			++linesCount;
+		}
+
+		g_ResearchListTop = 4;
+		if (linesCount)
+			g_ResearchListTop += 14 * linesCount;
+	}
+
+	var dataCounter = Engine.GetGUIObjectByName("dataCounter");
+	dataCounter.caption = caption;
+	dataCounter.size = sprintf("100%%-100 40 100%%-5 %(bottom)s", { bottom: 40 + 14 * linesCount });
 }
