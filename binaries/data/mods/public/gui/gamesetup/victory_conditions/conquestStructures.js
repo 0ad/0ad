1675235759f186dc99e44bf9f56688dbae1723cd
leper <leper@3db68df2-c116-0410-a063-a993310a9797>
2015-07-21 21:19:41 +0000

    Some string fixes reported by translators.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@16874 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/gamesetup/victory_conditions/conquestStructures.js
+++ binaries/data/mods/public/gui/gamesetup/victory_conditions/conquestStructures.js
@@ -1,5 +1,5 @@
 g_VictoryConditions.conquestStructure = {
-	"name" : translate("Conquest Structure"),
+	"name" : translate("Conquest Structures"),
 	"description" : translate("Destroy all structures of enemies"),
 	"scripts" : ["scripts/ConquestCommon.js", "scripts/TriggerHelper.js", "scripts/ConquestStructures.js"]
 };
