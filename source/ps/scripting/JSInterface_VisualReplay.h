c53a84a604d41b4c78240b05d5af136c99ad5f23
Itms <Itms@3db68df2-c116-0410-a063-a993310a9797>
2015-09-21 17:14:26 +0000

    Fix previous commit, some new files didn't pass through.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@17055 3db68df2-c116-0410-a063-a993310a9797

---
+++ source/ps/scripting/JSInterface_VisualReplay.h
@@ -0,0 +1,35 @@
+/* Copyright (C) 2015 Wildfire Games.
+ * This file is part of 0 A.D.
+ *
+ * 0 A.D. is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * 0 A.D. is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDED_JSI_VISUALREPLAY
+#define INCLUDED_JSI_VISUALREPLAY
+
+#include "ps/VisualReplay.h"
+#include "scriptinterface/ScriptInterface.h"
+
+namespace JSI_VisualReplay
+{
+	void StartVisualReplay(ScriptInterface::CxPrivate* UNUSED(pCxPrivate), CStrW directory);
+	bool DeleteReplay(ScriptInterface::CxPrivate* UNUSED(pCxPrivate), CStrW replayFile);
+	JS::Value GetReplays(ScriptInterface::CxPrivate* pCxPrivate);
+	JS::Value GetReplayAttributes(ScriptInterface::CxPrivate* pCxPrivate, CStrW directoryName);
+	JS::Value GetReplayMetadata(ScriptInterface::CxPrivate* pCxPrivate, CStrW directoryName);
+	void SaveReplayMetadata(ScriptInterface::CxPrivate* UNUSED(pCxPrivate), CStrW data);
+	void RegisterScriptFunctions(ScriptInterface& scriptInterface);
+}
+
+#endif
