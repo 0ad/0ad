20c2fc3ccb2e3210742da0a6f7bfe270cad02a9f
leper <leper@3db68df2-c116-0410-a063-a993310a9797>
2015-11-19 23:09:49 +0000

    Update translations to be included in the release.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@17290 3db68df2-c116-0410-a063-a993310a9797

--- source/tools/dist/remove-incomplete-translations.sh
+++ source/tools/dist/remove-incomplete-translations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Included languages
-LANGS=("ca" "cs" "de" "en_GB" "es" "fr" "gd" "gl" "id" "it" "nl" "pt_PT" "pt_BR" "ru" "tr")
+LANGS=("ca" "cs" "de" "en_GB" "es" "fr" "gd" "gl" "id" "it" "nl" "pl" "pt_PT" "pt_BR" "ru" "sk" "tr")
 
 REGEX=$(printf "\|%s" "${LANGS[@]}")
 REGEX=".*/\("${REGEX:2}"\).[-A-Za-z0-9_.]\+\.po"
