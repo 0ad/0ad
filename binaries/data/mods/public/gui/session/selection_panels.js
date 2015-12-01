f62df0f21c9e885fded99e19c5eecc68b2ec106f
mimo <mimo@3db68df2-c116-0410-a063-a993310a9797>
2015-12-01 19:41:52 +0000

    fix affordable gui of gates
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@17351 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/session/selection_panels.js
+++ binaries/data/mods/public/gui/session/selection_panels.js
@@ -553,7 +553,7 @@ g_SelectionPanels.Gate = {
 	},
 	"setGraphics": function(data)
 	{
-		data.affordableMask.hidden == data.neededResources ? true : false;
+		data.affordableMask.hidden = data.neededResources ? false : true;
 		var gateIcon;
 		if (data.item.gate)
 		{
