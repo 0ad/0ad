369439c39092823293c39335a9922b547ed51d18
elexis <elexis@3db68df2-c116-0410-a063-a993310a9797>
2016-02-09 14:34:59 +0000

    Fix / finish previous commit.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@17747 3db68df2-c116-0410-a063-a993310a9797

--- source/network/NetTurnManager.cpp
+++ source/network/NetTurnManager.cpp
@@ -61,7 +61,7 @@ static std::string Hexify(const std::string& s)
 CNetTurnManager::CNetTurnManager(CSimulation2& simulation, u32 defaultTurnLength, int clientId, IReplayLogger& replay) :
 	m_Simulation2(simulation), m_CurrentTurn(0), m_ReadyTurn(1), m_TurnLength(defaultTurnLength), m_DeltaSimTime(0),
 	m_PlayerId(-1), m_ClientId(clientId), m_HasSyncError(false), m_Replay(replay),
-	m_TimeWarpNumTurns(0), m_FinalTurn(0)
+	m_TimeWarpNumTurns(0), m_FinalTurn(std::numeric_limits<u32>::max())
 {
 	// When we are on turn n, we schedule new commands for n+2.
 	// We know that all other clients have finished scheduling commands for n (else we couldn't have got here).
@@ -105,7 +105,6 @@ bool CNetTurnManager::WillUpdate(float simFrameLength)
 
 bool CNetTurnManager::Update(float simFrameLength, size_t maxTurns)
 {
-
 	if (m_CurrentTurn > m_FinalTurn)
 		return false;
 
