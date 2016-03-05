7eae22ad6004654ca25900d7fdbef670dd9a38cc
Itms <Itms@3db68df2-c116-0410-a063-a993310a9797>
2016-03-05 11:52:08 +0000

    New in-game Credits page, fixes #3744.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@17830 3db68df2-c116-0410-a063-a993310a9797

---
+++ binaries/data/mods/public/gui/credits/credits.js
@@ -0,0 +1,74 @@
+var g_PanelNames = ["special", "programming", "art", "translators", "misc", "donators"];
+var g_PanelTexts = [];
+var g_ActivePanel = -1;
+
+function init()
+{
+	// Load credits list from the disk and parse them
+	for (let name of g_PanelNames)
+		g_PanelTexts.push(parseJSONCredits(name));
+
+	selectPanel(0);
+}
+
+// Run through a "Content" list and parse elements for formatting and translation
+function parseHelper(list)
+{
+	let result = "";
+
+	for (let object of list)
+	{
+		if (object.Title)
+			result += "[font=\"sans-bold-stroke-14\"]" + translate(object.Title) + "\n";
+
+		if (object.Subtitle)
+			result += "[font=\"sans-bold-14\"]" + translate(object.Subtitle) + "\n";
+
+		if (object.List)
+		{
+			for (let element of object.List)
+			{
+				if (element.nick && element.name)
+					result += "[font=\"sans-14\"]" + sprintf(translate("%(nick)s - %(name)s"), { "nick": element.nick, "name": element.name }) + "\n";
+				else if (element.nick)
+					result += "[font=\"sans-14\"]" + element.nick + "\n";
+				else if (element.name)
+					result += "[font=\"sans-14\"]" + element.name + "\n";
+			}
+			result += "\n";
+		}
+
+		if (object.Content)
+			result += "\n" + parseHelper(object.Content) + "\n";
+	}
+
+	return result;
+}
+
+function parseJSONCredits(panelName)
+{
+	let json = Engine.ReadJSONFile("gui/credits/texts/" + panelName + ".json");
+	if (!json || !json.Content)
+	{
+		error("Could not load credits for " + panelName + "!");
+		return "";
+	}
+
+	return parseHelper(json.Content);
+}
+
+function selectPanel(i)
+{
+	if (g_ActivePanel != -1)
+	{
+		let oldPanelButton = Engine.GetGUIObjectByName(g_PanelNames[g_ActivePanel] + "PanelButton");
+		oldPanelButton.sprite = "BackgroundBox";
+	}
+
+	g_ActivePanel = i;
+	let newPanelButton = Engine.GetGUIObjectByName(g_PanelNames[g_ActivePanel] + "PanelButton");
+	newPanelButton.sprite = "ForegroundBox";
+
+	let creditsText = Engine.GetGUIObjectByName("creditsText");
+	creditsText.caption = g_PanelTexts[i];
+}
---
+++ source/tools/i18n/creditTranslators.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python2
+# -*- coding:utf-8 -*-
+#
+# Copyright (C) 2016 Wildfire Games.
+# This file is part of 0 A.D.
+#
+# 0 A.D. is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# 0 A.D. is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This file imports the translators credits located in the public mod GUI files and
+runs through .po files to add possible new translators to it.
+It only appends new people, so it is possible to manually add names in the credits
+file and they won't be overwritten by running this script.
+
+Translatable strings will be extracted from the generated file, so this should be ran
+before updateTemplates.py.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import json, os, glob, re
+
+# Credited languages - Keep in sync with source/tools/dist/remove-incomplete-translations.sh
+langs = {
+    'cs': 'Czech',
+    'de': 'German',
+    'en_GB': 'English (UK)',
+    'es': 'Spanish',
+    'fr': 'French',
+    'gd': 'Gaelic',
+    'gl': 'Galician',
+    'id': 'Indonesian',
+    'it': 'Italian',
+    'nl': 'Dutch',
+    'pl': 'Polish',
+    'pt_PT': 'Portuguese (Portugal)',
+    'pt_BR': 'Portuguese (Brazil)',
+    'ru': 'Russian',
+    'sk': 'Slovak',
+    'tr': 'Turkish'}
+
+root = '../../../'
+
+poLocations = [
+    'binaries/data/l10n/',
+    'binaries/data/mods/public/l10n/',
+    'binaries/data/mods/mod/l10n/']
+
+creditsLocation = 'binaries/data/mods/public/gui/credits/texts/translators.json'
+
+# Load JSON data
+creditsFile = open(root + creditsLocation)
+JSONData = json.load(creditsFile)
+creditsFile.close()
+
+# This dictionnary will hold creditors lists for each language, indexed by code
+langsLists = {}
+
+# Create the new JSON data
+newJSONData = {'Content': []}
+
+# First get the already existing lists. If they correspond with some of the credited languages,
+# add them to the new data after processing, else add them immediately.
+# NB: All of this is quite inefficient
+for element in JSONData['Content']:
+    if 'Title' not in element or element['Title'] not in langs.values():
+        newJSONData['Content'].append(element)
+        continue
+
+    for (langCode, langName) in langs.items():
+        if element['Title'] == langName:
+            langsLists[langCode] = element['List']
+            break
+
+# Now actually go through the list of languages and search the .po files for people
+
+# Prepare some regexes
+commentMatch = re.compile('#.*')
+translatorMatch = re.compile('# ([\w\s]*)(?: <.*>)?, [0-9-]', re.UNICODE)
+
+# Search
+for lang in langs.keys():
+    if lang not in langsLists.keys():
+        langsLists[lang] = []
+
+    for location in poLocations:
+        files = glob.glob(root + location + lang + '.*.po')
+        for file in files:
+            poFile = open(file.replace('\\', '/'))
+            reached = False
+            for line in poFile:
+                line = line.decode('utf8')
+                if reached:
+                    if not commentMatch.match(line):
+                        break
+                    m = translatorMatch.match(line)
+                    if m:
+                        langsLists[lang].append(m.group(1))
+                if line.strip() == '# Translators:':
+                    reached = True
+            poFile.close()
+
+    # Sort and remove duplicates
+    # Sorting should ignore case to have a neat credits list
+    langsLists[lang] = sorted(set(langsLists[lang]), cmp=lambda x,y: cmp(x.lower(), y.lower()))
+
+# Now insert the new data into the new JSON file
+for (langCode, langList) in sorted(langsLists.items()):
+    newJSONData['Content'].append({'Title': langs[langCode], 'List': []})
+    for name in langList:
+        newJSONData['Content'][-1]['List'].append({'name': name})
+
+# Save the JSON data to the credits file
+creditsFile = open(root + creditsLocation, 'w')
+json.dump(newJSONData, creditsFile, indent=4)
+creditsFile.close()
