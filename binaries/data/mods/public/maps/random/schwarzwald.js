0ad/0ad

commit 47f034bfa89969a20290730c0d009b4c96fb8596

Separate global heightmap manipulation functions to a library. Fixes #3764
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@18141 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/maps/random/schwarzwald.js
@@ -1,11 +1,5 @@
-// Created by Niek ten Brinke (aka niektb)
-// Based on FeXoR's Daimond Square Algorithm for heightmap generation and several official random maps
-
-'use strict';
-
 RMS.LoadLibrary('rmgen');
-
-// initialize map
+RMS.LoadLibrary("heightmap");
 
 log('Initializing map...');
 
@@ -178,378 +172,6 @@ HeightPlacer.prototype.place = function (constraint) {
     return ret;
 };
 
-/*
-Takes an array of 2D points (arrays of length 2)
-Returns the order to go through the points for the shortest closed path (array of indices)
-*/
-function getOrderOfPointsForShortestClosePath(points)
-{
-	var order = [];
-	var distances = [];
-
-	if (points.length <= 3)
-	{
-		for (var i = 0; i < points.length; i++)
-			order.push(i);
-		
-		return order;
-	}
-	
-	// Just add the first 3 points
-	var pointsToAdd = deepcopy(points);
-	for (var i = 0; i < min(points.length, 3); i++)
-	{
-		order.push(i);
-		pointsToAdd.shift(i);
-		if (i)
-			distances.push(getDistance(points[order[i]][0], points[order[i]][1], points[order[i - 1]][0], points[order[i - 1]][1]));
-	}
-	distances.push(getDistance(points[order[0]][0], points[order[0]][1], points[order[order.length - 1]][0], points[order[order.length - 1]][1]));
-	
-	// Add remaining points so the path lengthens the least
-	var numPointsToAdd = pointsToAdd.length;
-	for (var i = 0; i < numPointsToAdd; i++)
-	{
-		var indexToAddTo = undefined;
-		var minEnlengthen = Infinity;
-		var minDist1 = 0;
-		var minDist2 = 0;
-
-		for (var k = 0; k < order.length; k++)
-		{
-			var dist1 = getDistance(pointsToAdd[0][0], pointsToAdd[0][1], points[order[k]][0], points[order[k]][1]);
-			var dist2 = getDistance(pointsToAdd[0][0], pointsToAdd[0][1], points[order[(k + 1) % order.length]][0], points[order[(k + 1) % order.length]][1]);
-			var enlengthen = dist1 + dist2 - distances[k];
-
-			if (enlengthen < minEnlengthen)
-			{
-				indexToAddTo = k;
-				minEnlengthen = enlengthen;
-				minDist1 = dist1;
-				minDist2 = dist2;
-			}
-		}
-
-		order.splice(indexToAddTo + 1, 0, i + 3);
-		distances.splice(indexToAddTo, 1, minDist1, minDist2);
-		pointsToAdd.shift();
-	}
-	
-	return order;
-}
-
-
-////////////////
-//
-//  Heightmap functionality
-//
-////////////////
-
-// Some heightmap constants
-const MIN_HEIGHT = - SEA_LEVEL; // -20
-const MAX_HEIGHT = 0xFFFF/HEIGHT_UNITS_PER_METRE - SEA_LEVEL; // A bit smaller than 90
-
-// Get the diferrence between minimum and maxumum height
-function getMinAndMaxHeight(reliefmap)
-{
-	var height = {};
-	height.min = Infinity;
-	height.max = - Infinity;
-
-	for (var x = 0; x < reliefmap.length; x++)
-		for (var y = 0; y < reliefmap[x].length; y++)
-		{
-			if (reliefmap[x][y] < height.min)
-				height.min = reliefmap[x][y];
-			else if (reliefmap[x][y] > height.max)
-				height.max = reliefmap[x][y];
-		}
-
-	return height;
-}
-
-function rescaleHeightmap(minHeight, maxHeight, heightmap)
-{
-	minHeight = (minHeight || - SEA_LEVEL);
-	maxHeight = (maxHeight || 0xFFFF / HEIGHT_UNITS_PER_METRE - SEA_LEVEL);
-	heightmap = (heightmap || g_Map.height);
-	
-	var oldHeightRange = getMinAndMaxHeight(heightmap);
-	var max_x = heightmap.length;
-	var max_y = heightmap[0].length;
-
-	for (var x = 0; x < max_x; x++)
-		for (var y = 0; y < max_y; y++)
-			heightmap[x][y] = minHeight + (heightmap[x][y] - oldHeightRange.min) / (oldHeightRange.max - oldHeightRange.min) * (maxHeight - minHeight);
-}
-
-/*
-getStartLocationsByHeightmap
-Takes
-	hightRange		An associative array with keys 'min' and 'max' each containing a float (the height range start locations are allowed)
-	heightmap		Optional, default is g_Map.height, an array of (map width) arrays of (map depth) floats
-	maxTries		Optional, default is 1000, an integer, how often random player distributions are rolled to be compared
-	minDistToBorder	Optional, default is 20, an integer, how far start locations have to be 
-	numberOfPlayers	Optional, default is getNumPlayers, an integer, how many start locations should be placed
-Returns
-	An array of 2D points (arrays of length 2)
-*/
-function getStartLocationsByHeightmap(hightRange, maxTries, minDistToBorder, numberOfPlayers, heightmap)
-{
-	maxTries = (maxTries || 1000);
-	minDistToBorder = (minDistToBorder || 20);
-	numberOfPlayers = (numberOfPlayers || getNumPlayers());
-	heightmap = (heightmap || g_Map.height);
-	
-	var validStartLocTiles = [];
-
-	for (var x = minDistToBorder; x < heightmap.length - minDistToBorder; x++)
-		for (var y = minDistToBorder; y < heightmap[0].length - minDistToBorder; y++)
-
-			if (heightmap[x][y] > hightRange.min && heightmap[x][y] < hightRange.max) // Has the right hight
-				validStartLocTiles.push([x, y]);
-	
-	var maxMinDist = 0;
-	for (var tries = 0; tries < maxTries; tries++)
-	{
-		var startLoc = [];
-		var minDist = heightmap.length;
-
-		for (var p = 0; p < numberOfPlayers; p++)
-			startLoc.push(validStartLocTiles[randInt(validStartLocTiles.length)]);
-
-		for (var p1 = 0; p1 < numberOfPlayers - 1; p1++)
-			for (var p2 = p1 + 1; p2 < numberOfPlayers; p2++)
-			{
-				var dist = getDistance(startLoc[p1][0], startLoc[p1][1], startLoc[p2][0], startLoc[p2][1]);
-				if (dist < minDist)
-					minDist = dist;
-			}
-
-		if (minDist > maxMinDist)
-		{
-			maxMinDist = minDist;
-			var finalStartLoc = startLoc;
-		}
-	}
-	
-	return finalStartLoc;
-}
-
-/*
-derivateEntitiesByHeight
-Takes
-	hightRange		An associative array with keys 'min' and 'max' each containing a float (the height range start locations are allowed)
-	startLoc		An array of 2D points (arrays of length 2)
-	heightmap		Optional, default is g_Map.height, an array of (map width) arrays of (map depth) floats
-	entityList		Array of entities/actors (strings to be placed with placeObject())
-	maxTries		Optional, default is 1000, an integer, how often random player distributions are rolled to be compared
-	minDistance		Optional, default is 30, an integer, how far start locations have to be away from start locations and the map border
-Returns
-	An array of 2D points (arrays of length 2)
-*/
-function derivateEntitiesByHeight(hightRange, startLoc, entityList, maxTries, minDistance, heightmap)
-{
-	entityList = (entityList || [templateMetalMine, templateStoneMine]);
-	maxTries = (maxTries || 1000);
-	minDistance = (minDistance || 40);
-	heightmap = (heightmap || g_Map.height);
-	
-	var placements = deepcopy(startLoc);
-	var validTiles = [];
-
-	for (var x = minDistance; x < heightmap.length - minDistance; x++)
-		for (var y = minDistance; y < heightmap[0].length - minDistance; y++)
-			if (heightmap[x][y] > hightRange.min && heightmap[x][y] < hightRange.max) // Has the right hight
-				validTiles.push([x, y]);
-	
-	if (!validTiles.length)
-		return;
-	
-	for (var tries = 0; tries < maxTries; tries++)
-	{
-		var tile = validTiles[randInt(validTiles.length)];
-		var isValid = true;
-
-		for (var p = 0; p < placements.length; p++)
-			if (getDistance(placements[p][0], placements[p][1], tile[0], tile[1]) < minDistance)
-			{
-				isValid = false;
-				break;
-			}
-
-		if (isValid)
-		{
-			placeObject(tile[0], tile[1], entityList[randInt(entityList.length)], 0, randFloat(0, 2*PI));
-			// placeObject(tile[0], tile[1], 'actor|geology/decal_stone_medit_b.xml', 0, randFloat(0, 2*PI));
-			placements.push(tile);
-		}
-	}
-}
-
-
-////////////////
-//
-//  Base terrain generation functionality
-//
-////////////////
-
-function setBaseTerrainDiamondSquare(minHeight, maxHeight, smoothness, initialHeightmap, heightmap)
-{
-	// Make some arguments optional
-	minHeight = (minHeight || 0);
-	maxHeight = (maxHeight || 1);
-
-	var heightRange = maxHeight - minHeight;
-	if (heightRange <= 0)
-		warn('setBaseTerrainDiamondSquare: heightRange < 0');
-
-	smoothness = (smoothness || 1);
-	
-	var offset = heightRange / 2;
-	initialHeightmap = (initialHeightmap || [[randFloat(minHeight / 2, maxHeight / 2), randFloat(minHeight / 2, maxHeight / 2)], [randFloat(minHeight / 2, maxHeight / 2), randFloat(minHeight / 2, maxHeight / 2)]]);
-	
-	// Double initialHeightmap width untill target width is reached (diamond square method)
-	while (initialHeightmap.length < heightmap.length)
-	{
-		var newHeightmap = [];
-		var oldWidth = initialHeightmap.length;
-
-		// Square
-		for (var x = 0; x < 2 * oldWidth - 1; x++)
-		{
-			newHeightmap.push([]);
-			for (var y = 0; y < 2 * oldWidth - 1; y++)
-			{
-				if (x % 2 === 0 && y % 2 === 0) // Old tile
-					newHeightmap[x].push(initialHeightmap[x/2][y/2]);
-				else if (x % 2 == 1 && y % 2 == 1) // New tile with diagonal old tile neighbors
-				{
-					newHeightmap[x].push((initialHeightmap[(x-1)/2][(y-1)/2] + initialHeightmap[(x+1)/2][(y-1)/2] + initialHeightmap[(x-1)/2][(y+1)/2] + initialHeightmap[(x+1)/2][(y+1)/2]) / 4);
-					newHeightmap[x][y] += (newHeightmap[x][y] - minHeight) / heightRange * randFloat(-offset, offset);
-				}
-				else // New tile with straight old tile neighbors
-					newHeightmap[x].push(undefined); // Define later
-			}
-		}
-
-		// Diamond
-		for (var x = 0; x < 2 * oldWidth - 1; x++)
-			for (var y = 0; y < 2 * oldWidth - 1; y++)
-			{
-				if (newHeightmap[x][y] === undefined)
-				{
-					if (x > 0 && x + 1 < newHeightmap.length - 1 && y > 0 && y + 1 < newHeightmap.length - 1) // Not a border tile
-					{
-						newHeightmap[x][y] = (newHeightmap[x+1][y] + newHeightmap[x][y+1] + newHeightmap[x-1][y] + newHeightmap[x][y-1]) / 4;
-						newHeightmap[x][y] += (newHeightmap[x][y] - minHeight) / heightRange * randFloat(-offset, offset);
-					}
-					else if (x < newHeightmap.length - 1 && y > 0 && y < newHeightmap.length - 1) // Left border
-					{
-						newHeightmap[x][y] = (newHeightmap[x+1][y] + newHeightmap[x][y+1] + newHeightmap[x][y-1]) / 3;
-						newHeightmap[x][y] += (newHeightmap[x][y] - minHeight) / heightRange * randFloat(-offset, offset);
-					}
-					else if (x > 0 && y > 0 && y < newHeightmap.length - 1) // Right border
-					{
-						newHeightmap[x][y] = (newHeightmap[x][y+1] + newHeightmap[x-1][y] + newHeightmap[x][y-1]) / 3;
-						newHeightmap[x][y] += (newHeightmap[x][y] - minHeight) / heightRange * randFloat(-offset, offset);
-					}
-					else if (x > 0 && x < newHeightmap.length - 1 && y < newHeightmap.length - 1) // Bottom border
-					{
-						newHeightmap[x][y] = (newHeightmap[x+1][y] + newHeightmap[x][y+1] + newHeightmap[x-1][y]) / 3;
-						newHeightmap[x][y] += (newHeightmap[x][y] - minHeight) / heightRange * randFloat(-offset, offset);
-					}
-					else if (x > 0 && x < newHeightmap.length - 1 && y > 0) // Top border
-					{
-						newHeightmap[x][y] = (newHeightmap[x+1][y] + newHeightmap[x-1][y] + newHeightmap[x][y-1]) / 3;
-						newHeightmap[x][y] += (newHeightmap[x][y] - minHeight) / heightRange * randFloat(-offset, offset);
-					}
-				}
-			}
-
-		initialHeightmap = deepcopy(newHeightmap);
-		offset /= Math.pow(2, smoothness);
-	}
-	
-	// Cut initialHeightmap to fit target width
-	var shift = [floor((newHeightmap.length - heightmap.length) / 2), floor((newHeightmap[0].length - heightmap[0].length) / 2)];
-	for (var x = 0; x < heightmap.length; x++)
-		for (var y = 0; y < heightmap[0].length; y++)
-			heightmap[x][y] = newHeightmap[x + shift[0]][y + shift[1]];
-}
-
-
-////////////////
-//
-//  Terrain erosion functionality
-//
-////////////////
-
-function decayErrodeHeightmap(strength, heightmap)
-{
-	strength = (strength || 0.9); // 0 to 1
-	heightmap = (heightmap || g_Map.height);
-	
-	var referenceHeightmap = deepcopy(heightmap);
-	// var map = [[1, 0], [0, 1], [-1, 0], [0, -1]]; // faster
-	var map = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]; // smoother
-	var max_x = heightmap.length;
-	var max_y = heightmap[0].length;
-
-	for (var x = 0; x < max_x; x++)
-		for (var y = 0; y < max_y; y++)
-			for (var i = 0; i < map.length; i++)
-				heightmap[x][y] += strength / map.length * (referenceHeightmap[(x + map[i][0] + max_x) % max_x][(y + map[i][1] + max_y) % max_y] - referenceHeightmap[x][y]); // Not entirely sure if scaling with map.length is perfect but tested values seam to indicate it is
-}
-
-function rectangularSmoothToHeight(center, dx, dy, targetHeight, strength, heightmap)
-{
-	var x = round(center[0]);
-	var y = round(center[1]);
-	dx = round(dx);
-	dy = round(dy);
-	strength = (strength || 1);
-	heightmap = (heightmap || g_Map.height);
-	
-	var heightmapWin = [];
-	for (var wx = 0; wx < 2 * dx + 1; wx++)
-	{
-		heightmapWin.push([]);
-		for (var wy = 0; wy < 2 * dy + 1; wy++)
-		{
-			var actualX = x - dx + wx;
-			var actualY = y - dy + wy;
-
-			if (actualX >= 0 && actualX < heightmap.length - 1 && actualY >= 0 && actualY < heightmap[0].length - 1) // Is in map
-				heightmapWin[wx].push(heightmap[actualX][actualY]);
-			else
-				heightmapWin[wx].push(targetHeight);
-		}
-	}
-
-	for (var wx = 0; wx < 2 * dx + 1; wx++)
-		for (var wy = 0; wy < 2 * dy + 1; wy++)
-		{
-			var actualX = x - dx + wx;
-			var actualY = y - dy + wy;
-
-			if (actualX >= 0 && actualX < heightmap.length - 1 && actualY >= 0 && actualY < heightmap[0].length - 1) // Is in map
-			{
-				// Window function polynomial 2nd degree
-				var scaleX = 1 - (wx / dx - 1) * (wx / dx - 1);
-				var scaleY = 1 - (wy / dy - 1) * (wy / dy - 1);
-
-				heightmap[actualX][actualY] = heightmapWin[wx][wy] + strength * scaleX * scaleY * (targetHeight - heightmapWin[wx][wy]);
-			}
-		}
-}
-
-
-////////////////
-//
-//  Actually do stuff
-//
-////////////////
 
 ////////////////
 // Set height limits and water level by map size
@@ -571,10 +193,10 @@ setWaterHeight(waterHeight);
 // Setting a 3x3 Grid as initial heightmap
 var initialReliefmap = [[heightRange.max, heightRange.max, heightRange.max], [heightRange.max, heightRange.min, heightRange.max], [heightRange.max, heightRange.max, heightRange.max]];
 
-setBaseTerrainDiamondSquare(heightRange.min, heightRange.max, 0.5, initialReliefmap, g_Map.height);
+setBaseTerrainDiamondSquare(heightRange.min, heightRange.max, initialReliefmap);
 // Apply simple erosion
 for (var i = 0; i < 5; i++)
-	decayErrodeHeightmap(0.5);
+	globalSmoothHeightmap();
 rescaleHeightmap(heightRange.min, heightRange.max);
 
 RMS.SetProgress(50);
@@ -642,8 +264,8 @@ for (var i=0; i < numPlayers; i++)
 }
 
 // Add further stone and metal mines
-derivateEntitiesByHeight({'min': heighLimits[3], 'max': ((heighLimits[4]+heighLimits[3])/2)}, startLocations);
-derivateEntitiesByHeight({'min': ((heighLimits[5]+heighLimits[6])/2), 'max': heighLimits[7]}, startLocations);
+distributeEntitiesByHeight({ 'min': heighLimits[3], 'max': ((heighLimits[4] + heighLimits[3]) / 2) }, startLocations, 40);
+distributeEntitiesByHeight({ 'min': ((heighLimits[5] + heighLimits[6]) / 2), 'max': heighLimits[7] }, startLocations, 40);
 
 RMS.SetProgress(50);
 

binaries/data/mods/public/maps/random/belgian_uplands.js
@@ -4,6 +4,7 @@ timeArray.push(new Date().getTime());
 
 // Importing rmgen libraries
 RMS.LoadLibrary("rmgen");
+RMS.LoadLibrary("heightmap");
 
 const BUILDING_ANGlE = -PI/4;
 
@@ -17,37 +18,7 @@ var numPlayers = getNumPlayers();
 var mapSize = getMapSize();
 
 
-//////////
-// Heightmap functionality
-//////////
-
-// Some general heightmap settings
-const MIN_HEIGHT = - SEA_LEVEL; // 20, should be set in the libs!
-const MAX_HEIGHT = 0xFFFF/HEIGHT_UNITS_PER_METRE - SEA_LEVEL; // A bit smaler than 90, should be set in the libs!
-
-// Add random heightmap generation functionality
-function getRandomReliefmap(minHeight, maxHeight)
-{
-	minHeight = (minHeight || MIN_HEIGHT);
-	maxHeight = (maxHeight || MAX_HEIGHT);
-
-	if (minHeight < MIN_HEIGHT)
-		warn("getRandomReliefmap: Argument minHeight is smaler then the supported minimum height of " + MIN_HEIGHT + " (const MIN_HEIGHT): " + minHeight)
-
-	if (maxHeight > MAX_HEIGHT)
-		warn("getRandomReliefmap: Argument maxHeight is smaler then the supported maximum height of " + MAX_HEIGHT + " (const MAX_HEIGHT): " + maxHeight)
-
-	var reliefmap = [];
-	for (var x = 0; x <= mapSize; x++)
-	{
-		reliefmap.push([]);
-		for (var y = 0; y <= mapSize; y++)
-			reliefmap[x].push(randFloat(minHeight, maxHeight));
-	}
-	return reliefmap;
-}
-
-// Apply a heightmap
+// Function to apply a heightmap
 function setReliefmap(reliefmap)
 {
 	// g_Map.height = reliefmap;
@@ -56,70 +27,6 @@ function setReliefmap(reliefmap)
 			setHeight(x, y, reliefmap[x][y]);
 }
 
-// Get minimum and maxumum height used in a heightmap
-function getMinAndMaxHeight(reliefmap)
-{
-	var height = {};
-	height.min = Infinity;
-	height.max = -Infinity;
-
-	for (var x = 0; x <= mapSize; x++)
-		for (var y = 0; y <= mapSize; y++)
-		{
-			if (reliefmap[x][y] < height.min)
-				height.min = reliefmap[x][y];
-			else if (reliefmap[x][y] > height.max)
-				height.max = reliefmap[x][y];
-		}
-
-	return height;
-}
-
-// Rescale a heightmap (Waterlevel is not taken into consideration!)
-function getRescaledReliefmap(reliefmap, minHeight, maxHeight)
-{
-	var newReliefmap = deepcopy(reliefmap);
-	minHeight = (minHeight || MIN_HEIGHT);
-	maxHeight = (maxHeight || MAX_HEIGHT);
-
-	if (minHeight < MIN_HEIGHT)
-		warn("getRescaledReliefmap: Argument minHeight is smaler then the supported minimum height of " + MIN_HEIGHT + " (const MIN_HEIGHT): " + minHeight)
-
-	if (maxHeight > MAX_HEIGHT)
-		warn("getRescaledReliefmap: Argument maxHeight is smaler then the supported maximum height of " + MAX_HEIGHT + " (const MAX_HEIGHT): " + maxHeight)
-
-	var oldHeightRange = getMinAndMaxHeight(reliefmap);
-
-	for (var x = 0; x <= mapSize; x++)
-		for (var y = 0; y <= mapSize; y++)
-			newReliefmap[x][y] = minHeight + (reliefmap[x][y] - oldHeightRange.min) / (oldHeightRange.max - oldHeightRange.min) * (maxHeight - minHeight);
-
-	return newReliefmap
-}
-
-// Applying decay errosion (terrain independent)
-function getHeightErrosionedReliefmap(reliefmap, strength)
-{
-	var newReliefmap = deepcopy(reliefmap);
-	strength = (strength || 1.0); // Values much higher then 1 (1.32+ for an 8 tile map, 1.45+ for a 12 tile map, 1.62+ @ 20 tile map, 0.99 @ 4 tiles) will result in a resonance disaster/self interference
-
-	var map = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]; // Default
-
-	for (var x = 0; x <= mapSize; x++)
-		for (var y = 0; y <= mapSize; y++)
-		{
-			var div = 0;
-			for (var i = 0; i < map.length; i++)
-				newReliefmap[x][y] += strength / map.length * (reliefmap[(x + map[i][0] + mapSize + 1) % (mapSize + 1)][(y + map[i][1] + mapSize + 1) % (mapSize + 1)] - reliefmap[x][y]); // Not entirely sure if scaling with map.length is perfect but tested values seam to indicate it is
-		}
-
-	return newReliefmap;
-}
-
-
-//////////
-// Prepare for hightmap munipulation
-//////////
 
 // Set target min and max height depending on map size to make average stepness the same on all map sizes
 var heightRange = {"min": MIN_HEIGHT * mapSize / 8192, "max": MAX_HEIGHT * mapSize / 8192};
@@ -215,11 +122,12 @@ while (!goodStartPositionsFound)
 	log("Starting giant while loop try " + tries);
 
 	// Generate reliefmap
-	var myReliefmap = getRandomReliefmap(heightRange.min, heightRange.max);
+	var myReliefmap = deepcopy(g_Map.height);
+	setRandomHeightmap(heightRange.min, heightRange.max, myReliefmap);
 	for (var i = 0; i < 50 + mapSize/4; i++) // Cycles depend on mapsize (more cycles -> bigger structures)
-		myReliefmap = getHeightErrosionedReliefmap(myReliefmap, 1);
+		globalSmoothHeightmap(0.8, myReliefmap);
 
-	myReliefmap = getRescaledReliefmap(myReliefmap, heightRange.min, heightRange.max);
+	rescaleHeightmap(heightRange.min, heightRange.max, myReliefmap);
 	setReliefmap(myReliefmap);
 	
 	// Find good start position tiles

commit d38f3f59d1fd775e0cfab00e52faf2d893d6517a

Fixes a bug in random map schwarzwald.js introduced in r18141 that prevented the starting positions to be flattened.
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@18211 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/maps/random/schwarzwald.js
@@ -238,7 +238,7 @@ for (var i=0; i < numPlayers; i++)
 
 	// Place starting entities
 
-	rectangularSmoothToHeight([x,z] , 20, 20, playerHeight, 0.8);
+	rectangularSmoothToHeight({"x": x,"y": z} , 20, 20, playerHeight, 0.8);
 
 	placeCivDefaultEntities(x, z, i+1, { 'iberWall': false });
 	