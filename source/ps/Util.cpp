46833ad833296cc2489c934403f6b87f227b027f
elexis <elexis@3db68df2-c116-0410-a063-a993310a9797>
2016-05-27 14:24:05 +0000

    Show the filepath in the command line output when taking screenshots.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@18245 3db68df2-c116-0410-a063-a993310a9797

--- source/ps/Util.cpp
+++ source/ps/Util.cpp
@@ -272,7 +272,12 @@ void WriteScreenshot(const VfsPath& extension)
 	{
 		OsPath realPath;
 		g_VFS->GetRealPath(filename, realPath);
+
 		LOGMESSAGERENDER(g_L10n.Translate("Screenshot written to '%s'"), realPath.string8());
+
+		debug_printf(
+			CStr(g_L10n.Translate("Screenshot written to '%s'") + "\n").c_str(),
+			realPath.string8().c_str());
 	}
 	else
 		LOGERROR("Error writing screenshot to '%s'", filename.string8());
@@ -405,7 +410,12 @@ void WriteBigScreenshot(const VfsPath& extension, int tiles)
 	{
 		OsPath realPath;
 		g_VFS->GetRealPath(filename, realPath);
+
 		LOGMESSAGERENDER(g_L10n.Translate("Screenshot written to '%s'"), realPath.string8());
+
+		debug_printf(
+			CStr(g_L10n.Translate("Screenshot written to '%s'") + "\n").c_str(),
+			realPath.string8().c_str());
 	}
 	else
 		LOGERROR("Error writing screenshot to '%s'", filename.string8());
