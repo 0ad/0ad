75c63f1b0f0615d883d6789ef320bce030e12fde
sanderd17 <sanderd17@3db68df2-c116-0410-a063-a993310a9797>
2016-05-29 12:15:24 +0000

    Reduce number of rallypoint recalculations when a target has moved before, but doesn't move now.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@18256 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/RallyPoint.js
+++ binaries/data/mods/public/simulation/components/RallyPoint.js
@@ -52,7 +52,9 @@ RallyPoint.prototype.GetPositions = function()
 		if (this.pos[i].x == targetPosition.x && this.pos[i].z == targetPosition.y)
 			continue;
 
-		ret[i] = {"x": targetPosition.x, "z": targetPosition.y};
+		// update the positions for ret and this.pos (referencing the same object)
+		this.pos[i].x = targetPosition.x;
+		this.pos[i].z = targetPosition.y;
 		var cmpRallyPointRenderer = Engine.QueryInterface(this.entity, IID_RallyPointRenderer);
 		if (cmpRallyPointRenderer)
 			cmpRallyPointRenderer.UpdatePosition(i, targetPosition);
