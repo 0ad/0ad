0ad/0ad

commit f506d92b727d61c84caff21942cc1e3c7b220c22

Fixes a bug for random maps where Map.validT didn't apply the distance parameter properly on rectangular maps. Fixes #4012
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@18312 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/maps/random/rmgen/map.js
@@ -102,7 +102,7 @@ Map.prototype.validT = function(x, z, distance)
 	}
 	else
 		// Within map square
-		return x >= 0 && z >= 0 && x < this.size && z < this.size;
+		return x >= distance && z >= distance && x < this.size - distance && z < this.size - distance;
 };
 
 // Check bounds on tile map

commit 686508169ea5917e46062d3b74dbe8b16209debb

Fixes a bug introduced in r18312 that prevents entity placement on some rectangular maps e.g. Phoenician Levant and Corsica vs Sardinia. Also fixes a typo in the Phoenician Levant .js and .json files. Fixes #4012
git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@18360 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/maps/random/rmgen/map.js
@@ -88,17 +88,14 @@ Map.prototype.getEntityID = function()
 }
 
 // Check bounds on tile map
-Map.prototype.validT = function(x, z, distance)
+Map.prototype.validT = function(x, z, distance = 0)
 {
 	if (g_MapSettings.CircularMap)
 	{	// Within map circle
 		var halfSize = Math.floor(0.5*this.size);
 		var dx = (x - halfSize);
 		var dz = (z - halfSize);
-		if (distance === undefined)
-			return Math.round(Math.sqrt(dx*dx + dz*dz)) < halfSize - 1;
-		else
-			return Math.round(Math.sqrt(dx*dx + dz*dz)) < halfSize - distance - 1;
+		return Math.round(Math.sqrt(dx*dx + dz*dz)) < halfSize - distance - 1;
 	}
 	else
 		// Within map square