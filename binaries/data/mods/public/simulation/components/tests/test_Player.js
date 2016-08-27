47109fdc0fcbccb1a7bf52b4185f07049daa9e4b
leper <leper@3db68df2-c116-0410-a063-a993310a9797>
2016-08-27 15:27:13 +0000

    Remove unneeded includes.
    Refs #4161.
    Reviewed by: fatherbushido
    
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@18647 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/tests/test_Player.js
+++ binaries/data/mods/public/simulation/components/tests/test_Player.js
@@ -1,13 +1,7 @@
 Engine.LoadHelperScript("ValueModification.js");
-Engine.LoadComponentScript("interfaces/EndGameManager.js")
-Engine.LoadComponentScript("interfaces/TechnologyManager.js")
-Engine.LoadComponentScript("interfaces/Timer.js")
-Engine.LoadComponentScript("EndGameManager.js")
+Engine.LoadComponentScript("interfaces/Player.js");
+Engine.LoadComponentScript("interfaces/TechnologyManager.js");
 Engine.LoadComponentScript("Player.js");
-Engine.LoadComponentScript("Timer.js")
-
-ConstructComponent(SYSTEM_ENTITY, "EndGameManager");
-ConstructComponent(SYSTEM_ENTITY, "Timer");
 
 var cmpPlayer = ConstructComponent(10, "Player");
 
