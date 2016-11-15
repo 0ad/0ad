b8661887766807db82b543fab76b3d881286261f
elexis <elexis@3db68df2-c116-0410-a063-a993310a9797>
2016-11-15 15:10:14 +0000

    Partial EndGameManager test by Sandarac, refs #4143.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@18943 3db68df2-c116-0410-a063-a993310a9797

---
+++ binaries/data/mods/public/simulation/components/tests/test_EndGameManager.js
@@ -0,0 +1,30 @@
+Engine.LoadComponentScript("interfaces/EndGameManager.js");
+Engine.LoadComponentScript("EndGameManager.js");
+
+let cmpEndGameManager = ConstructComponent(SYSTEM_ENTITY, "EndGameManager");
+
+let playerEnt1 = 1;
+let wonderDuration = 2 * 60 * 1000;
+
+AddMock(SYSTEM_ENTITY, IID_PlayerManager, {
+	"GetNumPlayers": () => 4
+});
+
+AddMock(SYSTEM_ENTITY, IID_GuiInterface, {
+	"DeleteTimeNotification": () => null,
+	"AddTimeNotification": () => 1
+});
+
+AddMock(playerEnt1, IID_Player, {
+	"GetName": () => "Player 1",
+	"GetState": () => "active",
+});
+
+TS_ASSERT_EQUALS(cmpEndGameManager.skipAlliedVictoryCheck, true);
+cmpEndGameManager.SetAlliedVictory(true);
+TS_ASSERT_EQUALS(cmpEndGameManager.alliedVictory, true);
+cmpEndGameManager.SetGameType("wonder", { "wonderDuration": wonderDuration });
+TS_ASSERT_EQUALS(cmpEndGameManager.CheckGameType("regicide"), false);
+TS_ASSERT_EQUALS(cmpEndGameManager.skipAlliedVictoryCheck, false);
+TS_ASSERT(cmpEndGameManager.GetGameType() == "wonder");
+TS_ASSERT_EQUALS(cmpEndGameManager.GetGameTypeSettings().wonderDuration, wonderDuration);
