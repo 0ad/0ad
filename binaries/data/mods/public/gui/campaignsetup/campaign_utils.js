97f6df634d969945ec09e5d70f4f9e5512416aa3
Lancelot de Ferrière <wraitii@gmail.com>
2016-12-01 16:16:33 -0500

    Allow exiting directly

--- binaries/data/mods/public/gui/campaignsetup/campaign_utils.js
+++ binaries/data/mods/public/gui/campaignsetup/campaign_utils.js
@@ -85,10 +85,22 @@ function saveCampaign()
 	Engine.SaveCampaign(g_CurrentCampaign, g_CampaignData);
 }
 
-function exitCampaignMode()
+function exitCampaignMode(exitGame = false)
 {
 	// TODO: should this be here?
 	saveCampaign();
+
+	if (exitGame)
+	{
+		messageBox(
+		400, 200,
+		translate("Are you sure you want to quit 0 A.D.?"),
+		translate("Confirmation"),
+		[translate("No"), translate("Yes")],
+		[null, Engine.Exit]
+		);
+		return;
+	}
 
 	// TODO: might be safer to check all mods and remove all with type "campaign"
 	let mods = getExistingModsFromConfig();
