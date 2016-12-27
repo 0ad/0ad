cc411529c0118d06189d2798060e2bb99138df39
fatherbushido <fatherbushido@3db68df2-c116-0410-a063-a993310a9797>
2016-12-27 09:42:02 +0000

    Remove some semicolons at end of line in a python script.
    git-svn-id: http://svn.wildfiregames.com/public/ps/trunk@19073 3db68df2-c116-0410-a063-a993310a9797

--- source/tools/templatesanalyzer/unitTables.py
+++ source/tools/templatesanalyzer/unitTables.py
@@ -29,7 +29,7 @@ Resources = ["food", "wood", "stone", "metal"]
 # The way this works is it tries all generic templates
 # But only loads those who have one of the following parents
 # EG adding "template_unit.xml" will load all units.
-LoadTemplatesIfParent = ["template_unit_infantry.xml", "template_unit_cavalry.xml", "template_unit_champion.xml", "template_unit_hero.xml"];
+LoadTemplatesIfParent = ["template_unit_infantry.xml", "template_unit_cavalry.xml", "template_unit_champion.xml", "template_unit_hero.xml"]
 
 # Those describe Civs to analyze.
 # The script will load all entities that derive (to the nth degree) from one of the above templates.
@@ -50,7 +50,7 @@ AddSortingOverlay = True
 basePath = os.path.realpath(__file__).replace("unitTables.py","") + "../../../binaries/data/mods/public/simulation/templates/"
 
 # For performance purposes, cache opened templates files.
-globalTemplatesList = {};
+globalTemplatesList = {}
 
 def htbout(file, balise, value):
 	file.write("<" + balise + ">" + value + "</" + balise + ">\n" )
@@ -68,7 +68,7 @@ def hasParentTemplate(UnitName, parentName):
 	Template = fastParse(UnitName)
 
 	found = False
-	Name = UnitName;
+	Name = UnitName
 	while found != True and Template.getroot().get("parent") != None:
 		Name = Template.getroot().get("parent") + ".xml"
 		if Name == parentName:
@@ -341,7 +341,7 @@ f.write("</table>")
 CivTemplates = {}
 
 for Civ in Civs:
-	CivTemplates[Civ] = {};
+	CivTemplates[Civ] = {}
 	# Load all templates that start with that civ indicator
 	for template in list(glob.glob('units/' + Civ + '_*.xml')):
 		if os.path.isfile(template):
