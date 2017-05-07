cd283f34a85ea7c3be5be0f801e6cb3ef011ce1d
Itms <Itms@3db68df2-c116-0410-a063-a993310a9797>
2017-05-07 14:38:29 +0000

    Enable other languages than English in the Windows installer, patch by vladislavbelov.
    Reviewed By: Itms
    
    Differential Revision: https://code.wildfiregames.com/D314
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@19524 3db68df2-c116-0410-a063-a993310a9797

--- source/tools/dist/remove-incomplete-translations.sh
+++ source/tools/dist/remove-incomplete-translations.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Included languages
+# Keep in sync with source/tools/i18n/creditTranslators.py and with the installer languages in 0ad.nsi
 LANGS=("bg" "ca" "cs" "de" "en_GB" "es" "fr" "gd" "gl" "hu" "id" "it" "nb" "nl" "pl" "pt_BR" "pt_PT" "ru" "sk" "sv" "tr")
 
 REGEX=$(printf "\|%s" "${LANGS[@]}")
