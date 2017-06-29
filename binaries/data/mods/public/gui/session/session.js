bdcfaae2e0558b2ac7f8f565356d0e652e48dabb
elexis <elexis@3db68df2-c116-0410-a063-a993310a9797>
2017-06-29 17:25:24 +0000

    Fix observermode GUI for 1024x768.
    Display the follow-player label only if it fits entirely (i.e. >= 1280x1024), otherwise display the checkbox only.
    Increase the size of the player dropdown for resolutions >= 1280x1024.
    
    Differential Revision: https://code.wildfiregames.com/D689
    Patch By: temple
    
    Refs:
      Issues introduced by #3743 and rP13606
      This is affected by D692 D694
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@19843 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/session/session.js
+++ binaries/data/mods/public/gui/session/session.js
@@ -597,6 +597,10 @@ function updateTopPanel()
 	let viewPlayer = Engine.GetGUIObjectByName("viewPlayer");
 	viewPlayer.hidden = !g_IsObserver && !g_DevSettings.changePerspective;
 
+	let followPlayerLabel = Engine.GetGUIObjectByName("followPlayerLabel");
+	followPlayerLabel.hidden = Engine.GetTextWidth(followPlayerLabel.font, followPlayerLabel.caption + "  ") +
+		followPlayerLabel.getComputedSize().left > viewPlayer.getComputedSize().left;
+
 	let resCodes = g_ResourceData.GetCodes();
 	let r = 0;
 	for (let res of resCodes)
@@ -625,7 +629,7 @@ function updateTopPanel()
 
 	let alphaLabel = Engine.GetGUIObjectByName("alphaLabel");
 	alphaLabel.hidden = isPlayer && !viewPlayer.hidden;
-	alphaLabel.size = isPlayer ? "50%+20 0 100%-226 100%" : "200 0 100%-475 100%";
+	alphaLabel.size = isPlayer ? "50%+54 0 100%-293 100%" : "155 0 85%-279 100%";
 
 	Engine.GetGUIObjectByName("pauseButton").enabled = !g_IsObserver || !g_IsNetworked || g_IsController;
 	Engine.GetGUIObjectByName("menuResignButton").enabled = !g_IsObserver;
