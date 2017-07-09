13b64b18968415595da399f8f014c1691e5d8bbd
Itms <Itms@3db68df2-c116-0410-a063-a993310a9797>
2017-07-09 15:38:40 +0000

    Upgrade a few bundled Windows libraries, refs #3004.
    Upgrade enet to 1.3.13.
    Upgrade zlib to 1.2.11.
    Upgrade libpng to 1.6.29.
    Upgrade libxml2 to 2.9.4.
    
    Reviewed By: Imarok
    Differential Revision: https://code.wildfiregames.com/D718
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@19895 3db68df2-c116-0410-a063-a993310a9797

--- libraries/win32/libxml2/include/libxml/xmlversion.h
+++ libraries/win32/libxml2/include/libxml/xmlversion.h
@@ -29,21 +29,21 @@ XMLPUBFUN void XMLCALL xmlCheckVersion(int version);
  *
  * the version string like "1.2.3"
  */
-#define LIBXML_DOTTED_VERSION "2.9.3"
+#define LIBXML_DOTTED_VERSION "2.9.4"
 
 /**
  * LIBXML_VERSION:
  *
  * the version number: 1.2.3 value is 10203
  */
-#define LIBXML_VERSION 20903
+#define LIBXML_VERSION 20904
 
 /**
  * LIBXML_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "10203"
  */
-#define LIBXML_VERSION_STRING "20903"
+#define LIBXML_VERSION_STRING "20904"
 
 /**
  * LIBXML_VERSION_EXTRA:
@@ -58,7 +58,7 @@ XMLPUBFUN void XMLCALL xmlCheckVersion(int version);
  * Macro to check that the libxml version in use is compatible with
  * the version the software has been compiled against
  */
-#define LIBXML_TEST_VERSION xmlCheckVersion(20903);
+#define LIBXML_TEST_VERSION xmlCheckVersion(20904);
 
 #ifndef VMS
 #if 0
