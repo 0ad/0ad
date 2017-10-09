4e1c3ad8f4d4b4982f249c2104859a58c0625ead
mimo <mimo@3db68df2-c116-0410-a063-a993310a9797>
2017-10-09 19:38:56 +0000

    Cache a list of TerritoryDecay components in the TerritoryDecayManager instead of retrieving it each time
    Reviewed By: bb
    
    Differential Revision: https://code.wildfiregames.com/D951
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@20276 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/TerritoryDecayManager.js
+++ binaries/data/mods/public/simulation/components/TerritoryDecayManager.js
@@ -3,9 +3,24 @@ function TerritoryDecayManager() {}
 TerritoryDecayManager.prototype.Schema =
         "<a:component type='system'/><empty/>";
 
+TerritoryDecayManager.prototype.Init = function()
+{
+	this.list = new Set();
+};
+
+TerritoryDecayManager.prototype.Add = function(ent)
+{
+	this.list.add(ent);
+};
+
+TerritoryDecayManager.prototype.Remove = function(ent)
+{
+	this.list.delete(ent);
+};
+
 TerritoryDecayManager.prototype.SetBlinkingEntities = function()
 {
-	for (let ent of Engine.GetEntitiesWithInterface(IID_TerritoryDecay))
+	for (let ent of this.list.values())
 		Engine.QueryInterface(ent, IID_TerritoryDecay).IsConnected();
 };
 
