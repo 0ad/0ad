6fe9702c2f4328500d4751189011cee22e137494
fatherbushido <fatherbushido@3db68df2-c116-0410-a063-a993310a9797>
2017-10-22 15:39:50 +0000

    Fix a disabled template manager test. Fix #4522 Patch by: Sandarac. Reviewed by: elexis. Differential Revision: https://code.wildfiregames.com/D968
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@20326 3db68df2-c116-0410-a063-a993310a9797

--- source/simulation2/tests/test_CmpTemplateManager.h
+++ source/simulation2/tests/test_CmpTemplateManager.h
@@ -222,6 +222,7 @@ public:
 	void setUp()
 	{
 		g_VFS = CreateVfs(20 * MiB);
+		TS_ASSERT_OK(g_VFS->Mount(L"", DataDir()/"mods"/"mod", VFS_MOUNT_MUST_EXIST));
 		TS_ASSERT_OK(g_VFS->Mount(L"", DataDir()/"mods"/"public", VFS_MOUNT_MUST_EXIST));
 		TS_ASSERT_OK(g_VFS->Mount(L"cache", DataDir()/"_testcache"));
 		CXeromyces::Startup();
