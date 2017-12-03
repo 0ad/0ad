b129dd2ff32b92ded4583c02a553b3e11e6e77fd
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2017-12-03 10:10:13 +0000

    Fix individual combat when attacked
    In particular, better handle melee units attacking capturing units.
    
    Reviewed By: wraitii
    
    Patch By: temple
    
    Differential Revision: https://code.wildfiregames.com/D1047
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@20568 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/helpers/Entity.js
+++ binaries/data/mods/public/simulation/helpers/Entity.js
@@ -13,34 +13,4 @@ function DistanceBetweenEntities(first, second)
 	return firstPosition.distanceTo(secondPosition);
 }
 
-/**
- *  Returns entities ordered by decreasing priority
- *  Do not alter order when units have the same priority
- */
-function SortEntitiesByPriority(ents)
-{
-	// Priority list, weakers first
-	var types = ["Structure", "Worker"];
-
-	ents.sort(function  (a, b) {
-		var cmpIdentityA = Engine.QueryInterface(a, IID_Identity);
-		var cmpIdentityB = Engine.QueryInterface(b, IID_Identity);
-		if (!cmpIdentityA || !cmpIdentityB)
-			return 0;
-		var classesA = cmpIdentityA.GetClassesList();
-		var classesB = cmpIdentityB.GetClassesList();
-		for (var type of types)
-		{
-			var inA = classesA.indexOf(type) != -1;
-			var inB = classesB.indexOf(type) != -1;
-			if (inA && !inB)
-				return +1;
-			if (inB && !inA)
-				return -1;
-		}
-		return 0;
-	});
-}
-
 Engine.RegisterGlobal("DistanceBetweenEntities", DistanceBetweenEntities);
-Engine.RegisterGlobal("SortEntitiesByPriority", SortEntitiesByPriority);
