d65f93d98748563f3bffb17f8bb07b6a8ad46e79
mimo <mimo@3db68df2-c116-0410-a063-a993310a9797>
2017-12-07 21:40:42 +0000

    Adds test in ProductionQueue for the {civ} tech replacement from rP20551
    Reviewed By: elexis
    
    Differential Revision: https://code.wildfiregames.com/D1125
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@20604 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/tests/test_ProductionQueue.js
+++ binaries/data/mods/public/simulation/components/tests/test_ProductionQueue.js
@@ -3,6 +3,7 @@ Resources = {
 };
 
 Engine.LoadHelperScript("Player.js");
+Engine.LoadComponentScript("interfaces/TechnologyManager.js");
 Engine.LoadComponentScript("interfaces/ProductionQueue.js");
 Engine.LoadComponentScript("ProductionQueue.js");
 
@@ -12,12 +13,16 @@ const playerEntityID = 2;
 
 AddMock(SYSTEM_ENTITY, IID_TemplateManager, {
 	"TemplateExists": () => true,
-	"GetTemplate": name => {}
+	"GetTemplate": name => ({})
 });
 
 let cmpProductionQueue = ConstructComponent(productionQueueId, "ProductionQueue", {
-	"Entities": { "_string": "units/{civ}_cavalry_javelinist_b units/{civ}_infantry_swordsman_b " +
-	                         "units/{native}_support_female_citizen" }
+	"Entities": { "_string": "units/{civ}_cavalry_javelinist_b " +
+	                         "units/{civ}_infantry_swordsman_b " +
+	                         "units/{native}_support_female_citizen" },
+	"Technologies": { "_string": "gather_fishing_net " +
+	                             "phase_town_{civ} " +
+	                             "phase_city_{civ}" }
 });
 
 cmpProductionQueue.CalculateEntitiesList();
@@ -27,10 +32,22 @@ AddMock(SYSTEM_ENTITY, IID_PlayerManager, {
 	"GetPlayerByID": id => playerEntityID
 });
 
+AddMock(SYSTEM_ENTITY, IID_DataTemplateManager, {
+	"TechnologyExists": name => name == "phase_town_athen" || name == "phase_city_athen"
+});
+
 AddMock(playerEntityID, IID_Player, {
 	"GetCiv": () => "iber",
+	"GetDisabledTechnologies": () => ({}),
 	"GetDisabledTemplates": () => ({}),
 	"GetPlayerID": () => playerId
+});
+
+AddMock(playerEntityID, IID_TechnologyManager, {
+	"CheckTechnologyRequirements": () => true,
+	"GetTechnologyTemplate": tech => tech,
+	"IsInProgress": () => false,
+	"IsTechnologyResearched": () => false
 });
 
 AddMock(productionQueueId, IID_Ownership, {
@@ -42,12 +59,18 @@ AddMock(productionQueueId, IID_Identity, {
 });
 
 cmpProductionQueue.CalculateEntitiesList();
-TS_ASSERT_UNEVAL_EQUALS(cmpProductionQueue.GetEntitiesList(), ["units/iber_cavalry_javelinist_b", "units/iber_infantry_swordsman_b",
-                                                               "units/iber_support_female_citizen"]);
+TS_ASSERT_UNEVAL_EQUALS(
+	cmpProductionQueue.GetEntitiesList(),
+	["units/iber_cavalry_javelinist_b", "units/iber_infantry_swordsman_b", "units/iber_support_female_citizen"]
+);
+TS_ASSERT_UNEVAL_EQUALS(
+	cmpProductionQueue.GetTechnologiesList(),
+	["gather_fishing_net", "phase_town_generic", "phase_city_generic"]
+);
 
 AddMock(SYSTEM_ENTITY, IID_TemplateManager, {
 	"TemplateExists": name => name == "units/iber_support_female_citizen",
-	"GetTemplate": name => {}
+	"GetTemplate": name => ({})
 });
 
 cmpProductionQueue.CalculateEntitiesList();
@@ -55,33 +78,65 @@ TS_ASSERT_UNEVAL_EQUALS(cmpProductionQueue.GetEntitiesList(), ["units/iber_suppo
 
 AddMock(SYSTEM_ENTITY, IID_TemplateManager, {
 	"TemplateExists": () => true,
-	"GetTemplate": name => {}
+	"GetTemplate": name => ({})
 });
 
 AddMock(playerEntityID, IID_Player, {
 	"GetCiv": () => "iber",
+	"GetDisabledTechnologies": () => ({}),
 	"GetDisabledTemplates": () => ({ "units/athen_infantry_swordsman_b": true }),
 	"GetPlayerID": () => playerId
 });
 
 cmpProductionQueue.CalculateEntitiesList();
-TS_ASSERT_UNEVAL_EQUALS(cmpProductionQueue.GetEntitiesList(), ["units/iber_cavalry_javelinist_b", "units/iber_infantry_swordsman_b",
-                                                               "units/iber_support_female_citizen"]);
+TS_ASSERT_UNEVAL_EQUALS(
+	cmpProductionQueue.GetEntitiesList(),
+	["units/iber_cavalry_javelinist_b", "units/iber_infantry_swordsman_b", "units/iber_support_female_citizen"]
+);
 
 AddMock(playerEntityID, IID_Player, {
 	"GetCiv": () => "iber",
+	"GetDisabledTechnologies": () => ({}),
 	"GetDisabledTemplates": () => ({ "units/iber_infantry_swordsman_b": true }),
 	"GetPlayerID": () => playerId
 });
 
 cmpProductionQueue.CalculateEntitiesList();
-TS_ASSERT_UNEVAL_EQUALS(cmpProductionQueue.GetEntitiesList(), ["units/iber_cavalry_javelinist_b", "units/iber_support_female_citizen"]);
+TS_ASSERT_UNEVAL_EQUALS(
+	cmpProductionQueue.GetEntitiesList(),
+	["units/iber_cavalry_javelinist_b", "units/iber_support_female_citizen"]
+);
 
 AddMock(playerEntityID, IID_Player, {
 	"GetCiv": () => "athen",
+	"GetDisabledTechnologies": () => ({ "gather_fishing_net": true }),
 	"GetDisabledTemplates": () => ({ "units/athen_infantry_swordsman_b": true }),
 	"GetPlayerID": () => playerId
 });
 
 cmpProductionQueue.CalculateEntitiesList();
-TS_ASSERT_UNEVAL_EQUALS(cmpProductionQueue.GetEntitiesList(), ["units/athen_cavalry_javelinist_b", "units/iber_support_female_citizen"]);
+TS_ASSERT_UNEVAL_EQUALS(
+	cmpProductionQueue.GetEntitiesList(),
+	["units/athen_cavalry_javelinist_b", "units/iber_support_female_citizen"]
+);
+TS_ASSERT_UNEVAL_EQUALS(cmpProductionQueue.GetTechnologiesList(), ["phase_town_athen",
+                                                                   "phase_city_athen"]
+);
+
+AddMock(playerEntityID, IID_TechnologyManager, {
+	"CheckTechnologyRequirements": () => true,
+	"GetTechnologyTemplate": tech => tech,
+	"IsInProgress": () => false,
+	"IsTechnologyResearched": tech => tech == "phase_town_athen"
+});
+TS_ASSERT_UNEVAL_EQUALS(cmpProductionQueue.GetTechnologiesList(), [undefined, "phase_city_athen"]);
+
+AddMock(playerEntityID, IID_Player, {
+	"GetCiv": () => "iber",
+	"GetDisabledTechnologies": () => ({}),
+	"GetPlayerID": () => playerId
+});
+TS_ASSERT_UNEVAL_EQUALS(
+	cmpProductionQueue.GetTechnologiesList(),
+	["gather_fishing_net", "phase_town_generic", "phase_city_generic"]
+);
