fa57ef4dfc280855931e9f7ce1931e25452c375c
elexis <elexis@3db68df2-c116-0410-a063-a993310a9797>
2017-12-31 01:34:22 +0000

    Use a helper variable and fix some whitespace in shaders as recommended by Vladislav in rP20584.
    Differential Revision: https://code.wildfiregames.com/D1144
    Comments By: wraitii
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@20738 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/shaders/glsl/solid_player.fs
+++ binaries/data/mods/public/shaders/glsl/solid_player.fs
@@ -8,23 +8,23 @@ vec3 get_fog(vec3 color)
 {
 	float density = fogParams.x;
 	float maxFog = fogParams.y;
-	
+
 	const float LOG2 = 1.442695;
 	float z = gl_FragCoord.z / gl_FragCoord.w;
 	float fogFactor = exp2(-density * density * z * z * LOG2);
-	
+
 	fogFactor = fogFactor * (1.0 - maxFog) + maxFog;
-	
+
 	fogFactor = clamp(fogFactor, 0.0, 1.0);
-	
+
 	return mix(fogColor, color, fogFactor);
 }
 
 void main()
 {
-	#if USE_FOG
-		gl_FragColor = vec4(get_fog(playerColor.rgb), playerColor.a);
-	#else
-		gl_FragColor = vec4(playerColor.rgb, playerColor.a);
-	#endif
+#if USE_FOG
+	gl_FragColor = vec4(get_fog(playerColor.rgb), playerColor.a);
+#else
+	gl_FragColor = vec4(playerColor.rgb, playerColor.a);
+#endif
 }
