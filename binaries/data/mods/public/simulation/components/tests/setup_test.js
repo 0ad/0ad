82e4768861c64c28c59a450c8bd152a1c55e21a7
elexis <elexis@3db68df2-c116-0410-a063-a993310a9797>
2018-01-23 20:00:52 +0000

    Rename expectException from rP20134 to TS_ASSERT_EXCEPTION. Move it from the test of the testsetup to the setup of the tests, so it can be reused by tests, refs #4759, D871.
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@20989 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/tests/setup_test.js
+++ binaries/data/mods/public/simulation/components/tests/setup_test.js
@@ -6,19 +6,11 @@ TestSetup.prototype.Init = function() {};
 Engine.RegisterSystemComponentType(IID_TestSetup, "TestSetup", TestSetup);
 let cmpTestSetup = ConstructComponent(SYSTEM_ENTITY, "TestSetup", { "property": "value" });
 
-function expectException(func)
-{
-	try {
-		func();
-		Engine.TS_FAIL("Missed exception at " + new Error().stack);
-	} catch (e) {}
-}
-
-expectException(() => { cmpTestSetup.template = "replacement forbidden"; });
-expectException(() => { cmpTestSetup.template.property = "modification forbidden"; });
-expectException(() => { cmpTestSetup.template.other_property = "insertion forbidden"; });
-expectException(() => { delete cmpTestSetup.entity; });
-expectException(() => { delete cmpTestSetup.template; });
-expectException(() => { delete cmpTestSetup.template.property; });
+TS_ASSERT_EXCEPTION(() => { cmpTestSetup.template = "replacement forbidden"; });
+TS_ASSERT_EXCEPTION(() => { cmpTestSetup.template.property = "modification forbidden"; });
+TS_ASSERT_EXCEPTION(() => { cmpTestSetup.template.other_property = "insertion forbidden"; });
+TS_ASSERT_EXCEPTION(() => { delete cmpTestSetup.entity; });
+TS_ASSERT_EXCEPTION(() => { delete cmpTestSetup.template; });
+TS_ASSERT_EXCEPTION(() => { delete cmpTestSetup.template.property; });
 
 TS_ASSERT_UNEVAL_EQUALS(cmpTestSetup.template, { "property": "value" });
