a2733ac6ce2b49351179fe2da217a2e5e490562d
elexis <elexis@3db68df2-c116-0410-a063-a993310a9797>
2018-01-31 04:02:59 +0000

    Rename some variables of the pathplacer and use JSdoc, refs #892, #4831.
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@21081 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/maps/random/rmgen/placer_noncentered.js
+++ binaries/data/mods/public/maps/random/rmgen/placer_noncentered.js
@@ -67,32 +67,28 @@ HeightPlacer.prototype.place = function(constraint)
 		point => this.withinHeightRange(point) && constraint.allows(point));
 };
 
-/////////////////////////////////////////////////////////////////////////////////////////
-//	PathPlacer
-//
-//	Class for creating a winding path between two points
-//
-//	x1,z1: 	Starting point of path
-//	x2,z2: 	Ending point of path
-//	width: 	Width of the path
-//	a:		Waviness - how wavy the path will be (higher is wavier, 0.0 is straight line)
-//	b:		Smoothness - how smooth the path will be (higher is smoother)
-//	c:		Offset - max amplitude of waves along the path (0.0 is straight line)
-// 	taper:	Tapering - how much the width of the path changes from start to end
-//				if positive, the width will decrease by that factor, if negative the width
-//				will increase by that factor
-//
-/////////////////////////////////////////////////////////////////////////////////////////
-
-function PathPlacer(start, end, width, a, b, c, taper, failFraction = 0)
+/**
+ * Creates a winding path between two points.
+ *
+ * @param {Vector2D} start - Starting position of the path.
+ * @param {Vector2D} end - Endposition of the path.
+ * @param {number} width - Number of tiles between two sides of the path.
+ * @param {number} waviness - 0 is a straight line, higher numbers are.
+ * @param {number} smoothness - the higher the number, the smoother the path.
+ * @param {number} offset - Maximum amplitude of waves along the path. 0 is straight line.
+ * @param {number} tapering - How much the width of the path changes from start to end.
+ *   If positive, the width will decrease by that factor.
+ *   If negative the width will increase by that factor.
+ */
+function PathPlacer(start, end, width, waviness, smoothness, offset, tapering, failFraction = 0)
 {
 	this.start = start;
 	this.end = end;
 	this.width = width;
-	this.a = a;
-	this.b = b;
-	this.c = c;
-	this.taper = taper;
+	this.waviness = waviness;
+	this.smoothness = smoothness;
+	this.offset = offset;
+	this.tapering = tapering;
 	this.failFraction = failFraction;
 }
 
@@ -101,41 +97,39 @@ PathPlacer.prototype.place = function(constraint)
 	var failed = 0;
 
 	let pathLength = this.start.distanceTo(this.end);
-	var numSteps = 1 + Math.floor(pathLength / 4 * this.a);
-	var numISteps = 1 + Math.floor(pathLength / 4 * this.b);
-	var totalSteps = numSteps*numISteps;
-	var offset = 1 + Math.floor(pathLength / 4 * this.c);
 
 	var size = g_Map.getSize();
 	var gotRet = [];
 	for (var i = 0; i < size; ++i)
 		gotRet[i] = new Uint8Array(size);			// bool / uint8
+	let numStepsWaviness = 1 + Math.floor(pathLength / 4 * this.waviness);
+	let numStepsLength = 1 + Math.floor(pathLength / 4 * this.smoothness);
+	let offset = 1 + Math.floor(pathLength / 4 * this.offset);
 
 	// Generate random offsets
-	var ctrlVals = new Float32Array(numSteps);		//float32
-	for (var j = 1; j < (numSteps-1); ++j)
-	{
+	let ctrlVals = new Float32Array(numStepsWaviness);
+	for (let j = 1; j < numStepsWaviness - 1; ++j)
 		ctrlVals[j] = randFloat(-offset, offset);
-	}
 
 	// Interpolate for smoothed 1D noise
-	var noise = new Float32Array(totalSteps+1);		//float32
-	for (var j = 0; j < numSteps; ++j)
-		for (let k = 0; k < numISteps; ++k)
-			noise[j * numISteps + k] = cubicInterpolation(
+	let totalSteps = numStepsWaviness * numStepsLength;
+	let noise = new Float32Array(totalSteps + 1);
+	for (let j = 0; j < numStepsWaviness; ++j)
+		for (let k = 0; k < numStepsLength; ++k)
+			noise[j * numStepsLength + k] = cubicInterpolation(
 				1,
-				k / numISteps,
-				ctrlVals[(j + numSteps - 1) % numSteps],
+				k / numStepsLength,
+				ctrlVals[(j + numStepsWaviness - 1) % numStepsWaviness],
 				ctrlVals[j],
-				ctrlVals[(j + 1) % numSteps],
-				ctrlVals[(j + 2) % numSteps]);
+				ctrlVals[(j + 1) % numStepsWaviness],
+				ctrlVals[(j + 2) % numStepsWaviness]);
 
 	// Add smoothed noise to straight path
 	let pathPerpendicular = Vector2D.sub(this.end, this.start).normalize().perpendicular();
-	var segments1 = [];
-	var segments2 = [];
+	let segments1 = [];
+	let segments2 = [];
 
-	for (var j = 0; j < totalSteps; ++j)
+	for (let j = 0; j < totalSteps; ++j)
 	{
 		// Interpolated points along straight path
 		let step1 = j / totalSteps;
@@ -148,7 +142,7 @@ PathPlacer.prototype.place = function(constraint)
 		let noiseEnd = Vector2D.add(stepEnd, Vector2D.mult(pathPerpendicular, noise[j + 1]));
 		let noisePerpendicular = Vector2D.sub(noiseEnd, noiseStart).normalize().perpendicular();
 
-		let taperedWidth = (1 - step1 * this.taper) * this.width / 2;
+		let taperedWidth = (1 - step1 * this.tapering) * this.width / 2;
 
 		let point1 = Vector2D.sub(noiseStart, Vector2D.mult(noisePerpendicular, taperedWidth)).round();
 		let point2 = Vector2D.add(noiseEnd, Vector2D.mult(noisePerpendicular, taperedWidth)).round();
