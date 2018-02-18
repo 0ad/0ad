28cfced441209a79a2156b6f39964e38113028df
Imarok <Imarok@3db68df2-c116-0410-a063-a993310a9797>
2018-02-18 10:50:06 +0000

    Small StatisticsTracker test
    Reviewed by: elexis
    Differential Revision: https://code.wildfiregames.com/D1305
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@21250 3db68df2-c116-0410-a063-a993310a9797

---
+++ binaries/data/mods/public/simulation/components/tests/test_StatisticsTracker.js
@@ -0,0 +1,36 @@
+Engine.LoadComponentScript("StatisticsTracker.js");
+Engine.LoadComponentScript("interfaces/StatisticsTracker.js");
+Engine.LoadComponentScript("interfaces/Timer.js");
+
+AddMock(SYSTEM_ENTITY, IID_Timer, {
+	"SetInterval": () => true
+});
+
+Resources = {
+	"GetCodes": () => ["food", "metal", "stone", "wood"]
+};
+
+let cmpStatisticsTracker = ConstructComponent(SYSTEM_ENTITY, "StatisticsTracker");
+let obj1 = {
+	"successfulBribes": 3,
+	"unitsTrained": {
+		"Infantry": 5,
+		"Worker": 7
+	}
+};
+let obj2 = {
+	"successfulBribes": [11, 13, 17],
+	"unitsTrained": {
+		"Infantry": [19, 23],
+		"Worker": 29
+	}
+};
+
+cmpStatisticsTracker.PushValue(obj1, obj2);
+TS_ASSERT_UNEVAL_EQUALS(obj2, {
+	"successfulBribes": [11, 13, 17, 3],
+	"unitsTrained": {
+		"Infantry": [19, 23, 5],
+		"Worker": [7]
+	}
+});
