109aadc38088a33b92cae8e361daa38ba738fe0d
vladislavbelov <vladislavbelov@3db68df2-c116-0410-a063-a993310a9797>
2018-03-13 20:26:30 +0000

    Little cleanup for the rP21540
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@21541 3db68df2-c116-0410-a063-a993310a9797

--- source/tools/atlas/AtlasUI/CustomControls/MapDialog/MapDialog.cpp
+++ source/tools/atlas/AtlasUI/CustomControls/MapDialog/MapDialog.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017 Wildfire Games.
+/* Copyright (C) 2018 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -205,7 +205,7 @@ void MapDialog::OnFilename(wxCommandEvent& evt)
 	m_FileName = evt.GetString();
 }
 
-void MapDialog::OnNotebookChanged(wxBookCtrlEvent& evt)
+void MapDialog::OnNotebookChanged(wxBookCtrlEvent& WXUNUSED(evt))
 {
 	if (m_Type != MAPDIALOG_OPEN)
 		return;
