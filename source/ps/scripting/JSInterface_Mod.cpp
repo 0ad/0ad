c30c07df84a12268347b9cebe5bc7c72fe8bab0b
elexis <elexis@3db68df2-c116-0410-a063-a993310a9797>
2018-04-24 13:12:08 +0000

    Move ModIo JS interface functions to a separate namespace, refs rP21759.
    Differential Revision: https://code.wildfiregames.com/D1466
    Reviewed By: Itms
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@21778 3db68df2-c116-0410-a063-a993310a9797

--- source/ps/scripting/JSInterface_Mod.cpp
+++ source/ps/scripting/JSInterface_Mod.cpp
@@ -19,9 +19,7 @@
 
 #include "JSInterface_Mod.h"
 
-#include "ps/CLogger.h"
 #include "ps/Mod.h"
-#include "ps/ModIo.h"
 
 extern void restart_engine();
 
@@ -55,142 +53,10 @@ void JSI_Mod::SetMods(ScriptInterface::CxPrivate* UNUSED(pCxPrivate), const std:
 	g_modsLoaded = mods;
 }
 
-void JSI_Mod::ModIoStartGetGameId(ScriptInterface::CxPrivate* UNUSED(pCxPrivate))
-{
-	if (!g_ModIo)
-		g_ModIo = new ModIo();
-
-	ENSURE(g_ModIo);
-
-	g_ModIo->StartGetGameId();
-}
-
-void JSI_Mod::ModIoStartListMods(ScriptInterface::CxPrivate* UNUSED(pCxPrivate))
-{
-	if (!g_ModIo)
-	{
-		LOGERROR("ModIoStartListMods called before ModIoStartGetGameId");
-		return;
-	}
-
-	g_ModIo->StartListMods();
-}
-
-void JSI_Mod::ModIoStartDownloadMod(ScriptInterface::CxPrivate* UNUSED(pCxPrivate), uint32_t idx)
-{
-	if (!g_ModIo)
-	{
-		LOGERROR("ModIoStartDownloadMod called before ModIoStartGetGameId");
-		return;
-	}
-
-	g_ModIo->StartDownloadMod(idx);
-}
-
-bool JSI_Mod::ModIoAdvanceRequest(ScriptInterface::CxPrivate* pCxPrivate)
-{
-	if (!g_ModIo)
-	{
-		LOGERROR("ModIoAdvanceRequest called before ModIoGetMods");
-		return false;
-	}
-
-	ScriptInterface* scriptInterface = pCxPrivate->pScriptInterface;
-	return g_ModIo->AdvanceRequest(*scriptInterface);
-}
-
-void JSI_Mod::ModIoCancelRequest(ScriptInterface::CxPrivate* UNUSED(pCxPrivate))
-{
-	if (!g_ModIo)
-	{
-		LOGERROR("ModIoCancelRequest called before ModIoGetMods");
-		return;
-	}
-
-	g_ModIo->CancelRequest();
-}
-
-JS::Value JSI_Mod::ModIoGetMods(ScriptInterface::CxPrivate* pCxPrivate)
-{
-	if (!g_ModIo)
-	{
-		LOGERROR("ModIoGetMods called before ModIoStartGetGameId");
-		return JS::NullValue();
-	}
-
-	ScriptInterface* scriptInterface = pCxPrivate->pScriptInterface;
-	JSContext* cx = scriptInterface->GetContext();
-	JSAutoRequest rq(cx);
-
-	const std::vector<ModIoModData>& availableMods = g_ModIo->GetMods();
-
-	JS::RootedObject mods(cx, JS_NewArrayObject(cx, availableMods.size()));
-	if (!mods)
-		return JS::NullValue();
-
-	u32 i = 0;
-	for (const ModIoModData& mod : availableMods)
-	{
-		JS::RootedValue m(cx, JS::ObjectValue(*JS_NewPlainObject(cx)));
-		for (const std::pair<std::string, std::string>& prop : mod.properties)
-			scriptInterface->SetProperty(m, prop.first.c_str(), prop.second, true);
-
-		scriptInterface->SetProperty(m, "dependencies", mod.dependencies, true);
-
-		JS_SetElement(cx, mods, i++, m);
-	}
-
-	return JS::ObjectValue(*mods);
-}
-
-const std::map<DownloadProgressStatus, std::string> statusStrings = {
-	{ DownloadProgressStatus::NONE, "none" },
-	{ DownloadProgressStatus::GAMEID, "gameid" },
-	{ DownloadProgressStatus::READY, "ready" },
-	{ DownloadProgressStatus::LISTING, "listing" },
-	{ DownloadProgressStatus::LISTED, "listed" },
-	{ DownloadProgressStatus::DOWNLOADING, "downloading" },
-	{ DownloadProgressStatus::SUCCESS, "success" },
-	{ DownloadProgressStatus::FAILED_GAMEID, "failed_gameid" },
-	{ DownloadProgressStatus::FAILED_LISTING, "failed_listing" },
-	{ DownloadProgressStatus::FAILED_DOWNLOADING, "failed_downloading" },
-	{ DownloadProgressStatus::FAILED_FILECHECK, "failed_filecheck" }
-};
-
-JS::Value JSI_Mod::ModIoGetDownloadProgress(ScriptInterface::CxPrivate* pCxPrivate)
-{
-	if (!g_ModIo)
-	{
-		LOGERROR("ModIoGetDownloadProgress called before ModIoGetMods");
-		return JS::NullValue();
-	}
-
-	ScriptInterface* scriptInterface = pCxPrivate->pScriptInterface;
-	JSContext* cx = scriptInterface->GetContext();
-	JSAutoRequest rq(cx);
-
-	JS::RootedValue progressData(cx, JS::ObjectValue(*JS_NewPlainObject(cx)));
-	const DownloadProgressData& progress = g_ModIo->GetDownloadProgress();
-
-	scriptInterface->SetProperty(progressData, "status", statusStrings.at(progress.status), true);
-	scriptInterface->SetProperty(progressData, "progress", progress.progress, true);
-	scriptInterface->SetProperty(progressData, "error", progress.error, true);
-
-	return progressData;
-}
-
 void JSI_Mod::RegisterScriptFunctions(const ScriptInterface& scriptInterface)
 {
 	scriptInterface.RegisterFunction<JS::Value, &GetEngineInfo>("GetEngineInfo");
 	scriptInterface.RegisterFunction<JS::Value, &JSI_Mod::GetAvailableMods>("GetAvailableMods");
 	scriptInterface.RegisterFunction<void, &JSI_Mod::RestartEngine>("RestartEngine");
 	scriptInterface.RegisterFunction<void, std::vector<CStr>, &JSI_Mod::SetMods>("SetMods");
-
-	scriptInterface.RegisterFunction<void, &JSI_Mod::ModIoStartGetGameId>("ModIoStartGetGameId");
-	scriptInterface.RegisterFunction<void, &JSI_Mod::ModIoStartListMods>("ModIoStartListMods");
-	scriptInterface.RegisterFunction<void, uint32_t, &JSI_Mod::ModIoStartDownloadMod>("ModIoStartDownloadMod");
-	scriptInterface.RegisterFunction<bool, &JSI_Mod::ModIoAdvanceRequest>("ModIoAdvanceRequest");
-	scriptInterface.RegisterFunction<void, &JSI_Mod::ModIoCancelRequest>("ModIoCancelRequest");
-	scriptInterface.RegisterFunction<JS::Value, &JSI_Mod::ModIoGetMods>("ModIoGetMods");
-	scriptInterface.RegisterFunction<JS::Value, &JSI_Mod::ModIoGetDownloadProgress>("ModIoGetDownloadProgress");
 }
