b36ed5f11b4876277f4af39e3ea62ca8003ae79f
andy5995 <andy400-dev@yahoo.com>
2018-08-22 06:56:17 -0500

    .travis.yml:remove osx test
    removing osx tests based on @wraitii's comment in
    https://wildfiregames.com/forum/index.php?/topic/24753-building-0ad-on-osx-high-sierra-using-brew/

--- .travis_before_osx_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-brew update
-brew install  \
-  enet libogg glew gloox libogg libsodium \
-  libvorbis libxml2 miniupnpc sdl2
-
-brew outdated cmake || brew upgrade cmake
-brew outdated pkgconfig || brew upgrade pkgconfig
