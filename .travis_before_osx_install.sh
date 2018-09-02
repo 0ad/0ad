b81159c133c9380fc550c0a5202ba08d74f4530a
andy5995 <andy400-dev@yahoo.com>
2018-09-02 11:47:52 -0500

    limit length of output

--- .travis_before_osx_install.sh
+++ .travis_before_osx_install.sh
@@ -4,7 +4,9 @@ brew update
 brew outdated cmake || brew upgrade cmake; brew outdated pkgconfig || brew upgrade pkgconfig
 
 cd libraries/osx
-./build-osx-libs.sh
+# Travis will terminate the build if the log exceeds 4MB
+# so we'll send the output of the script to /dev/null
+./build-osx-libs.sh &> /dev/null
 
 # Change back to src root. The commands to build are in .travis.yml
 cd ../..
