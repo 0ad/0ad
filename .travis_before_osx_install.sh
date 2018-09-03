2fcce90437a71685120f1e272beb1ff776bbf21a
andy5995 <andy400-dev@yahoo.com>
2018-09-02 20:54:03 -0500

    only redirect stdout to /dev/null
    Travis will terminate the build if it doesn't detect any output for a
    period of time. It assumes something wrong.

--- .travis_before_osx_install.sh
+++ .travis_before_osx_install.sh
@@ -5,8 +5,9 @@ brew outdated cmake || brew upgrade cmake; brew outdated pkgconfig || brew upgra
 
 cd libraries/osx
 # Travis will terminate the build if the log exceeds 4MB
-# so we'll send the output of the script to /dev/null
-./build-osx-libs.sh &> /dev/null
+# so we'll send stdout to /dev/null.
+#
+./build-osx-libs.sh > /dev/null
 
 # Change back to src root. The commands to build are in .travis.yml
 cd ../..
