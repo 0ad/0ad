518a157858ce0c019aa330c2b3772e1720e5bed4
elexis <elexis@3db68df2-c116-0410-a063-a993310a9797>
2018-09-29 21:25:04 +0000

    Language selection dropdown in the terms dialog.
    Allows the user to either read the english Terms and Conditions written by Wildfire Games or the version translated into the current locale by transifex users.
    The underlying problem that Wildfire Games cannot verify the accuracy or completeness of the translations while providing the services to a global audience remains.
    
    Refs #5257
    Differential Revision: https://code.wildfiregames.com/D1643
    Comments By: bb on irc, smiley, asterix
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@21894 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/mod/gui/termsdialog/termsdialog.js
+++ binaries/data/mods/mod/gui/termsdialog/termsdialog.js
@@ -1,17 +1,14 @@
 var g_TermsPage;
+var g_TermsFile;
 
 function init(data)
 {
 	g_TermsPage = data.page;
+	g_TermsFile = data.file;
 
 	Engine.GetGUIObjectByName("title").caption = data.title;
-
-	Engine.GetGUIObjectByName("mainText").caption =
-		Engine.FileExists(data.file) ?
-		Engine.TranslateLines(Engine.ReadFile(data.file)) :
-		data.file;
-
 	initURLButtons(data.urlButtons);
+	initLanguageSelection();
 }
 
 function initURLButtons(urlButtons)
@@ -28,6 +25,40 @@ function initURLButtons(urlButtons)
 			openURL(urlButton.url);
 		};
 	});
+}
+
+function initLanguageSelection()
+{
+	let languageLabel = Engine.GetGUIObjectByName("languageLabel");
+	let languageLabelWidth = Engine.GetTextWidth(languageLabel.font, languageLabel.caption)
+	languageLabel.size = "0 0 " + languageLabelWidth + " 100%";
+
+	let languageDropdown = Engine.GetGUIObjectByName("languageDropdown");
+	languageDropdown.size = (languageLabelWidth + 10) + " 4 100% 100%";
+
+	languageDropdown.list = (() => {
+		let displayNames = Engine.GetSupportedLocaleDisplayNames();
+		let baseNames = Engine.GetSupportedLocaleBaseNames();
+
+		// en-US
+		let list = [displayNames[0]];
+
+		// current locale
+		let currentLocaleDict = Engine.GetFallbackToAvailableDictLocale(Engine.GetCurrentLocale());
+		if (currentLocaleDict != baseNames[0])
+			list.push(displayNames[baseNames.indexOf(currentLocaleDict)]);
+
+		return list;
+	})();
+
+	languageDropdown.onSelectionChange = () => {
+		Engine.GetGUIObjectByName("mainText").caption =
+			Engine.FileExists(g_TermsFile) ?
+			(languageDropdown.selected == 1 ? Engine.TranslateLines(Engine.ReadFile(g_TermsFile)) : Engine.ReadFile(g_TermsFile)) :
+			g_TermsFile;
+	};
+
+	languageDropdown.selected = languageDropdown.list.length - 1;
 }
 
 function closeTerms(accepted)
