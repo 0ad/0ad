33275154b511e08ca2ce069ba7e97d8e1c4cbbc0
vladislavbelov <vladislavbelov@3db68df2-c116-0410-a063-a993310a9797>
2019-04-02 18:49:12 +0000

    Removes unused variable. It was introduced in rP3893, an interface to control the variable was removed in rP13877. A usage was removed in rP22039. And since then the usage is useless because we use infinity sky model.
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@22154 3db68df2-c116-0410-a063-a993310a9797

--- source/renderer/SkyManager.h
+++ source/renderer/SkyManager.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018 Wildfire Games.
+/* Copyright (C) 2019 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -75,7 +75,6 @@ private:
 	void LoadSkyTextures();
 
 	bool m_RenderSky;
-	float m_HorizonHeight;
 
 	/// Name of current skyset (a directory within art/textures/skies)
 	CStrW m_SkySet;
