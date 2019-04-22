253d6e73a0e36dc9e9468059540e5de8f5c10ad9
vladislavbelov <vladislavbelov@3db68df2-c116-0410-a063-a993310a9797>
2019-04-22 22:12:08 +0000

    Refactor and cleanup of CGameView.
    Commented By: asterix, elexis, Stan, wraitii
    Differential Revision: https://code.wildfiregames.com/D1571
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@22214 3db68df2-c116-0410-a063-a993310a9797

--- source/graphics/GameView.h
+++ source/graphics/GameView.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2010 Wildfire Games.
+/* Copyright (C) 2019 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -30,34 +30,11 @@ class CCinemaManager;
 class CVector3D;
 struct SViewPort;
 
-class JSObject;
-
 class CGameViewImpl;
 
 class CGameView : private Scene
 {
 	NONCOPYABLE(CGameView);
-private:
-	CGameViewImpl* m;
-
-	// Check whether lighting environment has changed and update vertex data if necessary
-	void CheckLightEnv();
-
-public:
-	//BEGIN: Implementation of Scene
-	virtual void EnumerateObjects(const CFrustum& frustum, SceneCollector* c);
-	virtual CLOSTexture& GetLOSTexture();
-	virtual CTerritoryTexture& GetTerritoryTexture();
-	//END: Implementation of Scene
-
-private:
-	// InitResources(): Load all graphics resources (textures, actor objects and
-	// alpha maps) required by the game
-	//void InitResources();
-
-	// UnloadResources(): Unload all graphics resources loaded by InitResources
-	void UnloadResources();
-
 public:
 	CGameView(CGame *pGame);
 	~CGameView();
@@ -66,8 +43,6 @@ public:
 
 	void RegisterInit();
 	int Initialize();
-
-	CObjectManager& GetObjectManager() const;
 
 	/**
 	 * Updates all the view information (i.e. rotate camera, scroll, whatever). This will *not* change any
@@ -82,29 +57,25 @@ public:
 
 	InReaction HandleEvent(const SDL_Event_* ev);
 
-	float GetCameraX();
-	float GetCameraZ();
-	float GetCameraPosX();
-	float GetCameraPosY();
-	float GetCameraPosZ();
-	float GetCameraRotX();
-	float GetCameraRotY();
-	float GetCameraZoom();
-	void SetCamera(CVector3D Pos, float RotX, float RotY, float Zoom);
-	void MoveCameraTarget(const CVector3D& target);
-	void ResetCameraTarget(const CVector3D& target);
-	void ResetCameraAngleZoom();
-	void CameraFollow(entity_id_t entity, bool firstPerson);
-	entity_id_t GetFollowedEntity();
-
-	CVector3D GetSmoothPivot(CCamera &camera) const;
-
+	CVector3D GetCameraPivot() const;
+	CVector3D GetCameraPosition() const;
+	CVector3D GetCameraRotation() const;
+	float GetCameraZoom() const;
 	float GetNear() const;
 	float GetFar() const;
 	float GetFOV() const;
 
+	void SetCamera(CVector3D Pos, float RotX, float RotY, float Zoom);
+	void MoveCameraTarget(const CVector3D& target);
+	void ResetCameraTarget(const CVector3D& target);
+	void CameraFollow(entity_id_t entity, bool firstPerson);
+	entity_id_t GetFollowedEntity();
+
+	// Set projection of current camera using near, far, and FOV values
+	void SetCameraProjection();
+
 	#define DECLARE_BOOLEAN_SETTING(NAME) \
-	bool Get##NAME##Enabled(); \
+	bool Get##NAME##Enabled() const; \
 	void Set##NAME##Enabled(bool Enabled);
 
 	DECLARE_BOOLEAN_SETTING(Culling);
@@ -113,13 +84,28 @@ public:
 
 	#undef DECLARE_BOOLEAN_SETTING
 
-	// Set projection of current camera using near, far, and FOV values
-	void SetCameraProjection();
-
-	CCamera *GetCamera();
+	CCamera* GetCamera();
 	CCinemaManager* GetCinema();
+	CObjectManager& GetObjectManager();
 
-	JSObject* GetScript();
+	// Implementations of Scene
+	void EnumerateObjects(const CFrustum& frustum, SceneCollector* c) override;
+	CLOSTexture& GetLOSTexture() override;
+	CTerritoryTexture& GetTerritoryTexture() override;
+
+private:
+	// Unloads all graphics resources loaded by RegisterInit.
+	void UnloadResources();
+
+	// Checks whether lighting environment has changed and update vertex data if necessary.
+	void CheckLightEnv();
+
+	CVector3D GetSmoothPivot(CCamera &camera) const;
+	void ResetCameraAngleZoom();
+
+	CGameViewImpl* m;
 };
+
 extern InReaction game_view_handler(const SDL_Event_* ev);
-#endif
+
+#endif // INCLUDED_GAMEVIEW
