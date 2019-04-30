fe6d493c1b79aa4abe7df7f9137b146a2a4db63b
Brian Broll <brian.broll@gmail.com>
2019-04-30 09:07:05 -0700

    WIP add two commands to the test client

--- test-client/test.py
+++ test-client/test.py
@@ -16,8 +16,14 @@ print('Connected')
 
 while True:
     # TODO: Create Action...
-    action = RLAPI_pb2.Actions()
-    obs = stub.Step(action)
+    cmds = RLAPI_pb2.Actions()
+    cmd1 = {"type": "aichat", "message": "/msg hello??"}
+    cmd2 = {"type": "aichat", "message": "/msg anyone there??"}
+    cmds.actions.extend([
+        RLAPI_pb2.Action(content=json.dumps(cmd1)),
+        RLAPI_pb2.Action(content=json.dumps(cmd2))
+    ])
+    obs = stub.Step(cmds)
     print('stepping... State is')
     print(json.loads(obs.content))
     time.sleep(1)
