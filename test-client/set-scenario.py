ee82a365878908c7bb08359ede0e14f47dd20f52
Brian Broll <brian.broll@gmail.com>
2019-05-18 07:40:27 -0700

    WIP Updated set-scenario test script for changing the config

--- test-client/set-scenario.py
+++ test-client/set-scenario.py
@@ -7,14 +7,16 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', default='localhost:50051')
-parser.add_argument('--map', default='scenarios/Battle for the Tiber')
+parser.add_argument('--type', default='scenario')
+parser.add_argument('--name', default='Battle for the Tiber')
 args = parser.parse_args()
 
 channel = grpc.insecure_channel(args.host)
 stub = RLAPI_pb2_grpc.RLAPIStub(channel)
 
 print('Connecting...', file=sys.stderr)
-req = RLAPI_pb2.ConnectRequest(map=args.map)
+scenario = RLAPI_pb2.ScenarioConfig(type=args.type, name=args.name)
+req = RLAPI_pb2.ConnectRequest(scenario=scenario)
 obs = stub.Connect(req)
 print('Connected', file=sys.stderr)
 print(obs.content)
