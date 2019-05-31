ec5e6eee6cafef287f99928a9660f3477ec3dbd0
Brian Broll <brian.broll@gmail.com>
2019-05-31 10:02:27 -0700

    Added zero_ad python client library

---
+++ source/tools/clients/python/README.md
@@ -0,0 +1,24 @@
+# 0 AD Python Client
+This directory contains `zero_ad`, a python client for 0 AD which enables users to control the environment headlessly.
+
+```
+from zero_ad import actions
+
+actions.Attack(entities, target)
+actions.Walk(entities, x, z)
+
+zero_ad.ScenarioConfig(entities, target)
+zero_ad.GameState(entities, target)
+
+zero_ad.Game(entities, target)
+
+zero_ad.Pyrogenesis(entities, target)
+zero_ad.GymEnv(entities, target)
+
+
+```
+
+# To Do:
+- How would I write run-scripted.py?
+- add player ids?
+- add player ids?
---
+++ source/tools/clients/python/zero_ad/actions.py
@@ -0,0 +1,46 @@
+def debug_print(message):
+    return {
+        'type': 'debug-print',
+        'message': message
+    }
+
+def chat(message):
+    return {
+        'type': 'chat',
+        'message': message
+    }
+
+def diplomacy(player, status='ally'):
+    return {
+        'type': 'diplomacy',
+        'player': player,
+        'to': status,  # ally, neutral or enemy
+    }
+
+def tribute(player, amounts):
+    return {
+        'type': 'diplomacy',
+        'player': player,
+        'amounts': amounts  # TODO: What should this be??
+    }
+
+def walk(units, x, z, queued=False):
+    ids = [ unit['id'] for unit in units ]
+    return {
+        'type': 'walk',
+        'entities': ids,
+        'x': x,
+        'z': z,
+        'queued': queued
+    }
+
+def attack(units, target, queued=False, allow_capture=True):
+    unit_ids = [ unit['id'] for unit in units ]
+    return {
+        'type': 'attack',
+        'entities': unit_ids,
+        'target': target['id'],
+        'allowCapture': allow_capture,
+        'queued': queued
+    }
+
