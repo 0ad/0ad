e7f46480713df46fd9907d4a413eac6e65ad53d0
vladislavbelov <vladislavbelov@3db68df2-c116-0410-a063-a993310a9797>
2019-06-12 19:21:44 +0000

    Refactors Developer Overlay using an object instead of global functions.
    Reviewed By: wraitii
    Commented By: elexis
    Differential Revision: https://code.wildfiregames.com/D1928
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@22370 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/session/selection.js
+++ binaries/data/mods/public/gui/session/selection.js
@@ -215,7 +215,7 @@ EntitySelection.prototype.update = function()
 	this.checkRenamedEntities();
 
 	let changed = false;
-	let removeOwnerChanges = !g_IsObserver && !g_DevSettings.controlAll && this.toList().length > 1;
+	let removeOwnerChanges = !g_IsObserver && !g_DeveloperOverlay.isControlAll() && this.toList().length > 1;
 
 	for (let ent in this.selected)
 	{
