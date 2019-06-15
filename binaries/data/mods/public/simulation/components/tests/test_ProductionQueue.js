73602a1d3c4f24bc1ed54840cd39d5a38b93aa9f
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2019-06-15 17:27:24 +0000

    Calculate entity limit counts correctly when SpawnUnits fails in ProductionQueue.
    When adding a batch of unit, these in-training units get added to the production queue and to the entity limit count.
    These in-training units need to be removed from the entity limit counts when spawning them, or we would be double-counting them. This was done when creating the cached entities, but this was too early: entities might fail to spawn, for example when there is no room around the foundation.
    
    Change that so the entity limit count is now decremented right before giving spawned entities the correct owner (which triggers EntityLimits OnGlobalOwnershipChanged, which adds the spawned entities to the entity limit count).
    
    Additionally, add Init to TrainingRestrictions so that the test setup doesn't complain. Other components have an empty Init instead of checking for Init in the test setup (and 61/67 have an Init function) so it seems more standard this way.
    
    
    Reported By: elexis
    Reviewed By: wraitii
    Patch By: Angen
    Tests By: wraitii
    
    Differential Revision: https://code.wildfiregames.com/D1879
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@22375 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/tests/test_ProductionQueue.js
+++ binaries/data/mods/public/simulation/components/tests/test_ProductionQueue.js
@@ -139,3 +139,145 @@ TS_ASSERT_UNEVAL_EQUALS(
 	cmpProductionQueue.GetTechnologiesList(),
 	["gather_fishing_net", "phase_town_generic", "phase_city_generic"]
 );
+
+Engine.LoadComponentScript("interfaces/BuildRestrictions.js");
+Engine.LoadComponentScript("interfaces/EntityLimits.js");
+Engine.LoadComponentScript("interfaces/Foundation.js");
+Engine.LoadComponentScript("interfaces/StatisticsTracker.js");
+Engine.LoadComponentScript("interfaces/Timer.js");
+Engine.LoadComponentScript("interfaces/TrainingRestrictions.js");
+Engine.LoadComponentScript("interfaces/Trigger.js");
+Engine.LoadComponentScript("EntityLimits.js");
+Engine.LoadComponentScript("TrainingRestrictions.js");
+Engine.LoadHelperScript("Sound.js");
+
+Engine.RegisterGlobal("ApplyValueModificationsToEntity", (_, value) => value);
+Engine.RegisterGlobal("ApplyValueModificationsToTemplate", (_, value) => value);
+
+function regression_test_d1879()
+{
+	// Setup
+	let playerEnt = 2;
+	let playerID = 1;
+	let testEntity = 3;
+	let spawedEntityIDs = [4, 5, 6, 7, 8];
+	let spawned = 0;
+
+	Engine.AddEntity = () => {
+		let id = spawedEntityIDs[spawned++];
+
+		ConstructComponent(id, "TrainingRestrictions", {
+			"Category": "some_limit"
+		});
+
+		AddMock(id, IID_Identity, {
+			"GetClassesList": () => []
+		});
+
+		AddMock(id, IID_Position, {
+			"JumpTo": () => {}
+		});
+
+		AddMock(id, IID_Ownership, {
+			"SetOwner": (pid) => {
+				let cmpEntLimits = QueryOwnerInterface(id, IID_EntityLimits);
+				cmpEntLimits.OnGlobalOwnershipChanged({
+					"entity": id,
+					"from": -1,
+					"to": pid
+				});
+			},
+			"GetOwner": () => playerID
+		});
+
+		return id;
+	};
+
+	ConstructComponent(playerEnt, "EntityLimits", {
+		"Limits": {
+			"some_limit": 8
+		},
+		"LimitChangers": {},
+		"LimitRemovers": {}
+	});
+
+	AddMock(SYSTEM_ENTITY, IID_GuiInterface, {
+		"PushNotification": () => {}
+	});
+
+	AddMock(SYSTEM_ENTITY, IID_Trigger, {
+		"CallEvent": () => {}
+	});
+
+	AddMock(SYSTEM_ENTITY, IID_Timer, {
+		"SetTimeout": (ent, iid, func) => {}
+	});
+
+	AddMock(SYSTEM_ENTITY, IID_TemplateManager, {
+		"TemplateExists": () => true,
+		"GetTemplate": name => ({
+			"Cost": {
+				"BuildTime": 0,
+				"Population": 1,
+				"Resources": {}
+			},
+			"TrainingRestrictions": {
+				"Category": "some_limit"
+			}
+		})
+	});
+
+	AddMock(SYSTEM_ENTITY, IID_PlayerManager, {
+		"GetPlayerByID": id => playerEnt
+	});
+
+	AddMock(playerEnt, IID_Player, {
+		"GetCiv": () => "iber",
+		"GetPlayerID": () => playerID,
+		"GetTimeMultiplier": () => 0,
+		"BlockTraining": () => {},
+		"UnBlockTraining": () => {},
+		"UnReservePopulationSlots": () => {},
+		"TrySubtractResources": () => true,
+		"TryReservePopulationSlots": () => false // Always have pop space.
+	});
+
+	AddMock(testEntity, IID_Ownership, {
+		"GetOwner": () => playerID
+	});
+
+	let cmpProdQueue = ConstructComponent(testEntity, "ProductionQueue", {
+		"Entities": { "_string": "some_template" },
+		"BatchTimeModifier": 1
+	});
+
+	let cmpEntLimits = QueryOwnerInterface(testEntity, IID_EntityLimits);
+	TS_ASSERT(cmpEntLimits.AllowedToTrain("some_limit", 8));
+	TS_ASSERT(!cmpEntLimits.AllowedToTrain("some_limit", 9));
+
+	// Check that the entity limits do get updated if the spawn succeeds.
+	AddMock(testEntity, IID_Footprint, {
+		"PickSpawnPoint": () => ({ "x": 0, "y": 1, "z": 0 })
+	});
+
+	cmpProdQueue.AddBatch("some_template", "unit", 3);
+
+	TS_ASSERT_EQUALS(cmpEntLimits.GetCounts().some_limit, 3);
+
+	Engine.QueryInterface(testEntity, IID_ProductionQueue).ProgressTimeout();
+
+	TS_ASSERT_EQUALS(cmpEntLimits.GetCounts().some_limit, 3);
+
+	// Now check that it doesn't get updated when the spawn doesn't succeed.
+	AddMock(testEntity, IID_Footprint, {
+		"PickSpawnPoint": () => ({ "x": -1, "y": -1, "z": -1 })
+	});
+
+	cmpProdQueue.AddBatch("some_template", "unit", 3);
+	Engine.QueryInterface(testEntity, IID_ProductionQueue).ProgressTimeout();
+
+	TS_ASSERT_EQUALS(cmpProdQueue.GetQueue().length, 1);
+	TS_ASSERT_EQUALS(cmpEntLimits.GetCounts().some_limit, 6);
+}
+
+regression_test_d1879();
