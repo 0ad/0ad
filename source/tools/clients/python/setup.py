27f88321bfe3e7ab768968fd77a18e0d6bd816a3
Brian Broll <brian.broll@gmail.com>
2019-06-26 11:07:22 -0700

    add dependencies to setup.py. Switch to semantic versioning

--- source/tools/clients/python/setup.py
+++ source/tools/clients/python/setup.py
@@ -1,11 +1,19 @@
+import os
 from setuptools import setup
 
+project_root = os.path.dirname(os.path.realpath(__file__))
+requirementPath = os.path.join(project_root, 'requirements.txt')
+install_requires = []
+with open(requirementPath) as f:
+    install_requires = f.read().splitlines()
+
 setup(name='zero_ad',
-      version='0.1',
+      version='0.0.1',
       description='Python client for 0 AD',
       url='http://github.com/brollb/0ad',
       author='Brian Broll',
       author_email='brian.broll@gmail.com',
+      install_requires=install_requires,
       license='MIT',
       packages=['zero_ad'],
       zip_safe=False)
