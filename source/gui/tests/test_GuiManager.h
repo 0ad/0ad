5951c33aa51e9fa44659fd901ab5fd89182182b5
Itms <Itms@3db68df2-c116-0410-a063-a993310a9797>
2019-09-17 19:27:38 +0000

    Fix rP22909 on Windows.
    Patch By: Angen
    Reviewed By: elexis
    Differential Revision: https://code.wildfiregames.com/D2299
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@22917 3db68df2-c116-0410-a063-a993310a9797

--- source/gui/tests/test_GuiManager.h
+++ source/gui/tests/test_GuiManager.h
@@ -35,7 +35,7 @@ public:
 	{
 		g_VFS = CreateVfs();
 		TS_ASSERT_OK(g_VFS->Mount(L"", DataDir()/"mods"/"_test.gui", VFS_MOUNT_MUST_EXIST));
-		TS_ASSERT_OK(g_VFS->Mount(L"cache", DataDir()/"mods"/"_testcache/"));
+		TS_ASSERT_OK(g_VFS->Mount(L"cache", DataDir()/"mods"/"_testcache"));
 
 		configDB = new CConfigDB;
 
