e738e74c7f9ce47329529ec793ad0c264407f61b
vladislavbelov <vladislavbelov@3db68df2-c116-0410-a063-a993310a9797>
2020-01-20 20:58:17 +0000

    Saves sidebar/bottombar width/height between Atlas sessions
    Tested By: Nescio, Stan
    Differential Revision: https://code.wildfiregames.com/D2583
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@23424 3db68df2-c116-0410-a063-a993310a9797

--- source/tools/atlas/AtlasUI/CustomControls/SnapSplitterWindow/SnapSplitterWindow.cpp
+++ source/tools/atlas/AtlasUI/CustomControls/SnapSplitterWindow/SnapSplitterWindow.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2009 Wildfire Games.
+/* Copyright (C) 2020 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -19,15 +19,19 @@
 
 #include "SnapSplitterWindow.h"
 
+#include <wx/confbase.h>
+
 BEGIN_EVENT_TABLE(SnapSplitterWindow, wxSplitterWindow)
 	EVT_SPLITTER_SASH_POS_CHANGING(wxID_ANY, SnapSplitterWindow::OnSashPosChanging)
+	EVT_SPLITTER_SASH_POS_CHANGED(wxID_ANY, SnapSplitterWindow::OnSashPosChanged)
 	EVT_SPLITTER_DCLICK(wxID_ANY, SnapSplitterWindow::OnDoubleClick)
 END_EVENT_TABLE()
 
-SnapSplitterWindow::SnapSplitterWindow(wxWindow* parent, long style)
+SnapSplitterWindow::SnapSplitterWindow(wxWindow* parent, long style, const wxString& configPath)
 	: wxSplitterWindow(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize,
 					   style | wxSP_LIVE_UPDATE),
-					   m_SnapTolerance(16)
+					   m_SnapTolerance(16),
+					   m_ConfigPath(configPath)
 {
 	// Set min size, to disable unsplitting
 	SetMinimumPaneSize(32);
@@ -50,6 +54,9 @@ bool SnapSplitterWindow::SplitVertically(wxWindow *window1, wxWindow *window2, i
 {
 	if (sashPosition == 0)
 		sashPosition = m_DefaultSashPosition;
+
+	LoadSashPositionIfSaved(&sashPosition);
+
 	return wxSplitterWindow::SplitVertically(window1, window2, sashPosition);
 }
 
@@ -57,6 +64,9 @@ bool SnapSplitterWindow::SplitHorizontally(wxWindow *window1, wxWindow *window2,
 {
 	if (sashPosition == 0)
 		sashPosition = m_DefaultSashPosition;
+
+	LoadSashPositionIfSaved(&sashPosition);
+
 	return wxSplitterWindow::SplitHorizontally(window1, window2, sashPosition);
 }
 
@@ -71,8 +81,31 @@ void SnapSplitterWindow::OnSashPosChanging(wxSplitterEvent& evt)
 	}
 }
 
+void SnapSplitterWindow::OnSashPosChanged(wxSplitterEvent& WXUNUSED(evt))
+{
+	SaveSashPositionIfChanged();
+}
+
 void SnapSplitterWindow::OnDoubleClick(wxSplitterEvent& WXUNUSED(evt))
 {
 	int defaultPos = ConvertSashPosition(m_DefaultSashPosition);
 	SetSashPosition(defaultPos);
+}
+
+bool SnapSplitterWindow::LoadSashPositionIfSaved(int* sashPosition)
+{
+	wxASSERT(sashPosition);
+	// We use the same config path for both horizontal and vertical splits,
+	// because we can't have both at the same moment.
+	wxConfigBase* cfg = wxConfigBase::Get(false);
+	// wxConfigBase::Read doesn't modify the value if there was an error.
+	return cfg && cfg->Read(m_ConfigPath + _T("SashPosition"), sashPosition);
+}
+
+void SnapSplitterWindow::SaveSashPositionIfChanged()
+{
+	wxConfigBase* cfg = wxConfigBase::Get(false);
+	if (!cfg)
+		return;
+	cfg->Write(m_ConfigPath + _T("SashPosition"), GetSashPosition());
 }
