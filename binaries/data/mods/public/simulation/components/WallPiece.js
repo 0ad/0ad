97f4a4a75f8f1bbb7b36ed6a5b0b2f00be19307b
s0600204 <s0600204@3db68df2-c116-0410-a063-a993310a9797>
2020-05-21 19:11:00 +0000

    Correct WallPiece schema help comments
    (Fairly sure I've got them right this time.)
    
    Refs: #2944, D900, rP23684
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@23687 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/WallPiece.js
+++ binaries/data/mods/public/simulation/components/WallPiece.js
@@ -4,21 +4,21 @@ WallPiece.prototype.Schema =
 	"<a:help></a:help>" +
 	"<a:example>" +
 	"</a:example>" +
-	"<element name='Length' a:help='Measured in Terrain Tiles. Used in rmgen wallbuilder and the in-game wall-placer.'>" +
+	"<element name='Length' a:help='Meters. Used in rmgen wallbuilder and the in-game wall-placer.'>" +
 		"<ref name='nonNegativeDecimal'/>" +
 	"</element>" +
 	"<optional>" +
-		"<element name='Orientation' a:help='Radians. Default: 1. Used in rmgen wallbuilder. The angle to rotate the wallpiece so it is orientated the same way as every other wallpiece: with the \"line\" of the wall running along a map&apos;s `z` axis and the \"outside face\" towards positive `x`. If the piece bends (see below), the orientation should be that of the start of the wallpiece, not the middle.'>" +
+		"<element name='Orientation' a:help='Multiples of Pi, measured anti-clockwise. Default: 1; full revolution: 2. Used in rmgen wallbuilder. How the wallpiece should be rotated so it is orientated the same way as every other wallpiece: with the \"line\" of the wall running along a map&apos;s `z` axis and the \"outside face\" towards positive `x`. If the piece bends (see below), the orientation should be that of the start of the wallpiece, not the middle.'>" +
 			"<ref name='nonNegativeDecimal'/>" +
 		"</element>" +
 	"</optional>" +
 	"<optional>" +
-		"<element name='Indent' a:help='Measured in Terrain Tiles. Default: 0. Used in rmgen wallbuilder. Permits piece to be placed in front, behind, or inline with a wall.'>" +
+		"<element name='Indent' a:help='Meters. Default: 0. Used in rmgen wallbuilder. Permits piece to be placed in front (-ve value) or behind (+ve value) a wall.'>" +
 			"<data type='decimal'/>" +
 		"</element>" +
 	"</optional>" +
 	"<optional>" +
-		"<element name='Bend' a:help='Radians. Default: 0. Used in rmgen wallbuilder. Defines the angle that the wallpiece bends or curves across its length.'>" +
+		"<element name='Bend' a:help='Multiples of Pi, measured anti-clockwise. Default: 0. Used in rmgen wallbuilder. The difference in orientation between the ends of a wallpiece.'>" +
 			"<data type='decimal'/>" +
 		"</element>" +
 	"</optional>";
