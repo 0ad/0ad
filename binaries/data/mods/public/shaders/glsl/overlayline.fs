ce12098dfea8d027ca1b026f30774cdfbfc10d4a
vladislavbelov <vladislavbelov@3db68df2-c116-0410-a063-a993310a9797>
2020-06-12 15:39:44 +0000

    Adds GLSL shaders of overlay lines.
    We were using ARB shaders in GLSL mode for overlay lines (unit selection, ranges and so on).
    
    Tested By: Freagarach, OptimusShepard, Stan
    
    Differential Revision: https://code.wildfiregames.com/D2621
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@23765 3db68df2-c116-0410-a063-a993310a9797

---
+++ binaries/data/mods/public/shaders/glsl/overlayline.fs
@@ -0,0 +1,37 @@
+#version 120
+
+uniform sampler2D baseTex;
+uniform sampler2D maskTex;
+uniform sampler2D losTex;
+
+#if USE_OBJECTCOLOR
+uniform vec4 objectColor;
+#else
+varying vec4 v_color;
+#endif
+
+varying vec2 v_tex;
+varying vec2 v_los;
+
+void main()
+{
+#if USE_OBJECTCOLOR
+    vec3 color = objectColor.rgb;
+    float alpha = objectColor.a;
+#else
+    vec3 color = v_color.rgb;
+    float alpha = v_color.a;
+#endif
+
+    vec4 base = texture2D(baseTex, v_tex);
+    vec4 mask = texture2D(maskTex, v_tex);
+    color = mix(base.rgb, color, mask.r);
+
+#if !IGNORE_LOS
+    float los = texture2D(losTex, v_los).a;
+    los = los < 0.03 ? 0.0 : los;
+    color *= los;
+#endif
+
+    gl_FragColor = vec4(color, alpha * base.a);
+}
