12b9a522fe6b7349f01bc71d20f24b52fa31b82b
Stan <Stan@3db68df2-c116-0410-a063-a993310a9797>
2020-11-19 15:25:27 +0000

    Add a hashing function for CStr8 & CStrW
    Reviewed by: @wraitii
    
    Differential Revision: https://code.wildfiregames.com/D3106
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24218 3db68df2-c116-0410-a063-a993310a9797

--- source/ps/CStr.h
+++ source/ps/CStr.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019 Wildfire Games.
+/* Copyright (C) 2020 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -338,5 +338,17 @@ public:
 	u8* Serialize(u8* buffer) const;
 	const u8* Deserialize(const u8* buffer, const u8* bufferend);
 };
+
+namespace std
+{
+template <>
+struct hash<CStr>
+{
+	std::size_t operator()(const CStr& str) const
+	{
+		return str.GetHashCode();
+	}
+};
+}
 
 #endif
