f483b49f11547d528484de7428f19ab651522480
Freagarach <Freagarach@3db68df2-c116-0410-a063-a993310a9797>
2020-12-17 12:56:21 +0000

    Reverts r24402
    Either the default handlers _are_ needed or we don't care much. But removing (at least the motion one) may give errors.
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24403 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/UnitAI.js
+++ binaries/data/mods/public/simulation/components/UnitAI.js
@@ -161,6 +161,16 @@ UnitAI.prototype.UnitFsmSpec = {
 
 	// Default event handlers:
 
+	"MovementUpdate": function(msg) {
+		// ignore spurious movement messages
+		// (these can happen when stopping moving at the same time
+		// as switching states)
+	},
+
+	"ConstructionFinished": function(msg) {
+		// ignore uninteresting construction messages
+	},
+
 	"LosRangeUpdate": function(msg) {
 		// Ignore newly-seen units by default.
 	},
@@ -181,7 +191,15 @@ UnitAI.prototype.UnitFsmSpec = {
 		// ignore
 	},
 
+	"PackFinished": function(msg) {
+		// ignore
+	},
+
 	"PickupCanceled": function(msg) {
+		// ignore
+	},
+
+	"TradingCanceled": function(msg) {
 		// ignore
 	},
 
