43acf01f9b153a00014c3b90bfe6297fe54abf8b
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2020-12-27 07:48:26 +0000

    Fix no-pch builds following rP24427
    Component.h is loaded by convention in all CmpComponents and is requireed to use CmpPtr.
    
    Fixes #5898
    
    Differential Revision: https://code.wildfiregames.com/D3259
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24454 3db68df2-c116-0410-a063-a993310a9797

--- source/simulation2/components/CCmpTemplateManager.cpp
+++ source/simulation2/components/CCmpTemplateManager.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017 Wildfire Games.
+/* Copyright (C) 2020 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -17,6 +17,7 @@
 
 #include "precompiled.h"
 
+#include "simulation2/system/Component.h"
 #include "ICmpTemplateManager.h"
 
 #include "simulation2/MessageTypes.h"
