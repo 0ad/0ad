3af6c54ed87a248522bf653249992e6592187eab
Stan <Stan@3db68df2-c116-0410-a063-a993310a9797>
2021-01-17 15:40:10 +0000

    Fix no pch build.
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24664 3db68df2-c116-0410-a063-a993310a9797

--- source/renderer/SkyManager.h
+++ source/renderer/SkyManager.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019 Wildfire Games.
+/* Copyright (C) 2021 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -23,6 +23,8 @@
 #define INCLUDED_SKYMANAGER
 
 #include "graphics/Texture.h"
+
+#include <vector>
 
 /**
  * Class SkyManager: Maintain sky settings and textures, and render the sky.
