e2e4e7dcb4b64348481df7c2553945141240884f
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2021-01-22 08:14:27 +0000

    Fix a crash in WaterRenderer.cpp
    This fixes a rare crash at map generation.
    The water renderer could be left in an invalid state when exiting a game, and would then crash on the next map generation in some cases.
    
    Tested by: Stan
    
    Differential Revision: https://code.wildfiregames.com/D3447
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24759 3db68df2-c116-0410-a063-a993310a9797

--- source/simulation2/components/CCmpWaterManager.cpp
+++ source/simulation2/components/CCmpWaterManager.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017 Wildfire Games.
+/* Copyright (C) 2021 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -55,6 +55,9 @@ public:
 
 	virtual void Deinit()
 	{
+		// Clear the map size & data.
+		if (CRenderer::IsInitialised())
+			g_Renderer.GetWaterManager()->SetMapSize(0);
 	}
 
 	virtual void Serialize(ISerializer& serialize)
