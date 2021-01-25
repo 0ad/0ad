36f7417217386a7f5c4c246930063fd1796877c9
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2021-01-25 11:12:11 +0000

    Fix NOPCH builds.
    Caused by rP24720
    
    Differential Revision: https://code.wildfiregames.com/D3469
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24785 3db68df2-c116-0410-a063-a993310a9797

--- source/simulation2/serialization/StdDeserializer.cpp
+++ source/simulation2/serialization/StdDeserializer.cpp
@@ -20,6 +20,7 @@
 #include "StdDeserializer.h"
 
 #include "lib/byte_order.h"
+#include "ps/CLogger.h"
 #include "ps/CStr.h"
 #include "scriptinterface/ScriptInterface.h"
 #include "scriptinterface/ScriptExtraHeaders.h" // For typed arrays and ArrayBuffer
