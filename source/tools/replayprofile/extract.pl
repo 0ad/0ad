918f6e50221e9b4f6868fe1a923a201596cd807f
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2021-03-02 08:35:39 +0000

    Fix replayprofile/ graphing tool for Profiler1
    All modern browsers block ajax request to local file.
    This changes extract.pl to generate a single HTML file with data embedded.
    This is now the default behaviour, --to-json to export, --from-json to load exported.
    
    Patch by: nwtour
    
    Reviewed By: wraitii
    
    Differential Revision: https://code.wildfiregames.com/D3621
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24975 3db68df2-c116-0410-a063-a993310a9797

--- source/tools/replayprofile/extract.pl
+++ source/tools/replayprofile/extract.pl
@@ -3,13 +3,21 @@
 use strict;
 use warnings;
 
+use Getopt::Long;
+use File::Basename qw(dirname);
+chdir(dirname(__FILE__));
+
 # Run the game normally to generate a replay.
 # Run the non-visual replay like:
 #  ./pyrogenesis -mod=public -replay=$HOME/.local/share/0ad/replays/0.0.23/2018-03-23_0010/commands.txt
 # to generate profile.txt.
 # Then run:
-#  perl extract.pl > data.json
-# then open graph.html.
+#  perl extract.pl
+#    OR
+#  perl extract.pl --to-json > data.json ...  [manipulation with data.json] ...  perl extract.pl --from-json
+# then open graph_onepage.html.
+
+my ($to_json, $from_json); GetOptions ("to-json" => \$to_json, "from-json" => \$from_json);
 
 open my $f, '../../../binaries/system/profile.txt' or die $!;
 my $turn = 0;
@@ -33,15 +41,55 @@ while (<$f>) {
     }
 }
 
-print "[\n";
+my $json = "[\n";
 my $n = 0;
 for my $k (sort keys %s) {
-    print ",\n" if $n++;
-    print "{label: '$k', data:[";
+    $json .= ",\n" if $n++;
+    $json .= "{label: '$k', data:[";
     for my $t (0..$#{$s{$k}}) {
-        print "," if $t;
-        print "[$t,".($s{$k}[$t] || 0)."]";
+        $json .= "," if $t;
+        $json .= "[$t,".($s{$k}[$t] || 0)."]";
     }
-    print "]}";
+    $json .= "]}";
 }
-print "\n]\n";
+$json .= "\n]\n";
+
+my $onepage = "graph_onepage.html";
+
+sub recursive_file_slurp {
+	my $filename = shift;
+	my $output = '';
+	open my $fd, $filename or die "Open $filename : $!";
+	$output .= "<script>\n" if $filename =~ /\.js$/;
+	while(<$fd>){
+		if(/-- include js --/) {
+			$output .= recursive_file_slurp("jquery.js");
+			$output .= recursive_file_slurp("jquery.flot.js");
+			$output .= recursive_file_slurp("jquery.flot.navigate.js");
+		}
+		elsif(/-- include graph js --/) {
+			$output .= recursive_file_slurp("graph.js");
+		}
+		elsif(/-- include data json --/) {
+			$output .= $json;
+		}
+		else {
+			$output .= $_;
+		}
+	}
+	$output .= "</script>\n" if $filename =~ /\.js$/;
+	close($fd);
+	return $output;
+}
+
+if($to_json) {
+	print $json;
+	exit;
+}
+elsif($from_json) {
+	$json = recursive_file_slurp("data.json");
+}
+
+open my $opfd, '>', $onepage or die $!;
+print $opfd recursive_file_slurp("graph.html");
+close($opfd);
