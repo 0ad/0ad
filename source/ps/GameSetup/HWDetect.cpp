b6f2f57cc733ff84ee697a7959baa99f318b6d26
Stan <Stan@3db68df2-c116-0410-a063-a993310a9797>
2021-03-03 15:13:47 +0000

    Enable support for powerpc64 systems. New problems with NVTT will be addressed in another diff.
    Patch by: @tpearson-raptor
    Comments by: @q66
    
    Refs: https://github.com/void-linux/void-packages/blob/master/srcpkgs/0ad/patches/ppc64.patch
    
    Differential Revision: https://code.wildfiregames.com/D1619
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24995 3db68df2-c116-0410-a063-a993310a9797

--- source/ps/GameSetup/HWDetect.cpp
+++ source/ps/GameSetup/HWDetect.cpp
@@ -124,6 +124,7 @@ void RunHardwareDetection()
 	scriptInterface.SetProperty(settings, "arch_arm", ARCH_ARM);
 	scriptInterface.SetProperty(settings, "arch_aarch64", ARCH_AARCH64);
 	scriptInterface.SetProperty(settings, "arch_e2k", ARCH_E2K);
+	scriptInterface.SetProperty(settings, "arch_ppc64", ARCH_PPC64);
 
 #ifdef NDEBUG
 	scriptInterface.SetProperty(settings, "build_debug", 0);
