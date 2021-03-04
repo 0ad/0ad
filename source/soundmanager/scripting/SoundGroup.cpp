0ad/0ad

commit e2e9132b727153a151177ee9a79f58cc3373cb03

Improve sound attenuation so that zooming out actually reduce the sound level.
Patch by: @Samulis
Differential Revision: https://code.wildfiregames.com/D3108

git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24382 3db68df2-c116-0410-a063-a993310a9797

source/soundmanager/scripting/SoundGroup.cpp
@@ -38,6 +38,10 @@
 extern CGame *g_Game;
 
 #if CONFIG2_AUDIO
+
+constexpr ALfloat DEFAULT_ROLLOFF = 0.7f;
+constexpr ALfloat MAX_ROLLOFF = 0.9f;
+
 /**
  * Low randomness, quite-a-lot-faster-than-std::mt19937 random number generator.
  * It matches the interface of UniformRandomBitGenerator for use in std::shuffle.
@@ -143,7 +147,7 @@ float CSoundGroup::RadiansOffCenter(const CVector3D& position, bool& onScreen, f
 	else
 	{
 		if (x < 0 || x > screenWidth)
-			itemRollOff = 0.5;
+			itemRollOff = MAX_ROLLOFF;
 
 		answer = radianCap * (x * 2 / screenWidth - 1);
 	}
@@ -158,7 +162,7 @@ float CSoundGroup::RadiansOffCenter(const CVector3D& position, bool& onScreen, f
 	}
 	else if (y < 0 || y > screenHeight)
 	{
-		itemRollOff = 0.5;
+		itemRollOff = MAX_ROLLOFF;
 	}
 
 	return answer;
@@ -175,7 +179,7 @@ void CSoundGroup::UploadPropertiesAndPlay(size_t index, const CVector3D& positio
 		return;
 
 	bool isOnscreen = false;
-	ALfloat itemRollOff = 0.1f;
+	ALfloat itemRollOff = DEFAULT_ROLLOFF;
 	float offset = RadiansOffCenter(position, isOnscreen, itemRollOff);
 
 	if (!isOnscreen && !TestFlag(eDistanceless) && !TestFlag(eOmnipresent))
@@ -198,11 +202,7 @@ void CSoundGroup::UploadPropertiesAndPlay(size_t index, const CVector3D& positio
 	if (!TestFlag(eOmnipresent))
 	{
 		CVector3D origin = g_Game->GetView()->GetCamera()->GetOrientation().GetTranslation();
-		float sndDist = origin.Y;
 		float itemDist = (position - origin).Length();
-
-		if (sndDist * 2 < itemDist)
-			sndDist = itemDist;
 
 		if (TestFlag(eDistanceless))
 			itemRollOff = 0;
@@ -210,7 +210,7 @@ void CSoundGroup::UploadPropertiesAndPlay(size_t index, const CVector3D& positio
 		if (sndData->IsStereo())
 			LOGWARNING("OpenAL: stereo sounds can't be positioned: %s", sndData->GetFileName().string8());
 
-		hSound->SetLocation(CVector3D(sndDist * sin(offset), 0, -sndDist * cos(offset)));
+		hSound->SetLocation(CVector3D(itemDist * sin(offset), 0, -itemDist * cos(offset)));
 		hSound->SetRollOff(itemRollOff);
 	}
 

commit 5c9dec0d734cb2d03b145ac1a1ce29c06b86062d

Fix compilation with --without-audio
Compilation --without-audio was broken since D3108 / rP24382

Patch by: nwtour

Reviewed By: wraitii

Differential Revision: https://code.wildfiregames.com/D3631



git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@25009 3db68df2-c116-0410-a063-a993310a9797

source/soundmanager/scripting/SoundGroup.cpp
@@ -123,6 +123,12 @@ CSoundGroup::~CSoundGroup()
 
 float CSoundGroup::RadiansOffCenter(const CVector3D& position, bool& onScreen, float& itemRollOff)
 {
+#if !CONFIG2_AUDIO
+        UNUSED2(position);
+        UNUSED2(onScreen);
+        UNUSED2(itemRollOff);
+	return 0.f;
+#else
 	const int screenWidth = g_Game->GetView()->GetCamera()->GetViewPort().m_Width;
 	const int screenHeight = g_Game->GetView()->GetCamera()->GetViewPort().m_Height;
 	const float xBufferSize = screenWidth * 0.1f;
@@ -166,6 +172,7 @@ float CSoundGroup::RadiansOffCenter(const CVector3D& position, bool& onScreen, f
 	}
 
 	return answer;
+#endif // !CONFIG2_AUDIO
 }
 
 void CSoundGroup::UploadPropertiesAndPlay(size_t index, const CVector3D& position, entity_id_t source)