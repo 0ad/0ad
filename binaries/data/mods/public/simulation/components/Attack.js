2c184b3672419851ed77d12f20770c7e48b801b3
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2021-04-12 16:16:57 +0000

    Revert rP25242 - infinite loop in UnitAI when slaughtering.
    Reverting to get some time to investigate.
    
    Reported by: langbart
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@25246 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/Attack.js
+++ binaries/data/mods/public/simulation/components/Attack.js
@@ -452,7 +452,7 @@ Attack.prototype.StartAttacking = function(target, type, callerIID)
 	if (this.target)
 		this.StopAttacking();
 
-	if (!this.CanAttack(target, [type]))
+	if (!this.CanAttack(target))
 		return false;
 
 	let timings = this.GetTimers(type);
@@ -522,7 +522,7 @@ Attack.prototype.StopAttacking = function(reason)
  */
 Attack.prototype.Attack = function(type, lateness)
 {
-	if (!this.CanAttack(this.target, [type]))
+	if (!this.CanAttack(this.target))
 	{
 		this.StopAttacking("TargetInvalidated");
 		return;
