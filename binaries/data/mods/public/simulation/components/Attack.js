3ff1d177ccd5b37d866e66f9b891b02bd1e00651
Freagarach <Freagarach@3db68df2-c116-0410-a063-a993310a9797>
2021-04-15 05:45:13 +0000

    Fix entities able to attack allied structures in rare cases (take 2).
    Caused by not checking the attack type whilst performing an attack.
    
    Fixes: #5511
    Differential revision: D3845
    Refs. #3484
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@25264 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/Attack.js
+++ binaries/data/mods/public/simulation/components/Attack.js
@@ -238,7 +238,7 @@ Attack.prototype.CanAttack = function(target, wantedTypes)
 	let cmpHealth = QueryMiragedInterface(target, IID_Health);
 	let targetClasses = cmpIdentity.GetClassesList();
 	if (targetClasses.indexOf("Domestic") != -1 && this.template.Slaughter && cmpHealth && cmpHealth.GetHitpoints() &&
-	   (!wantedTypes || !wantedTypes.filter(wType => wType.indexOf("!") != 0).length))
+	   (!wantedTypes || !wantedTypes.filter(wType => wType.indexOf("!") != 0).length || wantedTypes.indexOf("Slaughter") != -1))
 		return true;
 
 	let cmpEntityPlayer = QueryOwnerInterface(this.entity);
@@ -452,7 +452,7 @@ Attack.prototype.StartAttacking = function(target, type, callerIID)
 	if (this.target)
 		this.StopAttacking();
 
-	if (!this.CanAttack(target))
+	if (!this.CanAttack(target, [type]))
 		return false;
 
 	let timings = this.GetTimers(type);
@@ -522,7 +522,7 @@ Attack.prototype.StopAttacking = function(reason)
  */
 Attack.prototype.Attack = function(type, lateness)
 {
-	if (!this.CanAttack(this.target))
+	if (!this.CanAttack(this.target, [type]))
 	{
 		this.StopAttacking("TargetInvalidated");
 		return;
