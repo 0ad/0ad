0ad/0ad

commit cfd8c374c476803f843d958ea2c80bfe7cdec51e

Flush order queue when given an order to perform a action on the same entity.
Follows r24855 / rP24855.

Fixes: #6046
Differential revision: D3587
Reviewed by: @wraitii



git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@25100 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -5565,6 +5565,8 @@ UnitAI.prototype.Attack = function(target, allowCapture = true, queued = false, 
 	{
 		this.order.data.lastPos = order.lastPos;
 		this.order.data.force = order.force;
+		if (order.force)
+			this.orderQueue = [this.order];
 		return;
 	}
 
@@ -5650,6 +5652,8 @@ UnitAI.prototype.PerformGather = function(target, queued, force, pushFront = fal
 	{
 		this.order.data.lastPos = order.lastPos;
 		this.order.data.force = order.force;
+		if (order.force)
+			this.orderQueue = [this.order];
 		return;
 	}
 
@@ -5687,6 +5691,7 @@ UnitAI.prototype.Heal = function(target, queued, pushFront)
 		this.order.data.target === target)
 	{
 		this.order.data.force = true;
+		this.orderQueue = [this.order];
 		return;
 	}
 
@@ -5910,6 +5915,7 @@ UnitAI.prototype.Repair = function(target, autocontinue, queued, pushFront)
 		this.order.data.autocontinue === autocontinue)
 	{
 		this.order.data.force = true;
+		this.orderQueue = [this.order];
 		return;
 	}
 

commit 7bdaf1c8d27ad3cb542f6be6c40aad4d5702553d

Fix entities receiving double hunt order only attacking.
Caused by: r25100 / rP25100.
Noticed by @Asger and @Kate.
Special case hunting.

Differential revision: https://code.wildfiregames.com/D3986
Comment by: @wraitii



git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@25509 3db68df2-c116-0410-a063-a993310a9797

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -5548,7 +5548,12 @@ UnitAI.prototype.PerformGather = function(target, queued, force, pushFront = fal
 		this.order.data.lastPos = order.lastPos;
 		this.order.data.force = order.force;
 		if (order.force)
-			this.orderQueue = [this.order];
+		{
+			if (this.orderQueue[1]?.type === "Gather")
+				this.orderQueue = [this.order, this.orderQueue[1]];
+			else
+				this.orderQueue = [this.order];
+		}
 		return;
 	}
 