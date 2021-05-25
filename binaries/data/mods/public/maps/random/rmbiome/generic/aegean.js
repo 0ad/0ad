73ccac3aba3a07e17b02bb3fd522008d56abf6b1
Stan <Stan@3db68df2-c116-0410-a063-a993310a9797>
2021-05-25 12:25:39 +0000

    Use new biomes for random maps,
    Patch by: @wowgetoffyourcellphone
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@25562 3db68df2-c116-0410-a063-a993310a9797

---
+++ binaries/data/mods/public/maps/random/rmbiome/generic/aegean.js
@@ -0,0 +1,28 @@
+function setupBiome_aegean()
+{
+	[g_Gaia.tree1, g_Gaia.tree2] = pickRandom([[
+			"gaia/tree/cypress_wild",
+			"gaia/tree/pine_maritime_short",
+			"gaia/tree/cretan_date_palm_tall"
+		]]);
+
+	g_Gaia.tree3 = pickRandom([
+		"gaia/fruit/olive",
+		"gaia/tree/juniper_prickly",
+		"gaia/tree/date_palm",
+		"gaia/tree/cretan_date_palm_short",
+		"gaia/tree/medit_fan_palm"
+	]);
+
+	[g_Gaia.tree4, g_Gaia.tree5] = pickRandom([[
+		"gaia/tree/poplar_lombardy",
+		"gaia/tree/carob",
+		"gaia/tree/medit_fan_palm",
+		"gaia/tree/cretan_date_palm_tall"
+	]]);
+
+	g_Gaia.fruitBush = pickRandom([
+		"gaia/fruit/berry_01",
+		"gaia/fruit/grapes"
+	]);
+}
--- binaries/data/mods/public/maps/random/rmbiome/generic/sahara.js
+++ binaries/data/mods/public/maps/random/rmbiome/generic/sahara.js
@@ -1,18 +1,18 @@
-function setupBiome_desert()
+function setupBiome_sahara()
 {
 	[g_Gaia.tree1, g_Gaia.tree2] = pickRandom([
 		[
 			"gaia/tree/cretan_date_palm_short",
-			"gaia/tree/cretan_date_palm_tall"
+			"gaia/tree/date_palm"
 		],
 		[
 			"gaia/tree/date_palm",
-			"gaia/tree/date_palm"
+			"gaia/tree/cretan_date_palm_tall"
 		]
 	]);
 
 	[g_Gaia.tree4, g_Gaia.tree5] = new Array(2).fill(pickRandom([
-		"gaia/tree/tamarix",
-		"gaia/tree/senegal_date_palm"
+		"gaia/tree/date_palm",
+		"gaia/tree/cretan_date_palm_patch"
 	]));
 }
