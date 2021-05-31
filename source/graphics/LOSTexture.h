8cb147eb88d22b814cae49745ceda037dc635234
vladislavbelov <vladislavbelov@3db68df2-c116-0410-a063-a993310a9797>
2021-05-31 20:47:03 +0000

    Adds header safeguards to LOSTexture forgotten in rP8882.
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@25620 3db68df2-c116-0410-a063-a993310a9797

--- source/graphics/LOSTexture.h
+++ source/graphics/LOSTexture.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019 Wildfire Games.
+/* Copyright (C) 2021 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -14,6 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#ifndef INCLUDED_LOSTEXTURE
+#define INCLUDED_LOSTEXTURE
 
 #include "lib/ogl.h"
 
@@ -103,3 +106,5 @@ private:
 	CMatrix3D m_TextureMatrix;
 	CMatrix3D m_MinimapTextureMatrix;
 };
+
+#endif // INCLUDED_LOSTEXTURE
