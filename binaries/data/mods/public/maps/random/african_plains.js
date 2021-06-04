8b6b6874c42f0c133aa01482411bc5f3302cdaf9
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2021-06-04 16:00:37 +0000

    Map touchup: African plains
    - Support the 3 african biomes.
    - Bigger forests (same # of trees overall)
    - More hills/watering holes, more animals there.
    
    Differential Revision: https://code.wildfiregames.com/D4060
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@25673 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/maps/random/african_plains.js
+++ binaries/data/mods/public/maps/random/african_plains.js
@@ -1,21 +1,26 @@
 Engine.LoadLibrary("rmgen");
 Engine.LoadLibrary("rmgen-common");
+Engine.LoadLibrary("rmbiome");
 
-var tPrimary = "savanna_grass_a";
-var tForestFloor = "savanna_forestfloor_a";
+setSelectedBiome();
+
+// Pick some biome defaults and overload a few settings.
+var tPrimary = g_Terrains.mainTerrain;
+var tForestFloor = g_Terrains.forestFloor1;
 var tCliff = ["savanna_cliff_a", "savanna_cliff_a_red", "savanna_cliff_b", "savanna_cliff_b_red"];
-var tSecondary = "savanna_grass_b";
-var tGrassShrubs = "savanna_shrubs_a";
-var tDirt = "savanna_dirt_a";
+var tSecondary = g_Terrains.tier4Terrain;
+var tGrassShrubs = g_Terrains.tier1Terrain;
+var tDirt = g_Terrains.dirt;
 var tDirt2 = "savanna_dirt_a_red";
-var tDirt3 = "savanna_dirt_b";
-var tDirt4 = "savanna_dirt_rocks_a";
+var tDirt3 = g_Terrains.dirt;
+var tDirt4 = g_Terrains.hill;
 var tCitytiles = "savanna_tile_a";
-var tShore = "savanna_riparian_bank";
-var tWater = "savanna_riparian_wet";
+var tShore = g_Terrains.shore;
+var tWater = g_Terrains.water;
 
-var oBaobab = "gaia/tree/baobab";
-var oPalm = "gaia/tree/senegal_date_palm";
+var oBaobab = g_Gaia.tree1;
+var oPalm = g_Gaia.tree2;
+var oPalm2 = g_Gaia.tree3;
 var oBerryBush = "gaia/fruit/berry_01";
 var oWildebeest = "gaia/fauna_wildebeest";
 var oZebra = "gaia/fauna_zebra";
@@ -29,14 +34,14 @@ var oGazelle = "gaia/fauna_gazelle";
 var oElephant = "gaia/fauna_elephant_african_bush";
 var oElephant2 = "gaia/fauna_elephant_african_infant";
 var oCrocodile = "gaia/fauna_crocodile_nile";
-var oFish = "gaia/fish/generic";
-var oStoneSmall = "gaia/rock/savanna_small";
-var oMetalLarge = "gaia/ore/savanna_large";
+var oFish = g_Gaia.fish;
+var oStoneSmall = g_Gaia.stoneSmall;
+var oMetalLarge = g_Gaia.metalLarge;
 
-var aBush = "actor|props/flora/bush_medit_sm_dry.xml";
-var aRock = "actor|geology/stone_savanna_med.xml";
+var aBush = g_Decoratives.bushMedium;
+var aRock = g_Decoratives.rockMedium;
 
-const pForest = [tForestFloor + TERRAIN_SEPARATOR + oPalm, tForestFloor];
+const pForest = [tForestFloor + TERRAIN_SEPARATOR + oPalm, tForestFloor + TERRAIN_SEPARATOR + oPalm2, tForestFloor];
 
 var heightSeaGround = -5;
 var heightLand = 2;
@@ -94,41 +99,50 @@ placePlayerBases({
 });
 Engine.SetProgress(20);
 
-createHills([tDirt2, tCliff, tGrassShrubs], avoidClasses(clPlayer, 35, clForest, 20, clHill, 20, clWater, 2), clHill, scaleByMapSize(5, 8));
-Engine.SetProgress(30);
+// The specificity of this map is a bunch of watering holes & hills making it somewhat cluttered.
+const nbHills = scaleByMapSize(6, 16);
+const nbWateringHoles = scaleByMapSize(4, 10);
+{
+	g_Map.log("Creating hills");
+	createHills([tDirt2, tCliff, tGrassShrubs], avoidClasses(clPlayer, 30, clHill, 15), clHill, nbHills);
+	Engine.SetProgress(30);
 
-g_Map.log("Creating water holes");
-createAreas(
-	new ChainPlacer(1, Math.floor(scaleByMapSize(3, 5)), Math.floor(scaleByMapSize(60, 100)), Infinity),
-	[
-		new LayeredPainter([tShore, tWater], [1]),
-		new SmoothElevationPainter(ELEVATION_SET, heightSeaGround, 7),
-		new TileClassPainter(clWater)
-	],
-	avoidClasses(clPlayer, 22, clWater, 8, clHill, 2),
-	scaleByMapSize(2, 5));
+	g_Map.log("Creating water holes");
+	createAreas(
+		new ChainPlacer(1, Math.floor(scaleByMapSize(3, 5)), Math.floor(scaleByMapSize(60, 100)), Infinity),
+		[
+			new LayeredPainter([tShore, tWater], [1]),
+			new SmoothElevationPainter(ELEVATION_SET, heightSeaGround, 7),
+			new TileClassPainter(clWater)
+		],
+		avoidClasses(clPlayer, 22, clWater, 8, clHill, 2),
+		nbWateringHoles);
 
-Engine.SetProgress(45);
+	Engine.SetProgress(45);
 
-paintTerrainBasedOnHeight(heightCliff, Infinity, 0, tCliff);
+	paintTerrainBasedOnHeight(heightCliff, Infinity, 0, tCliff);
+}
 
 createBumps(avoidClasses(clWater, 2, clPlayer, 20));
 
-var [forestTrees, stragglerTrees] = getTreeCounts(500, 3000, 0.7);
-createForests(
-	[tPrimary, tForestFloor, tForestFloor, pForest, pForest],
-	avoidClasses(clPlayer, 20, clForest, 20, clHill, 0, clWater, 2),
+g_Map.log("Creating forests");
+
+createDefaultForests(
+	[tPrimary, pForest, tForestFloor, pForest, pForest],
+	avoidClasses(clPlayer, 20, clForest, 15, clHill, 0, clWater, 2),
 	clForest,
-	forestTrees);
+	scaleByMapSize(500, 3000));
+Engine.SetProgress(50);
 
 g_Map.log("Creating dirt patches");
 createLayeredPatches(
 	[scaleByMapSize(3, 6), scaleByMapSize(5, 10), scaleByMapSize(8, 21)],
-	[[tDirt,tDirt3], [tDirt2,tDirt4]],
+	[[tDirt, tDirt3], [tDirt2, tDirt4]],
 	[2],
 	avoidClasses(clWater, 3, clForest, 0, clHill, 0, clDirt, 5, clPlayer, 12),
 	scaleByMapSize(15, 45),
 	clDirt);
+Engine.SetProgress(55);
 
 g_Map.log("Creating shrubs");
 createPatches(
@@ -137,6 +151,7 @@ createPatches(
 	avoidClasses(clWater, 3, clForest, 0, clHill, 0, clDirt, 5, clPlayer, 12),
 	scaleByMapSize(15, 45),
 	clDirt);
+Engine.SetProgress(60);
 
 g_Map.log("Creating grass patches");
 createPatches(
@@ -145,7 +160,7 @@ createPatches(
 	avoidClasses(clWater, 3, clForest, 0, clHill, 0, clDirt, 5, clPlayer, 12),
 	scaleByMapSize(15, 45),
 	clDirt);
-Engine.SetProgress(60);
+Engine.SetProgress(65);
 
 g_Map.log("Creating stone mines");
 createMines(
@@ -178,64 +193,73 @@ createDecoration(
 	avoidClasses(clWater, 0, clForest, 0, clPlayer, 0, clHill, 0));
 Engine.SetProgress(75);
 
-g_Map.log("Creating giraffes");
-var group = new SimpleGroup(
-	[new SimpleObject(oGiraffe, 2,4, 0,4), new SimpleObject(oGiraffe2, 0,2, 0,4)],
-	true, clFood
-);
-createObjectGroupsDeprecated(group, 0,
-	avoidClasses(clWater, 3, clPlayer, 20, clFood, 11, clHill, 4),
-	scaleByMapSize(4,12), 50
-);
+// Roaming animals
+{
+	var placeRoaming = function(name, objs)
+	{
+		g_Map.log("Creating roaming " + name);
+		const group = new SimpleGroup(objs, true, clFood);
+		createObjectGroups(group, 0,
+			avoidClasses(clWater, 3, clPlayer, 20, clFood, 11, clHill, 4),
+			scaleByMapSize(3, 9), 50
+		);
+	};
 
-g_Map.log("Creating elephants");
-group = new SimpleGroup(
-	[new SimpleObject(oElephant, 2,4, 0,4), new SimpleObject(oElephant2, 0,2, 0,4)],
-	true, clFood
-);
-createObjectGroupsDeprecated(group, 0,
-	avoidClasses(clWater, 3, clPlayer, 20, clFood, 11, clHill, 4),
-	scaleByMapSize(4,12), 50
-);
+	placeRoaming("giraffes", [new SimpleObject(oGiraffe, 2, 4, 0, 4), new SimpleObject(oGiraffe2, 0, 2, 0, 4)]);
+	placeRoaming("elephants", [new SimpleObject(oElephant, 2, 4, 0, 4), new SimpleObject(oElephant2, 0, 2, 0, 4)]);
+	placeRoaming("lions", [new SimpleObject(oLion, 0, 1, 0, 4), new SimpleObject(oLioness, 2, 3, 0, 4)]);
 
-g_Map.log("Creating lions");
-group = new SimpleGroup(
-	[new SimpleObject(oLion, 0,1, 0,4), new SimpleObject(oLioness, 2,3, 0,4)],
-	true, clFood
-);
-createObjectGroupsDeprecated(group, 0,
-	avoidClasses(clWater, 3, clPlayer, 20, clFood, 11, clHill, 4),
-	scaleByMapSize(4,12), 50
-);
+	// Other roaming animals
+	createFood(
+		[
+			[new SimpleObject(oHawk, 1, 1, 0, 3)],
+			[new SimpleObject(oGazelle, 3, 5, 0, 3)],
+			[new SimpleObject(oZebra, 3, 5, 0, 3)],
+			[new SimpleObject(oWildebeest, 4, 6, 0, 3)],
+			[new SimpleObject(oRhino, 1, 1, 0, 3)]
+		],
+		[
+			3 * numPlayers,
+			3 * numPlayers,
+			3 * numPlayers,
+			3 * numPlayers,
+			3 * numPlayers,
+		],
+		avoidClasses(clFood, 20, clWater, 5, clHill, 2, clPlayer, 16),
+		clFood);
+}
 
-createFood(
-	[
-		[new SimpleObject(oHawk, 1, 1, 0, 3)],
-		[new SimpleObject(oGazelle, 3, 5, 0, 3)],
-		[new SimpleObject(oZebra, 3, 5, 0, 3)],
-		[new SimpleObject(oWildebeest, 4, 6, 0, 3)],
-		[new SimpleObject(oRhino, 1, 1, 0, 3)]
-	],
-	[
-		3 * numPlayers,
-		3 * numPlayers,
-		3 * numPlayers,
-		3 * numPlayers,
-		3 * numPlayers,
-	],
-	avoidClasses(clFood, 20, clWater, 5, clHill, 2, clPlayer, 16),
-	clFood);
+// Animals that hang around watering holes
+{
+	// TODO: these have a high retry factor because our mapgen constraint logic is bad.
+	var placeWateringHoleAnimals = function(name, objs, numberOfGroups)
+	{
+		g_Map.log("Creating " + name + " around watering holes");
+		const group = new SimpleGroup(objs, true, clFood);
+		createObjectGroups(group, 0,
+			borderClasses(clWater, 6, 3),
+			numberOfGroups, 50
+		);
+	};
+	placeWateringHoleAnimals(
+		"crocodiles",
+		[new SimpleObject(oCrocodile, 2, 3, 0, 3)],
+		nbWateringHoles * 0.8
+	);
+	placeWateringHoleAnimals(
+		"zebras",
+		[new SimpleObject(oZebra, 2, 5, 0, 3)],
+		nbWateringHoles
+	);
+	placeWateringHoleAnimals(
+		"gazelles",
+		[new SimpleObject(oGazelle, 2, 5, 0, 3)],
+		nbWateringHoles
+	);
+}
 
-createFood(
-	[
-		[new SimpleObject(oCrocodile, 2, 3, 0, 3)]
-	],
-	[
-		3 * numPlayers,
-	],
-	stayClasses(clWater, 6),
-	clFood);
 
+g_Map.log("Creating other food sources");
 createFood(
 	[
 		[new SimpleObject(oBerryBush, 5, 7, 0, 4)]
@@ -257,31 +281,22 @@ createFood(
 	clFood);
 Engine.SetProgress(85);
 
-createStragglerTrees(
-	[oBaobab],
-	avoidClasses(clWater, 5, clForest, 2, clHill, 3, clPlayer, 12, clMetal, 4, clRock, 4),
-	clForest,
-	stragglerTrees);
+
+g_Map.log("Creating straggler baobabs");
+const group = new SimpleGroup([new SimpleObject(oBaobab, 1, 3, 0, 3)], true, clForest);
+createObjectGroups(
+	group,
+	0,
+	avoidClasses(clWater, 0, clForest, 2, clHill, 3, clPlayer, 12, clMetal, 4, clRock, 4),
+	scaleByMapSize(15, 75)
+);
+
 
 placePlayersNomad(clPlayer, avoidClasses(clWater, 4, clForest, 1, clMetal, 4, clRock, 4, clHill, 4, clFood, 2));
 
-setPPEffect("hdr");
-setPPSaturation(0.48);
-setPPContrast(0.53);
-setPPBloom(0.12);
-
-setFogThickness(0.25);
-setFogFactor(0.25);
-setFogColor(0.8, 0.7, 0.5);
+// Adjust some biome settings;
 
 setSkySet("sunny");
-setSunRotation(randomAngle());
-setSunElevation(Math.PI * randFloat(1/4, 1/2));
-
-setWaterColor(0.223, 0.247, 0.2); // dark majestic blue
-setWaterTint(0.462, 0.756, 0.566); // light blue
-setWaterMurkiness(5.92);
-setWaterWaviness(0);
 setWaterType("clap");
 
 g_Map.ExportMap();
