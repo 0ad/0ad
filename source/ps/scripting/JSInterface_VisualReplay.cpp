0ad/0ad

commit 374dbe9d39a92d7d11d460277951b998d89b6be1

Refactor all usage of RegisterFunction to ScriptFunction::Register
- Replace ScriptInterface::RegisterFunction with ScriptFunction::Register
 - Mostly removing unused cmpPrivate*
 - Some usage introduces specific getters (mapgenerator, AIWorker, XmppClient,...)
 - Several passthrough functions are simply removed in favour of calling the original, reducing duplication
 - Make use of ScriptRequest/ScriptInterface capabilities where relevant.
- Make JSI_* headers only expose necessary functions, lightening them considerably and reducing duplication
 - Reuse namespaces in JSI_* implementations directly, reducing visual noise there

Follows rP24969

Differential Revision: https://code.wildfiregames.com/D3626



git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@24983 3db68df2-c116-0410-a063-a993310a9797

source/ps/scripting/JSInterface_VisualReplay.cpp
@@ -21,56 +21,25 @@
 
 #include "ps/CStr.h"
 #include "ps/VisualReplay.h"
+#include "scriptinterface/FunctionWrapper.h"
 #include "scriptinterface/ScriptInterface.h"
 
-bool JSI_VisualReplay::StartVisualReplay(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const CStrW& directory)
+namespace JSI_VisualReplay
 {
-	return VisualReplay::StartVisualReplay(directory);
+CStrW GetReplayDirectoryName(const CStrW& directoryName)
+{
+	return OsPath(VisualReplay::GetDirectoryPath() / directoryName).string();
 }
 
-bool JSI_VisualReplay::DeleteReplay(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const CStrW& replayFile)
+void RegisterScriptFunctions(const ScriptRequest& rq)
 {
-	return VisualReplay::DeleteReplay(replayFile);
+	ScriptFunction::Register<&VisualReplay::GetReplays>(rq, "GetReplays");
+	ScriptFunction::Register<&VisualReplay::DeleteReplay>(rq, "DeleteReplay");
+	ScriptFunction::Register<&VisualReplay::StartVisualReplay>(rq, "StartVisualReplay");
+	ScriptFunction::Register<&VisualReplay::GetReplayAttributes>(rq, "GetReplayAttributes");
+	ScriptFunction::Register<&VisualReplay::GetReplayMetadata>(rq, "GetReplayMetadata");
+	ScriptFunction::Register<&VisualReplay::HasReplayMetadata>(rq, "HasReplayMetadata");
+	ScriptFunction::Register<&VisualReplay::AddReplayToCache>(rq, "AddReplayToCache");
+	ScriptFunction::Register<&GetReplayDirectoryName>(rq, "GetReplayDirectoryName");
 }
-
-JS::Value JSI_VisualReplay::GetReplays(ScriptInterface::CmptPrivate* pCmptPrivate, bool compareFiles)
-{
-	return VisualReplay::GetReplays(*(pCmptPrivate->pScriptInterface), compareFiles);
-}
-
-JS::Value JSI_VisualReplay::GetReplayAttributes(ScriptInterface::CmptPrivate* pCmptPrivate, const CStrW& directoryName)
-{
-	return VisualReplay::GetReplayAttributes(pCmptPrivate, directoryName);
-}
-
-bool JSI_VisualReplay::HasReplayMetadata(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const CStrW& directoryName)
-{
-	return VisualReplay::HasReplayMetadata(directoryName);
-}
-
-JS::Value JSI_VisualReplay::GetReplayMetadata(ScriptInterface::CmptPrivate* pCmptPrivate, const CStrW& directoryName)
-{
-	return VisualReplay::GetReplayMetadata(pCmptPrivate, directoryName);
-}
-
-void JSI_VisualReplay::AddReplayToCache(ScriptInterface::CmptPrivate* pCmptPrivate, const CStrW& directoryName)
-{
-	VisualReplay::AddReplayToCache(*(pCmptPrivate->pScriptInterface), directoryName);
-}
-
-CStrW JSI_VisualReplay::GetReplayDirectoryName(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const CStrW& directoryName)
-{
-	return wstring_from_utf8(OsPath(VisualReplay::GetDirectoryPath() / directoryName).string8());
-}
-
-void JSI_VisualReplay::RegisterScriptFunctions(const ScriptInterface& scriptInterface)
-{
-	scriptInterface.RegisterFunction<JS::Value, bool, &GetReplays>("GetReplays");
-	scriptInterface.RegisterFunction<bool, CStrW, &DeleteReplay>("DeleteReplay");
-	scriptInterface.RegisterFunction<bool, CStrW, &StartVisualReplay>("StartVisualReplay");
-	scriptInterface.RegisterFunction<JS::Value, CStrW, &GetReplayAttributes>("GetReplayAttributes");
-	scriptInterface.RegisterFunction<JS::Value, CStrW, &GetReplayMetadata>("GetReplayMetadata");
-	scriptInterface.RegisterFunction<bool, CStrW, &HasReplayMetadata>("HasReplayMetadata");
-	scriptInterface.RegisterFunction<void, CStrW, &AddReplayToCache>("AddReplayToCache");
-	scriptInterface.RegisterFunction<CStrW, CStrW, &GetReplayDirectoryName>("GetReplayDirectoryName");
 }

source/graphics/CameraController.cpp
@@ -21,7 +21,6 @@
 
 #include "graphics/HFTracer.h"
 #include "graphics/Terrain.h"
-#include "graphics/scripting/JSInterface_GameView.h"
 #include "lib/input.h"
 #include "lib/timer.h"
 #include "maths/MathUtil.h"

source/graphics/GameView.cpp
@@ -35,7 +35,6 @@
 #include "graphics/TerritoryTexture.h"
 #include "graphics/Unit.h"
 #include "graphics/UnitManager.h"
-#include "graphics/scripting/JSInterface_GameView.h"
 #include "lib/input.h"
 #include "lib/timer.h"
 #include "lobby/IXmppClient.h"

source/graphics/MapGenerator.cpp
@@ -32,6 +32,7 @@
 #include "ps/Profile.h"
 #include "ps/Threading.h"
 #include "ps/scripting/JSInterface_VFS.h"
+#include "scriptinterface/FunctionWrapper.h"
 #include "scriptinterface/ScriptContext.h"
 #include "scriptinterface/ScriptConversions.h"
 #include "scriptinterface/ScriptInterface.h"
@@ -160,6 +161,11 @@ bool CMapGeneratorWorker::Run()
 	return true;
 }
 
+#define REGISTER_MAPGEN_FUNC(func) \
+	ScriptFunction::Register<&CMapGeneratorWorker::func, ScriptFunction::ObjectFromCBData<CMapGeneratorWorker>>(rq, #func);
+#define REGISTER_MAPGEN_FUNC_NAME(func, name) \
+	ScriptFunction::Register<&CMapGeneratorWorker::func, ScriptFunction::ObjectFromCBData<CMapGeneratorWorker>>(rq, name);
+
 void CMapGeneratorWorker::InitScriptInterface(const u32 seed)
 {
 	m_ScriptInterface->SetCallbackData(static_cast<void*>(this));
@@ -174,9 +180,10 @@ void CMapGeneratorWorker::InitScriptInterface(const u32 seed)
 	m_ScriptInterface->LoadGlobalScripts();
 
 	// File loading
-	m_ScriptInterface->RegisterFunction<bool, VfsPath, CMapGeneratorWorker::LoadLibrary>("LoadLibrary");
-	m_ScriptInterface->RegisterFunction<JS::Value, VfsPath, CMapGeneratorWorker::LoadHeightmap>("LoadHeightmapImage");
-	m_ScriptInterface->RegisterFunction<JS::Value, VfsPath, CMapGeneratorWorker::LoadMapTerrain>("LoadMapTerrain");
+	ScriptRequest rq(m_ScriptInterface);
+	REGISTER_MAPGEN_FUNC_NAME(LoadScripts, "LoadLibrary");
+	REGISTER_MAPGEN_FUNC_NAME(LoadHeightmap, "LoadHeightmapImage");
+	REGISTER_MAPGEN_FUNC(LoadMapTerrain);
 
 	// Engine constants
 
@@ -190,17 +197,22 @@ void CMapGeneratorWorker::InitScriptInterface(const u32 seed)
 
 void CMapGeneratorWorker::RegisterScriptFunctions_MapGenerator()
 {
+	ScriptRequest rq(m_ScriptInterface);
+
 	// Template functions
-	m_ScriptInterface->RegisterFunction<CParamNode, std::string, CMapGeneratorWorker::GetTemplate>("GetTemplate");
-	m_ScriptInterface->RegisterFunction<bool, std::string, CMapGeneratorWorker::TemplateExists>("TemplateExists");
-	m_ScriptInterface->RegisterFunction<std::vector<std::string>, std::string, bool, CMapGeneratorWorker::FindTemplates>("FindTemplates");
-	m_ScriptInterface->RegisterFunction<std::vector<std::string>, std::string, bool, CMapGeneratorWorker::FindActorTemplates>("FindActorTemplates");
+	REGISTER_MAPGEN_FUNC(GetTemplate);
+	REGISTER_MAPGEN_FUNC(TemplateExists);
+	REGISTER_MAPGEN_FUNC(FindTemplates);
+	REGISTER_MAPGEN_FUNC(FindActorTemplates);
 
 	// Progression and profiling
-	m_ScriptInterface->RegisterFunction<void, int, CMapGeneratorWorker::SetProgress>("SetProgress");
-	m_ScriptInterface->RegisterFunction<double, CMapGeneratorWorker::GetMicroseconds>("GetMicroseconds");
-	m_ScriptInterface->RegisterFunction<void, JS::HandleValue, CMapGeneratorWorker::ExportMap>("ExportMap");
+	REGISTER_MAPGEN_FUNC(SetProgress);
+	REGISTER_MAPGEN_FUNC(GetMicroseconds);
+	REGISTER_MAPGEN_FUNC(ExportMap);
 }
+
+#undef REGISTER_MAPGEN_FUNC
+#undef REGISTER_MAPGEN_FUNC_NAME
 
 int CMapGeneratorWorker::GetProgress()
 {
@@ -208,7 +220,7 @@ int CMapGeneratorWorker::GetProgress()
 	return m_Progress;
 }
 
-double CMapGeneratorWorker::GetMicroseconds(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate))
+double CMapGeneratorWorker::GetMicroseconds()
 {
 	return JS_Now();
 }
@@ -219,61 +231,47 @@ ScriptInterface::StructuredClone CMapGeneratorWorker::GetResults()
 	return m_MapData;
 }
 
-bool CMapGeneratorWorker::LoadLibrary(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& name)
+void CMapGeneratorWorker::ExportMap(JS::HandleValue data)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	return self->LoadScripts(name);
-}
-
-void CMapGeneratorWorker::ExportMap(ScriptInterface::CmptPrivate* pCmptPrivate, JS::HandleValue data)
-{
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-
 	// Copy results
-	std::lock_guard<std::mutex> lock(self->m_WorkerMutex);
-	self->m_MapData = self->m_ScriptInterface->WriteStructuredClone(data);
-	self->m_Progress = 0;
+	std::lock_guard<std::mutex> lock(m_WorkerMutex);
+	m_MapData = m_ScriptInterface->WriteStructuredClone(data);
+	m_Progress = 0;
 }
 
-void CMapGeneratorWorker::SetProgress(ScriptInterface::CmptPrivate* pCmptPrivate, int progress)
+void CMapGeneratorWorker::SetProgress(int progress)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-
 	// Copy data
-	std::lock_guard<std::mutex> lock(self->m_WorkerMutex);
+	std::lock_guard<std::mutex> lock(m_WorkerMutex);
 
-	if (progress >= self->m_Progress)
-		self->m_Progress = progress;
+	if (progress >= m_Progress)
+		m_Progress = progress;
 	else
-		LOGWARNING("The random map script tried to reduce the loading progress from %d to %d", self->m_Progress, progress);
+		LOGWARNING("The random map script tried to reduce the loading progress from %d to %d", m_Progress, progress);
 }
 
-CParamNode CMapGeneratorWorker::GetTemplate(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& templateName)
+CParamNode CMapGeneratorWorker::GetTemplate(const std::string& templateName)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	const CParamNode& templateRoot = self->m_TemplateLoader.GetTemplateFileData(templateName).GetChild("Entity");
+	const CParamNode& templateRoot = m_TemplateLoader.GetTemplateFileData(templateName).GetChild("Entity");
 	if (!templateRoot.IsOk())
 		LOGERROR("Invalid template found for '%s'", templateName.c_str());
 
 	return templateRoot;
 }
 
-bool CMapGeneratorWorker::TemplateExists(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& templateName)
+bool CMapGeneratorWorker::TemplateExists(const std::string& templateName)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	return self->m_TemplateLoader.TemplateExists(templateName);
+	return m_TemplateLoader.TemplateExists(templateName);
 }
 
-std::vector<std::string> CMapGeneratorWorker::FindTemplates(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& path, bool includeSubdirectories)
+std::vector<std::string> CMapGeneratorWorker::FindTemplates(const std::string& path, bool includeSubdirectories)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	return self->m_TemplateLoader.FindTemplates(path, includeSubdirectories, SIMULATION_TEMPLATES);
+	return m_TemplateLoader.FindTemplates(path, includeSubdirectories, SIMULATION_TEMPLATES);
 }
 
-std::vector<std::string> CMapGeneratorWorker::FindActorTemplates(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& path, bool includeSubdirectories)
+std::vector<std::string> CMapGeneratorWorker::FindActorTemplates(const std::string& path, bool includeSubdirectories)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	return self->m_TemplateLoader.FindTemplates(path, includeSubdirectories, ACTOR_TEMPLATES);
+	return m_TemplateLoader.FindTemplates(path, includeSubdirectories, ACTOR_TEMPLATES);
 }
 
 bool CMapGeneratorWorker::LoadScripts(const VfsPath& libraryName)
@@ -314,7 +312,7 @@ bool CMapGeneratorWorker::LoadScripts(const VfsPath& libraryName)
 	return true;
 }
 
-JS::Value CMapGeneratorWorker::LoadHeightmap(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& filename)
+JS::Value CMapGeneratorWorker::LoadHeightmap(const VfsPath& filename)
 {
 	std::vector<u16> heightmap;
 	if (LoadHeightmapImageVfs(filename, heightmap) != INFO::OK)
@@ -323,18 +321,16 @@ JS::Value CMapGeneratorWorker::LoadHeightmap(ScriptInterface::CmptPrivate* pCmpt
 		return JS::UndefinedValue();
 	}
 
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	ScriptRequest rq(self->m_ScriptInterface);
+	ScriptRequest rq(m_ScriptInterface);
 	JS::RootedValue returnValue(rq.cx);
 	ToJSVal_vector(rq, &returnValue, heightmap);
 	return returnValue;
 }
 
 // See CMapReader::UnpackTerrain, CMapReader::ParseTerrain for the reordering
-JS::Value CMapGeneratorWorker::LoadMapTerrain(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& filename)
+JS::Value CMapGeneratorWorker::LoadMapTerrain(const VfsPath& filename)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	ScriptRequest rq(self->m_ScriptInterface);
+	ScriptRequest rq(m_ScriptInterface);
 
 	if (!VfsFileExists(filename))
 	{

source/graphics/MapGenerator.h
@@ -134,54 +134,49 @@ private:
 	bool LoadScripts(const VfsPath& libraryName);
 
 	/**
-	 * Recursively load all script files in the given folder.
-	 */
-	static bool LoadLibrary(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& name);
-
-	/**
 	 * Finalize map generation and pass results from the script to the engine.
 	 */
-	static void ExportMap(ScriptInterface::CmptPrivate* pCmptPrivate, JS::HandleValue data);
+	void ExportMap(JS::HandleValue data);
 
 	/**
 	 * Load an image file and return it as a height array.
 	 */
-	static JS::Value LoadHeightmap(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& src);
+	JS::Value LoadHeightmap(const VfsPath& src);
 
 	/**
 	 * Load an Atlas terrain file (PMP) returning textures and heightmap.
 	 */
-	static JS::Value LoadMapTerrain(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& filename);
+	JS::Value LoadMapTerrain(const VfsPath& filename);
 
 	/**
 	 * Sets the map generation progress, which is one of multiple stages determining the loading screen progress.
 	 */
-	static void SetProgress(ScriptInterface::CmptPrivate* pCmptPrivate, int progress);
+	void SetProgress(int progress);
 
 	/**
 	 * Microseconds since the epoch.
 	 */
-	static double GetMicroseconds(ScriptInterface::CmptPrivate* pCmptPrivate);
+	double GetMicroseconds();
 
 	/**
 	 * Return the template data of the given template name.
 	 */
-	static CParamNode GetTemplate(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& templateName);
+	CParamNode GetTemplate(const std::string& templateName);
 
 	/**
 	 * Check whether the given template exists.
 	 */
-	static bool TemplateExists(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& templateName);
+	bool TemplateExists(const std::string& templateName);
 
 	/**
 	 * Returns all template names of simulation entity templates.
 	 */
-	static std::vector<std::string> FindTemplates(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& path, bool includeSubdirectories);
+	std::vector<std::string> FindTemplates(const std::string& path, bool includeSubdirectories);
 
 	/**
 	 * Returns all template names of actors.
 	 */
-	static std::vector<std::string> FindActorTemplates(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& path, bool includeSubdirectories);
+	std::vector<std::string> FindActorTemplates(const std::string& path, bool includeSubdirectories);
 
 	/**
 	 * Perform map generation in an independent thread.

source/graphics/scripting/JSInterface_GameView.cpp
@@ -22,13 +22,18 @@
 #include "graphics/Camera.h"
 #include "graphics/GameView.h"
 #include "graphics/Terrain.h"
+#include "maths/FixedVector3D.h"
 #include "ps/Game.h"
 #include "ps/World.h"
 #include "ps/CLogger.h"
+#include "scriptinterface/FunctionWrapper.h"
 #include "scriptinterface/ScriptInterface.h"
+#include "simulation2/helpers/Position.h"
 
+namespace JSI_GameView
+{
 #define IMPLEMENT_BOOLEAN_SCRIPT_SETTING(NAME) \
-bool JSI_GameView::Get##NAME##Enabled(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate)) \
+bool Get##NAME##Enabled() \
 { \
 	if (!g_Game || !g_Game->GetView()) \
 	{ \
@@ -38,7 +43,7 @@ bool JSI_GameView::Get##NAME##Enabled(ScriptInterface::CmptPrivate* UNUSED(pCmpt
 	return g_Game->GetView()->Get##NAME##Enabled(); \
 } \
 \
-void JSI_GameView::Set##NAME##Enabled(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), bool Enabled) \
+void Set##NAME##Enabled(bool Enabled) \
 { \
 	if (!g_Game || !g_Game->GetView()) \
 	{ \
@@ -56,10 +61,10 @@ IMPLEMENT_BOOLEAN_SCRIPT_SETTING(ConstrainCamera);
 
 
 #define REGISTER_BOOLEAN_SCRIPT_SETTING(NAME) \
-scriptInterface.RegisterFunction<bool, &JSI_GameView::Get##NAME##Enabled>("GameView_Get" #NAME "Enabled"); \
-scriptInterface.RegisterFunction<void, bool, &JSI_GameView::Set##NAME##Enabled>("GameView_Set" #NAME "Enabled");
+	ScriptFunction::Register<&Get##NAME##Enabled>(rq, "GameView_Get" #NAME "Enabled"); \
+	ScriptFunction::Register<&Set##NAME##Enabled>(rq, "GameView_Set" #NAME "Enabled");
 
-void JSI_GameView::RegisterScriptFunctions_Settings(const ScriptInterface& scriptInterface)
+void RegisterScriptFunctions_Settings(const ScriptRequest& rq)
 {
 	REGISTER_BOOLEAN_SCRIPT_SETTING(Culling);
 	REGISTER_BOOLEAN_SCRIPT_SETTING(LockCullCamera);
@@ -68,9 +73,8 @@ void JSI_GameView::RegisterScriptFunctions_Settings(const ScriptInterface& scrip
 
 #undef REGISTER_BOOLEAN_SCRIPT_SETTING
 
-JS::Value JSI_GameView::GetCameraPivot(ScriptInterface::CmptPrivate* pCmptPrivate)
+JS::Value GetCameraPivot(const ScriptRequest& rq)
 {
-	ScriptRequest rq(pCmptPrivate->pScriptInterface);
 	CVector3D pivot(-1, -1, -1);
 	if (g_Game && g_Game->GetView())
 		pivot = g_Game->GetView()->GetCameraPivot();
@@ -83,7 +87,7 @@ JS::Value JSI_GameView::GetCameraPivot(ScriptInterface::CmptPrivate* pCmptPrivat
 /**
  * Move camera to a 2D location.
  */
-void JSI_GameView::CameraMoveTo(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), entity_pos_t x, entity_pos_t z)
+void CameraMoveTo(entity_pos_t x, entity_pos_t z)
 {
 	if (!g_Game || !g_Game->GetWorld() || !g_Game->GetView() || !g_Game->GetWorld()->GetTerrain())
 		return;
@@ -101,7 +105,7 @@ void JSI_GameView::CameraMoveTo(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivat
 /**
  * Set the camera to look at the given location.
  */
-void JSI_GameView::SetCameraTarget(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), float x, float y, float z)
+void SetCameraTarget(float x, float y, float z)
 {
 	if (!g_Game || !g_Game->GetView())
 		return;
@@ -111,7 +115,7 @@ void JSI_GameView::SetCameraTarget(ScriptInterface::CmptPrivate* UNUSED(pCmptPri
 /**
  * Set the data (position, orientation and zoom) of the camera.
  */
-void JSI_GameView::SetCameraData(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), entity_pos_t x, entity_pos_t y, entity_pos_t z, entity_pos_t rotx, entity_pos_t roty, entity_pos_t zoom)
+void SetCameraData(entity_pos_t x, entity_pos_t y, entity_pos_t z, entity_pos_t rotx, entity_pos_t roty, entity_pos_t zoom)
 {
 	if (!g_Game || !g_Game->GetView())
 		return;
@@ -125,7 +129,7 @@ void JSI_GameView::SetCameraData(ScriptInterface::CmptPrivate* UNUSED(pCmptPriva
  * Start / stop camera following mode.
  * @param entityid unit id to follow. If zero, stop following mode
  */
-void JSI_GameView::CameraFollow(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), entity_id_t entityid)
+void CameraFollow(entity_id_t entityid)
 {
 	if (!g_Game || !g_Game->GetView())
 		return;
@@ -137,7 +141,7 @@ void JSI_GameView::CameraFollow(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivat
  * Start / stop first-person camera following mode.
  * @param entityid unit id to follow. If zero, stop following mode.
  */
-void JSI_GameView::CameraFollowFPS(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), entity_id_t entityid)
+void CameraFollowFPS(entity_id_t entityid)
 {
 	if (!g_Game || !g_Game->GetView())
 		return;
@@ -145,7 +149,7 @@ void JSI_GameView::CameraFollowFPS(ScriptInterface::CmptPrivate* UNUSED(pCmptPri
 	g_Game->GetView()->FollowEntity(entityid, true);
 }
 
-entity_id_t JSI_GameView::GetFollowedEntity(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate))
+entity_id_t GetFollowedEntity()
 {
 	if (!g_Game || !g_Game->GetView())
 		return INVALID_ENTITY;
@@ -153,22 +157,23 @@ entity_id_t JSI_GameView::GetFollowedEntity(ScriptInterface::CmptPrivate* UNUSED
 	return g_Game->GetView()->GetFollowedEntity();
 }
 
-CFixedVector3D JSI_GameView::GetTerrainAtScreenPoint(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), int x, int y)
+CFixedVector3D GetTerrainAtScreenPoint(int x, int y)
 {
 	CVector3D pos = g_Game->GetView()->GetCamera()->GetWorldCoordinates(x, y, true);
 	return CFixedVector3D(fixed::FromFloat(pos.X), fixed::FromFloat(pos.Y), fixed::FromFloat(pos.Z));
 }
 
-void JSI_GameView::RegisterScriptFunctions(const ScriptInterface& scriptInterface)
+void RegisterScriptFunctions(const ScriptRequest& rq)
 {
-	RegisterScriptFunctions_Settings(scriptInterface);
+	RegisterScriptFunctions_Settings(rq);
 
-	scriptInterface.RegisterFunction<JS::Value, &GetCameraPivot>("GetCameraPivot");
-	scriptInterface.RegisterFunction<void, entity_pos_t, entity_pos_t, &CameraMoveTo>("CameraMoveTo");
-	scriptInterface.RegisterFunction<void, float, float, float, &SetCameraTarget>("SetCameraTarget");
-	scriptInterface.RegisterFunction<void, entity_pos_t, entity_pos_t, entity_pos_t, entity_pos_t, entity_pos_t, entity_pos_t, &SetCameraData>("SetCameraData");
-	scriptInterface.RegisterFunction<void, entity_id_t, &CameraFollow>("CameraFollow");
-	scriptInterface.RegisterFunction<void, entity_id_t, &CameraFollowFPS>("CameraFollowFPS");
-	scriptInterface.RegisterFunction<entity_id_t, &GetFollowedEntity>("GetFollowedEntity");
-	scriptInterface.RegisterFunction<CFixedVector3D, int, int, &GetTerrainAtScreenPoint>("GetTerrainAtScreenPoint");
+	ScriptFunction::Register<&GetCameraPivot>(rq, "GetCameraPivot");
+	ScriptFunction::Register<&CameraMoveTo>(rq, "CameraMoveTo");
+	ScriptFunction::Register<&SetCameraTarget>(rq, "SetCameraTarget");
+	ScriptFunction::Register<&SetCameraData>(rq, "SetCameraData");
+	ScriptFunction::Register<&CameraFollow>(rq, "CameraFollow");
+	ScriptFunction::Register<&CameraFollowFPS>(rq, "CameraFollowFPS");
+	ScriptFunction::Register<&GetFollowedEntity>(rq, "GetFollowedEntity");
+	ScriptFunction::Register<&GetTerrainAtScreenPoint>(rq, "GetTerrainAtScreenPoint");
+}
 }

source/graphics/scripting/JSInterface_GameView.h
@@ -18,34 +18,11 @@
 #ifndef INCLUDED_JSINTERFACE_GAMEVIEW
 #define INCLUDED_JSINTERFACE_GAMEVIEW
 
-#include "maths/FixedVector3D.h"
-#include "scriptinterface/ScriptInterface.h"
-#include "simulation2/helpers/Position.h"
-#include "simulation2/system/Entity.h"
-
-#define DECLARE_BOOLEAN_SCRIPT_SETTING(NAME) \
-	bool Get##NAME##Enabled(ScriptInterface::CmptPrivate* pCmptPrivate); \
-	void Set##NAME##Enabled(ScriptInterface::CmptPrivate* pCmptPrivate, bool Enabled);
+class ScriptRequest;
 
 namespace JSI_GameView
 {
-	void RegisterScriptFunctions(const ScriptInterface& ScriptInterface);
-	void RegisterScriptFunctions_Settings(const ScriptInterface& scriptInterface);
-
-	DECLARE_BOOLEAN_SCRIPT_SETTING(Culling);
-	DECLARE_BOOLEAN_SCRIPT_SETTING(LockCullCamera);
-	DECLARE_BOOLEAN_SCRIPT_SETTING(ConstrainCamera);
-
-	JS::Value GetCameraPivot(ScriptInterface::CmptPrivate* pCmptPrivate);
-	void CameraMoveTo(ScriptInterface::CmptPrivate* pCmptPrivate, entity_pos_t x, entity_pos_t z);
-	void SetCameraTarget(ScriptInterface::CmptPrivate* pCmptPrivate, float x, float y, float z);
-	void SetCameraData(ScriptInterface::CmptPrivate* pCmptPrivate, entity_pos_t x, entity_pos_t y, entity_pos_t z, entity_pos_t rotx, entity_pos_t roty, entity_pos_t zoom);
-	void CameraFollow(ScriptInterface::CmptPrivate* pCmptPrivate, entity_id_t entityid);
-	void CameraFollowFPS(ScriptInterface::CmptPrivate* pCmptPrivate, entity_id_t entityid);
-	entity_id_t GetFollowedEntity(ScriptInterface::CmptPrivate* pCmptPrivate);
-	CFixedVector3D GetTerrainAtScreenPoint(ScriptInterface::CmptPrivate* pCmptPrivate, int x, int y);
+	void RegisterScriptFunctions(const ScriptRequest& rq);
 }
-
-#undef DECLARE_BOOLEAN_SCRIPT_SETTING
 
 #endif // INCLUDED_JSINTERFACE_GAMEVIEW

commit c826b59f0be906a42dd2fdeea87d42600e6e15d9

Fix replay folders with special characters.
Implemented in r19824, broken in r24983.

Patch by: @Langbart
Help by: @elexis
Differential revision: https://code.wildfiregames.com/D4345
Fixes: #6373
Reviewed by: @vladislavbelov
Tested by: @Freagarach



git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@26005 3db68df2-c116-0410-a063-a993310a9797

source/ps/scripting/JSInterface_VisualReplay.cpp
@@ -28,7 +28,8 @@ namespace JSI_VisualReplay
 {
 CStrW GetReplayDirectoryName(const CStrW& directoryName)
 {
-	return OsPath(VisualReplay::GetDirectoryPath() / directoryName).string();
+	// The string conversion is added to account for non-latin characters.
+	return wstring_from_utf8(OsPath(VisualReplay::GetDirectoryPath() / directoryName).string8());
 }
 
 void RegisterScriptFunctions(const ScriptRequest& rq)