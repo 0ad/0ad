c1a639c2ad10f440e1c63b2bf9e2139bdacd8955
wraitii <wraitii@3db68df2-c116-0410-a063-a993310a9797>
2022-01-24 15:20:56 +0000

    Fix AI counting exhausted resources in its dropsite list
    dropsiteSupplies kept a list of resources and did not remove exhausted resources, leading to possibly buggy resource-management in the AI code (and needless computations).
    
    Differential Revision: https://code.wildfiregames.com/D4442
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@26244 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/ai/petra/baseManager.js
+++ binaries/data/mods/public/simulation/ai/petra/baseManager.js
@@ -233,6 +233,18 @@ PETRA.BaseManager.prototype.assignResourceToDropsite = function(gameState, drops
 	});
 };
 
+PETRA.BaseManager.prototype.removeFromAssignedDropsite = function(ent)
+{
+	for (const type in this.dropsiteSupplies)
+		for (const proxim in this.dropsiteSupplies[type])
+		{
+			const resourcesList = this.dropsiteSupplies[type][proxim];
+			for (let i = 0; i < resourcesList.length; ++i)
+				if (resourcesList[i].id === ent.id())
+					resourcesList.splice(i--, 1);
+		}
+};
+
 // completely remove the dropsite resources from our list.
 PETRA.BaseManager.prototype.removeDropsite = function(gameState, ent)
 {
