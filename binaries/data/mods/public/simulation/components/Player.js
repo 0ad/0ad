07d9b7269a5840b9b1d31be94473a0df8274eb14
Freagarach <Freagarach@3db68df2-c116-0410-a063-a993310a9797>
2022-02-15 07:20:21 +0000

    Display defeated/won before victory.
    When the victory conditions dictate that someone loses when e.g. losing their CC, the message that the other player(s) had won was sent before the former player was actually defeated.
    When the victory conditions dictate that someone wins when e.g. capturing all relics, the message that they have won was sent after the message that the rest has lost.
    
    This changes those.
    
    Differential revision: https://code.wildfiregames.com/D4478
    Comments by: @bb, @Silier
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@26382 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/components/Player.js
+++ binaries/data/mods/public/simulation/components/Player.js
@@ -484,18 +484,15 @@ Player.prototype.SetState = function(newState, message)
 			});
 	}
 
-	Engine.PostMessage(this.entity, won ? MT_PlayerWon : MT_PlayerDefeated, { "playerId": this.playerID });
-
 	if (message)
-	{
-		let cmpGUIInterface = Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface);
-		cmpGUIInterface.PushNotification({
+		Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface).PushNotification({
 			"type": won ? "won" : "defeat",
 			"players": [this.playerID],
 			"allies": [this.playerID],
 			"message": message
 		});
-	}
+
+	Engine.PostMessage(this.entity, won ? MT_PlayerWon : MT_PlayerDefeated, { "playerId": this.playerID });
 };
 
 Player.prototype.GetTeam = function()
