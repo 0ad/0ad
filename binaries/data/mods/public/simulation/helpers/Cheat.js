dfa6fa94655c3958192ea22a5d654d4d02e86221
Freagarach <Freagarach@3db68df2-c116-0410-a063-a993310a9797>
2022-03-15 06:46:34 +0000

    Fix using numeric cheats with a non-numeric value.
    Requested by @Silier at r26328 / rP26328.
    This notifies players when using a non-numeric value and guards the simulation against such cases.
    
    Previous behaviour was that a cheat was executed with its default value. Now the cheat is not used.
    
    Differential revision: https://code.wildfiregames.com/D4485
    Comments by: @bb, @Silier
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@26657 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/simulation/helpers/Cheat.js
+++ binaries/data/mods/public/simulation/helpers/Cheat.js
@@ -1,52 +1,49 @@
 function Cheat(input)
 {
-	var cmpPlayerManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_PlayerManager);
-	if (!cmpPlayerManager || input.player < 0)
-		return;
-	var playerEnt = cmpPlayerManager.GetPlayerByID(input.player);
-	if (playerEnt == INVALID_ENTITY)
-		return;
-	var cmpPlayer = Engine.QueryInterface(playerEnt, IID_Player);
-	if (!cmpPlayer)
+	if (input.player < 0)
 		return;
 
-	var cmpGuiInterface = Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface);
-	if (!cmpPlayer.GetCheatsEnabled())
+	const cmpPlayerManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_PlayerManager);
+	const playerEnt = cmpPlayerManager.GetPlayerByID(input.player);
+	if (playerEnt == INVALID_ENTITY)
 		return;
+	const cmpPlayer = Engine.QueryInterface(playerEnt, IID_Player);
+	if (!cmpPlayer?.GetCheatsEnabled())
+		return;
+
+	const cmpGuiInterface = Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface);
 
 	switch(input.action)
 	{
 	case "addresource":
+		if (isNaN(input.parameter))
+			return;
 		cmpPlayer.AddResource(input.text, input.parameter);
 		return;
 	case "revealmap":
-		var cmpRangeManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_RangeManager);
-		cmpRangeManager.SetLosRevealAll(-1, true);
+		Engine.QueryInterface(SYSTEM_ENTITY, IID_RangeManager).SetLosRevealAll(-1, true);
 		return;
 	case "maxpopulation":
 		cmpPlayer.SetPopulationBonuses((cmpPlayerManager.GetMaxWorldPopulation() || cmpPlayer.GetMaxPopulation()) + 500);
 		return;
 	case "changemaxpopulation":
 	{
-		let cmpModifiersManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_ModifiersManager);
-		cmpModifiersManager.AddModifiers("cheat/maxpopulation", {
+		Engine.QueryInterface(SYSTEM_ENTITY, IID_ModifiersManager).AddModifiers("cheat/maxpopulation", {
 			"Player/MaxPopulation": [{ "affects": ["Player"], "add": 500 }],
 		}, playerEnt);
 		return;
 	}
 	case "convertunit":
+		if (isNaN(input.parameter))
+			return;
 		const playerID = (input.parameter > -1 && QueryPlayerIDInterface(input.parameter) || cmpPlayer).GetPlayerID();
-		for (let ent of input.selected)
-		{
-			let cmpOwnership = Engine.QueryInterface(ent, IID_Ownership);
-			if (cmpOwnership)
-				cmpOwnership.SetOwner(playerID);
-		}
+		for (const ent of input.selected)
+			Engine.QueryInterface(ent, IID_Ownership)?.SetOwner(playerID);
 		return;
 	case "killunits":
-		for (let ent of input.selected)
+		for (const ent of input.selected)
 		{
-			let cmpHealth = Engine.QueryInterface(ent, IID_Health);
+			const cmpHealth = Engine.QueryInterface(ent, IID_Health);
 			if (cmpHealth)
 				cmpHealth.Kill();
 			else
@@ -54,12 +51,17 @@ function Cheat(input)
 		}
 		return;
 	case "defeatplayer":
-		cmpPlayer = QueryPlayerIDInterface(input.parameter);
-		if (cmpPlayer)
-			cmpPlayer.SetState("defeated", markForTranslation("%(player)s has been defeated (cheat)."));
+		if (isNaN(input.parameter))
+			return;
+		QueryPlayerIDInterface(input.parameter)?.SetState("defeated",
+			markForTranslation("%(player)s has been defeated (cheat).")
+		);
 		return;
 	case "createunits":
 	{
+		if (isNaN(input.player) || isNaN(input.parameter))
+			return;
+
 		const cmpTrainer = input.selected.length && Engine.QueryInterface(input.selected[0], IID_Trainer);
 		if (!cmpTrainer)
 		{
@@ -87,7 +89,7 @@ function Cheat(input)
 	}
 	case "fastactions":
 	{
-		let cmpModifiersManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_ModifiersManager);
+		const cmpModifiersManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_ModifiersManager);
 		if (cmpModifiersManager.HasAnyModifier("cheat/fastactions", playerEnt))
 			cmpModifiersManager.RemoveAllModifiers("cheat/fastactions", playerEnt);
 		else
@@ -101,12 +103,13 @@ function Cheat(input)
 		return;
 	}
 	case "changephase":
-		var cmpTechnologyManager = Engine.QueryInterface(playerEnt, IID_TechnologyManager);
+	{
+		const cmpTechnologyManager = Engine.QueryInterface(playerEnt, IID_TechnologyManager);
 		if (!cmpTechnologyManager)
 			return;
 
 		// store the phase we want in the next input parameter
-		var parameter;
+		let parameter;
 		if (!cmpTechnologyManager.IsTechnologyResearched("phase_town"))
 			parameter = "phase_town";
 		else if (!cmpTechnologyManager.IsTechnologyResearched("phase_city"))
@@ -119,15 +122,21 @@ function Cheat(input)
 		else
 			parameter += "_generic";
 
-		Cheat({ "player": input.player, "action": "researchTechnology", "parameter": parameter, "selected": input.selected });
+		Cheat({
+			"player": input.player,
+			"action": "researchTechnology",
+			"parameter": parameter,
+			"selected": input.selected
+		});
 		return;
+	}
 	case "researchTechnology":
 	{
 		if (!input.parameter.length)
 			return;
 
-		var techname = input.parameter;
-		var cmpTechnologyManager = Engine.QueryInterface(playerEnt, IID_TechnologyManager);
+		let techname = input.parameter;
+		const cmpTechnologyManager = Engine.QueryInterface(playerEnt, IID_TechnologyManager);
 		if (!cmpTechnologyManager)
 			return;
 
@@ -138,18 +147,18 @@ function Cheat(input)
 			if (cmpResearcher)
 			{
 				// try to spilt the input
-				var tmp = input.parameter.split(/\s+/);
-				var number = +tmp[0];
-				var pair = tmp.length > 1 && (tmp[1] == "top" || tmp[1] == "bottom") ? tmp[1] : "top"; // use top as default value
+				const tmp = input.parameter.split(/\s+/);
+				const number = +tmp[0];
+				const pair = tmp.length > 1 && (tmp[1] == "top" || tmp[1] == "bottom") ? tmp[1] : "top"; // use top as default value
 
 				// check, if valid number was parsed.
-				if (number || number === 0)
+				if (!isNaN(number))
 				{
 					// get name of tech
 					const techs = cmpResearcher.GetTechnologiesList();
 					if (number > 0 && number <= techs.length)
 					{
-						var tech = techs[number-1];
+						const tech = techs[number-1];
 						if (!tech)
 							return;
 
@@ -170,16 +179,16 @@ function Cheat(input)
 		return;
 	}
 	case "metaCheat":
-		for (let resource of Resources.GetCodes())
+		for (const resource of Resources.GetCodes())
 			Cheat({ "player": input.player, "action": "addresource", "text": resource, "parameter": input.parameter });
 		Cheat({ "player": input.player, "action": "maxpopulation" });
 		Cheat({ "player": input.player, "action": "changemaxpopulation" });
 		Cheat({ "player": input.player, "action": "fastactions" });
-		for (let i=0; i<2; ++i)
+		for (let i = 0; i < 2; ++i)
 			Cheat({ "player": input.player, "action": "changephase", "selected": input.selected });
 		return;
 	case "playRetro":
-		let play = input.parameter.toLowerCase() != "off";
+		const play = input.parameter.toLowerCase() != "off";
 		cmpGuiInterface.PushNotification({
 			"type": "play-tracks",
 			"tracks": play && input.parameter.split(" "),
