ea1b3697636aa9a82475e933e1dd1e2bc819ed3f
vladislavbelov <vladislavbelov@3db68df2-c116-0410-a063-a993310a9797>
2022-10-07 23:19:12 +0000

    Merges a duplication of CColor tests files introduced in rP15278.
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@27133 3db68df2-c116-0410-a063-a993310a9797

--- source/graphics/tests/test_Color.h
+++ source/graphics/tests/test_Color.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020 Wildfire Games.
+/* Copyright (C) 2022 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -36,6 +36,61 @@ public:
 		CheckColor(0, 1, 0, 0x00ff00);
 		CheckColor(0, 0, 1, 0xff0000);
 		CheckColor(1, 1, 1, 0xffffff);
+	}
+
+	void test_parse()
+	{
+		TestLogger nolog;
+#define CHECK_CCOLOR_EQUAL(v,r,g,b,a) \
+	{ \
+		CStr str = v; \
+		CColor c; \
+		TS_ASSERT(c.ParseString(str, 255)); \
+		CColor c_(r/255.f,g/255.f,b/255.f,a/255.f); \
+		TS_ASSERT_EQUALS(c, c_); \
+	}
+
+		CHECK_CCOLOR_EQUAL("0 0 0 0", 0, 0, 0, 0);
+		CHECK_CCOLOR_EQUAL("255 0 255 0", 255, 0, 255, 0);
+		CHECK_CCOLOR_EQUAL("0 123 0 0", 0, 123, 0, 0);
+		CHECK_CCOLOR_EQUAL("0 0 0 55", 0, 0, 0, 55);
+		CHECK_CCOLOR_EQUAL("76 24 0", 76, 24, 0, 255);
+		CHECK_CCOLOR_EQUAL("159 98 24", 159, 98, 24, 255);
+#undef CHECK_CCOLOR_EQUAL
+	}
+
+	void test_parse_failure()
+	{
+		TestLogger nolog;
+
+#define CHECK_CCOLOR_FAIL(v) \
+	{ \
+		CStr str = v; \
+		CColor a; \
+		TS_ASSERT(!a.ParseString(str, 255)); \
+	}
+
+		CHECK_CCOLOR_FAIL("abc");
+		CHECK_CCOLOR_FAIL("0.123 1 2 3");
+		CHECK_CCOLOR_FAIL("0 0 0 0 adfasd");
+		CHECK_CCOLOR_FAIL("0 a0 b0 ax");
+		CHECK_CCOLOR_FAIL("-124dsaf");
+		CHECK_CCOLOR_FAIL("\0");
+		CHECK_CCOLOR_FAIL("1 2 xxxx");
+
+		// Not enough parameters
+		CHECK_CCOLOR_FAIL("");
+		CHECK_CCOLOR_FAIL("124");
+		CHECK_CCOLOR_FAIL("0 55");
+
+		// More parameters than allowed
+		CHECK_CCOLOR_FAIL("0 5 1 5 6");
+
+		// Out of bounds
+		CHECK_CCOLOR_FAIL("-1 0 0");
+		CHECK_CCOLOR_FAIL("256 0 0 0");
+		CHECK_CCOLOR_FAIL("255 256 -1 0");
+#undef CHECK_CCLOLO_FAIL
 	}
 
 private:
