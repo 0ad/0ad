da5136cd670228524e1dcdf5f656356d1a4e8fd9
vladislavbelov <vladislavbelov@3db68df2-c116-0410-a063-a993310a9797>
2022-10-07 22:52:23 +0000

    Removes unused minimap shader variants.
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@27130 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/shaders/glsl/minimap.fs
+++ binaries/data/mods/public/shaders/glsl/minimap.fs
@@ -5,33 +5,14 @@
   varying vec2 v_tex;
 #endif
 
-#if MINIMAP_MASK
-  uniform sampler2D maskTex;
-  varying vec2 v_maskUV;
-#endif
-
 #if MINIMAP_POINT
   varying vec3 color;
 #endif
 
-#if MINIMAP_LINE
-  uniform vec4 color;
-#endif
-
 void main()
 {
-  #if MINIMAP_MASK
-    float mask = texture2D(maskTex, v_maskUV).a;
-  #endif
-
   #if MINIMAP_BASE
-  #if MINIMAP_MASK
-    vec4 color = texture2D(baseTex, v_tex);
-    gl_FragColor.rgb = color.rgb;
-    gl_FragColor.a = color.a * mask;
-  #else
     gl_FragColor = texture2D(baseTex, v_tex);
-  #endif
   #endif
 
   #if MINIMAP_LOS
@@ -40,9 +21,5 @@ void main()
 
   #if MINIMAP_POINT
     gl_FragColor = vec4(color, 1.0);
-  #endif
-
-  #if MINIMAP_LINE
-    gl_FragColor = color;
   #endif
 }
