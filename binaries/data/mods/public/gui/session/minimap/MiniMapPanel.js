6529c77ca2bd402a4dad21c879556829dd9d4fb7
Stan <Stan@3db68df2-c116-0410-a063-a993310a9797>
2022-10-23 16:50:30 +0000

    Civ backgrounds for minimap Comments by: @marder, @Freagarach Differential Revision: https://code.wildfiregames.com/D4271
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@27169 3db68df2-c116-0410-a063-a993310a9797

--- binaries/data/mods/public/gui/session/minimap/MiniMapPanel.js
+++ binaries/data/mods/public/gui/session/minimap/MiniMapPanel.js
@@ -9,6 +9,8 @@ class MiniMapPanel
 		this.idleWorkerButton = new MiniMapIdleWorkerButton(playerViewControl, idleWorkerClasses);
 		this.flareButton = new MiniMapFlareButton(playerViewControl);
 		this.miniMap = new MiniMap();
+		playerViewControl.registerViewedPlayerChangeHandler(this.rebuild.bind(this));
+		registerHotkeyChangeHandler(this.rebuild.bind(this));
 	}
 
 	flare(target, playerID)
@@ -19,5 +21,17 @@ class MiniMapPanel
 	isMouseOverMiniMap()
 	{
 		return this.miniMap.isMouseOverMiniMap();
+	}
+
+	rebuild()
+	{
+		this.setCivBackgroundTexture();
+	}
+
+	setCivBackgroundTexture()
+	{
+		const playerCiv = g_ViewedPlayer > 0 ? g_Players[g_ViewedPlayer].civ : "gaia";
+		const backgroundObject = Engine.GetGUIObjectByName("minimapBackgroundTexture");
+		backgroundObject.sprite = `stretched:session/icons/bkg/background_circle_${playerCiv}.png`;
 	}
 }
