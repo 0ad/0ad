aa76c92a8ec376e38e04d90876cf8b72abeada07
Stan <Stan@3db68df2-c116-0410-a063-a993310a9797>
2023-04-27 14:20:26 +0000

    Use the new client for the pull script.
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@27620 3db68df2-c116-0410-a063-a993310a9797

--- source/tools/i18n/pullTranslations.py
+++ source/tools/i18n/pullTranslations.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (C) 2022 Wildfire Games.
+# Copyright (C) 2023 Wildfire Games.
 # This file is part of 0 A.D.
 #
 # 0 A.D. is free software: you can redistribute it and/or modify
@@ -16,21 +16,20 @@
 # You should have received a copy of the GNU General Public License
 # along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, sys
-
-from txclib.project import Project
+import os
+import subprocess
 
 from i18n_helper import l10nFolderName, transifexClientFolder, projectRootDirectory
 
 def main():
-    for root, folders, filenames in os.walk(projectRootDirectory):
+    for root, folders, _ in os.walk(projectRootDirectory):
         for folder in folders:
             if folder == l10nFolderName:
                 if os.path.exists(os.path.join(root, folder, transifexClientFolder)):
                     path = os.path.join(root, folder)
                     os.chdir(path)
-                    project = Project(path)
-                    project.pull(fetchall=True, force=True, parallel=True)
+                    print(f"INFO: Starting to pull translations in {path}...")
+                    subprocess.run("tx pull -f")
 
 
 if __name__ == "__main__":
