839edc3ae61cd313d37a14a9a24e2ef1bd4e5808
Stan <Stan@3db68df2-c116-0410-a063-a993310a9797>
2023-05-31 12:03:04 +0000

    Fix the removal of implicit conversions in libfmt 10 by using explicit casts. Patch by: @phosit Accepted by: @vladislavbelov Comments by: @sera
    Differential Revision: https://code.wildfiregames.com/D4998
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@27657 3db68df2-c116-0410-a063-a993310a9797

--- source/network/NetMessage.cpp
+++ source/network/NetMessage.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017 Wildfire Games.
+/* Copyright (C) 2023 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -216,7 +216,8 @@ CNetMessage* CNetMessageFactory::CreateMessage(const void* pData,
 		break;
 
 	default:
-		LOGERROR("CNetMessageFactory::CreateMessage(): Unknown message type '%d' received", header.GetType());
+		LOGERROR("CNetMessageFactory::CreateMessage(): Unknown message type '%d' received",
+			static_cast<int>(header.GetType()));
 		break;
 	}
 
