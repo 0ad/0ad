2119572c31514699ccc674f52460b95c869a6f02
phosit <phosit@3db68df2-c116-0410-a063-a993310a9797>
2023-10-28 12:57:01 +0000

    Use std::unique_ptr in CUnitManager and remove unused member functions.
    Accepted By: @vladislavbelov
    
    Differential Revision: https://code.wildfiregames.com/D5173
    
    
    
    git-svn-id: https://svn.wildfiregames.com/public/ps/trunk@27905 3db68df2-c116-0410-a063-a993310a9797

--- source/graphics/UnitManager.h
+++ source/graphics/UnitManager.h
@@ -25,8 +25,9 @@
 #include "ps/CStrForward.h"
 #include "simulation2/system/Entity.h"
 
-#include <vector>
+#include <memory>
 #include <set>
+#include <vector>
 
 class CUnit;
 class CObjectManager;
@@ -41,19 +42,12 @@ public:
 	~CUnitManager();
 
 	// add given unit to world
-	void AddUnit(CUnit* unit);
-	// remove given unit from world, but don't delete it
-	void RemoveUnit(CUnit* unit);
+	CUnit* AddUnit(std::unique_ptr<CUnit> unit);
 	// remove given unit from world and delete it
 	void DeleteUnit(CUnit* unit);
-	// remove and delete all units
-	void DeleteAll();
 
 	// creates a new unit and adds it to the world
 	CUnit* CreateUnit(const CStrW& actorName, const entity_id_t id, const uint32_t seed);
-
-	// return the units
-	const std::vector<CUnit*>& GetUnits() const { return m_Units; }
 
 	void SetObjectManager(CObjectManager& objectManager) { m_ObjectManager = &objectManager; }
 
@@ -65,7 +59,7 @@ public:
 
 private:
 	// list of all known units
-	std::vector<CUnit*> m_Units;
+	std::vector<std::unique_ptr<CUnit>> m_Units;
 	// graphical object manager; may be NULL if not set up
 	CObjectManager* m_ObjectManager;
 };
